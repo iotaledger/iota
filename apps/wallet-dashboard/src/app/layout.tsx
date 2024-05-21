9df486cb5d12221c61e1b20c7b402d24c8f575cd
cpl121 <cpeon@boxfish.studio>
2024-05-21 09:17:57 +0200

    feat(wallet-dashboard): add license header

--- apps/wallet-dashboard/src/app/layout.tsx
+++ apps/wallet-dashboard/src/app/layout.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
--- apps/wallet-dashboard/src/app/page.tsx
+++ apps/wallet-dashboard/src/app/page.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 import Image from "next/image";
 
 export default function Home() {
