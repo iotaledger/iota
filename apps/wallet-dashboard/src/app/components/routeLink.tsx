03ec301c4f15a18ad3819c2e7139b1df8e9430ff
cpl121 <cpeon@boxfish.studio>
2024-05-22 11:44:16 +0200

    fix(dashboard): split groups between hooks and their derivatives

--- apps/wallet-dashboard/src/app/components/routeLink.tsx
+++ apps/wallet-dashboard/src/app/components/routeLink.tsx
@@ -10,6 +10,7 @@ import type { IRouteLink } from '../lib/interfaces';
 
 function RouteLink({ title, path }: IRouteLink): JSX.Element {
 	const currentPath = usePathname();
+
     const isActive = currentPath && (path === currentPath || path.startsWith(currentPath))
 
 	return (
