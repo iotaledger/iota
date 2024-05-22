6f6c2624190bdf3ae528ee4d1afbd8bcd673d309
evavirseda <evirseda@boxfish.studio>
2024-05-22 11:22:33 +0200

    feat(wallet-dashboard): use kit hooks

--- apps/wallet-dashboard/src/app/page.tsx
+++ apps/wallet-dashboard/src/app/page.tsx
@@ -2,18 +2,20 @@
 // SPDX-License-Identifier: Apache-2.0
 'use client';
 
-import { ConnectButton, useCurrentWallet } from '@mysten/dapp-kit';
+import { ConnectButton, useCurrentAccount, useCurrentWallet } from '@mysten/dapp-kit';
 
 export default function Home() {
 	const { connectionStatus } = useCurrentWallet();
+	const account = useCurrentAccount();
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<main className="flex min-h-screen flex-col items-center space-y-8 p-24">
 			<ConnectButton />
-			{connectionStatus === 'connected' ? (
-				<div className="flex flex-col items-center justify-center">
+			{connectionStatus === 'connected' && account ? (
+				<div className="flex flex-col items-center justify-center space-y-2">
 					<h1>Welcome</h1>
 					<p>Connection status: {connectionStatus}</p>
+					<div>Address: {account.address}</div>
 				</div>
 			) : (
 				<div>Connection status: {connectionStatus}</div>
