55b413afadf12d69af68ce1ddc63b550cb47dd75
Mario Sarcevic <mario.sarcevic@iota.org>
2024-05-22 14:22:09 +0200

    feat(tooling-wallet-dashboard): Add basic Notification manager

---
+++ apps/wallet-dashboard/src/state/notificationStore.ts
@@ -0,0 +1,69 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { create } from 'zustand';
+
+function* indexGenerator(cap: number): Generator<number, number, number> {
+	let index = 0;
+	while (true) {
+		if (index >= cap) {
+			index = 0;
+			yield index;
+		} else {
+			yield index++;
+		}
+	}
+}
+
+const indexGen = indexGenerator(200);
+
+export enum NotificationType {
+	Success = 'success',
+	Error = 'error',
+	Warning = 'warning',
+	Info = 'info',
+}
+
+export type Notification = {
+	index: number;
+	message: string;
+	type: NotificationType;
+	duration: number;
+};
+
+interface NotificationsState {
+	notifications: Notification[];
+	addNotification: (message: string, type?: NotificationType, duration?: number) => void;
+	clearNotification: (index: number) => void;
+}
+
+export const useNotificationStore = create<NotificationsState>()((set) => ({
+	notifications: [],
+	addNotification: (
+		message: string,
+		type: NotificationType = NotificationType.Success,
+		duration: number = 3000,
+	) =>
+		set((state) => {
+			const index = indexGen.next().value;
+
+			const newNotification: Notification = {
+				index,
+				message,
+				type,
+				duration,
+			};
+
+			return {
+				notifications: [...state.notifications, newNotification],
+			};
+		}),
+	clearNotification: (index: number) =>
+		set((state) => {
+			return {
+				notifications: [
+					...state.notifications.filter((notification) => notification.index !== index),
+				],
+			};
+		}),
+}));
