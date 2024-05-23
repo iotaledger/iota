2332d3b022869f8d5c0a1f2e0aa40d19a3c751a9
Mario Sarcevic <mario.sarcevic@iota.org>
2024-05-23 12:15:06 +0200

    feat(tooling-general): Use 4 spaces for indentation

--- prettier.config.js
+++ prettier.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	printWidth: 100,
 	semi: true,
 	singleQuote: true,
-	tabWidth: 2,
+	tabWidth: 4,
 	trailingComma: 'all',
 	useTabs: false,
 	plugins: ['@ianvs/prettier-plugin-sort-imports'],
