0711d228c6a4c54ff894b0b4e99f852d51e4049f
Philipp Gackstatter <philipp.gackstatter@iota.org>
2024-05-23 14:21:56 +0200

    Add test comment

--- crates/sui-genesis-builder/src/stardust/migration.rs
+++ crates/sui-genesis-builder/src/stardust/migration.rs
@@ -1210,7 +1210,7 @@ mod tests {
         (random_output_header(), foundry_output)
     }
 
-    /// TODO
+    /// Test that an Alias that owns Native Tokens can extract those tokens from the contained bag.
     #[test]
     fn test_alias_migration_with_native_tokens() {
         let random_address = Ed25519Address::from(rand::random::<[u8; Ed25519Address::LENGTH]>());
