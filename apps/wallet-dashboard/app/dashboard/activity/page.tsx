4ea6f0f357c22dc96992c184fb3ef01827d4c55d
cpl121 <cpeon@boxfish.studio>
2024-05-27 17:04:53 +0200

    fix(dashboard): move mock activities to bottom file and rename the container ref

--- apps/wallet-dashboard/app/dashboard/activity/page.tsx
+++ apps/wallet-dashboard/app/dashboard/activity/page.tsx
@@ -4,87 +4,15 @@
 'use client';
 
 import ActivityTile from '@/components/ActivityTile';
+import { ActivityState } from '@/lib/interfaces';
 import { useVirtualizer } from '@tanstack/react-virtual';
 import React from 'react';
 
 function StakingDashboardPage(): JSX.Element {
-    const MOCK_ACTIVITIES = [
-        {
-            action: 'Send',
-            success: true,
-            timestamp: 1716538921485,
-        },
-        {
-            action: 'Transaction',
-            success: true,
-            timestamp: 1715868828552,
-        },
-        {
-            action: 'Send',
-            success: true,
-            timestamp: 1712186639729,
-        },
-        {
-            action: 'Rewards',
-            success: true,
-            timestamp: 1715868828552,
-        },
-        {
-            action: 'Receive',
-            success: true,
-            timestamp: 1712186639729,
-        },
-        {
-            action: 'Transaction',
-            success: true,
-            timestamp: 1715868828552,
-        },
-        {
-            action: 'Send',
-            success: false,
-            timestamp: 1712186639729,
-        },
-        {
-            action: 'Send',
-            success: true,
-            timestamp: 1716538921485,
-        },
-        {
-            action: 'Transaction',
-            success: true,
-            timestamp: 1715868828552,
-        },
-        {
-            action: 'Send',
-            success: true,
-            timestamp: 1712186639729,
-        },
-        {
-            action: 'Rewards',
-            success: true,
-            timestamp: 1715868828552,
-        },
-        {
-            action: 'Receive',
-            success: true,
-            timestamp: 1712186639729,
-        },
-        {
-            action: 'Transaction',
-            success: true,
-            timestamp: 1715868828552,
-        },
-        {
-            action: 'Send',
-            success: false,
-            timestamp: 1712186639729,
-        },
-    ];
-
-    const parentRef = React.useRef(null);
+    const containerRef = React.useRef(null);
     const virtualizer = useVirtualizer({
         count: MOCK_ACTIVITIES.length,
-        getScrollElement: () => parentRef.current,
+        getScrollElement: () => containerRef.current,
         estimateSize: () => 100,
     });
 
@@ -92,22 +20,22 @@ function StakingDashboardPage(): JSX.Element {
 
     return (
         <div
-            className="flex flex-col items-center justify-center space-y-4 pt-12 h-full w-full"
+            className='flex flex-col items-center justify-center space-y-4 pt-12 h-full w-full'
         >
             <h1>Your Activity</h1>
-            <div className="relative w-1/3 overflow-auto h-[50vh]" ref={parentRef}>
+            <div className='relative w-1/3 overflow-auto h-[50vh]' ref={containerRef}>
                 {virtualItems.map((virtualItem) => {
                     const activity = MOCK_ACTIVITIES[virtualItem.index];
                     return (
                         <div
                             key={virtualItem.key}
-                            className="absolute w-full pr-4 pb-4"
+                            className='absolute w-full pr-4 pb-4'
                             style={{
                                 transform: `translateY(${virtualItem.start}px)`,
                                 height: `${virtualItem.size}px`,
                             }}
                         >
-                            <ActivityTile {...activity} />
+                            <ActivityTile activity={activity} />
                         </div>
                     );
                 })}
@@ -115,5 +43,78 @@ function StakingDashboardPage(): JSX.Element {
         </div>
     );
 }
+
+const MOCK_ACTIVITIES = [
+    {
+        action: 'Send',
+        state: ActivityState.Successful,
+        timestamp: 1716538921485,
+    },
+    {
+        action: 'Transaction',
+        state: ActivityState.Successful,
+        timestamp: 1715868828552,
+    },
+    {
+        action: 'Send',
+        state: ActivityState.Successful,
+        timestamp: 1712186639729,
+    },
+    {
+        action: 'Rewards',
+        state: ActivityState.Successful,
+        timestamp: 1715868828552,
+    },
+    {
+        action: 'Receive',
+        state: ActivityState.Successful,
+        timestamp: 1712186639729,
+    },
+    {
+        action: 'Transaction',
+        state: ActivityState.Successful,
+        timestamp: 1715868828552,
+    },
+    {
+        action: 'Send',
+        state: ActivityState.Failed,
+        timestamp: 1712186639729,
+    },
+    {
+        action: 'Send',
+        state: ActivityState.Successful,
+        timestamp: 1716538921485,
+    },
+    {
+        action: 'Transaction',
+        state: ActivityState.Successful,
+        timestamp: 1715868828552,
+    },
+    {
+        action: 'Send',
+        state: ActivityState.Successful,
+        timestamp: 1712186639729,
+    },
+    {
+        action: 'Rewards',
+        state: ActivityState.Successful,
+        timestamp: 1715868828552,
+    },
+    {
+        action: 'Receive',
+        state: ActivityState.Successful,
+        timestamp: 1712186639729,
+    },
+    {
+        action: 'Transaction',
+        state: ActivityState.Successful,
+        timestamp: 1715868828552,
+    },
+    {
+        action: 'Send',
+        state: ActivityState.Failed,
+        timestamp: 1712186639729,
+    },
+];
 
 export default StakingDashboardPage;
