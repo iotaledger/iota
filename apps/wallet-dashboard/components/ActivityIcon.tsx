c9b3155e20cc010101880257260bec58ddd380e6
cpl121 <cpeon@boxfish.studio>
2024-05-27 17:04:12 +0200

    fix(dashboard): modify transactionFailed by the state of the activity

--- apps/wallet-dashboard/components/ActivityIcon.tsx
+++ apps/wallet-dashboard/components/ActivityIcon.tsx
@@ -1,10 +1,10 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+import { ActivityState } from '@/lib/interfaces';
 import {
 	Account24,
 	ArrowRight16,
-	Info16,
 	Sui,
 	Swap16,
 	Unstaked,
@@ -13,31 +13,31 @@ import {
 
 const icons = {
 	Send: (
-		<ArrowRight16 fill="currentColor" className="text-gradient-blue-start text-body -rotate-45" />
+		<ArrowRight16 fill='currentColor' className='text-gradient-blue-start text-body -rotate-45' />
 	),
 	Receive: (
-		<ArrowRight16 fill="currentColor" className="text-gradient-blue-start text-body rotate-135" />
+		<ArrowRight16 fill='currentColor' className='text-gradient-blue-start text-body rotate-135' />
 	),
 	Transaction: (
-		<ArrowRight16 fill="currentColor" className="text-gradient-blue-start text-body -rotate-45" />
+		<ArrowRight16 fill='currentColor' className='text-gradient-blue-start text-body -rotate-45' />
 	),
-	Staked: <WalletActionStake24 className="text-gradient-blue-start text-heading2 bg-transparent" />,
-	Unstaked: <Unstaked className="text-gradient-blue-start text-heading3" />,
-	Rewards: <Sui className="text-gradient-blue-start text-body" />,
-	Swapped: <Swap16 className="text-gradient-blue-start text-heading6" />,
-	Failed: <Info16 className="text-issue-dark text-heading6" />,
-	PersonalMessage: <Account24 fill="currentColor" className="text-gradient-blue-start text-body" />,
+	Staked: <WalletActionStake24 className='text-gradient-blue-start text-heading2 bg-transparent' />,
+	Unstaked: <Unstaked className='text-gradient-blue-start text-heading3' />,
+	Rewards: <Sui className='text-gradient-blue-start text-body' />,
+	Swapped: <Swap16 className='text-gradient-blue-start text-heading6' />,
+	PersonalMessage: <Account24 fill='currentColor' className='text-gradient-blue-start text-body' />,
 };
 
 interface ActivityIconProps {
-	transactionFailed?: boolean;
+	state: ActivityState;
 	action: keyof typeof icons;
 }
 
-function ActivityIcon({ transactionFailed, action }: ActivityIconProps) {
+function ActivityIcon({ state, action }: ActivityIconProps) {
+	const isError = state === ActivityState.Failed;
 	return (
-		<div className={`${transactionFailed ? 'bg-issue-light' : 'bg-gray-40'} w-7.5 h-7.5 flex justify-center items-center rounded-2lg`}>
-			{icons[transactionFailed ? 'Failed' : action]}
+		<div className={`${isError ? 'bg-issue-light' : 'bg-gray-40'} w-7.5 h-7.5 flex justify-center items-center rounded-2lg`}>
+			{icons[action]}	
 		</div>
 	);
 }
