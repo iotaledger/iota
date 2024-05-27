82784016592705733983f28799548a11413fa0c8
cpl121 <cpeon@boxfish.studio>
2024-05-27 17:39:12 +0200

    fix(dashboard): remove space

--- apps/wallet-dashboard/lib/interfaces/activities.interface.ts
+++ apps/wallet-dashboard/lib/interfaces/activities.interface.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export interface Activity {
-    action:  'Send' | 'Receive' | 'Transaction' | 'Staked' | 'Unstaked' | 'Rewards' | 'Swapped' | 'PersonalMessage'
+    action: 'Send' | 'Receive' | 'Transaction' | 'Staked' | 'Unstaked' | 'Rewards' | 'Swapped' | 'PersonalMessage'
     timestamp: number
     state: ActivityState
 }
