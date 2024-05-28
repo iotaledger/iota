deea638a71ab6f3c46a45bdac6a6177e26ebca65
evavirseda <evirseda@boxfish.studio>
2024-05-28 10:01:46 +0200

    feat(wallet-dashboard): add empty line

--- apps/wallet-dashboard/contexts/PopupContext.ts
+++ apps/wallet-dashboard/contexts/PopupContext.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { ReactNode, createContext } from 'react';
+
 export interface PopupManager {
     popups: ReactNode[];
     openPopup: (content: ReactNode) => void;
