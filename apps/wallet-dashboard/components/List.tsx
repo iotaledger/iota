4cacc2860b97a4a99b2c8cfd3057d1676591aaa0
evavirseda <evirseda@boxfish.studio>
2024-05-28 09:05:03 +0200

    feat(wallet-dashboard): update types

--- apps/wallet-dashboard/components/List.tsx
+++ apps/wallet-dashboard/components/List.tsx
@@ -3,18 +3,22 @@
 
 import React from 'react';
 
-interface ListProps {
-    data: (unknown | Record<string, unknown>)[];
+interface ListProps<T extends Record<string, unknown>> {
+    data: T[];
     title?: string;
     keysToShow: string[];
 }
 
-function List({ data, title, keysToShow }: ListProps): JSX.Element {
+function List<T extends Record<string, unknown>>({
+    data,
+    title,
+    keysToShow,
+}: ListProps<T>): JSX.Element {
     return (
         <div className="flex flex-col gap-2">
             {title && <h2>{title}</h2>}
             <ul>
-                {(data as Record<string, unknown>[]).map((item, index) => (
+                {data.map((item) => (
                     <li key={item.id as string} className="flex gap-2">
                         {keysToShow.map((key) => (
                             <div key={key} className="flex">
