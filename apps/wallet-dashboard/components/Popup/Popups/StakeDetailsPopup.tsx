c4f5e530df3b99a5caa34fd5cfb42990a228ece2
evavirseda <evirseda@boxfish.studio>
2024-05-28 17:06:14 +0200

    feat(wallet-dashboard): add StakeDetailsPopup compoentn

---
+++ apps/wallet-dashboard/components/Popup/Popups/StakeDetailsPopup.tsx
@@ -0,0 +1,30 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import React from 'react';
+import { Button } from '@/components/index';
+
+interface StakeDetailsPopupProps {
+    stake: {
+        id: string;
+        validator: string;
+        stake: string;
+        rewards: string;
+    };
+}
+
+const StakeDetailsPopup: React.FC<StakeDetailsPopupProps> = ({ stake }) => {
+    return (
+        <div className="flex min-w-[300px] flex-col gap-2">
+            <p>{stake.validator}</p>
+            <p>Stake: {stake.stake}</p>
+            <p>Rewards: {stake.rewards}</p>
+            <div className="flex justify-between gap-2">
+                <Button onClick={() => console.log('Unstake')}>Unstake</Button>
+                <Button onClick={() => console.log('Stake more')}>Stake more</Button>
+            </div>
+        </div>
+    );
+};
+
+export default StakeDetailsPopup;
