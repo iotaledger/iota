b8011896f2f20921d112757dc52821c26c835618
Philipp Gackstatter <philipp.gackstatter@iota.org>
2024-05-29 14:43:09 +0200

    refactor: Factor out object ownership test

--- crates/sui-genesis-builder/src/stardust/migration/tests/alias.rs
+++ crates/sui-genesis-builder/src/stardust/migration/tests/alias.rs
@@ -1,18 +1,22 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::stardust::migration::migration::PACKAGE_DEPS;
 use crate::stardust::migration::tests::extract_native_token_from_bag;
+use crate::stardust::migration::tests::object_migration_with_object_owner;
 use crate::stardust::migration::tests::run_migration;
 use crate::stardust::migration::tests::{create_foundry, random_output_header};
 use crate::stardust::types::stardust_to_sui_address;
 use crate::stardust::types::ALIAS_DYNAMIC_OBJECT_FIELD_KEY;
 use crate::stardust::types::ALIAS_DYNAMIC_OBJECT_FIELD_KEY_TYPE;
 use crate::stardust::types::ALIAS_OUTPUT_MODULE_NAME;
+use crate::stardust::types::NFT_OUTPUT_MODULE_NAME;
 use crate::stardust::types::{snapshot::OutputHeader, Alias, AliasOutput};
 
 use iota_sdk::types::block::address::Address;
 use iota_sdk::types::block::output::feature::Irc30Metadata;
+use iota_sdk::types::block::output::unlock_condition::AddressUnlockCondition;
+use iota_sdk::types::block::output::NftId;
+use iota_sdk::types::block::output::NftOutputBuilder;
 use iota_sdk::types::block::output::{NativeToken, SimpleTokenScheme};
 use iota_sdk::types::block::{
     address::Ed25519Address,
@@ -24,18 +28,14 @@ use iota_sdk::types::block::{
 };
 use iota_sdk::U256;
 use move_core_types::ident_str;
-use move_core_types::language_storage::StructTag;
 use std::str::FromStr;
-use sui_types::base_types::SuiAddress;
+use sui_types::base_types::ObjectID;
 use sui_types::dynamic_field::derive_dynamic_field_id;
 use sui_types::dynamic_field::DynamicFieldInfo;
 use sui_types::id::UID;
 use sui_types::object::Object;
 use sui_types::object::Owner;
-use sui_types::programmable_transaction_builder::ProgrammableTransactionBuilder;
-use sui_types::transaction::{Argument, CheckedInputObjects, ObjectArg};
 use sui_types::TypeTag;
-use sui_types::{base_types::ObjectID, STARDUST_PACKAGE_ID, SUI_FRAMEWORK_PACKAGE_ID};
 
 fn migrate_alias(
     header: OutputHeader,
@@ -171,26 +171,23 @@ fn alias_migration_with_zeroed_id() {
 /// Test that an Alias owned by another Alias can be received by the owning
 /// object.
 ///
-/// The PTB sends the extracted assets to the null address since it must be used
-/// in the transaction.
+/// The PTB sends the extracted assets to the null address since they must be used in the transaction.
 #[test]
 fn alias_migration_with_alias_owner() {
     let random_address = Ed25519Address::from(rand::random::<[u8; Ed25519Address::LENGTH]>());
 
     let alias1_header = random_output_header();
-    let alias1_amount = 1_000_000;
     let stardust_alias1 =
-        AliasOutputBuilder::new_with_amount(alias1_amount, AliasId::new(rand::random()))
+        AliasOutputBuilder::new_with_amount(1_000_000, AliasId::new(rand::random()))
             .add_unlock_condition(StateControllerAddressUnlockCondition::new(random_address))
             .add_unlock_condition(GovernorAddressUnlockCondition::new(random_address))
             .finish()
             .unwrap();
 
     let alias2_header = random_output_header();
-    let alias2_amount = 2_000_000;
     // stardust_alias1 is the owner of stardust_alias2.
     let stardust_alias2 =
-        AliasOutputBuilder::new_with_amount(alias2_amount, AliasId::new(rand::random()))
+        AliasOutputBuilder::new_with_amount(2_000_000, AliasId::new(rand::random()))
             .add_unlock_condition(StateControllerAddressUnlockCondition::new(Address::from(
                 *stardust_alias1.alias_id(),
             )))
@@ -200,137 +197,53 @@ fn alias_migration_with_alias_owner() {
             .finish()
             .unwrap();
 
-    let (mut executor, objects_map) = run_migration([
-        (alias1_header.clone(), stardust_alias1.into()),
-        (alias2_header.clone(), stardust_alias2.into()),
-    ]);
-
-    // Find the corresponding objects to the migrated aliases.
-    let alias1_created_objects = objects_map
-        .get(&alias1_header.output_id())
-        .expect("alias output should have created objects");
-    let alias2_created_objects = objects_map
-        .get(&alias2_header.output_id())
-        .expect("alias output should have created objects");
-
-    let alias_output1_object_ref = executor
-        .store()
-        .get_object(alias1_created_objects.output().unwrap())
-        .unwrap()
-        .compute_object_reference();
-    let alias_output2_object_ref = executor
-        .store()
-        .get_object(alias2_created_objects.output().unwrap())
-        .unwrap()
-        .compute_object_reference();
-
-    let pt = {
-        let mut builder = ProgrammableTransactionBuilder::new();
-        let alias1_arg = builder
-            .obj(ObjectArg::ImmOrOwnedObject(alias_output1_object_ref))
-            .unwrap();
-
-        let extracted_assets = builder.programmable_move_call(
-            STARDUST_PACKAGE_ID,
-            ALIAS_OUTPUT_MODULE_NAME.into(),
-            ident_str!("extract_assets").into(),
-            vec![],
-            vec![alias1_arg],
-        );
-
-        let Argument::Result(result_idx) = extracted_assets else {
-            panic!("expected Argument::Result");
-        };
-        let balance_arg = Argument::NestedResult(result_idx, 0);
-        let bag_arg = Argument::NestedResult(result_idx, 1);
-        let alias1_arg = Argument::NestedResult(result_idx, 2);
-
-        let receiving_alias2_arg = builder
-            .obj(ObjectArg::Receiving(alias_output2_object_ref))
-            .unwrap();
-        let received_alias_output2 = builder.programmable_move_call(
-            STARDUST_PACKAGE_ID,
-            ident_str!("address_unlock_condition").into(),
-            ident_str!("unlock_alias_address_owned_alias").into(),
-            vec![],
-            vec![alias1_arg, receiving_alias2_arg],
-        );
-
-        let coin_arg = builder.programmable_move_call(
-            SUI_FRAMEWORK_PACKAGE_ID,
-            ident_str!("coin").into(),
-            ident_str!("from_balance").into(),
-            vec![
-                StructTag::from_str(&format!("{}::sui::SUI", SUI_FRAMEWORK_PACKAGE_ID))
-                    .unwrap()
-                    .into(),
-            ],
-            vec![balance_arg],
-        );
-
-        // Destroying the bag only works if it's empty, hence asserting that it is in fact empty.
-        builder.programmable_move_call(
-            SUI_FRAMEWORK_PACKAGE_ID,
-            ident_str!("bag").into(),
-            ident_str!("destroy_empty").into(),
-            vec![],
-            vec![bag_arg],
-        );
-
-        // Transfer the coin to the zero address since we have to move it somewhere.
-        builder.transfer_arg(SuiAddress::default(), coin_arg);
-
-        // We have to use Alias Output as we cannot transfer it (since it lacks the
-        // `store` ability), so we extract its assets.
-        let extracted_assets = builder.programmable_move_call(
-            STARDUST_PACKAGE_ID,
-            ALIAS_OUTPUT_MODULE_NAME.into(),
-            ident_str!("extract_assets").into(),
-            vec![],
-            vec![received_alias_output2],
-        );
-        let Argument::Result(result_idx) = extracted_assets else {
-            panic!("expected Argument::Result");
-        };
-        let balance_arg = Argument::NestedResult(result_idx, 0);
-        let bag_arg = Argument::NestedResult(result_idx, 1);
-        let alias2_arg = Argument::NestedResult(result_idx, 2);
-
-        let coin_arg = builder.programmable_move_call(
-            SUI_FRAMEWORK_PACKAGE_ID,
-            ident_str!("coin").into(),
-            ident_str!("from_balance").into(),
-            vec![TypeTag::from_str(&format!("{}::sui::SUI", SUI_FRAMEWORK_PACKAGE_ID)).unwrap()],
-            vec![balance_arg],
-        );
-
-        // Destroying the bag only works if it's empty, hence asserting that it is in fact empty.
-        builder.programmable_move_call(
-            SUI_FRAMEWORK_PACKAGE_ID,
-            ident_str!("bag").into(),
-            ident_str!("destroy_empty").into(),
-            vec![],
-            vec![bag_arg],
-        );
-
-        // Transfer the coin to the zero address since we have to move it somewhere.
-        builder.transfer_arg(SuiAddress::default(), coin_arg);
-
-        // We have successfully extracted the aliases which is what we want to test.
-        // Transfer to the zero address so the PTB doesn't fail.
-        builder.transfer_arg(SuiAddress::default(), alias1_arg);
-        builder.transfer_arg(SuiAddress::default(), alias2_arg);
-
-        builder.finish()
-    };
-
-    let input_objects = CheckedInputObjects::new_for_genesis(
-        executor
-            .load_input_objects([alias_output1_object_ref])
-            .chain(executor.load_packages(PACKAGE_DEPS))
-            .collect(),
+    object_migration_with_object_owner(
+        alias1_header.output_id(),
+        alias2_header.output_id(),
+        [
+            (alias1_header.clone(), stardust_alias1.into()),
+            (alias2_header.clone(), stardust_alias2.into()),
+        ],
+        ALIAS_OUTPUT_MODULE_NAME,
+        ALIAS_OUTPUT_MODULE_NAME,
+        ident_str!("unlock_alias_address_owned_alias"),
     );
-    executor.execute_pt_unmetered(input_objects, pt).unwrap();
+}
+
+/// Test that an Alias owned by an NFT can be received by the owning object.
+#[test]
+fn alias_migration_with_nft_owner() {
+    let random_address = Ed25519Address::from(rand::random::<[u8; Ed25519Address::LENGTH]>());
+
+    let nft_header = random_output_header();
+    let nft = NftOutputBuilder::new_with_amount(1_000_000, NftId::new(rand::random()))
+        .add_unlock_condition(AddressUnlockCondition::new(random_address))
+        .finish()
+        .unwrap();
+
+    let alias_header = random_output_header();
+    // nft is the owner (governor) of alias.
+    let alias = AliasOutputBuilder::new_with_amount(2_000_000, AliasId::new(rand::random()))
+        .add_unlock_condition(StateControllerAddressUnlockCondition::new(
+            Ed25519Address::from(rand::random::<[u8; Ed25519Address::LENGTH]>()),
+        ))
+        .add_unlock_condition(GovernorAddressUnlockCondition::new(Address::from(
+            nft.nft_id().clone(),
+        )))
+        .finish()
+        .unwrap();
+
+    object_migration_with_object_owner(
+        nft_header.output_id(),
+        alias_header.output_id(),
+        [
+            (nft_header.clone(), nft.into()),
+            (alias_header.clone(), alias.into()),
+        ],
+        NFT_OUTPUT_MODULE_NAME,
+        ALIAS_OUTPUT_MODULE_NAME,
+        ident_str!("unlock_nft_address_owned_alias"),
+    );
 }
 
 /// Test that an Alias that owns Native Tokens can extract those tokens from the
