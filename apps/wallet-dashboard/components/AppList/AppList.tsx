c2ebc63eeca76917dd14652d93fc1ae8901930d7
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2024-05-29 14:30:03 +0300

    feat(wallet-dashboard): add import React.
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- apps/wallet-dashboard/components/AppList/AppList.tsx
+++ apps/wallet-dashboard/components/AppList/AppList.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+import React from 'react';
 import Image from 'next/image';
 import { useAppsBackend } from '@mysten/core';
 import { useQuery } from '@tanstack/react-query';
