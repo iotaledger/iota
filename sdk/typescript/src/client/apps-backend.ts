1fc21d3ad73a761ed7bdaf7dea73da903e1f0731
Mario Sarcevic <mario.sarcevic@iota.org>
2024-05-30 13:32:44 +0200

    fix(tooling-sdk) Format (prettier) ALL for sdk/typescript

--- sdk/typescript/src/client/apps-backend.ts
+++ sdk/typescript/src/client/apps-backend.ts
@@ -1,3 +1,5 @@
+//[object Object]
+// SPDX-License-Identifier: Apache-2.0
 export function getAppsBackend(): string {
-    return process.env.APPS_BACKEND as string
+    return process.env.APPS_BACKEND as string;
 }
