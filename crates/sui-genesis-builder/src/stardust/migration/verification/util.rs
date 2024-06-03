f8e55297debcfe9d24c6b38fda8f78a7fa50823f
Thoralf Müller <thoralf.mue@gmail.com>
2024-06-03 11:21:32 +0200

    Update alias

--- crates/sui-genesis-builder/src/stardust/migration/verification/util.rs
+++ crates/sui-genesis-builder/src/stardust/migration/verification/util.rs
@@ -305,28 +305,6 @@ pub(super) fn verify_parent(address: &Address, storage: &InMemoryStorage) -> Res
     Ok(())
 }
 
-pub(super) fn verify_issuer_feature(
-    original: Option<&sdk_output::feature::IssuerFeature>,
-    created: Option<SuiAddress>,
-) -> Result<()> {
-    if let Some(issuer) = original {
-        let sui_issuer_address = issuer.address().to_string().parse::<SuiAddress>()?;
-        if let Some(obj_issuer) = created {
-            ensure!(
-                obj_issuer == sui_issuer_address,
-                "issuer mismatch: found {}, expected {}",
-                obj_issuer,
-                sui_issuer_address
-            );
-        } else {
-            bail!("missing issuer on object");
-        }
-    } else {
-        ensure!(created.is_none(), "erroneous issuer on object");
-    }
-    Ok(())
-}
-
 pub(super) trait NativeTokenKind {
     fn token_type(&self) -> String;
 
