0e856ed6d462162707ac2d9141cbeb8aa11bc9ec
cpl121 <cpeon@boxfish.studio>
2024-06-03 17:31:55 +0200

    feat(dashboard): split sendCoin form into two components

--- apps/wallet-dashboard/components/Popup/Popups/SendCoinPopup.tsx
+++ apps/wallet-dashboard/components/Popup/Popups/SendCoinPopup.tsx
@@ -5,6 +5,98 @@ import React, { useState } from 'react';
 import { Button } from '@/components';
 import { CoinStruct } from '@mysten/sui.js/client';
 
+interface FormDataValues {
+    amount: string;
+    recipientAddress: string;
+    senderAddress: string;
+}
+
+interface EnterValuesProps {
+    coin: CoinStruct;
+    formData: FormDataValues;
+    setFormData: React.Dispatch<React.SetStateAction<FormDataValues>>;
+    onClose: () => void;
+    handleNext: () => void;
+}
+
+const EnterValuesForm = ({
+    coin: { balance, coinObjectId },
+    formData: { amount, recipientAddress },
+    setFormData,
+    onClose,
+    handleNext,
+}: EnterValuesProps) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }));
+    };
+
+    return (
+        <div className="flex flex-col gap-4">
+            <h1 className="mb-4 text-center text-xl">Send coins</h1>
+            <div className="flex flex-col gap-4">
+                <p>Coin: {coinObjectId}</p>
+                <p>Balance: {balance}</p>
+                <label htmlFor="amount">Coin amount to send: </label>
+                <input
+                    type="number"
+                    id="amount"
+                    min="1"
+                    value={amount}
+                    onChange={handleChange}
+                    placeholder="Enter the amount to send coins"
+                />
+                <label htmlFor="address">Address: </label>
+                <input
+                    type="text"
+                    id="address"
+                    value={recipientAddress}
+                    onChange={handleChange}
+                    placeholder="Enter the address to send coins"
+                />
+            </div>
+            <div className="mt-4 flex justify-around">
+                <Button onClick={onClose}>Cancel</Button>
+                <Button onClick={handleNext} disabled={!recipientAddress || !amount}>
+                    Next
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+interface ReviewValuesProps {
+    formData: FormDataValues;
+    handleBack: () => void;
+}
+
+const ReviewValuesForm = ({
+    formData: { amount, senderAddress, recipientAddress },
+    handleBack,
+}: ReviewValuesProps) => {
+    function onSend(): void {
+        console.log('Sending coins');
+    }
+
+    return (
+        <div className="flex flex-col gap-4">
+            <h1 className="mb-4 text-center text-xl">Review & Send</h1>
+            <div className="flex flex-col gap-4">
+                <p>Sending: {amount}</p>
+                <p>From: {senderAddress}</p>
+                <p>To: {recipientAddress}</p>
+            </div>
+            <div className="mt-4 flex justify-around">
+                <Button onClick={handleBack}>Back</Button>
+                <Button onClick={onSend}>Send now</Button>
+            </div>
+        </div>
+    );
+};
+
 interface SendCoinPopupProps {
     coin: CoinStruct;
     senderAddress: string;
@@ -16,18 +108,13 @@ enum Steps {
     ReviewValues,
 }
 
-function SendCoinPopup({
-    coin: { balance, coinObjectId },
-    senderAddress,
-    onClose,
-}: SendCoinPopupProps): JSX.Element {
-    const [recipientAddress, setRecipientAddress] = useState<string>('');
-    const [amount, setAmount] = useState<string>('');
+function SendCoinPopup({ coin, senderAddress, onClose }: SendCoinPopupProps): JSX.Element {
     const [step, setStep] = useState<Steps>(Steps.EnterValues);
-
-    function onSend(): void {
-        console.log('Sending coins');
-    }
+    const [formData, setFormData] = useState<FormDataValues>({
+        amount: '',
+        recipientAddress: '',
+        senderAddress,
+    });
 
     const handleNext = () => {
         setStep(Steps.ReviewValues);
@@ -40,50 +127,16 @@ function SendCoinPopup({
     return (
         <>
             {step === Steps.EnterValues && (
-                <div className="flex flex-col gap-4">
-                    <h1 className="mb-4 text-center text-xl">Send coins</h1>
-                    <div className="flex flex-col gap-4">
-                        <p>Coin: {coinObjectId}</p>
-                        <p>Balance: {balance}</p>
-                        <label htmlFor="amount">Coin amount to send: </label>
-                        <input
-                            type="number"
-                            id="amount"
-                            min="1"
-                            value={amount}
-                            onChange={(e) => setAmount(e.target.value)}
-                            placeholder="Enter the amount to send coins"
-                        />
-                        <label htmlFor="address">Address: </label>
-                        <input
-                            type="text"
-                            id="address"
-                            value={recipientAddress}
-                            onChange={(e) => setRecipientAddress(e.target.value)}
-                            placeholder="Enter the address to send coins"
-                        />
-                    </div>
-                    <div className="mt-4 flex justify-around">
-                        <Button onClick={onClose}>Cancel</Button>
-                        <Button onClick={handleNext}>Next</Button>
-                    </div>
-                </div>
+                <EnterValuesForm
+                    coin={coin}
+                    onClose={onClose}
+                    handleNext={handleNext}
+                    formData={formData}
+                    setFormData={setFormData}
+                />
             )}
             {step === Steps.ReviewValues && (
-                <div className="flex flex-col gap-4">
-                    <h1 className="mb-4 text-center text-xl">Review & Send</h1>
-                    <div className="flex flex-col gap-4">
-                        <p>Sending: {amount}</p>
-                        <p>From: {senderAddress}</p>
-                        <p>To: {recipientAddress}</p>
-                    </div>
-                    <div className="mt-4 flex justify-around">
-                        <Button onClick={handleBack}>Back</Button>
-                        <Button onClick={onSend} disabled={!recipientAddress || !amount}>
-                            Send now
-                        </Button>
-                    </div>
-                </div>
+                <ReviewValuesForm formData={formData} handleBack={handleBack} />
             )}
         </>
     );
