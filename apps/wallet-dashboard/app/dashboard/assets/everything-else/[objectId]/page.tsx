ab915bc0a5d5e3f2f5f49fddd0452151ffb5ce42
evavirseda <evirseda@boxfish.studio>
2024-06-04 09:57:26 +0200

    feat(wallet-dashboard): use AssetCard in asset details route

--- apps/wallet-dashboard/app/dashboard/assets/everything-else/[objectId]/page.tsx
+++ apps/wallet-dashboard/app/dashboard/assets/everything-else/[objectId]/page.tsx
@@ -5,7 +5,7 @@
 
 import React from 'react';
 import { useParams } from 'next/navigation';
-import { RouteLink } from '@/components';
+import { AssetCard, RouteLink } from '@/components';
 import { HARDCODED_NON_VISUAL_ASSETS } from '@/lib/mocks';
 
 const VisualAssetDetailPage = () => {
@@ -18,14 +18,7 @@ const VisualAssetDetailPage = () => {
         <div className="flex h-full w-full flex-col space-y-4 px-40">
             <RouteLink path="/dashboard/assets/everything-else" title="Back" />
             {asset ? (
-                <div className="flex h-full w-full flex-col items-center justify-center space-y-4">
-                    <h1>Asset Details</h1>
-                    <div>
-                        <p>Digest: {asset.digest}</p>
-                        <p>Object ID: {asset.objectId}</p>
-                        <p>Version: {asset.version}</p>
-                    </div>
-                </div>
+                <AssetCard key={asset.objectId} asset={asset} />
             ) : (
                 <div className="flex justify-center p-20">Asset not found</div>
             )}
--- apps/wallet-dashboard/app/dashboard/assets/visual-assets/[objectId]/page.tsx
+++ apps/wallet-dashboard/app/dashboard/assets/visual-assets/[objectId]/page.tsx
@@ -4,9 +4,8 @@
 'use client';
 
 import React from 'react';
-import Image from 'next/image';
 import { useParams } from 'next/navigation';
-import { RouteLink } from '@/components';
+import { AssetCard, RouteLink } from '@/components';
 import { HARCODED_VISUAL_ASSETS } from '@/lib/mocks';
 
 const VisualAssetDetailPage = () => {
@@ -19,22 +18,7 @@ const VisualAssetDetailPage = () => {
         <div className="flex h-full w-full flex-col space-y-4 px-40">
             <RouteLink path="/dashboard/assets/visual-assets" title="Back" />
             {asset ? (
-                <div className="flex h-full w-full flex-col items-center justify-center space-y-4">
-                    <h1>Asset Details</h1>
-                    {asset.display && asset.display.data && asset.display.data.image && (
-                        <Image
-                            src={asset.display.data.image}
-                            alt={asset.display.data.name}
-                            width={80}
-                            height={40}
-                        />
-                    )}
-                    <div>
-                        <p>Digest: {asset.digest}</p>
-                        <p>Object ID: {asset.objectId}</p>
-                        <p>Version: {asset.version}</p>
-                    </div>
-                </div>
+                <AssetCard key={asset.objectId} asset={asset} />
             ) : (
                 <div className="flex justify-center p-20">Asset not found</div>
             )}
