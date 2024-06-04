d861fb2a654de42f1b27c9cfa4a349afa4510e0b
JCNoguera <jcnoguera@boxfish.studio>
2024-06-04 20:00:12 +0200

    chore: revert unneeded changes

--- apps/wallet/src/ui/app/hooks/useGetNFTs.ts
+++ apps/wallet/src/ui/app/hooks/useGetNFTs.ts
@@ -1,9 +1,6 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-// Modifications Copyright (c) 2024 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
 import { hasDisplayData, useGetOwnedObjects } from '@mysten/core';
 import { type SuiObjectData } from '@mysten/sui.js/client';
 import { useMemo } from 'react';
