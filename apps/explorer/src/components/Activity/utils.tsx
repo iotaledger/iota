6b9e7fbe706fcfc85f4d28b8033da2fcddbd8ebd
evavirseda <evirseda@boxfish.studio>
2024-06-05 15:41:41 +0200

    refactor(explorer): apply regular functions conventions

--- apps/explorer/src/components/Activity/utils.tsx
+++ apps/explorer/src/components/Activity/utils.tsx
@@ -1,6 +1,9 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+// Modifications Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 import { type EpochMetricsPage } from '@mysten/sui.js/client';
 import { Text } from '@mysten/ui';
 
@@ -9,57 +12,79 @@ import { TxTimeType } from '../tx-time/TxTimeType';
 import { HighlightedTableCol } from '~/components/Table/HighlightedTableCol';
 import { CheckpointSequenceLink, EpochLink } from '~/ui/InternalLink';
 import { getEpochStorageFundFlow } from '~/utils/getStorageFundFlow';
+import { type ReactNode } from 'react';
+
+interface EpochData {
+    epoch: ReactNode;
+    transactions: ReactNode;
+    stakeRewards: ReactNode;
+    checkpointSet: ReactNode;
+    storageNetInflow: ReactNode;
+    time: ReactNode;
+}
+
+interface TableColumn {
+    header: string;
+    accessorKey: keyof EpochData;
+}
+
+interface EpochTableData {
+    data: EpochData[];
+    columns: TableColumn[];
+}
 
 // Generate table data from the epochs data
-export const genTableDataFromEpochsData = (results: EpochMetricsPage) => ({
-    data: results?.data.map((epoch) => ({
-        epoch: (
-            <HighlightedTableCol first>
-                <EpochLink epoch={epoch.epoch.toString()} />
-            </HighlightedTableCol>
-        ),
-        transactions: <Text variant="bodySmall/medium">{epoch.epochTotalTransactions}</Text>,
-        stakeRewards: <SuiAmount amount={epoch.endOfEpochInfo?.totalStakeRewardsDistributed} />,
-        checkpointSet: (
-            <div>
-                <CheckpointSequenceLink sequence={epoch.firstCheckpointId.toString()} />
-                {` - `}
-                <CheckpointSequenceLink
-                    sequence={epoch.endOfEpochInfo?.lastCheckpointId.toString() ?? ''}
-                />
-            </div>
-        ),
-        storageNetInflow: (
-            <div className="pl-3">
-                <SuiAmount amount={getEpochStorageFundFlow(epoch.endOfEpochInfo).netInflow} />
-            </div>
-        ),
-        time: <TxTimeType timestamp={Number(epoch.endOfEpochInfo?.epochEndTimestamp ?? 0)} />,
-    })),
-    columns: [
-        {
-            header: 'Epoch',
-            accessorKey: 'epoch',
-        },
-        {
-            header: 'Transaction Blocks',
-            accessorKey: 'transactions',
-        },
-        {
-            header: 'Stake Rewards',
-            accessorKey: 'stakeRewards',
-        },
-        {
-            header: 'Checkpoint Set',
-            accessorKey: 'checkpointSet',
-        },
-        {
-            header: 'Storage Net Inflow',
-            accessorKey: 'storageNetInflow',
-        },
-        {
-            header: 'Epoch End',
-            accessorKey: 'time',
-        },
-    ],
-});
+export function generateTableDataFromEpochsData(results: EpochMetricsPage): EpochTableData {
+    return {
+        data: results?.data.map((epoch) => ({
+            epoch: (
+                <HighlightedTableCol first>
+                    <EpochLink epoch={epoch.epoch.toString()} />
+                </HighlightedTableCol>
+            ),
+            transactions: <Text variant="bodySmall/medium">{epoch.epochTotalTransactions}</Text>,
+            stakeRewards: <SuiAmount amount={epoch.endOfEpochInfo?.totalStakeRewardsDistributed} />,
+            checkpointSet: (
+                <div>
+                    <CheckpointSequenceLink sequence={epoch.firstCheckpointId.toString()} />
+                    {` - `}
+                    <CheckpointSequenceLink
+                        sequence={epoch.endOfEpochInfo?.lastCheckpointId.toString() ?? ''}
+                    />
+                </div>
+            ),
+            storageNetInflow: (
+                <div className="pl-3">
+                    <SuiAmount amount={getEpochStorageFundFlow(epoch.endOfEpochInfo).netInflow} />
+                </div>
+            ),
+            time: <TxTimeType timestamp={Number(epoch.endOfEpochInfo?.epochEndTimestamp ?? 0)} />,
+        })),
+        columns: [
+            {
+                header: 'Epoch',
+                accessorKey: 'epoch',
+            },
+            {
+                header: 'Transaction Blocks',
+                accessorKey: 'transactions',
+            },
+            {
+                header: 'Stake Rewards',
+                accessorKey: 'stakeRewards',
+            },
+            {
+                header: 'Checkpoint Set',
+                accessorKey: 'checkpointSet',
+            },
+            {
+                header: 'Storage Net Inflow',
+                accessorKey: 'storageNetInflow',
+            },
+            {
+                header: 'Epoch End',
+                accessorKey: 'time',
+            },
+        ],
+    };
+}
--- apps/explorer/src/components/checkpoints/utils.tsx
+++ apps/explorer/src/components/checkpoints/utils.tsx
@@ -1,45 +1,69 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+// Modifications Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 import { type CheckpointPage } from '@mysten/sui.js/client';
 import { Text } from '@mysten/ui';
 
 import { TxTimeType } from '../tx-time/TxTimeType';
 import { HighlightedTableCol } from '~/components/Table/HighlightedTableCol';
 import { CheckpointLink, CheckpointSequenceLink } from '~/ui/InternalLink';
+import { type ReactNode } from 'react';
+
+interface CheckpointData {
+    digest: ReactNode;
+    time: ReactNode;
+    sequenceNumber: ReactNode;
+    transactionBlockCount: ReactNode;
+}
+
+interface TableColumn {
+    header: () => string;
+    accessorKey: keyof CheckpointData;
+}
+
+interface CheckpointTableData {
+    data: CheckpointData[];
+    columns: TableColumn[];
+}
 
 // Generate table data from the checkpoints data
-export const genTableDataFromCheckpointsData = (data: CheckpointPage) => ({
-    data: data?.data.map((checkpoint) => ({
-        digest: (
-            <HighlightedTableCol first>
-                <CheckpointLink digest={checkpoint.digest} />
-            </HighlightedTableCol>
-        ),
-        time: <TxTimeType timestamp={Number(checkpoint.timestampMs)} />,
-        sequenceNumber: <CheckpointSequenceLink sequence={checkpoint.sequenceNumber} />,
-        transactionBlockCount: (
-            <Text variant="bodySmall/medium" color="steel-darker">
-                {checkpoint.transactions.length}
-            </Text>
-        ),
-    })),
-    columns: [
-        {
-            header: () => 'Digest',
-            accessorKey: 'digest',
-        },
-        {
-            header: () => 'Sequence Number',
-            accessorKey: 'sequenceNumber',
-        },
-        {
-            header: () => 'Time',
-            accessorKey: 'time',
-        },
-        {
-            header: () => 'Transaction Block Count',
-            accessorKey: 'transactionBlockCount',
-        },
-    ],
-});
+export function generateTableDataFromCheckpointsData(data: CheckpointPage): CheckpointTableData {
+    return {
+        data:
+            data?.data.map((checkpoint) => ({
+                digest: (
+                    <HighlightedTableCol first>
+                        <CheckpointLink digest={checkpoint.digest} />
+                    </HighlightedTableCol>
+                ),
+                time: <TxTimeType timestamp={Number(checkpoint.timestampMs)} />,
+                sequenceNumber: <CheckpointSequenceLink sequence={checkpoint.sequenceNumber} />,
+                transactionBlockCount: (
+                    <Text variant="bodySmall/medium" color="steel-darker">
+                        {checkpoint.transactions.length}
+                    </Text>
+                ),
+            })) ?? [],
+        columns: [
+            {
+                header: () => 'Digest',
+                accessorKey: 'digest',
+            },
+            {
+                header: () => 'Sequence Number',
+                accessorKey: 'sequenceNumber',
+            },
+            {
+                header: () => 'Time',
+                accessorKey: 'time',
+            },
+            {
+                header: () => 'Transaction Block Count',
+                accessorKey: 'transactionBlockCount',
+            },
+        ],
+    };
+}
