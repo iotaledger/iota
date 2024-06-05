a6ff602c8e2f89a59ccf148db6f92a6b7180f730
evavirseda <evirseda@boxfish.studio>
2024-06-05 09:21:18 +0200

    feat(wallet-dashboard): split steps in two different components

---
+++ apps/wallet-dashboard/components/Popup/Popups/NewStakePopup/views/index.ts
@@ -0,0 +1,5 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+export { default as EnterAmount } from './EnterAmountView';
+export { default as SelectValidator } from './SelectValidatorView';
