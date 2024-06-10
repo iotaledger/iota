2e509c984af001fa40dcd2c3bd9b71f207d7be05
Begoña Alvarez <balvarez@boxfish.studio>
2024-06-10 13:12:09 +0200

    refactor: improve ts index files

--- apps/wallet-dashboard/components/Popup/Popups/index.ts
+++ apps/wallet-dashboard/components/Popup/Popups/index.ts
@@ -5,3 +5,5 @@ export { default as NewStakePopup } from './NewStakePopup/NewStakePopup';
 export { default as ActivityDetailsPopup } from './ActivityDetailsPopup';
 export { default as StakeDetailsPopup } from './StakeDetailsPopup';
 export { default as UnstakePopup } from './UnstakePopup';
+
+export * from './NewStakePopup';
