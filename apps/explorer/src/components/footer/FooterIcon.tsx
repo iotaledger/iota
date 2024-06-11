2104eb816b3df5865fd7feb7aada865c51a2e5f8
evavirseda <evirseda@boxfish.studio>
2024-06-11 10:33:07 +0200

    feat(explorer): cleanup

--- apps/explorer/src/components/footer/FooterIcon.tsx
+++ apps/explorer/src/components/footer/FooterIcon.tsx
@@ -2,14 +2,8 @@
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { type ReactNode } from 'react';
+import { type PropsWithChildren } from 'react';
 
-type FooterItem = {
-    category: string;
-    items: { title: string; children: ReactNode; href: string }[];
-};
-export type FooterItems = FooterItem[];
-
-export function FooterIcon({ children }: { children: ReactNode }) {
+export function FooterIcon({ children }: PropsWithChildren): JSX.Element {
     return <div className="flex items-center text-steel-darker">{children}</div>;
 }
