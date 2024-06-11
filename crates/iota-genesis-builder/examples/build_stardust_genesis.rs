9252113b26dbeb32348f243daaee2ae5bcde0a87
Mirko Zichichi <miker83z@proton.me>
2024-06-11 20:34:09 +0200

    feat(iota-genesis-builder): make genesis build example complete

--- crates/iota-genesis-builder/examples/build_stardust_genesis.rs
+++ crates/iota-genesis-builder/examples/build_stardust_genesis.rs
@@ -7,6 +7,16 @@ use iota_genesis_builder::Builder;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
+use iota_config::local_ip_utils;
+use iota_genesis_builder::{validator_info::ValidatorInfo, OBJECT_SNAPSHOT_FILE_PATH};
+use iota_types::{
+    base_types::IotaAddress,
+    crypto::{
+        generate_proof_of_possession, get_key_pair, AccountKeyPair, AuthorityKeyPair, IotaKeyPair,
+        KeypairTraits, NetworkKeyPair,
+    },
+};
+
 fn main() -> anyhow::Result<()> {
     // Initialize tracing
     let subscriber = FmtSubscriber::builder()
@@ -14,7 +24,39 @@ fn main() -> anyhow::Result<()> {
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
-    let builder = Builder::new(); //.load_stardust_migration_objects(OBJECT_SNAPSHOT_FILE_PATH)?;
-    let a = builder.build();
+    let mut builder = Builder::new().load_stardust_migration_objects(OBJECT_SNAPSHOT_FILE_PATH)?;
+    let mut key_pairs = Vec::new();
+    for i in 0..4 {
+        let key_pair: AuthorityKeyPair = get_key_pair().1;
+        let authority_name = key_pair.public().into();
+        let worker_key_pair: NetworkKeyPair = get_key_pair().1;
+        let worker_name = worker_key_pair.public().clone();
+        let account_key_pair: IotaKeyPair = get_key_pair::<AccountKeyPair>().1.into();
+        let network_key_pair: NetworkKeyPair = get_key_pair().1;
+        let validator_info = ValidatorInfo {
+            name: format!("validator-{i}"),
+            protocol_key: authority_name,
+            worker_key: worker_name,
+            account_address: IotaAddress::from(&account_key_pair.public()),
+            network_key: network_key_pair.public().clone(),
+            gas_price: 1,
+            commission_rate: 0,
+            network_address: local_ip_utils::new_local_tcp_address_for_testing(),
+            p2p_address: local_ip_utils::new_local_udp_address_for_testing(),
+            narwhal_primary_address: local_ip_utils::new_local_udp_address_for_testing(),
+            narwhal_worker_address: local_ip_utils::new_local_udp_address_for_testing(),
+            description: String::new(),
+            image_url: String::new(),
+            project_url: String::new(),
+        };
+        let pop = generate_proof_of_possession(&key_pair, (&account_key_pair.public()).into());
+        builder = builder.add_validator(validator_info, pop);
+        key_pairs.push((authority_name, key_pair));
+    }
+    for (_, key) in &key_pairs {
+        builder = builder.add_validator_signature(key);
+    }
+    let _genesis = builder.build();
+    println!("{:?}", _genesis);
     Ok(())
 }
