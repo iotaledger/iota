0cf086131d337c0d0de3b6cd9951622e8103eea0
Mirko Zichichi <miker83z@proton.me>
2024-06-12 15:50:45 +0200

    fix(iota-genesis-builder): pass total supply info to migration

--- crates/iota-genesis-builder/src/stardust/error.rs
+++ crates/iota-genesis-builder/src/stardust/error.rs
@@ -35,6 +35,8 @@ pub enum StardustError {
     MeltingTokensMustNotBeGreaterThanMintedTokens,
     #[error("circulating supply must not be greater than maximum supply")]
     CirculatingSupplyMustNotBeGreaterThanMaximumSupply,
+    #[error("stardust snapshot parameters not found")]
+    HornetSnapshotParametersNotFound,
 }
 
 impl From<Infallible> for StardustError {
