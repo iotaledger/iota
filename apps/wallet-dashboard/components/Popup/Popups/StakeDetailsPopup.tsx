1635b7b0cad1ab8d7dc0c83ce83d98ced4585d8f
evavirseda <evirseda@boxfish.studio>
2024-06-14 13:40:03 +0200

    feat(wallet): update components to use new hooks

--- apps/wallet-dashboard/components/Popup/Popups/StakeDetailsPopup.tsx
+++ apps/wallet-dashboard/components/Popup/Popups/StakeDetailsPopup.tsx
@@ -5,10 +5,10 @@ import React from 'react';
 import { Button } from '@/components/index';
 import { usePopups } from '@/hooks';
 import UnstakePopup from './UnstakePopup';
-import { Stake } from '@/lib/types';
+import { DelegatedStakeWithValidator } from '@iota/core';
 
 interface StakeDetailsPopupProps {
-    stake: Stake;
+    stake: DelegatedStakeWithValidator;
 }
 
 function StakeDetailsPopup({ stake }: StakeDetailsPopupProps): JSX.Element {
@@ -24,8 +24,8 @@ function StakeDetailsPopup({ stake }: StakeDetailsPopupProps): JSX.Element {
 
     return (
         <div className="flex min-w-[400px] flex-col gap-2">
-            <p>{stake.validator}</p>
-            <p>Stake: {stake.stake}</p>
+            <p>{stake.validatorAddress}</p>
+            <p>Stake: {stake.principal}</p>
             <p>Stake Active Epoch: {stake.stakeActiveEpoch}</p>
             <p>Stake Request Epoch: {stake.stakeRequestEpoch}</p>
             {stake.status === 'Active' && <p>Estimated reward: {stake.estimatedReward}</p>}
