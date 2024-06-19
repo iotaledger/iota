1a0da180667f692495a159ce0bb25fe790ffd0fb
Thibault Martinez <thibault@iota.org>
2024-06-19 11:09:59 +0200

    initial add_snapshot_test_data setup

---
+++ crates/iota-genesis-builder/src/stardust/test_data/mod.rs
@@ -0,0 +1,40 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+mod vesting_schedule_multiple_addresses;
+
+use std::{
+    fs::{File, OpenOptions},
+    io::BufWriter,
+    path::Path,
+};
+
+use packable::{packer::IoPacker, Packable};
+
+use crate::stardust::parse::FullSnapshotParser;
+
+pub fn add_snapshot_test_data<P: AsRef<Path> + core::fmt::Debug>(
+    current_path: P,
+    new_path: P,
+) -> anyhow::Result<()> {
+    let current_file = File::open(current_path)?;
+    let new_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(new_path)?;
+    let mut writer = IoPacker::new(BufWriter::new(new_file));
+
+    let parser = FullSnapshotParser::new(current_file)?;
+    parser.header.pack(&mut writer)?;
+
+    parser.outputs().for_each(|res| {
+        let (output_header, output) = res.unwrap();
+        output_header.pack(&mut writer).unwrap();
+        output.pack(&mut writer).unwrap();
+    });
+
+    // let outputs = vesting_schedule_multiple_addresses::outputs();
+
+    Ok(())
+}
