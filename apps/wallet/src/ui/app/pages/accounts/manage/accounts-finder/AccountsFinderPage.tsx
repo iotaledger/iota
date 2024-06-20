8db2b8eeb528e3e7e8f509a3ef25403a7b3d3f1a
evavirseda <evirseda@boxfish.studio>
2024-06-20 11:51:02 +0200

    feat(wallet): update headers

--- apps/wallet/src/ui/app/pages/accounts/manage/accounts-finder/AccountsFinderPage.tsx
+++ apps/wallet/src/ui/app/pages/accounts/manage/accounts-finder/AccountsFinderPage.tsx
@@ -1,6 +1,5 @@
-// Copyright (c) Mysten Labs, Inc.
-// Modifications Copyright (c) 2024 IOTA Stiftung
-// SPDX-License-Identifie: Apache-2.0
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
 
 import { useInitializedGuard } from '_src/ui/app/hooks';
 import { useNavigate } from 'react-router-dom';
