86c3e7485bd13af80e52bd48a3a0f86961069174
evavirseda <evirseda@boxfish.studio>
2024-06-20 11:50:43 +0200

    feat(wallet): rename file

--- apps/wallet/src/ui/app/components/accounts/AccountBalanceItem.tsx
+++ apps/wallet/src/ui/app/components/accounts/AccountBalanceItem.tsx
@@ -1,6 +1,5 @@
-// Copyright (c) Mysten Labs, Inc.
-// Modifications Copyright (c) 2024 IOTA Stiftung
-// SPDX-License-Identifie: Apache-2.0
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
 
 import { Copy12 } from '@iota/icons';
 import { Text } from '_src/ui/app/shared/text';
