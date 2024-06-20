f81b1601a495b7225eae22a6a0e99204b8ce18c1
Alex Coats <alexandcoats@gmail.com>
2024-06-20 11:25:10 -0400

    feat(iota-genesis-builder): Scale output amounts

--- crates/iota-genesis-builder/src/main.rs
+++ crates/iota-genesis-builder/src/main.rs
@@ -12,9 +12,14 @@ use iota_genesis_builder::{
     stardust::{
         migration::{CoinType, Migration, MigrationTargetNetwork},
         parse::FullSnapshotParser,
+        types::snapshot::OutputHeader,
     },
     BROTLI_COMPRESSOR_BUFFER_SIZE, BROTLI_COMPRESSOR_LG_WINDOW_SIZE, BROTLI_COMPRESSOR_QUALITY,
     OBJECT_SNAPSHOT_FILE_PATH,
+};
+use iota_sdk::types::block::output::{
+    unlock_condition::StorageDepositReturnUnlockCondition, AliasOutputBuilder, BasicOutputBuilder,
+    FoundryOutputBuilder, NftOutputBuilder, Output,
 };
 use itertools::Itertools;
 use tracing::Level;
@@ -25,6 +30,9 @@ use tracing_subscriber::FmtSubscriber;
 struct Cli {
     #[clap(subcommand)]
     snapshot: Snapshot,
+    // A scaling factor which will be applied to all output amounts.
+    #[clap(default_value_t = 1.0)]
+    amount_multiplier: f64,
 }
 
 #[derive(Subcommand, Debug)]
@@ -87,6 +95,84 @@ fn main() -> Result<()> {
     // Run the migration and write the objects snapshot
     snapshot_parser
         .outputs()
+        .map(|res: anyhow::Result<(OutputHeader, Output)>| {
+            let (header, mut output) = res?;
+            scale_output_amount(&mut output, cli.amount_multiplier)?;
+            anyhow::Result::<(OutputHeader, Output)>::Ok((header, output))
+        })
         .process_results(|outputs| migration.run(outputs, object_snapshot_writer))??;
     Ok(())
+}
+
+fn scale_output_amount(output: &mut Output, multiplier: f64) -> anyhow::Result<()> {
+    if multiplier == 1.0 {
+        return Ok(());
+    }
+    *output = match output {
+        Output::Basic(ref basic_output) => {
+            // Update amount
+            let mut builder = BasicOutputBuilder::from(basic_output)
+                .with_amount(f64_to_u64(basic_output.amount() as f64 * multiplier)?);
+
+            // Update amount in potential storage deposit return unlock condition
+            if let Some(sdr_uc) = basic_output
+                .unlock_conditions()
+                .get(StorageDepositReturnUnlockCondition::KIND)
+            {
+                let sdr_uc = sdr_uc.as_storage_deposit_return();
+                builder = builder.replace_unlock_condition(
+                    StorageDepositReturnUnlockCondition::new(
+                        sdr_uc.return_address(),
+                        f64_to_u64(sdr_uc.amount() as f64 * multiplier)?,
+                        u64::MAX,
+                    )
+                    .unwrap(),
+                );
+            };
+
+            Output::from(builder.finish()?)
+        }
+        Output::Alias(ref alias_output) => Output::from(
+            AliasOutputBuilder::from(alias_output)
+                .with_amount(f64_to_u64(alias_output.amount() as f64 * multiplier)?)
+                .finish()?,
+        ),
+        Output::Foundry(ref foundry_output) => Output::from(
+            FoundryOutputBuilder::from(foundry_output)
+                .with_amount(f64_to_u64(foundry_output.amount() as f64 * multiplier)?)
+                .finish()?,
+        ),
+        Output::Nft(ref nft_output) => {
+            // Update amount
+            let mut builder = NftOutputBuilder::from(nft_output)
+                .with_amount(f64_to_u64(nft_output.amount() as f64 * multiplier)?);
+
+            // Update amount in potential storage deposit return unlock condition
+            if let Some(sdr_uc) = nft_output
+                .unlock_conditions()
+                .get(StorageDepositReturnUnlockCondition::KIND)
+            {
+                let sdr_uc = sdr_uc.as_storage_deposit_return();
+                builder = builder.replace_unlock_condition(
+                    StorageDepositReturnUnlockCondition::new(
+                        sdr_uc.return_address(),
+                        f64_to_u64(sdr_uc.amount() as f64 * multiplier)?,
+                        u64::MAX,
+                    )
+                    .unwrap(),
+                );
+            };
+
+            Output::from(builder.finish()?)
+        }
+        Output::Treasury(_) => return Ok(()),
+    };
+    Ok(())
+}
+
+fn f64_to_u64(float: f64) -> anyhow::Result<u64> {
+    if float > u64::MAX as f64 || float < u64::MIN as f64 {
+        anyhow::bail!("overflow converting {float} to u64");
+    }
+    Ok(float as u64)
 }
