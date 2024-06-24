a8c66d7bf3791c1e60d82f3d1296efb3b8dd4afb
Begoña Alvarez <balvarez@boxfish.studio>
2024-06-24 17:33:56 +0200

    refactor: revert unnecessary changes

--- sdk/dapp-kit/src/components/WalletProvider.tsx
+++ sdk/dapp-kit/src/components/WalletProvider.tsx
@@ -1,6 +1,3 @@
-// Copyright (c) 2024 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
 // Copyright (c) Mysten Labs, Inc.
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
