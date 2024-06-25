e9cb2fa3aeff7cbdb06a8826176d4e9aceb84f34
marc2332 <mespinsanz@gmail.com>
2024-06-25 13:13:35 +0200

    clean up

---
+++ apps/wallet/src/background/accounts-finder/accounts-finder.test.ts
@@ -0,0 +1,128 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { IotaClient } from '@iota/iota.js/client';
+import { mnemonicToSeedHex } from '@iota/iota.js/cryptography';
+import { requestIotaFromFaucetV1 } from '@iota/iota.js/faucet';
+import { Ed25519Keypair } from '@iota/iota.js/keypairs/ed25519';
+import { entropyToMnemonic, getRandomEntropy } from '_shared/utils/bip39';
+import { recoverAccounts, FoundAccount } from './accounts-finder';
+import { test } from 'vitest';
+
+const GAS_TYPE_ARG = '0x2::iota::IOTA';
+const IOTA_COIN_TYPE = 4218; // 4219 for Shimmer
+
+test('existing accounts', async () => {
+    const client = new IotaClient({
+        url: 'http://127.0.0.1:9000',
+    });
+    const faucetUrl = 'http://127.0.0.1:9123';
+
+    const coinType = IOTA_COIN_TYPE;
+    const mnemonic =
+        'power dragon mercy range fee book twenty cash room coil trend first seed apple accuse purity remain rather tip use card sock south retreat';
+    const seed = mnemonicToSeedHex(mnemonic);
+
+    // First time get funds to two addresses
+    const firstAddress = Ed25519Keypair.deriveKeypairFromSeed(seed, `m/44'/${coinType}'/0'/0'/0'`)
+        .getPublicKey()
+        .toIotaAddress();
+    const objects = await client.getOwnedObjects({ owner: firstAddress });
+    if (objects.data.length == 0) {
+        await requestIotaFromFaucetV1({ host: faucetUrl, recipient: firstAddress });
+        await requestIotaFromFaucetV1({
+            host: faucetUrl,
+            recipient: Ed25519Keypair.deriveKeypair(mnemonic, `m/44'/${coinType}'/1'/0'/15'`)
+                .getPublicKey()
+                .toIotaAddress(),
+        });
+    }
+
+    let accounts: FoundAccount[] = [
+        {
+            index: 0,
+            addresses: [],
+        },
+    ];
+
+    accounts = await recoverAccounts(0, 0, 0, 1, accounts, seed, coinType, client, GAS_TYPE_ARG);
+    // console.log(JSON.stringify(accounts))
+    let expectedAccounts = 1;
+    console.assert(
+        accounts.length == expectedAccounts,
+        `accounts length mismatch ${accounts.length}/${expectedAccounts}`,
+    );
+    let expectedAddresses = 2;
+    console.assert(
+        accounts[0].addresses.length == expectedAddresses,
+        `addresses length mismatch ${accounts[0].addresses.length}/${expectedAddresses}`,
+    );
+
+    accounts = await recoverAccounts(0, 1, 0, 16, accounts, seed, coinType, client, GAS_TYPE_ARG);
+    // console.log(JSON.stringify(accounts, null, 2))
+    console.assert(
+        accounts.length == accounts[accounts.length - 1].index + 1,
+        `accounts length mismatch`,
+    );
+    expectedAccounts = 3;
+    console.assert(
+        accounts.length == expectedAccounts,
+        `accounts length mismatch ${accounts.length}/${expectedAccounts}`,
+    );
+    expectedAddresses = 18;
+    console.assert(
+        accounts[0].addresses.length == expectedAddresses,
+        `addresses length mismatch ${accounts[0].addresses.length}/${expectedAddresses}`,
+    );
+    expectedAddresses = 32;
+    console.assert(
+        accounts[1].addresses.length == expectedAddresses,
+        `addresses length mismatch ${accounts[1].addresses.length}/${expectedAddresses}`,
+    );
+});
+
+// One existing account without object
+test('empty accounts', async () => {
+    const client = new IotaClient({
+        url: 'http://127.0.0.1:9000',
+    });
+
+    const coinType = IOTA_COIN_TYPE;
+    const mnemonic = entropyToMnemonic(getRandomEntropy());
+    const seed = mnemonicToSeedHex(mnemonic);
+
+    let accounts: FoundAccount[] = [];
+
+    accounts = await recoverAccounts(0, 0, 0, 10, accounts, seed, coinType, client, GAS_TYPE_ARG);
+    let expectedAccounts = 0;
+    console.assert(
+        accounts.length == expectedAccounts,
+        `accounts length mismatch ${accounts.length}/${expectedAccounts}`,
+    );
+
+    accounts = await recoverAccounts(0, 1, 0, 10, accounts, seed, coinType, client, GAS_TYPE_ARG);
+    // console.log(JSON.stringify(accounts))
+    expectedAccounts = 1;
+    console.assert(
+        accounts.length == expectedAccounts,
+        `accounts length mismatch ${accounts.length}/${expectedAccounts}`,
+    );
+    let expectedAddresses = 10;
+    console.assert(
+        accounts[0].addresses.length == expectedAddresses,
+        `addresses length mismatch ${accounts[0].addresses.length}/${expectedAddresses}`,
+    );
+
+    accounts = await recoverAccounts(0, 5, 0, 5, accounts, seed, coinType, client, GAS_TYPE_ARG);
+    // console.log(JSON.stringify(accounts))
+    expectedAccounts = 6;
+    console.assert(
+        accounts.length == expectedAccounts,
+        `accounts length mismatch ${accounts.length}/${expectedAccounts}`,
+    );
+    expectedAddresses = 15;
+    console.assert(
+        accounts[0].addresses.length == expectedAddresses,
+        `addresses length mismatch ${accounts[0].addresses.length}/${expectedAddresses}`,
+    );
+});
---
+++ apps/wallet/src/background/accounts-finder/accounts-finder.ts
@@ -0,0 +1,153 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { type CoinBalance, type IotaClient } from '@iota/iota.js/client';
+import { Ed25519Keypair } from '@iota/iota.js/keypairs/ed25519';
+
+/**
+ * Function to search for accounts/addresses with objects
+ * Simplified implementation which just returns all generated accounts+addresses without deleting new generated that have no objects
+ * Assumes that change, accounts and address indexes have no gaps and are ordered (so having only 0,1,3 or 0,2,1 would be invalid)
+ *   @param {number} accountStartIndex The index of the first account to search for.
+ *   @param {number} accountGapLimit The number of accounts to search for, after the last account with unspent outputs.
+ *   TODO: addressStartIndex not used, maybe also not needed
+ *   @param {number} addressStartIndex The index of the first address to search for.
+ *   @param {number} addressGapLimit The number of addresses to search for, after the last address with unspent outputs, in each account.
+ */
+export async function recoverAccounts(
+    accountStartIndex: number,
+    accountGapLimit: number,
+    addressStartIndex: number,
+    addressGapLimit: number,
+    accounts: FoundAccount[],
+    seed: string,
+    coinType: number,
+    client: IotaClient,
+    gasTypeArg: string,
+): Promise<FoundAccount[]> {
+    // TODO: first check that accounts: Account[] is correctly sorted, if not, throw exception or somethintg
+    // Check new addresses for existing accounts
+    if (addressGapLimit > 0) {
+        for (const account of accounts) {
+            if (account.index < accountStartIndex) {
+                continue;
+            }
+            accounts[account.index] = await searchAddressesWithObjects(
+                addressStartIndex,
+                addressGapLimit,
+                account,
+                seed,
+                coinType,
+                client,
+                gasTypeArg,
+            );
+        }
+    }
+    const accountsLength = accounts.length;
+    let targetIndex = accountsLength + accountGapLimit;
+
+    // Check addresses for new accounts
+    for (let accountIndex = accountsLength; accountIndex < targetIndex; accountIndex += 1) {
+        // console.log(`generating account ${accountIndex}`);
+        let account: FoundAccount = {
+            index: accountIndex,
+            addresses: [],
+        };
+        account = await searchAddressesWithObjects(
+            addressStartIndex,
+            addressGapLimit,
+            account,
+            seed,
+            coinType,
+            client,
+            gasTypeArg,
+        );
+        accounts[accountIndex] = account;
+        if (account.addresses.flat().find((a: Address) => hasBalance(a.balance))) {
+            // Generate more accounts if something was found
+            targetIndex = accountIndex + 1 + accountGapLimit;
+        }
+    }
+
+    return accounts;
+}
+
+async function searchAddressesWithObjects(
+    addressStartIndex: number,
+    addressGapLimit: number,
+    account: FoundAccount,
+    seed: string,
+    coinType: number,
+    client: IotaClient,
+    gasTypeArg: string,
+): Promise<FoundAccount> {
+    const accountAddressLength = account.addresses.length;
+    let targetIndex = accountAddressLength + addressGapLimit;
+
+    for (let addressIndex = accountAddressLength; addressIndex < targetIndex; addressIndex += 1) {
+        // console.log(`generating account ${account.index} address with index: ` + addressIndex)
+        const changeIndexes = [0, 1]; // in the past the change indexes were used as 0=deposit & 1=internal
+        for (const changeIndex of changeIndexes) {
+            const bipPath = `m/44'/${coinType}'/${account.index}'/${changeIndex}'/${addressIndex}'`;
+            const pubKeyHash = Ed25519Keypair.deriveKeypairFromSeed(seed, bipPath)
+                .getPublicKey()
+                .toIotaAddress();
+
+            const balance = await client.getBalance({
+                owner: pubKeyHash,
+                coinType: gasTypeArg,
+            });
+
+            if (hasBalance(balance)) {
+                console.log(
+                    `Found objects for account ${account.index} change ${changeIndex} address ${addressIndex}`,
+                );
+                // Generate more addresses if something was found
+                targetIndex = addressIndex + 1 + addressGapLimit;
+            }
+
+            if (!account.addresses[addressIndex]) {
+                account.addresses[addressIndex] = [];
+            }
+
+            account.addresses[addressIndex][changeIndex] = {
+                pubKeyHash,
+                bipPath: {
+                    addressIndex,
+                    accountIndex: account.index,
+                    changeIndex,
+                },
+                balance,
+            };
+        }
+    }
+
+    return account;
+}
+
+function hasBalance(balance: CoinBalance): boolean {
+    return balance.coinObjectCount > 0;
+}
+
+interface Address {
+    pubKeyHash: string;
+    bipPath: Bip44Path;
+    // TODO: extend this balance in the future to include eg staking, vesting schedules, assets, ...
+    balance: CoinBalance;
+}
+
+export interface Bip44Path {
+    accountIndex: number;
+    addressIndex: number;
+    changeIndex: number;
+}
+
+export interface FoundAccount {
+    index: number;
+    addresses: Array<Array<Address>>;
+    // addresses: [
+    //     [change0, change1], // address index 0
+    //     [change0, change1], // address index 1
+    //     ...
+    //  ]
+}
