f1f4f3a1f3bea1d0ec01b27f54339e93ce8dd00e
marc2332 <mespinsanz@gmail.com>
2024-06-25 09:56:13 +0200

    fix import path

--- apps/wallet/src/shared/accounts.ts
+++ apps/wallet/src/shared/accounts.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { type Bip44Path } from '_src/background/account-sources/bipPath';
+import { type Bip44Path } from '_src/background/account-sources/bip44Path';
 
 export interface FoundAccount {
     address: string;
