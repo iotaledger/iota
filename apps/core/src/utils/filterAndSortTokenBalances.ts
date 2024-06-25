36b76e9989181bcdb909405387e64fb35b18a7aa
evavirseda <evirseda@boxfish.studio>
2024-06-25 16:46:43 +0200

    feat(wallet-dashboard): remove useCoinMetadata from filterAndSortTokenBalances

--- apps/core/src/utils/filterAndSortTokenBalances.ts
+++ apps/core/src/utils/filterAndSortTokenBalances.ts
@@ -1,21 +1,15 @@
-// Copyright (c) Mysten Labs, Inc.
-// Modifications Copyright (c) 2024 IOTA Stiftung
+// Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
 import { type CoinBalance } from '@iota/iota.js/client';
-import { getCoinSymbol, useCoinMetadata } from '../hooks';
+import { getCoinSymbol } from '../hooks';
 
 export function filterAndSortTokenBalances(tokens: CoinBalance[]) {
     return tokens
         .filter((token) => Number(token.totalBalance) > 0)
         .sort((a, b) => {
-            const aCoin = useCoinMetadata(a.coinType);
-            const { data: aData } = aCoin;
-            const aSymbol = aData?.symbol ?? getCoinSymbol(a.coinType);
-
-            const bCoin = useCoinMetadata(b.coinType);
-            const { data: bData } = bCoin;
-            const bSymbol = bData?.symbol ?? getCoinSymbol(b.coinType);
+            const aSymbol = getCoinSymbol(a.coinType);
+            const bSymbol = getCoinSymbol(b.coinType);
 
             // Verify if any of the tokens is IOTA
             if (aSymbol === 'IOTA' && bSymbol !== 'IOTA') {
