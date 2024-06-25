4a8afd35f65dc369da0ecad7abdd79b50095430c
Marc Espin <mespinsanz@gmail.com>
2024-06-25 15:32:18 +0200

    fix(docs): Fix the interactive examples (#710)

--- docs/site/src/components/sdk-examples/wallet-components.tsx
+++ docs/site/src/components/sdk-examples/wallet-components.tsx
@@ -3,80 +3,78 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import {
-    ConnectButton,
-    ConnectModal,
-    IotaClientProvider,
-    useCurrentAccount,
-    WalletProvider,
+  ConnectButton,
+  ConnectModal,
+  IotaClientProvider,
+  useCurrentAccount,
+  WalletProvider,
 } from '@iota/dapp-kit';
-import { getFullnodeUrl } from '@iota/iota.js/client';
+import { getDefaultNetwork, getFullnodeUrl } from '@iota/iota.js/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import '@iota/dapp-kit/dist/index.css';
+import React from 'react';
 
 export const ConnectButtonExample = withProviders(() => {
-    return <ConnectButton />;
+  return <ConnectButton />;
 });
 
 export const ControlledConnectModalExample = withProviders(() => {
-    const currentAccount = useCurrentAccount();
-    const [open, setOpen] = useState(false);
+  const currentAccount = useCurrentAccount();
+  const [open, setOpen] = useState(false);
 
-    return (
-        <ConnectModal
-            trigger={
-                <button disabled={!!currentAccount}>
-                    {' '}
-                    {currentAccount ? 'Connected' : 'Connect'}
-                </button>
-            }
-            open={open}
-            onOpenChange={(isOpen) => setOpen(isOpen)}
-        />
-    );
+  return (
+    <ConnectModal
+      trigger={
+        <button disabled={!!currentAccount}>
+          {' '}
+          {currentAccount ? 'Connected' : 'Connect'}
+        </button>
+      }
+      open={open}
+      onOpenChange={(isOpen) => setOpen(isOpen)}
+    />
+  );
 });
 
 export const UncontrolledConnectModalExample = withProviders(() => {
-    const currentAccount = useCurrentAccount();
+  const currentAccount = useCurrentAccount();
 
-    return (
-        <ConnectModal
-            trigger={
-                <button disabled={!!currentAccount}>
-                    {' '}
-                    {currentAccount ? 'Connected' : 'Connect'}
-                </button>
-            }
-        />
-    );
+  return (
+    <ConnectModal
+      trigger={
+        <button disabled={!!currentAccount}>
+          {' '}
+          {currentAccount ? 'Connected' : 'Connect'}
+        </button>
+      }
+    />
+  );
 });
 
-function withProviders(Component: React.FunctionComponent<object>) {
+const NETWORKS = {
+  [getDefaultNetwork()]: { url: getFullnodeUrl(getDefaultNetwork()) },
+};
+
+function withProviders(Component: () => React.JSX.Element) {
+  return () => {
+
+    if(typeof window === 'undefined') {
+      return null
+    }
+
     // Work around server-side pre-rendering
-    const queryClient = new QueryClient();
-    const networks = {
-        mainnet: { url: getFullnodeUrl('mainnet') },
-    };
+    const queryClient = useMemo(() => new QueryClient(), []);
 
-    return () => {
-        const [shouldRender, setShouldRender] = useState(false);
-        useEffect(() => {
-            setShouldRender(true);
-        }, [setShouldRender]);
-
-        if (!shouldRender) {
-            return null;
-        }
-
-        return (
-            <QueryClientProvider client={queryClient}>
-                <IotaClientProvider networks={networks}>
-                    <WalletProvider>
-                        <Component />
-                    </WalletProvider>
-                </IotaClientProvider>
-            </QueryClientProvider>
-        );
-    };
+    return (
+      <QueryClientProvider client={queryClient}>
+        <IotaClientProvider networks={NETWORKS}>
+          <WalletProvider>
+            <Component />
+          </WalletProvider>
+        </IotaClientProvider>
+      </QueryClientProvider>
+    );
+  };
 }
