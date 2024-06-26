a08e41ce972beaf8c8954b7f45e70edb1394f374
Thibault Martinez <thibault@iota.org>
2024-06-26 15:28:40 +0200

    factorize some code

--- crates/iota-genesis-builder/src/stardust/test_outputs/vesting_schedule_entity.rs
+++ crates/iota-genesis-builder/src/stardust/test_outputs/vesting_schedule_entity.rs
@@ -7,56 +7,20 @@
 
 use iota_sdk::{
     client::secret::{mnemonic::MnemonicSecretManager, SecretManage},
-    types::block::{
-        address::Ed25519Address,
-        output::{
-            unlock_condition::{AddressUnlockCondition, TimelockUnlockCondition},
-            BasicOutputBuilder, Output,
-        },
-    },
+    types::block::output::Output,
 };
 use iota_types::timelock::timelock::VESTED_REWARD_ID_PREFIX;
 use rand::{random, rngs::StdRng, Rng, SeedableRng};
 
-use crate::stardust::types::output_header::OutputHeader;
+use crate::stardust::{
+    test_outputs::{new_vested_output, MERGE_TIMESTAMP_SECS},
+    types::output_header::OutputHeader,
+};
 
 const MNEMONIC: &str = "chunk beach oval twist manage spread street width view pig hen oak size fix lab tent say home team cube loop van they suit";
 const COIN_TYPE: u32 = 4218;
 const VESTING_WEEKS: usize = 208;
 const VESTING_WEEKS_FREQUENCY: usize = 2;
-const MERGE_MILESTONE_INDEX: u32 = 7669900;
-const MERGE_TIMESTAMP_SECS: u32 = 1696406475;
-
-fn new_output(
-    transaction_id: &mut [u8; 32],
-    vested_index: &mut u32,
-    amount: u64,
-    address: Ed25519Address,
-    timelock: Option<u32>,
-) -> anyhow::Result<(OutputHeader, Output)> {
-    transaction_id[28..32].copy_from_slice(&vested_index.to_le_bytes());
-    *vested_index -= 1;
-
-    let output_header = OutputHeader::new_testing(
-        *transaction_id,
-        // % 128 to pass the output index syntactic validation.
-        random::<u16>() % 128,
-        [0; 32],
-        MERGE_MILESTONE_INDEX,
-        MERGE_TIMESTAMP_SECS,
-    );
-
-    let mut builder = BasicOutputBuilder::new_with_amount(amount)
-        .add_unlock_condition(AddressUnlockCondition::new(address));
-
-    if let Some(timelock) = timelock {
-        builder = builder.add_unlock_condition(TimelockUnlockCondition::new(timelock)?);
-    }
-
-    let output = Output::from(builder.finish().unwrap());
-
-    Ok((output_header, output))
-}
 
 pub(crate) async fn outputs(vested_index: &mut u32) -> anyhow::Result<Vec<(OutputHeader, Output)>> {
     let mut outputs = Vec::new();
@@ -84,7 +48,7 @@ pub(crate) async fn outputs(vested_index: &mut u32) -> anyhow::Result<Vec<(Outpu
     // schedule.
     let vested_amount = amount * 90 / 100 / (VESTING_WEEKS as u64 / VESTING_WEEKS_FREQUENCY as u64);
 
-    outputs.push(new_output(
+    outputs.push(new_vested_output(
         &mut transaction_id,
         vested_index,
         initial_unlock_amount,
@@ -95,7 +59,7 @@ pub(crate) async fn outputs(vested_index: &mut u32) -> anyhow::Result<Vec<(Outpu
     for offset in (0..=VESTING_WEEKS).step_by(VESTING_WEEKS_FREQUENCY) {
         let timelock = MERGE_TIMESTAMP_SECS + offset as u32 * 604_800;
 
-        outputs.push(new_output(
+        outputs.push(new_vested_output(
             &mut transaction_id,
             vested_index,
             vested_amount,
