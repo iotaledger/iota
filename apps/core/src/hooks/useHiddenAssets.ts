e7493b8cb978445872870bc32c1380d4a92b8b24
JCNoguera <jcnoguera@boxfish.studio>
2024-06-26 12:22:02 +0200

    refactor!: remove unused code

--- apps/core/src/hooks/useHiddenAssets.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-import { useContext } from 'react';
-import { HiddenAssetsContext } from '../contexts';
-
-export function useHiddenAssets() {
-    return useContext(HiddenAssetsContext);
-}
