7049b4c69ec8abba994a9b451d3676cd1aeed2fc
samuel_rufi <samuel.rufinatscha@iota.org>
2024-06-27 13:58:45 +0200

    fix: Rename

--- crates/iota-genesis-builder/src/main.rs
+++ crates/iota-genesis-builder/src/main.rs
@@ -73,7 +73,7 @@ fn main() -> Result<()> {
     // Start the Hornet snapshot parser
     let snapshot_parser = HornetGenesisSnapshotParser::new(File::open(snapshot_path)?)?;
     let total_supply = match coin_type {
-        CoinType::Iota => scale_amount(snapshot_parser.total_supply()?)?,
+        CoinType::Iota => scale_amount_for_iota(snapshot_parser.total_supply()?)?,
         CoinType::Shimmer => snapshot_parser.total_supply()?,
     };
 
@@ -114,7 +114,7 @@ fn scale_output_amount_for_iota(output: &mut Output) -> Result<()> {
         Output::Basic(ref basic_output) => {
             // Update amount
             let mut builder = BasicOutputBuilder::from(basic_output)
-                .with_amount(scale_amount(basic_output.amount())?);
+                .with_amount(scale_amount_for_iota(basic_output.amount())?);
 
             // Update amount in potential storage deposit return unlock condition
             if let Some(sdr_uc) = basic_output
@@ -125,7 +125,7 @@ fn scale_output_amount_for_iota(output: &mut Output) -> Result<()> {
                 builder = builder.replace_unlock_condition(
                     StorageDepositReturnUnlockCondition::new(
                         sdr_uc.return_address(),
-                        scale_amount(sdr_uc.amount())?,
+                        scale_amount_for_iota(sdr_uc.amount())?,
                         u64::MAX,
                     )
                     .unwrap(),
@@ -136,18 +136,18 @@ fn scale_output_amount_for_iota(output: &mut Output) -> Result<()> {
         }
         Output::Alias(ref alias_output) => Output::from(
             AliasOutputBuilder::from(alias_output)
-                .with_amount(scale_amount(alias_output.amount())?)
+                .with_amount(scale_amount_for_iota(alias_output.amount())?)
                 .finish()?,
         ),
         Output::Foundry(ref foundry_output) => Output::from(
             FoundryOutputBuilder::from(foundry_output)
-                .with_amount(scale_amount(foundry_output.amount())?)
+                .with_amount(scale_amount_for_iota(foundry_output.amount())?)
                 .finish()?,
         ),
         Output::Nft(ref nft_output) => {
             // Update amount
-            let mut builder =
-                NftOutputBuilder::from(nft_output).with_amount(scale_amount(nft_output.amount())?);
+            let mut builder = NftOutputBuilder::from(nft_output)
+                .with_amount(scale_amount_for_iota(nft_output.amount())?);
 
             // Update amount in potential storage deposit return unlock condition
             if let Some(sdr_uc) = nft_output
@@ -158,7 +158,7 @@ fn scale_output_amount_for_iota(output: &mut Output) -> Result<()> {
                 builder = builder.replace_unlock_condition(
                     StorageDepositReturnUnlockCondition::new(
                         sdr_uc.return_address(),
-                        scale_amount(sdr_uc.amount())?,
+                        scale_amount_for_iota(sdr_uc.amount())?,
                         u64::MAX,
                     )
                     .unwrap(),
@@ -172,7 +172,7 @@ fn scale_output_amount_for_iota(output: &mut Output) -> Result<()> {
     Ok(())
 }
 
-fn scale_amount(amount: u64) -> Result<u64> {
+fn scale_amount_for_iota(amount: u64) -> Result<u64> {
     const IOTA_MULTIPLIER: u64 = 1000;
 
     amount
