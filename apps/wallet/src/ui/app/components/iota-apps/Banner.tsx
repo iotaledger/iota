842c69e1eb8a538f55f6f5255d1a02ab71b2493a
evavirseda <evirseda@boxfish.studio>
2024-06-27 19:15:37 +0200

    feat(wallet): fix build

--- apps/wallet/src/ui/app/components/iota-apps/Banner.tsx
+++ apps/wallet/src/ui/app/components/iota-apps/Banner.tsx
@@ -3,19 +3,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { ampli } from '_src/shared/analytics/ampli';
-import { Feature } from '_src/shared/experimentation/features';
+import { FEATURES } from '_src/shared/experimentation/features';
 import { useFeature } from '@growthbook/growthbook-react';
 
 import ExternalLink from '../external-link';
 
-export interface BannerProps {
+export type BannerProps = {
     enabled: boolean;
     bannerUrl?: string;
     imageUrl?: string;
-}
+};
 
 export function AppsPageBanner() {
-    const AppsBannerConfig = useFeature<BannerProps>(Feature.WalletAppsBannerConfig);
+    const AppsBannerConfig = useFeature<BannerProps>(FEATURES.WALLET_APPS_BANNER_CONFIG);
 
     if (!AppsBannerConfig.value?.enabled) {
         return null;
