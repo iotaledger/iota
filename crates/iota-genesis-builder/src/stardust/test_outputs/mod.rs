54db596f5924e5ce3db127ed66d8c95a30a8ef02
Alexander Schmidt <alexander.schmidt@iota.org>
2024-06-27 15:50:20 +0200

    prepare

--- crates/iota-genesis-builder/src/stardust/test_outputs/mod.rs
+++ crates/iota-genesis-builder/src/stardust/test_outputs/mod.rs
@@ -3,6 +3,7 @@
 
 mod vesting_schedule_entity;
 mod vesting_schedule_iota_airdrop;
+mod vesting_schedule_portfolio_mix;
 
 use std::{
     fs::{File, OpenOptions},
@@ -80,6 +81,7 @@ pub async fn add_snapshot_test_outputs<P: AsRef<Path> + core::fmt::Debug>(
     let new_outputs = [
         vesting_schedule_entity::outputs(&mut vested_index).await?,
         vesting_schedule_iota_airdrop::outputs(&mut vested_index).await?,
+        vesting_schedule_portfolio_mix::outputs(&mut vested_index).await?,
     ]
     .concat();
     let new_amount = new_outputs.iter().map(|o| o.1.amount()).sum::<u64>();
