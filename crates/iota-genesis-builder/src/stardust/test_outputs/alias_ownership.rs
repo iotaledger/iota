9bb2d50c8af272aac0a1fa05cdfa5b21e8052338
Alexander Schmidt <alexander.schmidt@iota.org>
2024-07-02 19:56:38 +0200

    rm supply check

--- crates/iota-genesis-builder/src/stardust/test_outputs/alias_ownership.rs
+++ crates/iota-genesis-builder/src/stardust/test_outputs/alias_ownership.rs
@@ -162,15 +162,15 @@ fn random_alias_output(rng: &mut StdRng, owner: impl Into<Address>) -> (OutputHe
 fn random_foundry_output(owner: impl Into<AliasAddress>) -> (OutputHeader, FoundryOutput) {
     let foundry_output_header = random_output_header();
 
+    let amount = rng.gen_range(1_000_000..10_000_000);
     let supply = rng.gen_range(1_000_000..100_000_000);
-    let amount = rng.gen_range(1_000_000..10_000_000).min(supply);
     let token_scheme = SimpleTokenScheme::new(supply, 0, supply).unwrap();
     let foundry_output =
         FoundryOutputBuilder::new_with_amount(amount, *serial_number, token_scheme.into())
             .with_unlock_conditions([UnlockCondition::from(
                 ImmutableAliasAddressUnlockCondition::new(owner),
             )])
-            .finish_with_params(supply)
+            .finish()
             .unwrap();
 
     *serial_number += 1;
