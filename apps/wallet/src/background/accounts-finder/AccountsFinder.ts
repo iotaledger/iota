be67a06a460448e9c398156dc36a2d0d2cb9baaf
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2024-07-03 17:29:21 +0300

    feat(wallet): add description to find accounts function. Refactoring.
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- apps/wallet/src/background/accounts-finder/AccountsFinder.ts
+++ apps/wallet/src/background/accounts-finder/AccountsFinder.ts
@@ -14,7 +14,7 @@ import { getAccountSourceByID } from '../account-sources';
 import { type MakeDerivationOptions } from '_src/background/account-sources/bip44Path';
 
 interface SearchBoundaries {
-    [x: number]: {
+    [accountIndex: number]: {
         start: number;
         end: number;
         accountMax: number;
@@ -22,53 +22,95 @@ interface SearchBoundaries {
 }
 
 class AccountsFinder {
+    /** indicate if user is searching for the first time */
     private isFirstSearch: boolean = true;
-    private lastColumn: number = 0;
+
+    /**
+     * This property controls what range we need to use for search.
+     * Params start, end, accountMax are used to control the search range.
+     * - key - index of column or X position.
+     * - start - index of start Y position
+     * - end - end Y index for search
+     * - accountMax - index of accountGapLimit.
+     */
     private searchBoundaries: SearchBoundaries = {};
+
+    /**
+     * Index of last column in searchBoundaries.
+     * This property uses to know how many columns we have in searchBoundaries.
+     * */
+    private lastAccountIndex: number = 0;
+
+    /** account gap limit provided by findMore function */
     private accountGapLimit: number = 0;
+
+    /** address gap limit provided by findMore function */
     private addressGapLimit: number = 0;
+
+    /** 4218 for IOTA or 4219 for Shimmer */
     private coinType: number = 0;
+
     private gasTypeArg: string = '';
     private sourceID: string = '';
     public client: IotaClient | null = null;
 
+    /** Found accounts with balances. */
     accounts: AccountFromFinder[] = [];
 
+    /**
+     * Initializes or resets the accounts array.
+     */
     init() {
         this.accounts = [];
     }
 
+    /**
+     * Init the search boundaries for the first time.
+     */
     setupInitialBoundaries() {
-        for (let x = 0; x < this.accountGapLimit; x++) {
-            this.addColumnBoundary(x);
+        for (let accountIndex = 0; accountIndex < this.accountGapLimit; accountIndex++) {
+            this.addColumnBoundary(accountIndex);
         }
 
         this.isFirstSearch = false;
     }
 
+    /**
+     * Expand the search boundaries when we need to search more.
+     * This function change search range for columns that already exist and add more columns to search.
+     */
     expandSearchBoundaries() {
-        for (let x = 0; x <= this.lastColumn; x++) {
-            const { end } = this.searchBoundaries[x];
+        for (let accountIndex = 0; accountIndex <= this.lastAccountIndex; accountIndex++) {
+            const { end } = this.searchBoundaries[accountIndex];
 
-            this.searchBoundaries[x].start = end + 1;
-            this.searchBoundaries[x].end = end + this.addressGapLimit;
+            this.searchBoundaries[accountIndex].start = end + 1;
+            this.searchBoundaries[accountIndex].end = end + this.addressGapLimit;
         }
 
-        this.addColumnsToBoundaries(this.lastColumn);
+        this.addColumnsToBoundaries(this.lastAccountIndex);
     }
 
-    addColumnsToBoundaries(columnIndex: number) {
-        const columnsLeft = this.lastColumn - columnIndex;
+    /**
+     * Calculate how many columns we need to add to search boundaries.
+     * If we have a match, we need to add always "this.accountGapLimit" number of columns.
+     * @param currAccountIndex - index of current active column.
+     */
+    addColumnsToBoundaries(currAccountIndex: number) {
+        const columnsLeft = this.lastAccountIndex - currAccountIndex;
         const columnsToAdd = this.accountGapLimit - columnsLeft;
 
-        const fromIndex = this.lastColumn + 1;
-        const toIndex = this.lastColumn + columnsToAdd;
+        const fromIndex = this.lastAccountIndex + 1;
+        const toIndex = this.lastAccountIndex + columnsToAdd;
 
         for (let i = fromIndex; i <= toIndex; i++) {
             this.addColumnBoundary(i);
         }
     }
 
+    /**
+     * Add a new column to search boundaries.
+     * @param columnIndex - index of new column.
+     */
     addColumnBoundary(columnIndex: number) {
         const accountLimitIndex = this.accountGapLimit - 1;
         this.searchBoundaries[columnIndex] = {
@@ -76,43 +118,52 @@ class AccountsFinder {
             end: accountLimitIndex + this.addressGapLimit,
             accountMax: accountLimitIndex,
         };
-        this.lastColumn = columnIndex;
+        this.lastAccountIndex = columnIndex;
     }
 
-    updateSearchRange(x: number, y: number) {
-        const { end, accountMax } = this.searchBoundaries[x];
+    /**
+     * Calculate if we need to add new columns or increase the search range.
+     * @param currentAccountIndex - index of column/account that we are searching.
+     * @param currentAddressIndex - index of row/address that we are searching.
+     */
+    updateSearchRange(currentAccountIndex: number, currentAddressIndex: number) {
+        const { end, accountMax } = this.searchBoundaries[currentAccountIndex];
 
-        if (y <= accountMax) {
+        if (currentAddressIndex <= accountMax) {
             // increase limit in column
-            const accountLimitDiff = accountMax - y;
+            const accountLimitDiff = accountMax - currentAddressIndex;
             const accountsToAdd = this.accountGapLimit - accountLimitDiff;
-            this.searchBoundaries[x].accountMax += accountsToAdd;
-            this.searchBoundaries[x].end += accountsToAdd;
+            this.searchBoundaries[currentAccountIndex].accountMax += accountsToAdd;
+            this.searchBoundaries[currentAccountIndex].end += accountsToAdd;
 
             // add more columns
-            this.addColumnsToBoundaries(x);
-        } else if (y <= end) {
-            const addressLimitDiff = end - y;
+            this.addColumnsToBoundaries(currentAccountIndex);
+        } else if (currentAddressIndex <= end) {
+            const addressLimitDiff = end - currentAddressIndex;
             const addressesToAdd = this.addressGapLimit - addressLimitDiff;
-            this.searchBoundaries[x].end += addressesToAdd;
+            this.searchBoundaries[currentAccountIndex].end += addressesToAdd;
         }
     }
 
-    async searchByColumn(x: number) {
+    /**
+     * Take the current column and check if balance exists.
+     * @param accountIndex - index of column/account
+     */
+    async searchByColumn(accountIndex: number) {
         const res: string[] = [];
         const account: AccountFromFinder = {
-            index: x,
+            index: accountIndex,
             addresses: [],
         };
-        const { start, end } = this.searchBoundaries[x];
-        for (let y = start; y <= end; y++) {
+        const { start, end } = this.searchBoundaries[accountIndex];
+        for (let addressIndex = start; addressIndex <= end; addressIndex++) {
             const changeIndexes = [0, 1]; // in the past the change indexes were used as 0=deposit & 1=internal
 
             for (const changeIndex of changeIndexes) {
                 const bipPath = {
                     coinType: this.coinType,
-                    accountIndex: x,
-                    addressIndex: y,
+                    accountIndex: accountIndex,
+                    addressIndex: addressIndex,
                     changeIndex,
                 };
                 const pubKeyHash = await this.getPublicKey(bipPath);
@@ -123,16 +174,16 @@ class AccountsFinder {
                 });
 
                 if (balance && hasBalance(balance)) {
-                    this.updateSearchRange(x, y);
+                    this.updateSearchRange(accountIndex, addressIndex);
 
-                    if (!account.addresses[y]) {
-                        account.addresses[y] = [];
+                    if (!account.addresses[addressIndex]) {
+                        account.addresses[addressIndex] = [];
                     }
 
-                    account.addresses[y][changeIndex] = {
+                    account.addresses[addressIndex][changeIndex] = {
                         pubKeyHash,
                         bipPath: {
-                            addressIndex: y,
+                            addressIndex: addressIndex,
                             accountIndex: account.index,
                             changeIndex,
                         },
@@ -147,6 +198,14 @@ class AccountsFinder {
         return res;
     }
 
+    /**
+     * This function calls each time when user press "Search" button
+     * @param coinType
+     * @param gasTypeArg
+     * @param sourceID
+     * @param accountGapLimit
+     * @param addressGaspLimit
+     */
     async findMore(
         coinType: number,
         gasTypeArg: string,
@@ -171,8 +230,8 @@ class AccountsFinder {
             this.expandSearchBoundaries();
         }
 
-        for (let x = 0; x <= this.lastColumn; x++) {
-            await this.searchByColumn(x);
+        for (let accountIndex = 0; accountIndex <= this.lastAccountIndex; accountIndex++) {
+            await this.searchByColumn(accountIndex);
         }
     }
 
