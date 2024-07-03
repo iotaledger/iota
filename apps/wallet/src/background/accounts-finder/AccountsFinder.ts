383f5d014ed7be6642fb2dccfd6ab65cdd4c17c1
Begoña Alvarez <balvarez@boxfish.studio>
2024-07-03 18:04:46 +0200

    refactor(wallet): reimplement search algorithm

--- apps/wallet/src/background/accounts-finder/AccountsFinder.ts
+++ apps/wallet/src/background/accounts-finder/AccountsFinder.ts
@@ -1,179 +1,209 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { type AccountFromFinder, type AddressFromFinder } from '_src/shared/accounts';
-import { hasBalance } from './accounts-finder';
-import NetworkEnv from '../NetworkEnv';
 import {
     IotaClient,
     getFullnodeUrl,
-    type GetBalanceParams,
     type CoinBalance,
+    type GetBalanceParams,
 } from '@iota/iota.js/client';
-import { getAccountSourceByID } from '../account-sources';
 import { type MakeDerivationOptions } from '_src/background/account-sources/bip44Path';
+import { type AccountFromFinder, type AddressFromFinder } from '_src/shared/accounts';
+import NetworkEnv from '../NetworkEnv';
+import { getAccountSourceByID } from '../account-sources';
+import { AccountType } from '../accounts/Account';
+import { hasBalance } from './accounts-finder';
 
-interface SearchBoundaries {
-    [x: number]: {
-        start: number;
-        end: number;
-        accountMax: number;
-    };
+interface GapConfiguration {
+    accountGapLimit: number;
+    addressGapLimit: number;
 }
 
+export enum SearchAlgorithm {
+    BREADTH,
+    DEPTH,
+    ITERATIVE_DEEPENING_BREADTH_FIRST,
+}
+
+const GAP_CONFIGURATION: { [key in AccountType]: GapConfiguration } = {
+    [AccountType.Ledger]: {
+        accountGapLimit: 1,
+        addressGapLimit: 5,
+    },
+    [AccountType.MnemonicDerived]: {
+        accountGapLimit: 3,
+        addressGapLimit: 30,
+    },
+    [AccountType.SeedDerived]: {
+        accountGapLimit: 3,
+        addressGapLimit: 30,
+    },
+    // Private key accounts are fixed bip paths, so we don't need to search for them
+    [AccountType.Imported]: {
+        accountGapLimit: 0,
+        addressGapLimit: 0,
+    },
+};
+
 class AccountsFinder {
-    private isFirstSearch: boolean = true;
-    private lastColumn: number = 0;
-    private searchBoundaries: SearchBoundaries = {};
+    // configured with parameters at init()
+    private algorithm: SearchAlgorithm = SearchAlgorithm.ITERATIVE_DEEPENING_BREADTH_FIRST;
+    private bip44CoinType: number = 0;
+    private coinType: string = '';
+    private sourceID: string = '';
+    // configured automatically after init()
     private accountGapLimit: number = 0;
     private addressGapLimit: number = 0;
-    private coinType: number = 0;
-    private gasTypeArg: string = '';
-    private sourceID: string = '';
+
     public client: IotaClient | null = null;
 
     accounts: AccountFromFinder[] = [];
 
-    init() {
+    init(
+        accountType: AccountType,
+        algorithm: SearchAlgorithm,
+        bip44CoinType: number,
+        coinType: string,
+        sourceID: string,
+    ) {
         this.accounts = [];
+
+        this.algorithm = algorithm;
+        this.bip44CoinType = bip44CoinType;
+        this.coinType = coinType;
+        this.sourceID = sourceID;
+
+        this.accountGapLimit = GAP_CONFIGURATION[accountType].accountGapLimit;
+        this.addressGapLimit = GAP_CONFIGURATION[accountType].addressGapLimit;
     }
 
-    setupInitialBoundaries() {
-        for (let x = 0; x < this.accountGapLimit; x++) {
-            this.addColumnBoundary(x);
+    async find() {
+        if (
+            this.algorithm === SearchAlgorithm.BREADTH ||
+            this.algorithm === SearchAlgorithm.ITERATIVE_DEEPENING_BREADTH_FIRST
+        ) {
+            this.accounts = await this.runBreadthSearch();
         }
-
-        this.isFirstSearch = false;
-    }
-
-    expandSearchBoundaries() {
-        for (let x = 0; x <= this.lastColumn; x++) {
-            const { end } = this.searchBoundaries[x];
-
-            this.searchBoundaries[x].start = end + 1;
-            this.searchBoundaries[x].end = end + this.addressGapLimit;
-        }
-
-        this.addColumnsToBoundaries(this.lastColumn);
-    }
-
-    addColumnsToBoundaries(columnIndex: number) {
-        const columnsLeft = this.lastColumn - columnIndex;
-        const columnsToAdd = this.accountGapLimit - columnsLeft;
-
-        const fromIndex = this.lastColumn + 1;
-        const toIndex = this.lastColumn + columnsToAdd;
-
-        for (let i = fromIndex; i <= toIndex; i++) {
-            this.addColumnBoundary(i);
+        if (
+            this.algorithm === SearchAlgorithm.DEPTH ||
+            this.algorithm === SearchAlgorithm.ITERATIVE_DEEPENING_BREADTH_FIRST
+        ) {
+            this.accounts = await this.runDepthSearch();
         }
     }
 
-    addColumnBoundary(columnIndex: number) {
-        const accountLimitIndex = this.accountGapLimit - 1;
-        this.searchBoundaries[columnIndex] = {
-            start: 0,
-            end: accountLimitIndex + this.addressGapLimit,
-            accountMax: accountLimitIndex,
-        };
-        this.lastColumn = columnIndex;
+    async runBreadthSearch() {
+        // during breadth search we always search the last known account index + 1
+        const initialAccountIndex = this.accounts.length
+            ? this.accounts[this.accounts.length - 1].index + 1
+            : 0;
+        // during breadth search we always search starting from the address index 0
+        const initialAddressIndex = 0;
+
+        const searchedAccounts = await this.recoverAccounts(
+            initialAccountIndex,
+            this.accountGapLimit,
+            initialAddressIndex,
+            this.addressGapLimit,
+        );
+        return this.accounts.concat(searchedAccounts);
     }
 
-    updateSearchRange(x: number, y: number) {
-        const { end, accountMax } = this.searchBoundaries[x];
-
-        if (y <= accountMax) {
-            // increase limit in column
-            const accountLimitDiff = accountMax - y;
-            const accountsToAdd = this.accountGapLimit - accountLimitDiff;
-            this.searchBoundaries[x].accountMax += accountsToAdd;
-            this.searchBoundaries[x].end += accountsToAdd;
-
-            // add more columns
-            this.addColumnsToBoundaries(x);
-        } else if (y <= end) {
-            const addressLimitDiff = end - y;
-            const addressesToAdd = this.addressGapLimit - addressLimitDiff;
-            this.searchBoundaries[x].end += addressesToAdd;
-        }
-    }
-
-    async searchByColumn(x: number) {
-        const res: string[] = [];
-        const account: AccountFromFinder = {
-            index: x,
-            addresses: [],
-        };
-        const { start, end } = this.searchBoundaries[x];
-        for (let y = start; y <= end; y++) {
-            const changeIndexes = [0, 1]; // in the past the change indexes were used as 0=deposit & 1=internal
-
-            for (const changeIndex of changeIndexes) {
-                const bipPath = {
-                    coinType: this.coinType,
-                    accountIndex: x,
-                    addressIndex: y,
-                    changeIndex,
-                };
-                const pubKeyHash = await this.getPublicKey(bipPath);
-
-                const balance = await this.getBalance({
-                    owner: pubKeyHash,
-                    coinType: this.gasTypeArg,
+    async runDepthSearch() {
+        const depthAccounts = this.accounts;
+        // if we have no accounts yet, we start with the first account
+        if (!depthAccounts.length) {
+            for (let accountIndex = 0; accountIndex < this.accountGapLimit; accountIndex++) {
+                depthAccounts.push({
+                    index: accountIndex,
+                    addresses: [],
                 });
-
-                if (balance && hasBalance(balance)) {
-                    this.updateSearchRange(x, y);
-
-                    if (!account.addresses[y]) {
-                        account.addresses[y] = [];
-                    }
-
-                    account.addresses[y][changeIndex] = {
-                        pubKeyHash,
-                        bipPath: {
-                            addressIndex: y,
-                            accountIndex: account.index,
-                            changeIndex,
-                        },
-                        balance,
-                    };
-
-                    this.accounts.push(account);
-                }
             }
         }
-
-        return res;
+        for (const account of depthAccounts) {
+            const searchedAccounts = await this.recoverAccounts(
+                account.index,
+                0,
+                account.addresses.length,
+                this.addressGapLimit,
+            );
+            depthAccounts[account.index].addresses = depthAccounts[account.index].addresses.concat(
+                searchedAccounts[account.index].addresses,
+            );
+        }
+        return depthAccounts;
     }
 
-    async findMore(
-        coinType: number,
-        gasTypeArg: string,
-        sourceID: string,
-        accountGapLimit: number,
-        addressGaspLimit: number,
+    // Generic low level function that can be used to implement different search algorithms
+    async recoverAccounts(
+        accountStartIndex: number = 0,
+        accountGapLimit: number = 0,
+        addressStartIndex: number = 0,
+        addressGapLimit: number = 0,
     ) {
         const network = await NetworkEnv.getActiveNetwork();
         this.client = new IotaClient({
             url: network.customRpcUrl ? network.customRpcUrl : getFullnodeUrl(network.network),
         });
+        const accounts: AccountFromFinder[] = [];
+        let targetAccountIndex = accountStartIndex + accountGapLimit;
+        for (
+            let accountIndex = accountStartIndex;
+            accountIndex < targetAccountIndex;
+            accountIndex++
+        ) {
+            const account = this.accounts.find((acc) => acc.index === accountIndex) ?? {
+                index: accountIndex,
+                addresses: [],
+            };
 
-        this.coinType = coinType;
-        this.gasTypeArg = gasTypeArg;
-        this.accountGapLimit = accountGapLimit;
-        this.addressGapLimit = addressGaspLimit;
-        this.sourceID = sourceID;
-
-        if (this.isFirstSearch) {
-            this.setupInitialBoundaries();
-        } else {
-            this.expandSearchBoundaries();
+            let targetAddressIndex = addressStartIndex + addressGapLimit;
+            for (
+                let addressIndex = addressStartIndex;
+                addressIndex < targetAddressIndex;
+                addressIndex++
+            ) {
+                const addresses = await this.searchBalances(accountIndex, addressIndex);
+                if (addresses.some((addr) => addr.balance && hasBalance(addr.balance))) {
+                    targetAddressIndex = addressIndex + 1 + addressGapLimit;
+                }
+                account.addresses.push(addresses);
+            }
+            if (
+                account.addresses.some((addrIndexSet) =>
+                    addrIndexSet.some((address) => address.balance && hasBalance(address.balance)),
+                )
+            ) {
+                targetAccountIndex = accountIndex + 1 + accountGapLimit;
+            }
+            accounts.push(account);
         }
+        return accounts;
+    }
 
-        for (let x = 0; x <= this.lastColumn; x++) {
-            await this.searchByColumn(x);
+    async searchBalances(accountIndex: number, addressIndex: number) {
+        const changeIndexes = [0, 1]; // in the past the change indexes were used as 0=deposit & 1=internal
+        const addresses = [];
+        for (const changeIndex of changeIndexes) {
+            const bipPath = {
+                bip44CoinType: this.bip44CoinType,
+                accountIndex: accountIndex,
+                addressIndex: addressIndex,
+                changeIndex,
+            };
+            const pubKeyHash = await this.getPublicKey(bipPath);
+
+            const balance = await this.getBalance({
+                owner: pubKeyHash,
+                coinType: this.coinType,
+            });
+            addresses.push({
+                pubKeyHash,
+                bipPath,
+                balance,
+            });
         }
+        return addresses;
     }
 
     async getPublicKey(options: MakeDerivationOptions) {
