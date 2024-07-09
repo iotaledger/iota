2f70a8bb45ab10601b666140312fa09104a92954
samuel_rufi <samuel.rufinatscha@iota.org>
2024-07-09 16:59:30 +0200

    fix: Intro `disable_global_snapshot_verification` CLI flag

--- crates/iota-genesis-builder/src/main.rs
+++ crates/iota-genesis-builder/src/main.rs
@@ -45,7 +45,7 @@ struct Cli {
         help = "Enable global snapshot verification",
         default_value_t = false
     )]
-    global_snapshot_verification: bool,
+    disable_global_snapshot_verification: bool,
 }
 
 #[derive(Subcommand, Debug)]
@@ -87,10 +87,10 @@ fn main() -> Result<()> {
     };
 
     // Start the Hornet snapshot parser
-    let mut snapshot_parser = if cli.global_snapshot_verification {
-        HornetSnapshotParser::new::<true>(File::open(snapshot_path)?)?
-    } else {
+    let mut snapshot_parser = if cli.disable_global_snapshot_verification {
         HornetSnapshotParser::new::<false>(File::open(snapshot_path)?)?
+    } else {
+        HornetSnapshotParser::new::<true>(File::open(snapshot_path)?)?
     };
     let total_supply = match coin_type {
         CoinType::Iota => scale_amount_for_iota(snapshot_parser.total_supply()?)?,
