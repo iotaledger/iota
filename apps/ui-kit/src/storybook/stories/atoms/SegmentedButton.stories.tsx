35eff9bb1520bbc47f4a573f8a92d13bb932d1a2
Branko Bosnic <brankobosnic1@gmail.com>
2024-07-10 14:52:40 +0200

    feat: add click handling in story

--- apps/ui-kit/src/storybook/stories/atoms/SegmentedButton.stories.tsx
+++ apps/ui-kit/src/storybook/stories/atoms/SegmentedButton.stories.tsx
@@ -5,14 +5,34 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { SegmentedButton } from '@/components/atoms/segmented-button/SegmentedButton';
 import { SegmentedButtonType } from '@/lib/components/atoms/segmented-button';
+import { useState } from 'react';
+import { ButtonSegmentProps } from '@/lib/components/atoms/button-segment';
 
 const meta = {
     component: SegmentedButton,
     tags: ['autodocs'],
     render: (props) => {
+        console.log('props', props);
+        const [elements, setElements] = useState<ButtonSegmentProps[]>(props.elements || []);
+
+        const handleElementClick = (clickedElementLabel: string) => {
+            console.log('clickedElementLabel', clickedElementLabel);
+            const updatedElements = elements.map((element: ButtonSegmentProps) => ({
+                ...element,
+                selected: element.label === clickedElementLabel,
+            }));
+            console.log('updatedElements', updatedElements);
+            setElements(updatedElements);
+        };
+
         return (
             <div className="flex flex-col items-start gap-2">
-                <SegmentedButton {...props} />
+                <SegmentedButton
+                    elements={elements}
+                    onSelected={(selectedElement: ButtonSegmentProps) =>
+                        handleElementClick(selectedElement.label)
+                    }
+                />
             </div>
         );
     },
