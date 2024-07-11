bba277fb14e2cb5d5d2e5359fe095f1679fdca8a
JCNoguera <jcnoguera@boxfish.studio>
2024-07-11 19:32:42 +0200

    fix: add missing license

--- apps/ui-icons/scripts/preprocess.mjs
+++ apps/ui-icons/scripts/preprocess.mjs
@@ -1,3 +1,7 @@
+// Copyright (c) Mysten Labs, Inc.
+// Modifications Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
