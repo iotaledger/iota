f82e99e65152233472b606cfb0af5f1bd721a444
Lucas Tortora <lucas.tortora@iota.org>
2024-07-12 09:54:33 -0300

    (feat): add copyright information in the footer

--- docs/site/docusaurus.config.js
+++ docs/site/docusaurus.config.js
@@ -137,6 +137,7 @@ const config = {
   themes: ["@docusaurus/theme-mermaid"],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+
     ({
       algolia: {
         // The application ID provided by Algolia
@@ -207,12 +208,15 @@ const config = {
           alt: "IOTA Wiki Logo",
           src: "img/iota-logo.svg",
         },
+        copyright: `Copyright © ${new Date().getFullYear()} <a href='https://www.iota.org/'>IOTA Stiftung</a>, licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>. 
+                    The documentation on this website is adapted from the <a href='https://docs.sui.io/'>SUI Documentation</a>, © 2024 by <a href='https://sui.io/'>SUI Foundation</a>, licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>.`, // You can also put own HTML here
       },
       prism: {
         theme: themes.github,
         darkTheme: themes.jettwaveDark,
         additionalLanguages: ["rust", "typescript", "toml", "solidity"],
       },
+
     }),
 };
 
