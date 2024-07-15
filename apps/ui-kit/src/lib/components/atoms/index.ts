6df99c979f987519f7f926953d72f9ddec334447
evavirseda <evirseda@boxfish.studio>
2024-07-15 16:26:45 +0200

    feat(apps-ui-kit): add LabelText compoennt

--- apps/ui-kit/src/lib/components/atoms/index.ts
+++ apps/ui-kit/src/lib/components/atoms/index.ts
@@ -6,3 +6,4 @@ export * from './badge';
 export * from './button';
 export * from './header';
 export * from './divider';
+export * from './labelText';
---
+++ apps/ui-kit/src/lib/components/atoms/labelText/LabelText.tsx
@@ -0,0 +1,70 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import React from 'react';
+import cx from 'classnames';
+import { LabelTextSize } from './labelText.enums';
+import { LABEL_TEXT_SIZE, SUPPORTING_TEXT_SIZE, VALUE_TEXT_SIZE } from './labelText.classes';
+
+interface LabelTextProps {
+    /**
+     * The size of the LabelText.
+     */
+    size: LabelTextSize;
+    /**
+     * The position of the LabelText.
+     */
+    isCentered?: boolean;
+    /**
+     * The supporting label of the LabelText.
+     */
+    supportingLabel?: string;
+    /**
+     * The text of the LabelText.
+     */
+    text: string;
+    /**
+     * Show the supporting label.
+     */
+    showSupportingLabel: boolean;
+    /**
+     * The value of the LabelText.
+     */
+    value: string;
+}
+
+export function LabelText({
+    size,
+    isCentered,
+    supportingLabel,
+    text,
+    showSupportingLabel,
+    value,
+}: LabelTextProps): React.JSX.Element {
+    const valueTextClasses = VALUE_TEXT_SIZE[size];
+    const supportingLabelClasses = SUPPORTING_TEXT_SIZE[size];
+    const labelTextClasses = LABEL_TEXT_SIZE[size];
+    const centeredClasses = isCentered ? 'items-center' : 'items-start';
+    return (
+        <div className={cx('flex flex-col gap-0.5', centeredClasses)}>
+            <div className="flex flex-row items-center space-x-0.5">
+                <span className={cx('text-neutral-10 dark:text-neutral-92', valueTextClasses)}>
+                    {value}
+                </span>
+                {showSupportingLabel && supportingLabel && (
+                    <span
+                        className={cx(
+                            'pb-xxxs text-neutral-60 dark:text-neutral-40',
+                            supportingLabelClasses,
+                        )}
+                    >
+                        {supportingLabel}
+                    </span>
+                )}
+            </div>
+            <span className={cx('text-neutral-60 dark:text-neutral-40', labelTextClasses)}>
+                {text}
+            </span>
+        </div>
+    );
+}
