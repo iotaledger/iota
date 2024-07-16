27b99c0daa5c94df5963cf6a1882cc135ef59e11
Mirko Zichichi <miker83z@proton.me>
2024-07-16 12:47:46 +0200

    feat(iota-genesis-builder): example for generating test data only snapshot

--- crates/iota-genesis-builder/src/stardust/test_outputs/mod.rs
+++ crates/iota-genesis-builder/src/stardust/test_outputs/mod.rs
@@ -16,10 +16,12 @@ use iota_sdk::types::block::{
         BasicOutputBuilder, Output, OutputId,
     },
 };
+use iota_types::{gas_coin::TOTAL_SUPPLY_IOTA, timelock::timelock::VESTED_REWARD_ID_PREFIX};
 use packable::{
     packer::{IoPacker, Packer},
     Packable,
 };
+use rand::random;
 
 use crate::stardust::{
     parse::HornetSnapshotParser,
@@ -30,6 +32,31 @@ const OUTPUT_TO_DECREASE_AMOUNT_FROM: &str =
     "0xb462c8b2595d40d3ff19924e3731f501aab13e215613ce3e248d0ed9f212db160000";
 const MERGE_MILESTONE_INDEX: u32 = 7669900;
 const MERGE_TIMESTAMP_SECS: u32 = 1696406475;
+
+const DELEGATOR_GAS_COIN_NUM: u8 = 100;
+const DELEGATOR_GAS_COIN_AMOUNT_PER_OUTPUT: u64 = 1_000_000 * 1_000_000;
+const DELEGATOR_TIMELOCKS_NUM: u8 = 100;
+const DELEGATOR_TIMELOCKS_AMOUNT_PER_OUTPUT: u64 = 1_000_000 * 1_000_000;
+
+pub(crate) fn new_simple_basic_output(
+    amount: u64,
+    address: Ed25519Address,
+) -> anyhow::Result<(OutputHeader, Output)> {
+    let output_header = OutputHeader::new_testing(
+        random::<[u8; 32]>(),
+        random_output_index(),
+        [0; 32],
+        MERGE_MILESTONE_INDEX,
+        MERGE_TIMESTAMP_SECS,
+    );
+
+    let builder = BasicOutputBuilder::new_with_amount(amount)
+        .add_unlock_condition(AddressUnlockCondition::new(address));
+
+    let output = Output::from(builder.finish().unwrap());
+
+    Ok((output_header, output))
+}
 
 pub(crate) fn new_vested_output(
     transaction_id: &mut [u8; 32],
@@ -111,6 +138,86 @@ pub async fn add_snapshot_test_outputs<const VERIFY: bool>(
         } else {
             output.pack(&mut writer)?;
         }
+    }
+
+    // Add the solid entry points from the snapshot
+    writer.pack_bytes(parser.solid_entry_points_bytes()?)?;
+
+    Ok(())
+}
+
+/// Takes a full snapshot, empties it and adds outputs compliant with a
+/// timelocked staking in order to test specific and intricate scenarios.
+pub async fn only_snapshot_test_outputs<const VERIFY: bool>(
+    current_path: impl AsRef<Path> + core::fmt::Debug,
+    new_path: impl AsRef<Path> + core::fmt::Debug,
+    delegator: Ed25519Address,
+) -> anyhow::Result<()> {
+    let current_file = File::open(current_path)?;
+    let new_file = File::create(new_path)?;
+
+    let mut writer = IoPacker::new(BufWriter::new(new_file));
+    let parser = HornetSnapshotParser::new::<VERIFY>(current_file)?;
+    let mut new_header = parser.header.clone();
+    let mut vested_index = u32::MAX;
+
+    let mut new_outputs = [
+        alias_ownership::outputs().await?,
+        stardust_mix::outputs(&mut vested_index).await?,
+        vesting_schedule_entity::outputs(&mut vested_index).await?,
+        vesting_schedule_iota_airdrop::outputs(&mut vested_index).await?,
+        vesting_schedule_portfolio_mix::outputs(&mut vested_index).await?,
+    ]
+    .concat();
+
+    // Delegator
+    // Add gas coins to delegator
+    for _ in 0..DELEGATOR_GAS_COIN_NUM {
+        new_outputs.push(new_simple_basic_output(
+            DELEGATOR_GAS_COIN_AMOUNT_PER_OUTPUT,
+            delegator,
+        )?);
+    }
+
+    // Add timelocks to delegator
+    let mut transaction_id = [0; 32];
+    transaction_id[0..28]
+        .copy_from_slice(&prefix_hex::decode::<[u8; 28]>(VESTED_REWARD_ID_PREFIX)?);
+    for _ in 0..DELEGATOR_TIMELOCKS_NUM {
+        new_outputs.push(new_vested_output(
+            &mut transaction_id,
+            &mut vested_index,
+            DELEGATOR_TIMELOCKS_AMOUNT_PER_OUTPUT,
+            delegator,
+            Some(MERGE_TIMESTAMP_SECS + 125_798_400),
+        )?);
+    }
+
+    // Add all the remainder tokens to the zero address
+    let zero_address = Ed25519Address::new([0; 32]);
+    let new_amount_from_test_objects = new_outputs.iter().map(|o| o.1.amount()).sum::<u64>();
+    let remainder = (TOTAL_SUPPLY_IOTA * 1_000_000)
+        .checked_sub(new_amount_from_test_objects)
+        .expect("new amount should not be higher than total supply");
+    let remainder_per_output = remainder / 4;
+    let difference = remainder % 4;
+    for _ in 0..4 {
+        new_outputs.push(new_simple_basic_output(remainder_per_output, zero_address)?);
+    }
+    if difference > 0 {
+        new_outputs.push(new_simple_basic_output(difference, zero_address)?);
+    }
+
+    // Set the output count to the newly generated outputs.
+    new_header.output_count = new_outputs.len() as u64;
+
+    // Writes the new header.
+    new_header.pack(&mut writer)?;
+
+    // Writes only the new outputs.
+    for (output_header, output) in new_outputs {
+        output_header.pack(&mut writer)?;
+        output.pack(&mut writer)?;
     }
 
     // Add the solid entry points from the snapshot
