6536cdb8124773ac048266b4d22427a644d5ab3d
marc2332 <mespinsanz@gmail.com>
2024-07-16 15:54:22 +0200

    bring happiness to the CI

--- apps/wallet/src/shared/messaging/messages/payloads/accounts-finder/PersistAccountsFinder.ts
+++ apps/wallet/src/shared/messaging/messages/payloads/accounts-finder/PersistAccountsFinder.ts
@@ -3,8 +3,8 @@
 
 import { isBasePayload } from '_payloads';
 import type { BasePayload, Payload } from '_payloads';
-import { type AccountFinderConfigParams } from '_src/ui/app/accounts-finder';
-import { SourceStrategyToPersist } from './types';
+import type { AccountFinderConfigParams } from '_src/ui/app/accounts-finder';
+import { type SourceStrategyToPersist } from './types';
 
 export interface PersistAccountsFinder extends BasePayload {
     type: 'persist-accounts-finder';
