efb0ab842b4657eb82c3bc8c390ed649abcfa813
JCNoguera <jcnoguera@boxfish.studio>
2024-07-16 16:28:06 +0200

    refactor: use the icons from internal package

--- apps/ui-kit/src/lib/components/atoms/checkbox/Checkbox.tsx
+++ apps/ui-kit/src/lib/components/atoms/checkbox/Checkbox.tsx
@@ -3,6 +3,7 @@
 
 import { useEffect, useRef } from 'react';
 import cx from 'classnames';
+import { Dash, Checkmark } from '@iota/ui-icons';
 
 interface CheckboxProps {
     /**
@@ -47,6 +48,8 @@ export function Checkbox({
         }
     }, [isIndeterminate, inputRef]);
 
+    const CheckmarkIcon = isIndeterminate ? Dash : Checkmark;
+
     return (
         <label className={cx('group flex flex-row gap-x-2', { disabled: isDisabled })}>
             {isLabelFirst && <Label label={label} />}
@@ -60,7 +63,7 @@ export function Checkbox({
                     onChange={(e) => onChange?.(e.target.checked)}
                 />
                 <span className="absolute inset-0 flex h-full w-full items-center justify-center text-neutral-40 peer-disabled:text-neutral-70 peer-disabled:text-opacity-40 peer-[&:is(:checked,:indeterminate)]:text-white peer-[&:not(:checked,:indeterminate)]:text-opacity-40 dark:text-neutral-60 dark:peer-disabled:text-neutral-30 dark:peer-disabled:text-opacity-40">
-                    {isIndeterminate ? <IndeterminateIcon /> : <CheckmarkIcon />}
+                    <CheckmarkIcon width={16} height={16} />
                 </span>
             </div>
             {!isLabelFirst && <Label label={label} />}
@@ -73,38 +76,5 @@ function Label({ label }: Pick<CheckboxProps, 'label'>) {
         <span className="text-label-lg text-neutral-40 group-[.disabled]:text-opacity-40 dark:text-neutral-60 group-[.disabled]:dark:text-opacity-40">
             {label}
         </span>
-    );
-}
-
-function IndeterminateIcon() {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            fill="none"
-        >
-            <rect x="5.83325" y="9" width="8.33333" height="2" rx="1" fill="currentColor" />
-        </svg>
-    );
-}
-
-function CheckmarkIcon() {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            fill="none"
-        >
-            <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M8.54993 11.538L13.508 6.57981C13.8334 6.25438 14.3578 6.25117 14.6793 6.57263C15.0008 6.8941 14.9976 7.41851 14.6721 7.74394L9.12483 13.2913C8.79939 13.6168 8.27496 13.62 7.95349 13.2985L5.58206 10.9272C5.26059 10.6057 5.2638 10.0813 5.58924 9.75585C5.91469 9.43042 6.43911 9.4272 6.76059 9.74867L8.54993 11.538Z"
-                fill="currentColor"
-            />
-        </svg>
     );
 }
