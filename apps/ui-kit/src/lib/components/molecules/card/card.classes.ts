4528b227a0a1e6bc230908a6ef6de9fabf8a63f8
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2024-07-17 20:27:09 +0300

    feat(ui-kit): improve dark mode
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- apps/ui-kit/src/lib/components/molecules/card/card.classes.ts
+++ apps/ui-kit/src/lib/components/molecules/card/card.classes.ts
@@ -12,21 +12,22 @@ export const IMAGE: { [key in ImageVariant]: string } = {
     [ImageVariant.Rounded]: `${IMAGE_SIZE} rounded-full`,
 };
 
-const CARD_HOVER = `hover:cursor-pointer hover:bg-primary-60 hover:bg-opacity-8`;
+const CARD_HOVER = `hover:cursor-pointer hover:bg-primary-60 hover:bg-opacity-8 hover:dark:bg-primary-60 hover:dark:bg-opacity-8`;
+const CARD_ACTIVE = `active:cursor-pointer active:bg-primary-60 active:bg-opacity-12 dark:active:bg-primary-60 dark:active:bg-opacity-12`;
 export const CARD_CLASSES_VARIANT = {
     [CardVariant.Default]: {
         default: '',
         hover: CARD_HOVER,
-        active: `active:cursor-pointer active:bg-primary-60 active:bg-opacity-12`,
+        active: CARD_ACTIVE,
     },
     [CardVariant.Outlined]: {
         default: 'border border-shader-neutral-light-8 dark:border-shader-primary-dark-8 p-xs',
         hover: CARD_HOVER,
-        active: '',
+        active: CARD_ACTIVE,
     },
     [CardVariant.Filled]: {
         default: 'bg-shader-neutral-light-8 p-xs',
         hover: CARD_HOVER,
-        active: '',
+        active: CARD_ACTIVE,
     },
 };
