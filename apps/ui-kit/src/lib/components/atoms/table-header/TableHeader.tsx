0fa0d0a9a6add3da04024f0a86987016a2347122
evavirseda <evirseda@boxfish.studio>
2024-07-22 14:17:43 +0200

    feat(apps-ui-kit): update styles and cleanup

--- apps/ui-kit/src/lib/components/atoms/table-header/TableHeader.tsx
+++ apps/ui-kit/src/lib/components/atoms/table-header/TableHeader.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import React, { PropsWithChildren } from 'react';
+import React from 'react';
 
 interface TableHeaderProps {
     /**
@@ -10,9 +10,9 @@ interface TableHeaderProps {
     text: string;
 }
 
-export function TableHeader({ text }: PropsWithChildren<TableHeaderProps>): React.JSX.Element {
+export function TableHeader({ text }: TableHeaderProps): React.JSX.Element {
     return (
-        <div className="state-layer relative flex h-12 w-full shrink-0 flex-row items-center justify-between bg-neutral-96 px-md dark:bg-neutral-10 hover:dark:bg-neutral-10">
+        <div className="state-layer relative flex w-full shrink-0 flex-row items-center justify-between bg-neutral-96 px-md py-sm dark:bg-neutral-10 hover:dark:bg-neutral-10">
             <span className="text-label-lg text-neutral-40 dark:text-neutral-60">{text}</span>
         </div>
     );
