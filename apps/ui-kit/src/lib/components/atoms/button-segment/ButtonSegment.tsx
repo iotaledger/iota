8282e12d311df1cae82a5bec00a0e2764e53d480
evavirseda <evirseda@boxfish.studio>
2024-07-24 10:40:06 +0200

    feat: update button segment

--- apps/ui-kit/src/lib/components/atoms/button-segment/ButtonSegment.tsx
+++ apps/ui-kit/src/lib/components/atoms/button-segment/ButtonSegment.tsx
@@ -7,8 +7,10 @@ import {
     BACKGROUND_COLORS_SELECTED,
     TEXT_COLORS,
     TEXT_COLORS_SELECTED,
+    UNDERLINED_BACKGROUND_SELECTED,
 } from './button-segment.classes';
 import cx from 'classnames';
+import { ButtonSegmentType } from './button-segment.enums';
 
 interface ButtonSegmentProps {
     /**
@@ -31,6 +33,10 @@ interface ButtonSegmentProps {
      * The onClick event of the button.
      */
     onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    /**
+     * The type of the button.
+     */
+    type?: ButtonSegmentType;
 }
 
 export function ButtonSegment({
@@ -39,18 +45,30 @@ export function ButtonSegment({
     selected,
     disabled,
     onClick,
+    type = ButtonSegmentType.Rounded,
 }: ButtonSegmentProps): React.JSX.Element {
-    const backgroundColors = selected ? BACKGROUND_COLORS_SELECTED : BACKGROUND_COLORS;
+    const isUnderlined = type === ButtonSegmentType.Underlined;
+
+    const backgroundColors = selected
+        ? isUnderlined
+            ? UNDERLINED_BACKGROUND_SELECTED
+            : BACKGROUND_COLORS_SELECTED
+        : BACKGROUND_COLORS;
+
     const textColors = selected ? TEXT_COLORS_SELECTED : TEXT_COLORS;
+    const padding = isUnderlined ? 'px-lg py-md' : 'px-sm py-[6px]';
+    const borderRadius = isUnderlined ? '' : 'rounded-full';
     return (
         <button
             onClick={onClick}
             className={cx(
-                'enabled:state-layer relative flex items-center rounded-full px-sm py-[6px] disabled:opacity-40',
+                'enabled:state-layer relative flex items-center disabled:opacity-40',
                 backgroundColors,
                 textColors,
+                padding,
+                borderRadius,
                 {
-                    'pl-xs': !!icon,
+                    'pl-xs': !!icon && !isUnderlined,
                 },
             )}
             disabled={disabled}
