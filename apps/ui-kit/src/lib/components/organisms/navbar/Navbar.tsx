74ee948c43bd3de4c2d01e72e2633c6e07249d49
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2024-07-25 17:17:48 +0300

    feat(apps-ui-kit): Add Navbar vertical.
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- apps/ui-kit/src/lib/components/organisms/navbar/Navbar.tsx
+++ apps/ui-kit/src/lib/components/organisms/navbar/Navbar.tsx
@@ -3,11 +3,19 @@
 
 import React from 'react';
 import cx from 'classnames';
+import { IotaLogoMark, Menu } from '@iota/ui-icons';
 import { NavbarItem, NavbarItemProps } from '@/components/molecules/navbar-item/NavbarItem';
 
 export type NavbarItemWithID = NavbarItemProps & { id: string };
 
+export enum NavbarType {
+    Horizontal = 'horizontal',
+    Vertical = 'vertical',
+}
+
 export interface NavbarProps {
+    type?: NavbarType;
+
     /**
      * List of elements to be displayed in the navbar.
      */
@@ -30,18 +38,48 @@ export interface NavbarProps {
     className?: string;
 }
 
-function Navbar({ items, activeId, onClick, className }: NavbarProps) {
+const NAVBAR_CLASSES = {
+    [NavbarType.Horizontal]: 'flex w-full justify-between gap-2',
+    [NavbarType.Vertical]: 'flex-col justify-between gap-2 w-full py-xs px-sm sm:py-xl sm:w-[80px]',
+} as const;
+
+function Navbar({
+    items,
+    activeId,
+    onClick,
+    className,
+    type = NavbarType.Horizontal,
+}: NavbarProps) {
+    const handleMenuClick = () => {
+        console.info('Menu clicked');
+    };
+
     return (
-        <div className={cx('flex w-full justify-between gap-2', className)}>
-            {items.map((item) => (
-                <div key={item.id} className="px-xs py-xxs">
-                    <NavbarItem
-                        {...item}
-                        isSelected={item.id === activeId}
-                        onClick={() => onClick(item.id)}
-                    />
+        <div className={cx(className, NAVBAR_CLASSES[type])}>
+            {type === NavbarType.Vertical && (
+                <div className="flex w-full justify-between sm:mb-[48px] sm:flex-col">
+                    <div className="flex justify-center">
+                        <IotaLogoMark width={38} height={38} />
+                    </div>
+                    <div
+                        className="state-layer relative rounded-full p-xs hover:cursor-pointer sm:hidden"
+                        onClick={handleMenuClick}
+                    >
+                        <Menu width={24} height={24} />
+                    </div>
                 </div>
-            ))}
+            )}
+            <div className={cx('hidden sm:flex', { 'flex-col': type === NavbarType.Vertical })}>
+                {items.map((item) => (
+                    <div key={item.id} className=" px-xs py-xxs  ">
+                        <NavbarItem
+                            {...item}
+                            isSelected={item.id === activeId}
+                            onClick={() => onClick(item.id)}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
--- apps/ui-kit/src/storybook/stories/organisms/Navbar.stories.tsx
+++ apps/ui-kit/src/storybook/stories/organisms/Navbar.stories.tsx
@@ -3,21 +3,21 @@
 // import { ButtonSize } from '@/lib/components';
 
 import type { Meta, StoryObj } from '@storybook/react';
-import { Home, Assets, Apps, Activity } from '@iota/ui-icons';
-import { Navbar, NavbarProps, NavbarItemWithID } from '@/components';
+import { Activity, Apps, Assets, Home } from '@iota/ui-icons';
+import { Navbar, NavbarItemWithID, NavbarProps, NavbarType } from '@/components';
 import { useState } from 'react';
+
+const NAVBAR_ITEMS: NavbarItemWithID[] = [
+    { id: 'home', icon: <Home /> },
+    { id: 'assets', icon: <Assets /> },
+    { id: 'apps', icon: <Apps /> },
+    { id: 'activity', icon: <Activity /> },
+];
 
 const meta: Meta<NavbarProps> = {
     component: Navbar,
     tags: ['autodocs'],
     render: () => {
-        const NAVBAR_ITEMS: NavbarItemWithID[] = [
-            { id: 'home', icon: <Home /> },
-            { id: 'assets', icon: <Assets /> },
-            { id: 'apps', icon: <Apps /> },
-            { id: 'activity', icon: <Activity /> },
-        ];
-
         const [activeId, setActiveId] = useState<string>(NAVBAR_ITEMS[0].id);
 
         return (
@@ -37,3 +37,22 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const Vertical: Story = {
+    args: {},
+    argTypes: {},
+    render: (args) => {
+        const [activeId, setActiveId] = useState<string>(NAVBAR_ITEMS[0].id);
+
+        return (
+            <div className="flex border border-gray-200">
+                <Navbar
+                    type={NavbarType.Vertical}
+                    items={NAVBAR_ITEMS}
+                    activeId={activeId}
+                    onClick={(id) => setActiveId(id)}
+                />
+            </div>
+        );
+    },
+};
