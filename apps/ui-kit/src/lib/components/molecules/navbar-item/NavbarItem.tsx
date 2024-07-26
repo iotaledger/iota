e6a5485a51d6833fb16da8138031109ae7ec470f
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2024-07-26 16:39:09 +0300

    feat(apps-ui-kit): Navbar. Split item to horizontal & vertical.
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- apps/ui-kit/src/lib/components/molecules/navbar-item/NavbarItem.tsx
+++ apps/ui-kit/src/lib/components/molecules/navbar-item/NavbarItem.tsx
@@ -2,19 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import React from 'react';
-import cx from 'classnames';
-import {
-    BADGE_WITH_TEXT,
-    BADGE_WITHOUT_TEXT,
-    PADDING_WITH_TEXT,
-    PADDING_WITHOUT_TEXT,
-    SELECTED_BACKGROUND,
-    SELECTED_ICON,
-    SELECTED_TEXT,
-    UNSELECTED_ICON,
-    UNSELECTED_TEXT,
-} from './navbarItem.classes';
-import { Badge, BadgeType } from '../../atoms';
+import { NavbarItemHorizontal } from './NavbarItemHorizontal';
+import { NavbarItemVertical } from './NavbarItemVertical';
 import { NavbarItemType } from './navbarItem.enums';
 
 export interface NavbarItemProps {
@@ -45,53 +34,12 @@ export interface NavbarItemProps {
     onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
-export function NavbarItem({
-    type = NavbarItemType.Vertical,
-    icon,
-    text,
-    isSelected,
-    hasBadge,
-    onClick,
-}: NavbarItemProps): React.JSX.Element {
-    const fillClasses = isSelected ? SELECTED_ICON : UNSELECTED_ICON;
-    const paddingClasses = text ? PADDING_WITH_TEXT : PADDING_WITHOUT_TEXT;
-    const backgroundColors = isSelected && SELECTED_BACKGROUND;
-    const badgePositionClasses = text ? BADGE_WITH_TEXT : BADGE_WITHOUT_TEXT;
-    const textClasses = isSelected ? SELECTED_TEXT : UNSELECTED_TEXT;
-    return (
-        <div
-            onClick={onClick}
-            className={cx('inline-flex cursor-pointer items-center', {
-                'flex w-full gap-3 p-sm': type === NavbarItemType.Horizontal,
-                'flex-col justify-center space-y-1': type === NavbarItemType.Vertical,
-            })}
-        >
-            <div
-                className={cx('inline-flex rounded-full', backgroundColors, {
-                    'state-layer relative': type === NavbarItemType.Vertical,
-                    [paddingClasses]: type === NavbarItemType.Vertical,
-                    'p-none': type === NavbarItemType.Horizontal,
-                })}
-            >
-                {React.cloneElement(icon as React.ReactElement, {
-                    className: cx('w-6 h-6', fillClasses),
-                })}
-                {hasBadge && (
-                    <div className={cx('absolute', badgePositionClasses)}>
-                        <Badge type={BadgeType.PrimarySolid} />
-                    </div>
-                )}
-            </div>
-            {text && (
-                <span
-                    className={cx(textClasses, {
-                        'text-center text-label-md': type === NavbarItemType.Vertical,
-                        'text-label-lg': type === NavbarItemType.Horizontal,
-                    })}
-                >
-                    {text}
-                </span>
-            )}
-        </div>
-    );
+export function NavbarItem(props: NavbarItemProps): React.JSX.Element {
+    const { type } = props;
+
+    if (type === NavbarItemType.Horizontal) {
+        return <NavbarItemHorizontal {...props} />;
+    }
+
+    return <NavbarItemVertical {...props} />;
 }
