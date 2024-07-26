3157b1c1ef5f7628c5cd37f29c35dc9e58967bb6
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2024-07-26 19:54:17 +0300

    feat(wallet): Welcome screen. Update ui.
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- apps/wallet/src/ui/app/pages/accounts/WelcomePage.tsx
+++ apps/wallet/src/ui/app/pages/accounts/WelcomePage.tsx
@@ -2,13 +2,11 @@
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { Button } from '_app/shared/ButtonUI';
-import { Heading } from '_app/shared/heading';
-import { Text } from '_app/shared/text';
+// import { Button } from '_app/shared/ButtonUI';
 import Loading from '_components/loading';
-import Logo from '_components/logo';
 import { useFullscreenGuard, useInitializedGuard } from '_hooks';
-import WelcomeSplash from '_src/ui/assets/images/WelcomeSplash.svg';
+import { Button, ButtonType } from '@iota/apps-ui-kit';
+import { IotaLogoWeb } from '@iota/icons';
 
 import { useCreateAccountsMutation } from '../../hooks/useCreateAccountMutation';
 
@@ -21,37 +19,27 @@ export function WelcomePage() {
     );
     return (
         <Loading loading={isInitializedLoading || isFullscreenGuardLoading}>
-            <div className="flex h-full flex-col items-center overflow-auto rounded-20 bg-iota-lightest px-7 py-6 shadow-wallet-content">
-                <div className="shrink-0">
-                    <Logo />
+            <div className="flex h-full w-full flex-col items-center justify-between overflow-auto rounded-20 bg-white px-xl py-2xl shadow-wallet-content">
+                <div>
+                    <IotaLogoWeb width={130} height={32} />
                 </div>
-                <div className="mx-auto mt-2 text-center">
-                    <Heading variant="heading2" color="gray-90" as="h1" weight="bold">
-                        Welcome to Iota Wallet
-                    </Heading>
-                    <div className="mt-2">
-                        <Text variant="pBody" color="steel-dark" weight="medium">
-                            Connecting you to the decentralized web and Iota network.
-                        </Text>
+                <div className={'flex flex-col items-center gap-8 text-center'}>
+                    <div className="flex flex-col items-center gap-4">
+                        <h4 className={'text-headline-sm text-neutral-40'}>Welcome to</h4>
+                        <h1 className={'text-display-md text-neutral-10'}>IOTA Wallet</h1>
+                        <h3 className={'text-title-md text-neutral-40'}>
+                            Connecting you to the decentralized
+                            <br />
+                            web and IOTA network
+                        </h3>
+                    </div>
+
+                    <div>
+                        <Button type={ButtonType.Primary} text={'Add Profile'} />
                     </div>
                 </div>
-                <div className="mt-3.5 flex h-full w-full items-center justify-center">
-                    <WelcomeSplash role="img" />
-                </div>
-                <div className="mt-3.5 flex w-full flex-col items-center gap-3">
-                    <Text variant="pBody" color="steel-dark" weight="medium">
-                        Sign in with your preferred service
-                    </Text>
-                    <Button
-                        to="/accounts/add-account?sourceFlow=Onboarding"
-                        size="tall"
-                        variant="secondary"
-                        text="More Options"
-                        disabled={
-                            createAccountsMutation.isPending || createAccountsMutation.isSuccess
-                        }
-                    />
-                </div>
+
+                <div className={'text-body-lg text-neutral-80'}>&copy; IOTA Foundation 2024</div>
             </div>
         </Loading>
     );
