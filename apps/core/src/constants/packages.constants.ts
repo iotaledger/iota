292311bd26645d3beaeb996e4163cd7f0019eeea
cpl121 <cpeon@boxfish.studio>
2024-07-29 17:30:36 +0200

    fix(wallet-dashboard): remove testing constants

--- apps/core/src/constants/packages.constants.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2024 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-export const TIMELOCK_PACKAGE_ID =
-    '0xa9ed4baf8210bda422feaf348dd5dd730618bf736372871bfcf4d755e942dd61'; // TODO: modify with real package https://github.com/iotaledger/iota/issues/1219
