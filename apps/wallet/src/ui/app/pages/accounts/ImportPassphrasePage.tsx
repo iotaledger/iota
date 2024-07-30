767adc36eeeb778fddcb8e7e0dc58138657f2bf9
JCNoguera <jcnoguera@boxfish.studio>
2024-07-30 16:15:42 +0200

    feat(wallet): rebrand import mnemonic screen

--- apps/wallet/src/ui/app/pages/accounts/ImportPassphrasePage.tsx
+++ apps/wallet/src/ui/app/pages/accounts/ImportPassphrasePage.tsx
@@ -2,7 +2,6 @@
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { Text } from '_app/shared/text';
 import { entropyToSerialized, mnemonicToEntropy } from '_src/shared/utils';
 import { useNavigate } from 'react-router-dom';
 
@@ -11,45 +10,59 @@ import {
     useAccountsFormContext,
 } from '../../components/accounts/AccountsFormContext';
 import { ImportRecoveryPhraseForm } from '../../components/accounts/ImportRecoveryPhraseForm';
-import { Heading } from '../../shared/heading';
+import { Header } from '@iota/apps-ui-kit';
 
 export function ImportPassphrasePage() {
     const navigate = useNavigate();
     const [, setFormValues] = useAccountsFormContext();
     return (
-        <div className="bg-iota-lightest flex h-full flex-col items-center overflow-auto rounded-20 px-6 py-10 shadow-wallet-content">
-            <Text variant="caption" color="steel-dark" weight="semibold">
-                Wallet Setup
-            </Text>
-            <div className="mt-2.5 text-center">
-                <Heading variant="heading1" color="gray-90" as="h1" weight="bold">
-                    Add Existing Account
-                </Heading>
-            </div>
-            <div className="mt-6 flex grow flex-col gap-3">
-                <div className="pl-2.5">
-                    <Text variant="pBody" color="steel-darker" weight="semibold">
-                        Enter your 24-word Recovery Phrase
-                    </Text>
+        <>
+            <Header
+                title="Import Mnemonic"
+                titleCentered
+                hasLeftIcon
+                onBack={() =>
+                    navigate(
+                        `/accounts/add-account?${new URLSearchParams({
+                            sourceFlow: 'Onboarding',
+                        }).toString()}`,
+                    )
+                }
+            />
+
+            <div className="flex flex-col items-center overflow-auto bg-neutral-100">
+                <div className="px-md py-xs">
+                    <div className="flex flex-col items-center justify-center gap-y-2 p-md text-center">
+                        <p className="text-label-lg text-neutral-40">Wallet Setup</p>
+                        <h2 className="text-headline-md text-neutral-10">Add Existing Account</h2>
+                        <p className="text-body-md text-neutral-40">
+                            Enter your 24-word Recovery Phrase
+                        </p>
+                    </div>
                 </div>
-                <ImportRecoveryPhraseForm
-                    cancelButtonText="Cancel"
-                    submitButtonText="Add Account"
-                    onSubmit={({ recoveryPhrase }) => {
-                        setFormValues({
-                            type: AccountsFormType.ImportMnemonic,
-                            entropy: entropyToSerialized(
-                                mnemonicToEntropy(recoveryPhrase.join(' ')),
-                            ),
-                        });
-                        navigate(
-                            `/accounts/protect-account?${new URLSearchParams({
-                                accountsFormType: AccountsFormType.ImportMnemonic,
-                            }).toString()}`,
-                        );
-                    }}
-                />
+
+                <div className="flex grow flex-col gap-3 p-md">
+                    <ImportRecoveryPhraseForm
+                        cancelButtonText="Cancel"
+                        submitButtonText="Add Account"
+                        onSubmit={({ recoveryPhrase }) => {
+                            setFormValues({
+                                type: AccountsFormType.ImportMnemonic,
+                                entropy: entropyToSerialized(
+                                    mnemonicToEntropy(recoveryPhrase.join(' ')),
+                                ),
+                            });
+                            navigate(
+                                `/accounts/protect-account?${new URLSearchParams({
+                                    accountsFormType: AccountsFormType.ImportMnemonic,
+                                }).toString()}`,
+                            );
+                        }}
+                    />
+                </div>
+
+                <div className="px-md"></div>
             </div>
-        </div>
+        </>
     );
 }
