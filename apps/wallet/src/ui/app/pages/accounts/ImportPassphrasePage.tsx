83422b19826c95a4e83e475d50e45c9c61e169ba
JCNoguera <jcnoguera@boxfish.studio>
2024-07-31 09:39:23 +0200

    fix: remove header

--- apps/wallet/src/ui/app/pages/accounts/ImportPassphrasePage.tsx
+++ apps/wallet/src/ui/app/pages/accounts/ImportPassphrasePage.tsx
@@ -10,7 +10,7 @@ import {
     useAccountsFormContext,
 } from '../../components/accounts/AccountsFormContext';
 import { ImportRecoveryPhraseForm } from '../../components/accounts/ImportRecoveryPhraseForm';
-import { Button, ButtonType, Header } from '@iota/apps-ui-kit';
+import { Button, ButtonType } from '@iota/apps-ui-kit';
 import { useState } from 'react';
 import { VisibilityOff, VisibilityOn } from '@iota/ui-icons';
 
@@ -25,66 +25,51 @@ export function ImportPassphrasePage() {
 
     const BUTTON_ICON_CLASSES = 'w-5 h-5 text-neutral-10';
     return (
-        <>
-            <Header
-                title="Import Mnemonic"
-                titleCentered
-                hasLeftIcon
-                onBack={() =>
-                    navigate(
-                        `/accounts/add-account?${new URLSearchParams({
-                            sourceFlow: 'Onboarding',
-                        }).toString()}`,
-                    )
-                }
-            />
-
-            <div className="flex flex-col overflow-auto bg-neutral-100">
-                <div className="px-md py-xs">
-                    <div className="flex flex-col items-center justify-center gap-y-2 p-md text-center">
-                        <p className="text-label-lg text-neutral-40">Wallet Setup</p>
-                        <h2 className="text-headline-md text-neutral-10">Add Existing Account</h2>
-                        <p className="text-body-md text-neutral-40">
-                            Enter your 24-word Recovery Phrase
-                        </p>
-                    </div>
-                </div>
-
-                <div className="flex flex-col items-center gap-3 p-md pb-0 ">
-                    <div>
-                        <Button
-                            text={isTextVisible ? 'Hide Text' : 'Show Text'}
-                            icon={
-                                isTextVisible ? (
-                                    <VisibilityOff className={BUTTON_ICON_CLASSES} />
-                                ) : (
-                                    <VisibilityOn className={BUTTON_ICON_CLASSES} />
-                                )
-                            }
-                            onClick={handleShowTextClick}
-                            type={ButtonType.Secondary}
-                        />
-                    </div>
-                    <ImportRecoveryPhraseForm
-                        cancelButtonText="Back"
-                        submitButtonText="Add Profile"
-                        isTextVisible={isTextVisible}
-                        onSubmit={({ recoveryPhrase }) => {
-                            setFormValues({
-                                type: AccountsFormType.ImportMnemonic,
-                                entropy: entropyToSerialized(
-                                    mnemonicToEntropy(recoveryPhrase.join(' ')),
-                                ),
-                            });
-                            navigate(
-                                `/accounts/protect-account?${new URLSearchParams({
-                                    accountsFormType: AccountsFormType.ImportMnemonic,
-                                }).toString()}`,
-                            );
-                        }}
-                    />
+        <div className="flex flex-col overflow-auto bg-neutral-100">
+            <div className="px-md py-xs">
+                <div className="flex flex-col items-center justify-center gap-y-2 p-md text-center">
+                    <p className="text-label-lg text-neutral-40">Wallet Setup</p>
+                    <h2 className="text-headline-md text-neutral-10">Add Existing Account</h2>
+                    <p className="text-body-md text-neutral-40">
+                        Enter your 24-word Recovery Phrase
+                    </p>
                 </div>
             </div>
-        </>
+
+            <div className="flex flex-col items-center gap-3 p-md pb-0 ">
+                <div>
+                    <Button
+                        text={isTextVisible ? 'Hide Text' : 'Show Text'}
+                        icon={
+                            isTextVisible ? (
+                                <VisibilityOff className={BUTTON_ICON_CLASSES} />
+                            ) : (
+                                <VisibilityOn className={BUTTON_ICON_CLASSES} />
+                            )
+                        }
+                        onClick={handleShowTextClick}
+                        type={ButtonType.Secondary}
+                    />
+                </div>
+                <ImportRecoveryPhraseForm
+                    cancelButtonText="Back"
+                    submitButtonText="Add Profile"
+                    isTextVisible={isTextVisible}
+                    onSubmit={({ recoveryPhrase }) => {
+                        setFormValues({
+                            type: AccountsFormType.ImportMnemonic,
+                            entropy: entropyToSerialized(
+                                mnemonicToEntropy(recoveryPhrase.join(' ')),
+                            ),
+                        });
+                        navigate(
+                            `/accounts/protect-account?${new URLSearchParams({
+                                accountsFormType: AccountsFormType.ImportMnemonic,
+                            }).toString()}`,
+                        );
+                    }}
+                />
+            </div>
+        </div>
     );
 }
