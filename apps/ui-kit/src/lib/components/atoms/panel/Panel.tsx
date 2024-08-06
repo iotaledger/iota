6ae25f3ecad6bb45c7a7b99565b00b6265f1a4fa
evavirseda <evirseda@boxfish.studio>
2024-08-06 09:51:02 +0200

    feat: refine panel

--- apps/ui-kit/src/lib/components/atoms/panel/Panel.tsx
+++ apps/ui-kit/src/lib/components/atoms/panel/Panel.tsx
@@ -3,82 +3,35 @@
 
 import React from 'react';
 import cx from 'classnames';
-import { PanelSize, PanelTitleSize } from './panel.enums';
-import {
-    PADDING_BOTTOM_TITLE,
-    PADDING_TOP_CHILDREN_WITH_TITLE,
-    PADDING_TOP_WITH_TITLE,
-} from './panel.classes';
-import { Badge, BadgeType } from '../badge';
 
 interface PanelProps {
     /**
      * The title of the panel.
      */
-    title?: string;
-    /**
-     * The size of the title.
-     */
-    titleSize?: PanelTitleSize;
-    /**
-     * The type of the badge.
-     */
-    badgeType?: BadgeType;
-    /**
-     * The text of the badge.
-     */
-    badgeText?: string;
+    title?: React.ReactNode;
     /**
      * Show or hide border around the panel.
      */
     hasBorder?: boolean;
-    /**
-     * The size of the panel.
-     */
-    size?: PanelSize;
 }
 
 export function Panel({
     title,
-    titleSize = PanelTitleSize.Large,
     children,
-    badgeType,
-    badgeText,
     hasBorder,
-    size = PanelSize.Medium,
 }: React.PropsWithChildren<PanelProps>): React.JSX.Element {
-    const titleSizeClass = titleSize === PanelTitleSize.Medium ? 'text-title-md' : 'text-title-lg';
     const borderClass = hasBorder
         ? 'border border-shader-neutral-light-8 dark:border-shader-neutral-dark-8'
         : 'border border-transparent';
-    const panelPaddingClass = size === PanelSize.Medium ? 'px-md--rs pb-md--rs' : 'px-md pb-md';
-    const PADDING_TOP_WITHOUT_TITLE = size === PanelSize.Medium ? 'pt-md--rs' : 'pt-md';
-    const paddingTopClass = title ? PADDING_TOP_WITH_TITLE[size] : PADDING_TOP_WITHOUT_TITLE;
-    const paddingTopChildren = title ? PADDING_TOP_CHILDREN_WITH_TITLE[size] : 'pt-0';
-    const paddingBottomTitleClass = PADDING_BOTTOM_TITLE[size];
     return (
         <div
             className={cx(
-                'flex flex-col rounded-xl bg-neutral-100 px-md dark:bg-neutral-10',
+                'flex flex-col rounded-xl bg-neutral-100 dark:bg-neutral-10',
                 borderClass,
-                panelPaddingClass,
-                paddingTopClass,
             )}
         >
-            {title && (
-                <div className={cx('flex items-center gap-0.5', paddingBottomTitleClass)}>
-                    <span
-                        className={cx(
-                            'font-inter text-neutral-10 dark:text-neutral-92',
-                            titleSizeClass,
-                        )}
-                    >
-                        {title}
-                    </span>
-                    {badgeType && badgeText && <Badge type={badgeType} label={badgeText} />}
-                </div>
-            )}
-            <div className={cx(paddingTopChildren)}>{children}</div>
+            {title && <div>{title}</div>}
+            <div>{children}</div>
         </div>
     );
 }
--- apps/ui-kit/src/storybook/stories/atoms/Panel.stories.tsx
+++ apps/ui-kit/src/storybook/stories/atoms/Panel.stories.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { Meta, StoryObj } from '@storybook/react';
-import { Address, BadgeType, Panel, PanelSize, PanelTitleSize } from '@/components';
+import { Address, Panel, Title } from '@/components';
 
 const meta = {
     component: Panel,
@@ -10,7 +10,7 @@ const meta = {
     render: (props) => {
         return (
             <Panel {...props}>
-                <div className="flex flex-col items-start gap-2">
+                <div className="flex flex-col items-start gap-2 px-md--rs py-sm--rs">
                     <Address text="0x0d7...3f34" isCopyable />
                     <Address text="0x0d7...3f35" isCopyable />
                     <Address text="0x0d7...3f36" isCopyable />
@@ -25,34 +25,26 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {
-        title: 'Your Address',
-    },
     argTypes: {
         title: {
-            control: 'text',
-        },
-        titleSize: {
-            control: {
-                type: 'select',
-                options: Object.values(PanelTitleSize),
-            },
-        },
-        badgeType: {
-            control: 'select',
-            options: Object.values(BadgeType),
-        },
-        badgeText: {
             control: 'text',
         },
         hasBorder: {
             control: 'boolean',
         },
-        size: {
-            control: {
-                type: 'select',
-                options: Object.values(PanelSize),
-            },
+    },
+};
+
+export const WithTitle: Story = {
+    args: {
+        title: <Title title="Title" subtitle="subtitle" />,
+    },
+    argTypes: {
+        title: {
+            control: 'text',
+        },
+        hasBorder: {
+            control: 'boolean',
         },
     },
 };
