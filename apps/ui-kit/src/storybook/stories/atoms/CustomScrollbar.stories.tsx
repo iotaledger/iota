a3c7f3c95f2ae7873c516a0f0fa38301e63b6584
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2024-08-06 14:12:44 +0300

    feat(apps-ui-kit): Scrollbar. Update storybook & global styles.
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- apps/ui-kit/src/storybook/stories/atoms/CustomScrollbar.stories.tsx
+++ apps/ui-kit/src/storybook/stories/atoms/CustomScrollbar.stories.tsx
@@ -1,26 +1,22 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { CustomScrollbar } from '@/components/atoms/custom-scrollbar/CustomScrollbar';
-import type { Meta, StoryObj } from '@storybook/react';
+import type { StoryObj } from '@storybook/react';
 
 const meta = {
-    component: CustomScrollbar,
+    title: 'Token Showcase/Scrollbar',
     tags: ['autodocs'],
     render: () => {
         return (
-            <div>
-                <CustomScrollbar />
+            <div className="h-[300px] w-1/2 overflow-y-scroll">
+                <div className="h-[500px]"></div>
             </div>
         );
     },
-} satisfies Meta<typeof CustomScrollbar>;
+};
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-    args: {},
-    argTypes: {},
-};
+export const Default: Story = {};
