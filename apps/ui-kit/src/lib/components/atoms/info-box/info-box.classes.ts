b71d8edfe355512e03d969b8c25071fef5308099
evavirseda <evirseda@boxfish.studio>
2024-08-06 12:58:12 +0200

    feat: updat colors

--- apps/ui-kit/src/lib/components/atoms/info-box/info-box.classes.ts
+++ apps/ui-kit/src/lib/components/atoms/info-box/info-box.classes.ts
@@ -4,11 +4,11 @@
 import { InfoBoxType } from './info-box.enums';
 
 export const ICON_COLORS: Record<InfoBoxType, string> = {
-    [InfoBoxType.Default]: 'bg-neutral-92 dark:bg-neutral-12 text-neutral-10 dark:text-neutral-92',
-    [InfoBoxType.Warning]: 'bg-error-90 dark:bg-error-10 text-error-10 dark:text-error-90',
+    [InfoBoxType.Default]: 'bg-neutral-96 dark:bg-neutral-12 text-neutral-10 dark:text-neutral-92',
+    [InfoBoxType.Warning]: 'bg-error-90 dark:bg-error-10 text-error-20 dark:text-error-90',
 };
 
 export const BACKGROUND_COLORS: Record<InfoBoxType, string> = {
-    [InfoBoxType.Default]: 'bg-neutral-92 dark:bg-neutral-12',
+    [InfoBoxType.Default]: 'bg-neutral-96 dark:bg-neutral-12',
     [InfoBoxType.Warning]: 'bg-error-90 dark:bg-error-10',
 };
