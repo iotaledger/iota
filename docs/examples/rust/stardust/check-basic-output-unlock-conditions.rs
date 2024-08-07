2aaa2c9565a137a85bcaf1537b784ff781356b76
Mirko Zichichi <miker83z@proton.me>
2024-08-07 16:11:45 +0200

    Update docs/examples/rust/stardust/check-basic-output-unlock-conditions.rs
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

--- docs/examples/rust/stardust/check-basic-output-unlock-conditions.rs
+++ docs/examples/rust/stardust/check-basic-output-unlock-conditions.rs
@@ -46,13 +46,13 @@ async fn main() -> Result<(), anyhow::Error> {
     println!("Basic Output infos: {basic_output:?}");
 
     if let Some(sdruc) = basic_output.storage_deposit_return {
-        println!("Storage Deposit Return Unlock Condition infos: {:?}", sdruc);
+        println!("Storage Deposit Return Unlock Condition infos: {sdruc:?}");
     }
     if let Some(tuc) = basic_output.timelock {
         println!("Timelocked until: {}", tuc.unix_time);
     }
     if let Some(euc) = basic_output.expiration {
-        println!("Expiration Unlock Condition infos: {:?}", euc);
+        println!("Expiration Unlock Condition infos: {euc:?}");
     }
 
     Ok(())
