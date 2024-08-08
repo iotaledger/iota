c1280438802cddfad651f1377fe43349ce57a88b
evavirseda <evirseda@boxfish.studio>
2024-08-08 17:33:37 +0200

    feat: refine icon

--- apps/wallet/src/ui/app/components/coin-icon/index.tsx
+++ apps/wallet/src/ui/app/components/coin-icon/index.tsx
@@ -4,33 +4,8 @@
 
 import { ImageIcon } from '_app/shared/image-icon';
 import { useCoinMetadata } from '@iota/core';
-import { Unstaked } from '@iota/icons';
 import { IOTA_TYPE_ARG } from '@iota/iota.js/utils';
-import { cva, type VariantProps } from 'class-variance-authority';
-import { IotaLogoMark } from '@iota/ui-icons';
-
-const imageStyle = cva(['rounded-full flex'], {
-    variants: {
-        size: {
-            sm: 'w-6 h-6',
-            md: 'w-7.5 h-7.5',
-            lg: 'md:w-10 md:h-10 w-8 h-8',
-            xl: 'md:w-31.5 md:h-31.5 w-16 h-16 ',
-        },
-        fill: {
-            iota: 'bg-iota',
-            iotaPrimary2023: 'bg-iota-primaryBlue2023',
-        },
-    },
-    defaultVariants: {
-        size: 'md',
-        fill: 'iotaPrimary2023',
-    },
-});
-
-function IotaCoin() {
-    return <IotaLogoMark className="h-full w-full" />;
-}
+import { IotaLogoMark, PlaceholderReplace } from '@iota/ui-icons';
 
 interface NonIotaCoinProps {
     coinType: string;
@@ -39,29 +14,35 @@ interface NonIotaCoinProps {
 function NonIotaCoin({ coinType }: NonIotaCoinProps) {
     const { data: coinMeta } = useCoinMetadata(coinType);
     return (
-        <div className="bg-steel flex h-full w-full items-center justify-center rounded-full text-white">
+        <>
             {coinMeta?.iconUrl ? (
-                <ImageIcon
-                    src={coinMeta.iconUrl}
-                    label={coinMeta.name || coinType}
-                    fallback={coinMeta.name || coinType}
-                    rounded="full"
-                />
+                <div className="flex h-full w-full items-center justify-center rounded-full ">
+                    <ImageIcon
+                        src={coinMeta.iconUrl}
+                        label={coinMeta.name || coinType}
+                        fallback={coinMeta.name || coinType}
+                        rounded="full"
+                    />
+                </div>
             ) : (
-                <Unstaked />
+                <PlaceholderReplace className="h-full w-full" />
             )}
-        </div>
+        </>
     );
 }
 
-export interface CoinIconProps extends VariantProps<typeof imageStyle> {
+export interface CoinIconProps {
     coinType: string;
 }
 
 export function CoinIcon({ coinType, ...styleProps }: CoinIconProps) {
     return (
-        <div className={imageStyle(styleProps)}>
-            {coinType === IOTA_TYPE_ARG ? <IotaCoin /> : <NonIotaCoin coinType={coinType} />}
+        <div className="flex h-full w-full items-center justify-center p-1.5">
+            {coinType === IOTA_TYPE_ARG ? (
+                <IotaLogoMark className="h-full w-full" />
+            ) : (
+                <NonIotaCoin coinType={coinType} />
+            )}
         </div>
     );
 }
