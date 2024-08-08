2e0f86ec05105fede071dfae74672e6d0b9e5baf
evavirseda <evirseda@boxfish.studio>
2024-08-08 17:05:45 +0200

    feat: update CoinItem component

--- apps/wallet/src/ui/app/components/active-coins-card/CoinItem.tsx
+++ apps/wallet/src/ui/app/components/active-coins-card/CoinItem.tsx
@@ -25,19 +25,12 @@ interface CoinItemProps {
     subtitle?: string;
 }
 
-export function CoinItem({
-    coinType,
-    balance,
-    isActive,
-    usd,
-    centerAction,
-    subtitle,
-}: CoinItemProps) {
+export function CoinItem({ coinType, balance, usd }: CoinItemProps) {
     const [formatted, symbol, { data: coinMeta }] = useFormatCoin(balance, coinType);
     const isIota = coinType === IOTA_TYPE_ARG;
 
     return (
-        <Card type={CardType.Default} isDisabled={!isActive}>
+        <Card type={CardType.Default}>
             <CardImage type={ImageType.BgTransparent}>
                 <div className="flex h-10 w-10 items-center justify-center rounded-full border border-shader-neutral-light-8  text-neutral-10 ">
                     <CoinIcon coinType={coinType} />
@@ -45,6 +38,7 @@ export function CoinItem({
             </CardImage>
             <CardBody
                 title={isIota ? (coinMeta?.name || '').toUpperCase() : coinMeta?.name || symbol}
+                subtitle={symbol}
             />
             <CardAction
                 type={CardActionType.SupportingText}
@@ -52,50 +46,5 @@ export function CoinItem({
                 subtitle={usd?.toLocaleString('en-US')}
             />
         </Card>
-        // <div className="hover:bg-iota/10 flex w-full items-center justify-center gap-2.5 rounded py-3 pl-1.5 pr-2">
-        //     <CoinIcon coinType={coinType} size={isActive ? 'sm' : 'md'} />
-        //     <div className="flex flex-1 items-center justify-between gap-1.5">
-        //         <div className="max-w-token-width">
-        //             <Text variant="body" color="gray-90" weight="semibold" truncate>
-        //                 {coinMeta?.name || symbol} {isActive ? 'available' : ''}
-        //             </Text>
-        //             {!isActive && !subtitle ? (
-        //                 <div className="mt-1.5">
-        //                     <Text variant="subtitle" color="steel-dark" weight="medium">
-        //                         {symbol}
-        //                     </Text>
-        //                 </div>
-        //             ) : null}
-        //             {subtitle ? (
-        //                 <div className="mt-1.5">
-        //                     <Text variant="subtitle" color="steel" weight="medium">
-        //                         {subtitle}
-        //                     </Text>
-        //                 </div>
-        //             ) : null}
-        //         </div>
-
-        //         {centerAction}
-
-        //         <div className="flex flex-row items-center justify-center">
-        //             {isActive ? (
-        //                 <Text variant="body" color="steel-darker" weight="medium">
-        //                     {formatted}
-        //                 </Text>
-        //             ) : (
-        //                 <div data-testid={coinType} className="max-w-token-width">
-        //                     <Text variant="body" color="gray-90" weight="medium" truncate>
-        //                         {formatted} {symbol}
-        //                     </Text>
-        //                     {usd && (
-        //                         <Text variant="caption" color="steel-dark" weight="medium">
-        //                             ${usd.toLocaleString('en-US')}
-        //                         </Text>
-        //                     )}
-        //                 </div>
-        //             )}
-        //         </div>
-        //     </div>
-        // </div>
     );
 }
