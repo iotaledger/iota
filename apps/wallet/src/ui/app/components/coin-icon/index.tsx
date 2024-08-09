13613dad8db99c0106dd39b25d7e81e680d3136c
evavirseda <evirseda@boxfish.studio>
2024-08-09 11:04:30 +0200

    fix build

--- apps/wallet/src/ui/app/components/coin-icon/index.tsx
+++ apps/wallet/src/ui/app/components/coin-icon/index.tsx
@@ -6,7 +6,26 @@ import { ImageIcon } from '_app/shared/image-icon';
 import { useCoinMetadata } from '@iota/core';
 import { IOTA_TYPE_ARG } from '@iota/iota.js/utils';
 import { IotaLogoMark, PlaceholderReplace } from '@iota/ui-icons';
+import { cva, type VariantProps } from 'class-variance-authority';
 
+const imageStyle = cva(['flex h-full w-full items-center justify-center p-1.5'], {
+    variants: {
+        size: {
+            sm: 'w-6 h-6',
+            md: 'w-7.5 h-7.5',
+            lg: 'md:w-10 md:h-10 w-8 h-8',
+            xl: 'md:w-31.5 md:h-31.5 w-16 h-16 ',
+        },
+        fill: {
+            iota: 'bg-iota',
+            iotaPrimary2023: 'bg-iota-primaryBlue2023',
+        },
+    },
+    defaultVariants: {
+        size: 'md',
+        fill: 'iotaPrimary2023',
+    },
+});
 interface NonIotaCoinProps {
     coinType: string;
 }
@@ -31,13 +50,13 @@ function NonIotaCoin({ coinType }: NonIotaCoinProps) {
     );
 }
 
-export interface CoinIconProps {
+export interface CoinIconProps extends VariantProps<typeof imageStyle> {
     coinType: string;
 }
 
 export function CoinIcon({ coinType, ...styleProps }: CoinIconProps) {
     return (
-        <div className="flex h-full w-full items-center justify-center p-1.5">
+        <div className={imageStyle(styleProps)}>
             {coinType === IOTA_TYPE_ARG ? (
                 <IotaLogoMark className="h-full w-full" />
             ) : (
