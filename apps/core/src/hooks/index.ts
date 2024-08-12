c1ab238f28d459d64092a866ff3e0d426c482f2b
cpl121 <cpeon@boxfish.studio>
2024-08-12 14:46:31 +0200

    feat(wallet-dashboard): use new endpoint to get staked timelocked objects

--- apps/core/src/hooks/index.ts
+++ apps/core/src/hooks/index.ts
@@ -35,5 +35,6 @@ export * from './useSortedCoinsByCategories';
 export * from './useGetNFTMeta';
 export * from './useIotaAddressValidation';
 export * from './useGetAllOwnedObjects';
+export * from './useGetStakedTimelockedObjects';
 
 export * from './stake';
