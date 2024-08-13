0f170fa286e52c75961a49328271b8faaa62137f
evavirseda <evirseda@boxfish.studio>
2024-08-13 10:56:44 +0200

    feat: first steps updating top header

--- apps/wallet/src/ui/app/shared/page-main-layout/PageMainLayout.tsx
+++ apps/wallet/src/ui/app/shared/page-main-layout/PageMainLayout.tsx
@@ -7,13 +7,18 @@ import { MenuContent } from '_components/menu';
 import { Navigation } from '_components/navigation';
 import cn from 'clsx';
 import { createContext, useState, type ReactNode } from 'react';
-
-import { WalletSettingsButton } from '../../components/menu/button/WalletSettingsButton';
 import { useAppSelector } from '../../hooks';
 import { AppType } from '../../redux/slices/app/AppType';
 import DappStatus from '../dapp-status';
 import { Header } from '../header/Header';
 import { Toaster } from '../toaster';
+import { IotaLogoMark } from '@iota/ui-icons';
+import { WalletSettingsButton } from '../../components/menu/button/WalletSettingsButton';
+import { useActiveAccount } from '../../hooks/useActiveAccount';
+import { Link } from 'react-router-dom';
+import { formatAddress } from '@iota/iota-sdk/utils';
+import { useAccountSources } from '../../hooks/useAccountSources';
+import { isLedgerAccountSerializedUI } from '_src/background/accounts/LedgerAccount';
 
 export const PageMainLayoutContext = createContext<HTMLDivElement | null>(null);
 
@@ -32,25 +37,30 @@ export function PageMainLayout({
 }: PageMainLayoutProps) {
     const network = useAppSelector(({ app: { network } }) => network);
     const appType = useAppSelector((state) => state.app.appType);
+    const activeAccount = useActiveAccount();
     const isFullScreen = appType === AppType.Fullscreen;
     const [titlePortalContainer, setTitlePortalContainer] = useState<HTMLDivElement | null>(null);
-
+    const { data: accountSources } = useAccountSources();
+    const isLedgerAccount = activeAccount && isLedgerAccountSerializedUI(activeAccount);
+    console.log('accountSources', accountSources);
+    console.log('isLedeger', isLedgerAccount);
     return (
         <div
             className={cn(
-                'flex max-h-full w-full flex-1 flex-col flex-nowrap items-stretch justify-center overflow-hidden bg-gradients-graph-cards',
+                'flex max-h-full w-full flex-1 flex-col flex-nowrap items-stretch justify-center overflow-hidden',
                 isFullScreen ? 'rounded-xl' : '',
             )}
         >
             <Header
                 network={network}
+                leftContent={<LeftContent account={activeAccount?.address} />}
                 middleContent={
                     dappStatusEnabled ? <DappStatus /> : <div ref={setTitlePortalContainer} />
                 }
                 rightContent={topNavMenuEnabled ? <WalletSettingsButton /> : undefined}
             />
-            <div className="relative flex flex-grow flex-col flex-nowrap overflow-hidden rounded-t-xl shadow-wallet-content">
-                <div className="flex flex-grow flex-col flex-nowrap overflow-y-auto overflow-x-hidden rounded-t-xl bg-white">
+            <div className="relative flex flex-grow flex-col flex-nowrap overflow-hidden shadow-wallet-content">
+                <div className="flex flex-grow flex-col flex-nowrap overflow-y-auto overflow-x-hidden bg-white">
                     <main
                         className={cn('flex w-full flex-grow flex-col', {
                             'p-5': bottomNavEnabled,
@@ -60,11 +70,22 @@ export function PageMainLayout({
                             <ErrorBoundary>{children}</ErrorBoundary>
                         </PageMainLayoutContext.Provider>
                     </main>
-                    {bottomNavEnabled ? <Navigation /> : null}
                     <Toaster bottomNavEnabled={bottomNavEnabled} />
                 </div>
                 {topNavMenuEnabled ? <MenuContent /> : null}
             </div>
+            {bottomNavEnabled ? <Navigation /> : null}
+        </div>
+    );
+}
+
+function LeftContent({ account }: { account: string | undefined }) {
+    return (
+        <div className="flex flex-row items-center gap-sm">
+            <Link to="/" className="text-gray-90 no-underline">
+                <IotaLogoMark className="h-6 w-6" />
+            </Link>
+            <span className="text-title-sm text-neutral-10">{formatAddress(account || '')}</span>
         </div>
     );
 }
