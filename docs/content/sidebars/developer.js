4be27bf92aad69cc5a69076d3e58328908c176f0
Lucas Tortora <lucas.tortora@iota.org>
2024-08-20 11:18:13 -0300

    fix(devx): remove duplicate section

--- docs/content/sidebars/developer.js
+++ docs/content/sidebars/developer.js
@@ -30,21 +30,6 @@ const developer = [
     },
     {
         type: 'category',
-        label: 'From Solidity/EVM to Move',
-        collapsed: true,
-        link: {
-            type: 'doc',
-            id: 'developer/evm-to-move/evm-to-move',
-        },
-        items: [
-            'developer/evm-to-move/why-move',
-            'developer/evm-to-move/tooling-apis',
-            'developer/evm-to-move/creating-token',
-            'developer/evm-to-move/creating-nft',
-        ],
-    },
-    {
-        type: 'category',
         label: 'IOTA 101',
         link: {
             type: 'doc',
