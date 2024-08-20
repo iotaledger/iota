dadd0ea18b5427f394d40d238bddcc53d9e5d2c6
Mario Sarcevic <mario.sarcevic@iota.org>
2024-08-20 16:01:13 +0200

    feat(tooling-explorer): Move the colors in @iota/core tailwind preset to be inside extend, otherwise they are overwritten when using multiple presets (Explorer issue when adding apps-ui-kit preset)

--- apps/core/tailwind.config.ts
+++ apps/core/tailwind.config.ts
@@ -13,76 +13,6 @@ export default {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         // Overwrite colors to avoid accidental usage of Tailwind colors:
-        colors: {
-            white: colors.white,
-            black: colors.black,
-            transparent: colors.transparent,
-            inherit: colors.inherit,
-
-            gray: {
-                100: '#182435',
-                95: '#2A3645',
-                90: '#383F47',
-                85: '#5A6573',
-                80: '#636870',
-                75: '#767A81',
-                70: '#898D93',
-                65: '#9C9FA4',
-                60: '#C3C5C8',
-                55: '#D7D8DA',
-                50: '#E9EAEB',
-                45: '#E3E6E8',
-                40: '#F3F6F8',
-                35: '#FEFEFE',
-            },
-
-            iota: {
-                DEFAULT: '#6fbcf0',
-                bright: '#2A38EB',
-                light: '#E1F3FF',
-                primaryBlue2023: '#4CA3FF',
-                lightest: '#F1F8FD',
-                dark: '#1F6493',
-            },
-
-            steel: {
-                DEFAULT: '#A0B6C3',
-                dark: '#758F9E',
-                darker: '#566873',
-            },
-
-            issue: {
-                DEFAULT: '#FF794B',
-                dark: '#EB5A29',
-                light: '#FFECE6',
-            },
-            hero: {
-                DEFAULT: '#0284AD',
-                dark: '#007195',
-                darkest: '#15527B',
-            },
-            success: {
-                DEFAULT: '#2DD7A7',
-                dark: '#008C65',
-                light: '#D5F7EE',
-            },
-            warning: {
-                DEFAULT: '#F2BD24',
-                dark: '#8D6E15',
-                light: '#FFF8E2',
-            },
-            headerNav: '#2A4362',
-            search: {
-                fill: '#162A43',
-            },
-            offwhite: '#fefefe',
-            offblack: '#111111',
-            ebony: '#101828',
-            avocado: {
-                200: '#CBE5BE',
-            },
-        },
-
         extend: {
             scale: {
                 '101': '1.01',
@@ -96,6 +26,73 @@ export default {
                 xl: '32px',
             },
             colors: {
+                white: colors.white,
+                black: colors.black,
+                transparent: colors.transparent,
+                inherit: colors.inherit,
+
+                gray: {
+                    100: '#182435',
+                    95: '#2A3645',
+                    90: '#383F47',
+                    85: '#5A6573',
+                    80: '#636870',
+                    75: '#767A81',
+                    70: '#898D93',
+                    65: '#9C9FA4',
+                    60: '#C3C5C8',
+                    55: '#D7D8DA',
+                    50: '#E9EAEB',
+                    45: '#E3E6E8',
+                    40: '#F3F6F8',
+                    35: '#FEFEFE',
+                },
+
+                iota: {
+                    DEFAULT: '#6fbcf0',
+                    bright: '#2A38EB',
+                    light: '#E1F3FF',
+                    primaryBlue2023: '#4CA3FF',
+                    lightest: '#F1F8FD',
+                    dark: '#1F6493',
+                },
+
+                steel: {
+                    DEFAULT: '#A0B6C3',
+                    dark: '#758F9E',
+                    darker: '#566873',
+                },
+
+                issue: {
+                    DEFAULT: '#FF794B',
+                    dark: '#EB5A29',
+                    light: '#FFECE6',
+                },
+                hero: {
+                    DEFAULT: '#0284AD',
+                    dark: '#007195',
+                    darkest: '#15527B',
+                },
+                success: {
+                    DEFAULT: '#2DD7A7',
+                    dark: '#008C65',
+                    light: '#D5F7EE',
+                },
+                warning: {
+                    DEFAULT: '#F2BD24',
+                    dark: '#8D6E15',
+                    light: '#FFF8E2',
+                },
+                headerNav: '#2A4362',
+                search: {
+                    fill: '#162A43',
+                },
+                offwhite: '#fefefe',
+                offblack: '#111111',
+                ebony: '#101828',
+                avocado: {
+                    200: '#CBE5BE',
+                },
                 'gradient-blue-start': '#589AEA',
                 'gradient-blue-end': '#4C75A6',
                 'gradients-graph-cards-start': '#D2EBFA',
