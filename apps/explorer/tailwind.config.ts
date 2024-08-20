c87364de13f5905f123da74c32a398598fa97044
Mario Sarcevic <mario.sarcevic@iota.org>
2024-08-20 16:16:08 +0200

    feat(tooling-explorer): Configure apps-ui-kit in Explorer

--- apps/explorer/tailwind.config.ts
+++ apps/explorer/tailwind.config.ts
@@ -4,8 +4,21 @@
 
 import preset from '@iota/core/tailwind.config';
 import { type Config } from 'tailwindcss';
+import { uiKitStaticPreset } from '@iota/apps-ui-kit';
 
 export default {
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/@iota/ui/src/**/*.{js,jsx,ts,tsx}'],
-    presets: [preset],
+    presets: [preset, uiKitStaticPreset],
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}',
+        './node_modules/@iota/ui/src/**/*.{js,jsx,ts,tsx}',
+        './node_modules/@iota/apps-ui-kit/**/*.js',
+    ],
+    theme: {
+        screens: {
+            sm: '600px',
+            md: '905px',
+            lg: '1240kpx',
+            xl: '1440px',
+        },
+    },
 } satisfies Partial<Config>;
