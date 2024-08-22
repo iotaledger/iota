393f2442dd2d0e722b6a62f422d579fe4b847c1c
Thoralf Müller <thoralf.mue@gmail.com>
2024-08-22 16:13:45 +0200

    Capitalize comments

--- crates/iota-sdk/examples/pay_iota.rs
+++ crates/iota-sdk/examples/pay_iota.rs
@@ -20,11 +20,11 @@ use utils::setup_for_write;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    // 1) get the Iota client, the sender and recipient that we will use
+    // 1) Get the Iota client, the sender and recipient that we will use
     // for the transaction
     let (iota, sender, recipient) = setup_for_write().await?;
 
-    // 2) get the coin we will use as gas and for the payment
+    // 2) Get the coin we will use as gas and for the payment
     let coins = iota
         .coin_read_api()
         .get_coins(sender, None, None, None)
@@ -34,7 +34,7 @@ async fn main() -> Result<(), anyhow::Error> {
     let gas_budget = 5_000_000;
     let gas_price = iota.read_api().get_reference_gas_price().await?;
 
-    // 3) build the transaction data, to transfer 1_000 NANOS from the gas coin to
+    // 3) Build the transaction data, to transfer 1_000 NANOS from the gas coin to
     //    the recipient address
     let tx_data = TransactionData::new_pay_iota(
         sender,
@@ -46,11 +46,11 @@ async fn main() -> Result<(), anyhow::Error> {
         gas_price,
     )?;
 
-    // 4) sign transaction
+    // 4) Sign transaction
     let keystore = FileBasedKeystore::new(&iota_config_dir()?.join(IOTA_KEYSTORE_FILENAME))?;
     let signature = keystore.sign_secure(&sender, &tx_data, Intent::iota_transaction())?;
 
-    // 5) execute the transaction
+    // 5) Execute the transaction
     println!("Executing the transaction...");
     let transaction_response = iota
         .quorum_driver_api()
