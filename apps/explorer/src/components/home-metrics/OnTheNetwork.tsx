b037bfc175a46bf35b19d06a935f6db75f31bbd1
Mario Sarcevic <mario.sarcevic@iota.org>
2024-08-22 14:42:09 +0200

    feat(tooling-explorer): Rebrand OnTheNetwork.tsx

--- apps/explorer/src/components/home-metrics/OnTheNetwork.tsx
+++ apps/explorer/src/components/home-metrics/OnTheNetwork.tsx
@@ -4,11 +4,9 @@
 
 import { CoinFormat, formatBalance } from '@iota/core';
 import { useIotaClientQuery } from '@iota/dapp-kit';
-import { Heading } from '@iota/ui';
+import { Panel, Divider, LabelText, LabelTextSize, Title, TitleSize } from '@iota/apps-ui-kit';
 
-import { Card, Divider } from '~/components/ui';
 import { useGetNetworkMetrics } from '~/hooks/useGetNetworkMetrics';
-import { FormattedStatsAmount, StatsWrapper } from './FormattedStatsAmount';
 import { IOTA_DECIMALS, IOTA_TYPE_ARG } from '@iota/iota-sdk/utils';
 
 export function OnTheNetwork(): JSX.Element {
@@ -22,72 +20,73 @@ export function OnTheNetwork(): JSX.Element {
             ? formatBalance(referenceGasPrice, 0, CoinFormat.FULL)
             : null;
     const totalSupplyFormatted = totalSupply?.value
-        ? formatBalance(totalSupply.value, IOTA_DECIMALS, CoinFormat.FULL)
+        ? formatBalance(totalSupply.value, IOTA_DECIMALS, CoinFormat.ROUNDED)
         : null;
     return (
-        <Card bg="white/80" spacing="lg" height="full">
-            <div className="flex flex-col gap-4">
-                <Heading variant="heading4/semibold" color="steel-darker">
-                    Network Activity
-                </Heading>
-                <div className="flex gap-6">
-                    <FormattedStatsAmount
-                        label="TPS now"
-                        amount={
-                            networkMetrics?.currentTps
-                                ? Math.floor(networkMetrics.currentTps)
-                                : undefined
-                        }
-                        size="md"
-                    />
-                    <FormattedStatsAmount
+        <Panel>
+            <Title title="Network Activity" size={TitleSize.Small} />
+            <div className="mt-sm flex flex-col gap-6 px-md py-sm--rs">
+                <div className="flex">
+                    <div className="min-w-[164px]">
+                        <LabelText
+                            size={LabelTextSize.Large}
+                            label="TPS Now"
+                            text={
+                                networkMetrics?.currentTps
+                                    ? Math.floor(networkMetrics.currentTps).toString()
+                                    : '-'
+                            }
+                            showSupportingLabel={false}
+                        />
+                    </div>
+                    <LabelText
+                        size={LabelTextSize.Large}
                         label="Peak 30d TPS"
-                        tooltip="Peak TPS in the past 30 days excluding this epoch"
-                        amount={
+                        text={
                             networkMetrics?.tps30Days
-                                ? Math.floor(networkMetrics?.tps30Days)
-                                : undefined
+                                ? Math.floor(networkMetrics?.tps30Days).toString()
+                                : '-'
                         }
-                        size="md"
+                        showSupportingLabel={false}
                     />
                 </div>
-                <Divider color="hero/10" />
+                <Divider />
+                <div className="flex">
+                    <div className="min-w-[164px]">
+                        <LabelText
+                            size={LabelTextSize.Large}
+                            label="Total Packages"
+                            text={networkMetrics?.totalPackages ?? '-'}
+                            showSupportingLabel={false}
+                        />
+                    </div>
 
-                <StatsWrapper
-                    orientation="horizontal"
-                    label="Reference Gas Price"
-                    tooltip="The reference gas price of the current epoch"
-                    postfix={gasPriceFormatted !== null ? 'nano' : null}
-                    size="sm"
-                >
-                    {gasPriceFormatted}
-                </StatsWrapper>
-
-                <Divider color="hero/10" />
-
-                <div className="flex flex-1 flex-col gap-2">
-                    <FormattedStatsAmount
-                        orientation="horizontal"
-                        label="Total Packages"
-                        amount={networkMetrics?.totalPackages}
-                        size="sm"
-                    />
-                    <FormattedStatsAmount
-                        orientation="horizontal"
+                    <LabelText
+                        size={LabelTextSize.Large}
                         label="Objects"
-                        amount={networkMetrics?.totalObjects}
-                        size="sm"
+                        text={networkMetrics?.totalObjects ?? '-'}
+                        showSupportingLabel={false}
                     />
-                    <StatsWrapper
-                        orientation="horizontal"
+                </div>
+                <div className="flex">
+                    <div className="min-w-[164px]">
+                        <LabelText
+                            size={LabelTextSize.Large}
+                            label="Reference Gas Price"
+                            text={gasPriceFormatted ?? '-'}
+                            showSupportingLabel={gasPriceFormatted !== null}
+                            supportingLabel="IOTA"
+                        />
+                    </div>
+                    <LabelText
+                        size={LabelTextSize.Large}
                         label="Total Supply"
-                        size="sm"
-                        postfix={totalSupplyFormatted !== null ? 'IOTA' : null}
-                    >
-                        {totalSupplyFormatted}
-                    </StatsWrapper>
+                        text={totalSupplyFormatted ?? '-'}
+                        showSupportingLabel={totalSupplyFormatted !== null}
+                        supportingLabel="IOTA"
+                    />
                 </div>
             </div>
-        </Card>
+        </Panel>
     );
 }
