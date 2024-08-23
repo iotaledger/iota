4fd5d56f61859447c8962076000a8c9a24e88cc8
marc2332 <mespinsanz@gmail.com>
2024-08-23 13:35:23 +0200

    fix exmpty ts module

--- apps/explorer/src/lib/ui/interfaces/index.ts
+++ apps/explorer/src/lib/ui/interfaces/index.ts
@@ -1,2 +1,4 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
+
+export {}; // Empty export so TS doesn't complain, remove this once something else is actually exported.
