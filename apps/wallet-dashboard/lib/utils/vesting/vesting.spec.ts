f576613e85618806053ab91a0684c73a4e8744cc
Branko Bosnic <brankobosnic1@gmail.com>
2024-08-27 12:49:22 +0200

    fix: remaining vesting tests

--- apps/wallet-dashboard/lib/utils/vesting/vesting.spec.ts
+++ apps/wallet-dashboard/lib/utils/vesting/vesting.spec.ts
@@ -159,9 +159,10 @@ describe('vesting overview', () => {
             extendedTimelockedStakedObjects[extendedTimelockedStakedObjects.length - 1];
         const lastPayoutValue = Number(lastPayout.principal);
         const totalAmount =
-            SUPPLY_INCREASE_STAKER_VESTING_DURATION *
-            SUPPLY_INCREASE_VESTING_PAYOUTS_IN_1_YEAR *
-            lastPayoutValue;
+            (SUPPLY_INCREASE_STAKER_VESTING_DURATION *
+                SUPPLY_INCREASE_VESTING_PAYOUTS_IN_1_YEAR *
+                lastPayoutValue) /
+            0.9;
         const vestingOverview = getVestingOverview(extendedTimelockedStakedObjects, Date.now());
         expect(vestingOverview.totalVested).toEqual(totalAmount);
 
@@ -207,9 +208,10 @@ describe('vesting overview', () => {
 
         const lastPayout = getLastSupplyIncreaseVestingPayout(mixedObjects)!;
         const totalAmount =
-            SUPPLY_INCREASE_STAKER_VESTING_DURATION *
-            SUPPLY_INCREASE_VESTING_PAYOUTS_IN_1_YEAR *
-            lastPayout.amount;
+            (SUPPLY_INCREASE_STAKER_VESTING_DURATION *
+                SUPPLY_INCREASE_VESTING_PAYOUTS_IN_1_YEAR *
+                lastPayout.amount) /
+            0.9;
 
         const vestingOverview = getVestingOverview(mixedObjects, Date.now());
         expect(vestingOverview.totalVested).toEqual(totalAmount);
