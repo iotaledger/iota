307a5544efed34dad5dd814cb936dccc2d3fa9ba
Thoralf Müller <thoralf.mue@gmail.com>
2024-08-29 10:57:14 +0200

    Add more explanation text

--- crates/iota-sdk/examples/checkpoints.rs
+++ crates/iota-sdk/examples/checkpoints.rs
@@ -21,14 +21,17 @@ async fn main() -> Result<(), anyhow::Error> {
         .read_api()
         .get_checkpoint(latest_checkpoint_number.into())
         .await?;
-    println!("{}", serde_json::to_string_pretty(&checkpoint)?);
+    println!(
+        "Latest checkpoint:\n{}",
+        serde_json::to_string_pretty(&checkpoint)?
+    );
 
+    // Get the 2 checkpoints before the latest one
     let checkpoints = client
         .read_api()
         .get_checkpoints(Some((latest_checkpoint_number - 3).into()), Some(2), false)
         .await?;
-    println!("{:?}", checkpoints);
-    println!("{}", checkpoints.data.len());
+    println!("Second and third last checkpoint:\n{:?}", checkpoints);
 
     Ok(())
 }
