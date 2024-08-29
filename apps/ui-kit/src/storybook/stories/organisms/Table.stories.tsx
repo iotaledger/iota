26dc24623a9b6b273296b87e62d330696d09db9e
marc2332 <mespinsanz@gmail.com>
2024-08-29 10:46:32 +0200

    chore: lint

--- apps/ui-kit/src/storybook/stories/organisms/Table.stories.tsx
+++ apps/ui-kit/src/storybook/stories/organisms/Table.stories.tsx
@@ -106,8 +106,8 @@ export const Default: Story = {
     args: {
         supportingLabel: '10.7k records',
         paginationOptions: {
-            onFirstPageClick: () => console.log("First"),
-            onNextPageClick: () => console.log("Next")
+            onFirstPageClick: () => console.log('First'),
+            onNextPageClick: () => console.log('Next')
         },
         actionLabel: 'Action',
         hasCheckboxColumn: true,
