1cb3fbf2047c766ffb393be865e91a6871aa69b7
marc2332 <mespinsanz@gmail.com>
2024-09-05 08:56:17 +0200

    chore: Remove unnecessary import

--- apps/explorer/src/components/ui/PlaceholderTable.tsx
+++ apps/explorer/src/components/ui/PlaceholderTable.tsx
@@ -2,7 +2,6 @@
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { Placeholder } from '@iota/ui';
 import { useMemo } from 'react';
 
 import { TableCard } from './TableCard';
