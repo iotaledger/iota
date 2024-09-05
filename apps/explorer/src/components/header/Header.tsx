0f0bf763df4297be9f4f7a09086217570c125c88
Mario Sarcevic <mario.sarcevic@iota.org>
2024-09-05 15:36:41 +0200

    fix(tooling-explorer): Align navbar css more with figma (remove shadows and background blur etc)

--- apps/explorer/src/components/header/Header.tsx
+++ apps/explorer/src/components/header/Header.tsx
@@ -3,32 +3,14 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { IotaLogoWeb } from '@iota/ui-icons';
-import clsx from 'clsx';
-import { useEffect, useState } from 'react';
 
 import { NetworkSelector } from '../network';
 import Search from '../search/Search';
 import { LinkWithQuery } from '~/components/ui';
 
 function Header(): JSX.Element {
-    const [isScrolled, setIsScrolled] = useState(window.scrollY > 0);
-    useEffect(() => {
-        const callback = () => {
-            setIsScrolled(window.scrollY > 0);
-        };
-        document.addEventListener('scroll', callback, { passive: true });
-        return () => {
-            document.removeEventListener('scroll', callback);
-        };
-    }, []);
-
     return (
-        <header
-            className={clsx(
-                'flex h-header justify-center overflow-visible backdrop-blur-xl transition-shadow',
-                isScrolled && 'shadow-effect-ui-regular',
-            )}
-        >
+        <header className="flex h-header justify-center overflow-visible bg-white">
             <div className="2xl:p-0 flex h-full max-w-[1440px] flex-1 items-center gap-5 px-4 py-8 lg:px-6 xl:px-10">
                 <LinkWithQuery
                     data-testid="nav-logo-button"
