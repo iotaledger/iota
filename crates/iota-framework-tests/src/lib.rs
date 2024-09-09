778abeda1cf317d8d99707808f69a0c6cc0885d0
Alexander Sporn <github@alexsporn.de>
2024-09-09 15:33:40 +0200

    mainnet-v1.32.2 crates/iota-framework-tests

--- crates/iota-framework-tests/src/lib.rs
+++ crates/iota-framework-tests/src/lib.rs
@@ -1,8 +1,6 @@
 // Copyright (c) Mysten Labs, Inc.
+// Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
-
-#[cfg(test)]
-mod unit_tests;
 
 #[cfg(test)]
 mod metered_verifier;
