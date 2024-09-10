010131353c2815e71dbf8b1ef9f4a913005e2fdd
JCNoguera <jcnoguera@boxfish.studio>
2024-09-10 11:38:58 +0200

    refactor: cleanup interfaces

--- apps/explorer/src/pages/epochs/stats/TokenStats.tsx
+++ apps/explorer/src/pages/epochs/stats/TokenStats.tsx
@@ -5,15 +5,20 @@ import { LabelText, LabelTextSize } from '@iota/apps-ui-kit';
 import { CoinFormat, useFormatCoin } from '@iota/core';
 import { IOTA_TYPE_ARG } from '@iota/iota-sdk/utils';
 
+type LabelTextProps = Omit<React.ComponentProps<typeof LabelText>, 'text' | 'size'>;
+
+interface TokenStatsProps extends LabelTextProps {
+    amount: bigint | number | string | undefined | null;
+    showSign?: boolean;
+    size?: LabelTextSize;
+}
+
 export function TokenStats({
     amount,
     showSign,
     size = LabelTextSize.Large,
     ...props
-}: Omit<React.ComponentProps<typeof LabelText>, 'text'> & {
-    amount: bigint | number | string | undefined | null;
-    showSign?: boolean;
-}): JSX.Element {
+}: TokenStatsProps): React.JSX.Element {
     const [formattedAmount, symbol] = useFormatCoin(
         amount,
         IOTA_TYPE_ARG,
