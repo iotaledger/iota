7a488b723b2b5e42b780d20cf91ca03542c8094a
marc2332 <mespinsanz@gmail.com>
2024-09-16 10:49:21 +0200

    chore: Remove interfaces folder

--- apps/explorer/src/lib/ui/interfaces/index.ts
+++
@@ -1,4 +0,0 @@
-// Copyright (c) 2024 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-export {}; // Empty export so TS doesn't complain, remove this once something else is actually exported.
