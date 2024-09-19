ed93eef856769c1d379e95fc8ee988f56dd1dcdc
Dkwcs <pavlo.botnar@gmail.com>
2024-09-19 15:32:51 +0300

    refactor(iota-genesis-builder): reduce genesis.blob size from 6gb to 366KB by excluding migrated objects

--- crates/iota-genesis-builder/src/lib.rs
+++ crates/iota-genesis-builder/src/lib.rs
@@ -52,8 +52,7 @@ use iota_types::{
     programmable_transaction_builder::ProgrammableTransactionBuilder,
     stardust::stardust_to_iota_address,
     timelock::{
-        stardust_upgrade_label::STARDUST_UPGRADE_LABEL_VALUE,
-        timelocked_staked_iota::TimelockedStakedIota,
+        stardust_upgrade_label::STARDUST_UPGRADE_LABEL_VALUE, timelock::is_timelock, timelocked_staked_iota::TimelockedStakedIota
     },
     transaction::{
         CallArg, CheckedInputObjects, Command, InputObjectKind, ObjectArg, ObjectReadResult,
@@ -316,15 +315,19 @@ impl Builder {
                 .into_iter()
                 .map(|(id, _, _)| id),
         );
-        let mut objects = self.migration_objects.take_objects();
-        objects.extend(self.objects.values().cloned());
+        let mut timelock_objects = self.migration_objects.take_timelock_objects(self.genesis_stake
+            .take_timelocks_to_burn()
+            .into_iter()
+            .map(|(id, _, _)| id),);
+
+        timelock_objects.extend(self.objects.values().cloned());
 
         // Finally build the genesis data
         self.built_genesis = Some(build_unsigned_genesis_data(
             &self.parameters,
             &token_distribution_schedule,
             self.validators.values(),
-            objects,
+            timelock_objects,
             &mut self.genesis_stake,
         ));
 
