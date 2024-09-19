72de693da6fe1b249b135ec83910045e7b60f9af
marc2332 <mespinsanz@gmail.com>
2024-09-19 13:57:09 +0200

    chore: Add truncate import

--- apps/explorer/src/pages/transaction-result/transaction-summary/TransactionDetails.tsx
+++ apps/explorer/src/pages/transaction-result/transaction-summary/TransactionDetails.tsx
@@ -2,7 +2,7 @@
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { DisplayStats } from '@iota/apps-ui-kit';
+import { DisplayStats, truncate } from '@iota/apps-ui-kit';
 import { formatDate } from '@iota/core';
 import { AddressLink, CheckpointSequenceLink, EpochLink } from '~/components';
 
