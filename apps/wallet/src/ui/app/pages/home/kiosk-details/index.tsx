1eb9532c213248dd5941133637e753a5bd2cea4c
cpl121 <cpeon@boxfish.studio>
2024-09-25 12:43:00 +0200

    feat(wallet): rebrand kiosk details page

--- apps/wallet/src/ui/app/pages/home/kiosk-details/index.tsx
+++ apps/wallet/src/ui/app/pages/home/kiosk-details/index.tsx
@@ -7,19 +7,20 @@ import {
     ErrorBoundary,
     ExplorerLink,
     ExplorerLinkType,
-    LabelValueItem,
-    LabelValuesContainer,
     Loading,
     NFTDisplayCard,
+    PageTemplate,
 } from '_components';
 import { useUnlockedGuard } from '_src/ui/app/hooks/useUnlockedGuard';
 import { Collapsible } from '_src/ui/app/shared/collapse';
-import PageTitle from '_src/ui/app/shared/PageTitle';
 import { useGetKioskContents } from '@iota/core';
 import { formatAddress } from '@iota/iota-sdk/utils';
-import { Link, useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams, useNavigate } from 'react-router-dom';
+import cl from 'clsx';
+import { KeyValueInfo } from '@iota/apps-ui-kit';
 
 function KioskDetailsPage() {
+    const navigate = useNavigate();
     const [searchParams] = useSearchParams();
     const kioskId = searchParams.get('kioskId');
     const accountAddress = useActiveAddress();
@@ -32,56 +33,66 @@ function KioskDetailsPage() {
     }
 
     return (
-        <div className="flex flex-1 flex-col flex-nowrap gap-3.75">
-            <PageTitle title="Kiosk" back />
-            <Loading loading={isPending}>
-                {!items?.length ? (
-                    <div className="text-steel-darker flex flex-1 items-center self-center text-caption font-semibold">
-                        Kiosk is empty
-                    </div>
-                ) : (
-                    <>
-                        <div className="mb-auto grid grid-cols-3 items-center justify-center gap-3">
-                            {items.map((item) =>
-                                item.data?.objectId ? (
-                                    <Link
-                                        to={`/nft-details?${new URLSearchParams({
-                                            objectId: item.data.objectId,
-                                        }).toString()}`}
-                                        key={item.data?.objectId}
-                                        className="no-underline"
-                                    >
-                                        <ErrorBoundary>
-                                            <NFTDisplayCard
-                                                objectId={item.data.objectId}
-                                                isHoverable
-                                            />
-                                        </ErrorBoundary>
-                                    </Link>
-                                ) : null,
-                            )}
+        <PageTemplate title="Kiosk" isTitleCentered onClose={() => navigate(-1)}>
+            <div
+                className={cl('flex h-full flex-1 flex-col flex-nowrap gap-5', {
+                    'items-center': isPending,
+                })}
+            >
+                <Loading loading={isPending}>
+                    {!items?.length ? (
+                        <div className="text-steel-darker flex flex-1 items-center self-center text-caption font-semibold">
+                            Kiosk is empty
                         </div>
-                    </>
-                )}
-                <Collapsible defaultOpen title="Details">
-                    <LabelValuesContainer>
-                        <LabelValueItem label="Number of Items" value={items?.length || '0'} />
-                        <LabelValueItem
-                            label="Kiosk ID"
-                            value={
-                                <ExplorerLink
-                                    className="text-hero-dark font-mono no-underline"
-                                    objectID={kioskId!}
-                                    type={ExplorerLinkType.Object}
-                                >
-                                    {formatAddress(kioskId!)}
-                                </ExplorerLink>
-                            }
-                        />
-                    </LabelValuesContainer>
-                </Collapsible>
-            </Loading>
-        </div>
+                    ) : (
+                        <>
+                            <div className="mb-auto grid grid-cols-3 items-center justify-center gap-3">
+                                {items.map((item) =>
+                                    item.data?.objectId ? (
+                                        <Link
+                                            to={`/nft-details?${new URLSearchParams({
+                                                objectId: item.data.objectId,
+                                            }).toString()}`}
+                                            key={item.data?.objectId}
+                                            className="no-underline"
+                                        >
+                                            <ErrorBoundary>
+                                                <NFTDisplayCard
+                                                    objectId={item.data.objectId}
+                                                    isHoverable
+                                                />
+                                            </ErrorBoundary>
+                                        </Link>
+                                    ) : null,
+                                )}
+                            </div>
+                        </>
+                    )}
+                    <Collapsible defaultOpen title="Details">
+                        <div className="flex flex-col gap-y-sm px-md">
+                            <KeyValueInfo
+                                keyText="Number of Items"
+                                value={items?.length || '0'}
+                                fullwidth
+                            />
+                            <KeyValueInfo
+                                keyText="Kiosk ID"
+                                value={
+                                    <ExplorerLink
+                                        className="text-hero-dark font-mono no-underline"
+                                        objectID={kioskId!}
+                                        type={ExplorerLinkType.Object}
+                                    >
+                                        {formatAddress(kioskId!)}
+                                    </ExplorerLink>
+                                }
+                                fullwidth
+                            />
+                        </div>
+                    </Collapsible>
+                </Loading>
+            </div>
+        </PageTemplate>
     );
 }
 
