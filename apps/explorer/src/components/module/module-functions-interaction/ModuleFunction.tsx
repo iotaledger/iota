de8660b5c1e04ff004ce84b15e94bd20e6afa3c7
Mario Sarcevic <mario.sarcevic@iota.org>
2024-09-30 16:40:12 +0200

    fix(tooling-explorer): Fix ModuleFunction.tsx ask SDK 1.0 changes

--- apps/explorer/src/components/module/module-functions-interaction/ModuleFunction.tsx
+++ apps/explorer/src/components/module/module-functions-interaction/ModuleFunction.tsx
@@ -3,8 +3,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { useZodForm } from '@iota/core';
-import { ConnectModal, useCurrentAccount, useSignAndExecuteTransaction } from '@iota/dapp-kit';
-import { Transaction, getPureSerializationType } from '@iota/iota-sdk/transactions';
+import {
+    ConnectModal,
+    useCurrentAccount,
+    useIotaClient,
+    useSignAndExecuteTransaction,
+} from '@iota/dapp-kit';
+import { Transaction } from '@iota/iota-sdk/transactions';
 import { useMutation } from '@tanstack/react-query';
 import { useMemo, useState } from 'react';
 import { useWatch } from 'react-hook-form';
@@ -28,6 +33,11 @@ import {
     TitleSize,
 } from '@iota/apps-ui-kit';
 
+import {
+    getPureBcsSchema,
+    normalizedTypeToMoveTypeSignature,
+} from '@iota/iota-sdk/src/transactions/serializer';
+
 const argsSchema = z.object({
     params: z.optional(z.array(z.string().trim().min(1))),
     types: z.optional(z.array(z.string().trim().min(1))),
@@ -49,7 +59,21 @@ export function ModuleFunction({
     functionDetails,
 }: ModuleFunctionProps): JSX.Element {
     const currentAccount = useCurrentAccount();
-    const { mutateAsync: signAndExecuteTransactionBlock } = useSignAndExecuteTransaction();
+    const iotaClient = useIotaClient();
+    const { mutateAsync: signAndExecuteTransaction } = useSignAndExecuteTransaction({
+        execute: async ({ bytes, signature }) =>
+            await iotaClient.executeTransactionBlock({
+                transactionBlock: bytes,
+                signature,
+                options: {
+                    showRawEffects: true,
+                    showEffects: true,
+                    showEvents: true,
+                    showInput: true,
+                },
+            }),
+    });
+
     const { handleSubmit, formState, register, control } = useZodForm({
         schema: argsSchema,
     });
@@ -73,23 +97,23 @@ export function ModuleFunction({
                 target: `${packageId}::${moduleName}::${functionName}`,
                 typeArguments: types ?? [],
                 arguments:
-                    params?.map((param, i) =>
-                        getPureSerializationType(functionDetails.parameters[i], param)
-                            ? tx.pure(param)
-                            : tx.object(param),
-                    ) ?? [],
+                    params?.map((param, i) => {
+                        const moveTypeSignature = normalizedTypeToMoveTypeSignature(
+                            functionDetails.parameters[i],
+                        );
+
+                        const pureBcsSchema = getPureBcsSchema(moveTypeSignature.body);
+
+                        return pureBcsSchema ? pureBcsSchema.serialize(param) : tx.object(param);
+                    }) ?? [],
             });
-            const result = await signAndExecuteTransactionBlock({
-                transaction: tx,
-                options: {
-                    showEffects: true,
-                    showEvents: true,
-                    showInput: true,
-                },
-            });
+
+            const result = await signAndExecuteTransaction({ transaction: tx });
+
             if (result.effects?.status.status === 'failure') {
                 throw new Error(result.effects.status.error || 'Transaction failed');
             }
+
             return result;
         },
     });
