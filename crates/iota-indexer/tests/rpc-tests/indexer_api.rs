8ad5f8e300ff2afcd036c5d824f3d79c4480b423
Sergiu Popescu <sergiu.popescu@iota.org>
2024-09-30 13:35:29 +0200

    fix(iota-indexer): improve tests

--- crates/iota-indexer/tests/rpc-tests/indexer_api.rs
+++ crates/iota-indexer/tests/rpc-tests/indexer_api.rs
@@ -76,77 +76,75 @@ async fn query_events_unsupported_events() {
     // Get the current time in milliseconds since the UNIX epoch
     let now_millis = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
-        .expect("Time went backwards")
+        .unwrap()
         .as_millis();
 
     // Subtract 10 minutes from the current time
     let ten_minutes_ago = now_millis - (10 * 60 * 1000); // 600 seconds = 10 minutes
 
     let unsupported_filters = vec![
-        (EventFilter::All(vec![]), true),
-        (EventFilter::Any(vec![]), true),
-        (
-            EventFilter::And(
-                Box::new(EventFilter::Any(vec![])),
-                Box::new(EventFilter::Any(vec![])),
-            ),
-            true,
+        EventFilter::All(vec![]),
+        EventFilter::Any(vec![]),
+        EventFilter::And(
+            Box::new(EventFilter::Any(vec![])),
+            Box::new(EventFilter::Any(vec![])),
         ),
-        (
-            EventFilter::Or(
-                Box::new(EventFilter::Any(vec![])),
-                Box::new(EventFilter::Any(vec![])),
-            ),
-            true,
+        EventFilter::Or(
+            Box::new(EventFilter::Any(vec![])),
+            Box::new(EventFilter::Any(vec![])),
         ),
-        (
-            EventFilter::TimeRange {
-                start_time: ten_minutes_ago as u64,
-                end_time: now_millis as u64,
-            },
-            true,
-        ),
-        (
-            EventFilter::MoveEventField {
-                path: String::default(),
-                value: serde_json::Value::Bool(true),
-            },
-            true,
-        ),
-        (EventFilter::Sender(IotaAddress::ZERO), false),
-        (EventFilter::Transaction(TransactionDigest::ZERO), false),
-        (EventFilter::Package(ObjectID::ZERO), false),
-        (
-            EventFilter::MoveEventModule {
-                package: ObjectID::ZERO,
-                module: "x".parse().unwrap(),
-            },
-            false,
-        ),
-        (
-            EventFilter::MoveEventType("0xabcd::MyModule::Foo".parse().unwrap()),
-            false,
-        ),
-        (
-            EventFilter::MoveModule {
-                package: ObjectID::ZERO,
-                module: "x".parse().unwrap(),
-            },
-            false,
-        ),
+        EventFilter::TimeRange {
+            start_time: ten_minutes_ago as u64,
+            end_time: now_millis as u64,
+        },
+        EventFilter::MoveEventField {
+            path: String::default(),
+            value: serde_json::Value::Bool(true),
+        },
     ];
 
-    for (event_filter, expected) in unsupported_filters {
+    for event_filter in unsupported_filters {
         let result = indexer_client
             .query_events(event_filter, None, None, None)
             .await;
 
-        assert_eq!(
-            expected,
-            rpc_call_error_msg_matches(
-                result,
-                r#"{"code":-32603,"message": "Indexer does not support the feature with error: `This type of EventFilter is not supported.`"}"#,
-            )
-        );
+        assert!(rpc_call_error_msg_matches(
+            result,
+            r#"{"code":-32603,"message": "Indexer does not support the feature with error: `This type of EventFilter is not supported.`"}"#,
+        ));
+    }
+}
+
+#[tokio::test]
+#[serial]
+async fn query_events_supported_events() {
+    let (_cluster, pg_store, indexer_client) =
+        start_test_cluster_with_read_write_indexer(None).await;
+    indexer_wait_for_checkpoint(&pg_store, 1).await;
+
+    let supported_filters = vec![
+        EventFilter::Sender(IotaAddress::ZERO),
+        EventFilter::Transaction(TransactionDigest::ZERO),
+        EventFilter::Package(ObjectID::ZERO),
+        EventFilter::MoveEventModule {
+            package: ObjectID::ZERO,
+            module: "x".parse().unwrap(),
+        },
+        EventFilter::MoveEventType("0xabcd::MyModule::Foo".parse().unwrap()),
+        EventFilter::MoveModule {
+            package: ObjectID::ZERO,
+            module: "x".parse().unwrap(),
+        },
+    ];
+
+    for event_filter in supported_filters {
+        let result = indexer_client
+            .query_events(event_filter, None, None, None)
+            .await;
+
+        assert!(!rpc_call_error_msg_matches(
+            result,
+            r#"{"code":-32603,"message": "Indexer does not support the feature with error: `This type of EventFilter is not supported.`"}"#,
+        ));
     }
 }
