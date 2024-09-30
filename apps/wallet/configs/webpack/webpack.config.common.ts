5882d9263f62d2bd8a99cea30d54696696ce7973
cpl121 <cpeon@boxfish.studio>
2024-09-30 16:05:17 +0200

    fix(wallet): modify env naming

--- apps/wallet/configs/webpack/webpack.config.common.ts
+++ apps/wallet/configs/webpack/webpack.config.common.ts
@@ -17,7 +17,7 @@ import type { Configuration } from 'webpack';
 import packageJson from '../../package.json';
 
 const WALLET_RC = process.env.WALLET_RC === 'true';
-const RC_VERSION = WALLET_RC ? Number(process.env.RC_VERSION) : undefined;
+const WALLET_RC_VERSION = WALLET_RC ? Number(process.env.WALLET_RC_VERSION) : undefined;
 
 const SDK_ROOT = resolve(__dirname, '..', '..', '..', '..', 'sdk');
 const PROJECT_ROOT = resolve(__dirname, '..', '..');
@@ -97,11 +97,11 @@ async function generateAliasFromTs() {
 
 const commonConfig: () => Promise<Configuration> = async () => {
     const alias = await generateAliasFromTs();
-    console.log("RC_VERSION", RC_VERSION);
+    console.log("WALLET_RC_VERSION", WALLET_RC_VERSION);
     console.log("process.env", process.env);
-    console.log(process.env.RC_VERSION, process.env.WALLET_RC)
+    console.log(process.env.WALLET_RC_VERSION, process.env.WALLET_RC)
 
-    const walletVersionDetails = generateVersion(RC_VERSION);
+    const walletVersionDetails = generateVersion(WALLET_RC_VERSION);
     console.log("walletVersionDetails", walletVersionDetails);
     
     const sentryAuthToken = process.env.SENTRY_AUTH_TOKEN;
