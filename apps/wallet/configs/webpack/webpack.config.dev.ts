c9ffb9c4903ad1c983d7cfdb8051176b2dbf7514
cpl121 <cpeon@boxfish.studio>
2024-10-01 08:28:43 +0200

    fix(wallet): test

--- apps/wallet/configs/webpack/webpack.config.dev.ts
+++ apps/wallet/configs/webpack/webpack.config.dev.ts
@@ -3,15 +3,20 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import ESLintPlugin from 'eslint-webpack-plugin';
-import type { Configuration } from 'webpack';
+import { type Configuration, DefinePlugin } from 'webpack';
 import { merge } from 'webpack-merge';
 
 import configCommon from './webpack.config.common';
 
+console.log("process.env.WALLET_RC_VERSION config dev", process.env.WALLET_RC_VERSION);
+
+
 const configDev: Configuration = {
     mode: 'development',
     devtool: 'cheap-source-map',
-    plugins: [new ESLintPlugin({ extensions: ['ts', 'tsx', 'js', 'jsx'] })],
+    plugins: [new ESLintPlugin({ extensions: ['ts', 'tsx', 'js', 'jsx'] }), new DefinePlugin({
+        'process.env.WALLET_RC_VERSION': JSON.stringify(process.env.WALLET_RC_VERSION)
+      })],
     watchOptions: {
         aggregateTimeout: 600,
     },
