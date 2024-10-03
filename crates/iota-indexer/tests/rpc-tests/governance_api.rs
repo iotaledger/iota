b9cc1727c9e0976c796b771e5d00c03083b2f26a
samuel_rufi <samuel.rufinatscha@iota.org>
2024-10-03 14:27:30 +0200

    feat: Implement simple test cases

--- crates/iota-indexer/tests/rpc-tests/governance_api.rs
+++ crates/iota-indexer/tests/rpc-tests/governance_api.rs
@@ -1,44 +1,69 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-
-use crate::common::{ApiTestSetup, indexer_wait_for_checkpoint, rpc_call_error_msg_matches};
 use iota_json_rpc_api::GovernanceReadApiClient;
+use iota_json_rpc_types::DelegatedStake;
+use iota_types::iota_system_state::iota_system_state_summary::IotaSystemStateSummary;
 
-/**
+use crate::common::{indexer_wait_for_checkpoint, rpc_call_error_msg_matches, ApiTestSetup};
+
 #[test]
-fn get_validators_apy() {
-    let mut genesis_config = GenesisConfig::for_local_testing();
+fn get_stakes_by_id() {
+    let ApiTestSetup {
+        runtime,
+        store,
+        client,
+        cluster
+    } = ApiTestSetup::get_or_init();
 
-    let validator_config = genesis_config.validator_config_info.as_mut().unwrap();
+    runtime.block_on(async move {
+        indexer_wait_for_checkpoint(store, 1).await;
 
-    // Add a different commission rate to each validator
-    for (i, validator) in validator_config.iter_mut().enumerate() {
-        validator.commission_rate = i as u64;
-    }
+        let validator_address = cluster
+            .swarm
+            .active_validators()
+            .next()
+            .unwrap()
+            .config
+            .iota_address();
 
-    let test_cluster = TestClusterBuilder::new()
-        .set_genesis_config(genesis_config)
-        .with_epoch_duration_ms(10_000)
-        .with_num_validators(4)
-        .build()
-        .await;
+        let response: Vec<DelegatedStake> = client.get_stakes(validator_address).await.unwrap();
+        let stake_id = response.first().unwrap().stakes.first().unwrap().staked_iota_id;
 
-    let (pg_store, indexer_client) = start_read_write_indexer_with_existing_test_cluster(&test_cluster).await;
+        let response = client.get_stakes_by_ids(vec![stake_id]).await.unwrap();
 
-    indexer_wait_for_checkpoint(&pg_store, 1).await;
-
-    let response = indexer_client.get_validators_apy().await.unwrap();
-    let (apys, epoch) = (response.apys, response.epoch);
-
-    assert_eq!(epoch, 0);
-    assert_eq!(apys.len(), 4);
-    assert_eq!(apys.iter().find(|apy| apy.apy == 0.0).is_some(), true)
+        assert_eq!(response.len(), 1);
+    });
 }
-**/
 
 #[test]
-fn get_reference_gas_price() {
+fn get_stakes() {
+    let ApiTestSetup {
+        runtime,
+        store,
+        client,
+        cluster,
+    } = ApiTestSetup::get_or_init();
+
+    runtime.block_on(async move {
+        indexer_wait_for_checkpoint(store, 1).await;
+
+        let validator_address = cluster
+            .swarm
+            .active_validators()
+            .next()
+            .unwrap()
+            .config
+            .iota_address();
+
+        let response = client.get_stakes(validator_address).await.unwrap();
+
+        assert_eq!(response.len(), 1);
+    });
+}
+
+#[test]
+fn get_timelocked_stakes_by_id() {
     let ApiTestSetup {
         runtime,
         store,
@@ -49,32 +74,39 @@ fn get_reference_gas_price() {
     runtime.block_on(async move {
         indexer_wait_for_checkpoint(store, 1).await;
 
-        let response = client
-            .get_reference_gas_price()
-            .await
-            .unwrap();
-    });
+        let response = client.get_timelocked_stakes_by_ids(vec![]).await.unwrap();
 
+        assert_eq!(response.len(), 0);
+    });
 }
 
 #[test]
-fn get_latest_iota_system_state() {
+fn get_timelocked_stakes() {
     let ApiTestSetup {
         runtime,
         store,
         client,
-        ..
+        cluster,
     } = ApiTestSetup::get_or_init();
 
     runtime.block_on(async move {
         indexer_wait_for_checkpoint(store, 1).await;
 
+        let validator_address = cluster
+            .swarm
+            .active_validators()
+            .next()
+            .unwrap()
+            .config
+            .iota_address();
+
         let response = client
-            .get_latest_iota_system_state()
+            .get_timelocked_stakes(validator_address)
             .await
             .unwrap();
-    });
 
+        assert_eq!(response.len(), 0);
+    });
 }
 
 #[test]
@@ -98,10 +130,7 @@ fn get_committee_info() {
         assert_eq!(validators.len(), 4);
 
         // Test with specified epoch 0
-        let indexer_response = client
-            .get_committee_info(Some(0.into()))
-            .await
-            .unwrap();
+        let indexer_response = client.get_committee_info(Some(0.into())).await.unwrap();
 
         let (epoch_id, validators) = (indexer_response.epoch, indexer_response.validators);
 
@@ -116,35 +145,59 @@ fn get_committee_info() {
             r#"{"code":-32603,"message":"Invalid argument with error: `Missing epoch Some(1)`"}"#,
         );
     });
-
 }
 
 #[test]
-fn get_timelocked_stakes() {
+fn get_reference_gas_price() {
     let ApiTestSetup {
         runtime,
         store,
         client,
-        cluster
+        ..
     } = ApiTestSetup::get_or_init();
 
     runtime.block_on(async move {
         indexer_wait_for_checkpoint(store, 1).await;
 
-        let validator_address = cluster
-            .swarm
-            .active_validators()
-            .next()
-            .unwrap()
-            .config
-            .iota_address();
-
-        let response = client
-            .get_timelocked_stakes(validator_address)
-            .await
-            .unwrap();
-
-        assert_eq!(response.len(), 0);
+        let response = client.get_reference_gas_price().await.unwrap();
+        println!("{:?}", response)
     });
+}
 
+#[test]
+fn get_latest_iota_system_state() {
+    let ApiTestSetup {
+        runtime,
+        store,
+        client,
+        ..
+    } = ApiTestSetup::get_or_init();
+
+    runtime.block_on(async move {
+        indexer_wait_for_checkpoint(store, 1).await;
+
+        let response: IotaSystemStateSummary = client.get_latest_iota_system_state().await.unwrap();
+        println!("{:?}", response)
+    });
+}
+
+#[test]
+fn get_validators_apy() {
+    let ApiTestSetup {
+        runtime,
+        store,
+        client,
+        ..
+    } = ApiTestSetup::get_or_init();
+
+    runtime.block_on(async move {
+        indexer_wait_for_checkpoint(store, 1).await;
+
+        let response = client.get_validators_apy().await.unwrap();
+        let (apys, epoch) = (response.apys, response.epoch);
+
+        assert_eq!(epoch, 0);
+        assert_eq!(apys.len(), 4);
+        assert_eq!(apys.iter().find(|apy| apy.apy == 0.0).is_some(), true);
+    });
 }
