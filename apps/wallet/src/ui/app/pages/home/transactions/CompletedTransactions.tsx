d9470188580c7572d2bfe4efb40c86df7986a375
evavirseda <evirseda@boxfish.studio>
2024-10-03 13:54:08 +0200

    fix check

--- apps/wallet/src/ui/app/pages/home/transactions/CompletedTransactions.tsx
+++ apps/wallet/src/ui/app/pages/home/transactions/CompletedTransactions.tsx
@@ -21,7 +21,7 @@ export function CompletedTransactions() {
         return (
             <div className="mb-2 flex h-full w-full items-center justify-center p-2">
                 <Alert
-                    title="Something went worng"
+                    title="Something went wrong"
                     supportingText={error?.message ?? 'An error occurred'}
                     style={AlertStyle.Default}
                     type={AlertType.Warning}
