cce28ca4ad71d707abfceb018911f42852692b90
Vivek Jain <vkj.vivekkumarjain@gmail.com>
2024-10-09 16:17:01 +0530

    Update publish script

--- examples/move/reviews_rating/publish.sh
+++ examples/move/reviews_rating/publish.sh
@@ -8,7 +8,7 @@ else
   exit 1
 fi
 
-# Check if dependencies are available.
+# Check if dependencies are available
 for i in jq curl iota; do
   if ! command -V ${i} 2>/dev/null; then
     echo "${i} is not installed"
@@ -16,22 +16,17 @@ for i in jq curl iota; do
   fi
 done
 
-# Use Alphanet settings
-NETWORK="https://api.iota-rebased-alphanet.iota.cafe:443"
-FAUCET="https://faucet.iota-rebased-alphanet.iota.cafe/gas"
-MOVE_PACKAGE_PATH="."  # Since the package is in the current directory
-
 echo "- Admin Address is: ${ADMIN_ADDRESS}"
 
-# Import address and switch to admin account
+# Import address and switch to admin account using variables from .env
 import_address=$(iota keytool import "$ADMIN_PHRASE" ed25519)
 
 switch_res=$(iota client switch --address ${ADMIN_ADDRESS})
 
-# Commented out Faucet request since it's optional
-# faucet_res=$(curl --location --request POST "$FAUCET" --header 'Content-Type: application/json' --data-raw '{"FixedAmountRequest": { "recipient": '$ADMIN_ADDRESS'}}')
+# Optional: Request tokens from the faucet
+# faucet_res=$(curl --location --request POST "$IOTA_FAUCET" --header 'Content-Type: application/json' --data-raw '{"FixedAmountRequest": { "recipient": '$ADMIN_ADDRESS'}}')
 
-# Publish the Move package
+# Publish the Move package using values from .env
 publish_res=$(iota client publish --skip-fetch-latest-git-deps --gas-budget 2000000000 --json ${MOVE_PACKAGE_PATH} --skip-dependency-verification)
 
 echo ${publish_res} >.publish.res.json
@@ -46,9 +41,11 @@ fi
 publishedObjs=$(echo "$publish_res" | jq -r '.objectChanges[] | select(.type == "published")')
 PACKAGE_ID=$(echo "$publishedObjs" | jq -r '.packageId')
 
-# Generate environment files
+# Update the .env file with the new Package ID
 cat >.env<<-API_ENV
-IOTA_NETWORK=$NETWORK
+IOTA_NETWORK=$IOTA_NETWORK
+IOTA_FAUCET=$IOTA_FAUCET
+MOVE_PACKAGE_PATH=$MOVE_PACKAGE_PATH
 PACKAGE_ADDRESS=$PACKAGE_ID
 ADMIN_ADDRESS=$ADMIN_ADDRESS
 API_ENV
