f40f3106d326008ede1aa0407824399a596a3aff
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2024-10-11 18:35:44 +0300

    feat(apps-backend): add another path for iota-core.d.ts
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- apps/apps-backend/types/iota-core.d.ts
+++ apps/apps-backend/types/iota-core.d.ts
@@ -5,3 +5,8 @@
 declare module '@iota/core/dist/constants/features.enum' {
     export const Feature: never;
 }
+
+// types/iota-core.d.ts
+declare module '@iota/core/constants/features.enum' {
+    export const Feature: never;
+}
