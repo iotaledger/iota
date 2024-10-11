d286c0ef3562c819702db5994295b92b8fd3a86e
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2024-10-11 18:44:04 +0300

    feat(apps-backend): remve one declare from iota-core.d.ts
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- apps/apps-backend/types/iota-core.d.ts
+++ apps/apps-backend/types/iota-core.d.ts
@@ -2,11 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // types/iota-core.d.ts
-declare module '@iota/core/dist/constants/features.enum' {
-    export const Feature: never;
-}
-
-// types/iota-core.d.ts
 declare module '@iota/core/constants/features.enum' {
     export const Feature: never;
 }
