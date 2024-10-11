69e32ad183899b5ba67a7e0031ad06d845c64b46
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2024-10-11 18:34:36 +0300

    feat(apps-backend): add iota-core.d.ts
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

---
+++ apps/apps-backend/types/iota-core.d.ts
@@ -0,0 +1,7 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+// types/iota-core.d.ts
+declare module '@iota/core/dist/constants/features.enum' {
+    export const Feature: never;
+}
