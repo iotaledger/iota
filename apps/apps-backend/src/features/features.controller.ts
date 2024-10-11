30e9b59a0bd516ae234bc91e1c5ebc43779ea41d
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2024-10-11 19:57:33 +0300

    feat(apps-backend): return features.
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- apps/apps-backend/src/features/features.controller.ts
+++ apps/apps-backend/src/features/features.controller.ts
@@ -8,10 +8,63 @@ import { Feature } from '@iota/core/constants/features.enum';
 export class FeaturesController {
     @Get('/development')
     getDevelopmentFeatures() {
-        console.log(Feature);
         return {
             status: 200,
-            features: {},
+            features: {
+                [Feature.RecognizedPackages]: {
+                    defaultValue: [
+                        '0xb',
+                        '0x2',
+                        '0x3',
+                        '0x1',
+                        '0x107a',
+                        '0x0000000000000000000000000000000000000000000000000000000000000002',
+                        '0x0000000000000000000000000000000000000000000000000000000000000003',
+                        '0x0000000000000000000000000000000000000000000000000000000000000001',
+                        '0x000000000000000000000000000000000000000000000000000000000000107a',
+                    ],
+                },
+                [Feature.WalletSentryTracing]: {
+                    defaultValue: 0.0025,
+                },
+                // Note: we'll add wallet dapps when evm will be ready
+                [Feature.WalletDapps]: {
+                    defaultValue: [],
+                },
+                [Feature.WalletBalanceRefetchInterval]: {
+                    defaultValue: 1000,
+                },
+                [Feature.KioskOriginbytePackageid]: {
+                    defaultValue: '',
+                },
+                [Feature.WalletAppsBannerConfig]: {
+                    defaultValue: {
+                        enabled: false,
+                        bannerUrl: '',
+                        imageUrl: '',
+                    },
+                },
+                [Feature.WalletInterstitialConfig]: {
+                    defaultValue: {
+                        enabled: false,
+                        dismissKey: '',
+                        imageUrl: '',
+                        bannerUrl: '',
+                    },
+                },
+                [Feature.PollingTxnTable]: {
+                    defaultValue: true,
+                },
+                [Feature.NetworkOutageOverride]: {
+                    defaultValue: false,
+                },
+                [Feature.ModuleSourceVerification]: {
+                    defaultValue: true,
+                },
+                [Feature.AccountFinder]: {
+                    defaultValue: false,
+                },
+            },
             dateUpdated: new Date().toISOString(),
         };
     }
@@ -20,7 +73,61 @@ export class FeaturesController {
     getProductionFeatures() {
         return {
             status: 200,
-            features: {},
+            features: {
+                [Feature.RecognizedPackages]: {
+                    defaultValue: [
+                        '0xb',
+                        '0x2',
+                        '0x3',
+                        '0x1',
+                        '0x107a',
+                        '0x0000000000000000000000000000000000000000000000000000000000000002',
+                        '0x0000000000000000000000000000000000000000000000000000000000000003',
+                        '0x0000000000000000000000000000000000000000000000000000000000000001',
+                        '0x000000000000000000000000000000000000000000000000000000000000107a',
+                    ],
+                },
+                [Feature.WalletSentryTracing]: {
+                    defaultValue: 0.0025,
+                },
+                // Note: we'll add wallet dapps when evm will be ready
+                [Feature.WalletDapps]: {
+                    defaultValue: [],
+                },
+                [Feature.WalletBalanceRefetchInterval]: {
+                    defaultValue: 1000,
+                },
+                [Feature.KioskOriginbytePackageid]: {
+                    defaultValue: '',
+                },
+                [Feature.WalletAppsBannerConfig]: {
+                    defaultValue: {
+                        enabled: false,
+                        bannerUrl: '',
+                        imageUrl: '',
+                    },
+                },
+                [Feature.WalletInterstitialConfig]: {
+                    defaultValue: {
+                        enabled: false,
+                        dismissKey: '',
+                        imageUrl: '',
+                        bannerUrl: '',
+                    },
+                },
+                [Feature.PollingTxnTable]: {
+                    defaultValue: true,
+                },
+                [Feature.NetworkOutageOverride]: {
+                    defaultValue: false,
+                },
+                [Feature.ModuleSourceVerification]: {
+                    defaultValue: true,
+                },
+                [Feature.AccountFinder]: {
+                    defaultValue: false,
+                },
+            },
             dateUpdated: new Date().toISOString(),
         };
     }
--- apps/apps-backend/types/iota-core.d.ts
+++ apps/apps-backend/types/iota-core.d.ts
@@ -1,7 +1,4 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-// types/iota-core.d.ts
-declare module '@iota/core/constants/features.enum' {
-    export const Feature: never;
-}
+declare module '@iota/core/constants/features.enum';
