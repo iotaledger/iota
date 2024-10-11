2a63f6517b67fd3ac2788571b194460c440cb338
evavirseda <evirseda@boxfish.studio>
2024-10-11 11:38:31 +0200

    feat: update icons

--- apps/ui-icons/src/Copy.tsx
+++ apps/ui-icons/src/Copy.tsx
@@ -12,37 +12,10 @@ export default function SvgCopy(props: SVGProps<SVGSVGElement>) {
             {...props}
         >
             <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeWidth={2}
-                d="M20 13V7a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h4"
-            />
-            <path fill="currentColor" d="M4 5h16v3H4z" />
-            <rect width={4} height={2} x={7} y={10} fill="currentColor" rx={1} />
-            <rect
-                width={4}
-                height={2}
-                x={9}
-                y={2}
                 fill="currentColor"
-                rx={1}
-                transform="rotate(90 9 2)"
-            />
-            <rect
-                width={4}
-                height={2}
-                x={17}
-                y={2}
-                fill="currentColor"
-                rx={1}
-                transform="rotate(90 17 2)"
-            />
-            <circle cx={16} cy={19} r={4} fill="currentColor" />
-            <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeWidth={2}
-                d="M17 19a3 3 0 1 0 .102-.776"
+                fillRule="evenodd"
+                d="M3 6a3 3 0 0 1 3-3h8a3 3 0 0 1 3 3v1h1a3 3 0 0 1 3 3v8a3 3 0 0 1-3 3h-8a3 3 0 0 1-3-3v-1H6a3 3 0 0 1-3-3zm6 12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-8a1 1 0 0 0-1-1h-8a1 1 0 0 0-1 1zm6-11h-5a3 3 0 0 0-3 3v5H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1z"
+                clipRule="evenodd"
             />
         </svg>
     );
