32b29b868c64acca800583bd5d75c95134200b72
miker83z <mirko.zichichi@iota.org>
2024-10-18 19:15:58 +0200

    fix(iota-e2e-tests): change randomness test name

--- crates/iota-e2e-tests/tests/randomness_tests.rs
+++ crates/iota-e2e-tests/tests/randomness_tests.rs
@@ -8,7 +8,7 @@ use iota_types::IOTA_RANDOMNESS_STATE_OBJECT_ID;
 use test_cluster::TestClusterBuilder;
 
 #[sim_test]
-async fn test_create_randomness_state_object() {
+async fn test_check_randomness_state_object_exists() {
     let test_cluster = TestClusterBuilder::new()
         .with_protocol_version(1.into())
         .with_epoch_duration_ms(10000)
