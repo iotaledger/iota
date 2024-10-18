5f5e3753cb0717e998f3fa595ec22cb8d5d139b3
Vivek Jain <vkj.vivekkumarjain@gmail.com>
2024-10-18 11:46:39 +0530

    Minor fixes

--- docs/site/src/components/FeedbackForm/index.tsx
+++ docs/site/src/components/FeedbackForm/index.tsx
@@ -8,19 +8,19 @@ const FeedbackForm = () => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-  
-    const githubNewIssueUrl = `https://github.com/iotaledger/devx/issues/new?template=doc-bug.md&title=${title}&body=${body}`;
-    
+
+    const prefixedTitle = `Docs feedback: ${title}`;
+    const githubNewIssueUrl = `https://github.com/iotaledger/devx/issues/new?template=doc-bug.md&title=${encodeURIComponent(prefixedTitle)}&body=${encodeURIComponent(body)}`;
+
     // Open the GitHub issue page with pre-filled data in a new tab
     window.open(githubNewIssueUrl, '_blank');
     setTitle("");
     setBody("");
   };
-  
 
   return (
     <div className="feedback-container">
-      <div className="divider"></div> 
+      <div className="divider"></div>
       <div className={clsx("h3", "feedback-header")}>Feedback Form</div>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
@@ -52,7 +52,8 @@ const FeedbackForm = () => {
           disabled={!title}
         >
           Submit Feedback
-        </button>      </form>
+        </button>
+      </form>
       <div className="divider"></div>
     </div>
   );
