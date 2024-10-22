c40b20762d2e57bd5fa891f0632b28ced3b24afe
samuel_rufi <samuel.rufinatscha@iota.org>
2024-10-22 19:10:43 +0200

    fix: add timelocked staking test

--- crates/iota-indexer/tests/rpc-tests/governance_api.rs
+++ crates/iota-indexer/tests/rpc-tests/governance_api.rs
@@ -1,30 +1,32 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use iota_json::{call_args, type_args};
-use iota_json_rpc_api::GovernanceReadApiClient;
-use iota_json_rpc_types::DelegatedStake;
-use iota_protocol_config::ProtocolConfig;
+use iota_json_rpc_api::{
+    CoinReadApiClient, GovernanceReadApiClient, IndexerApiClient, TransactionBuilderClient,
+    WriteApiClient,
+};
+use iota_json_rpc_types::{
+    CoinPage, DelegatedStake, IotaObjectDataOptions, IotaObjectResponseQuery,
+    IotaTransactionBlockResponseOptions, ObjectsPage, StakeStatus, TransactionBlockBytes,
+};
 use iota_test_transaction_builder::TestTransactionBuilder;
 use iota_types::{
-    base_types::{IotaAddress, MoveObjectType, ObjectID, TransactionDigest},
+    IOTA_FRAMEWORK_ADDRESS, IOTA_SYSTEM_ADDRESS,
+    balance::Balance,
+    base_types::{ObjectID},
     gas_coin::GAS,
-    id::UID,
-    object::{Data, MoveObject, ObjectInner, Owner, OBJECT_START_VERSION},
     programmable_transaction_builder::ProgrammableTransactionBuilder,
-    timelock::{
-        label::label_struct_tag_to_string, stardust_upgrade_label::stardust_upgrade_label_type,
-        timelock::TimeLock,
-    },
-    transaction::{Argument, CallArg, ObjectArg},
-    IOTA_FRAMEWORK_ADDRESS,
+    quorum_driver_types::ExecuteTransactionRequestType,
+    transaction::{CallArg, ObjectArg},
 };
-use move_core_types::identifier::Identifier;
+use move_core_types::{identifier::Identifier, language_storage::TypeTag};
 
-use crate::common::{indexer_wait_for_checkpoint, rpc_call_error_msg_matches, ApiTestSetup};
+use crate::common::{
+    ApiTestSetup, indexer_wait_for_checkpoint, indexer_wait_for_latest_checkpoint,
+};
 
 #[test]
-fn get_stakes_by_id() {
+fn test_staking() {
     let ApiTestSetup {
         runtime,
         store,
@@ -35,64 +37,197 @@ fn get_stakes_by_id() {
     runtime.block_on(async move {
         indexer_wait_for_checkpoint(store, 1).await;
 
-        let validator_address = cluster
-            .swarm
-            .active_validators()
-            .next()
-            .unwrap()
-            .config
-            .iota_address();
+        let address = cluster.get_address_0();
 
-        let response: Vec<DelegatedStake> = client.get_stakes(validator_address).await.unwrap();
-        let stake_id = response
-            .first()
-            .unwrap()
-            .stakes
-            .first()
-            .unwrap()
-            .staked_iota_id;
-
-        let response = client.get_stakes_by_ids(vec![stake_id]).await.unwrap();
-        assert_eq!(response.len(), 1);
-
-        let response = client.get_stakes_by_ids(vec![]).await.unwrap();
-        assert_eq!(response.len(), 0);
-    });
-}
-
-#[test]
-fn get_stakes() {
-    let ApiTestSetup {
-        runtime,
-        store,
-        client,
-        cluster,
-    } = ApiTestSetup::get_or_init();
-
-    runtime.block_on(async move {
-        indexer_wait_for_checkpoint(store, 1).await;
-
-        let validator_address = cluster
-            .swarm
-            .active_validators()
-            .next()
-            .unwrap()
-            .config
-            .iota_address();
-
-        let response = client.get_stakes(validator_address).await.unwrap();
-        assert_eq!(response.len(), 1);
-
-        let response = client
-            .get_stakes(IotaAddress::random_for_testing_only())
+        let objects: ObjectsPage = client
+            .get_owned_objects(
+                address,
+                Some(IotaObjectResponseQuery::new_with_options(
+                    IotaObjectDataOptions::new()
+                        .with_type()
+                        .with_owner()
+                        .with_previous_transaction(),
+                )),
+                None,
+                None,
+            )
             .await
             .unwrap();
-        assert_eq!(response.len(), 0);
+        assert_eq!(5, objects.data.len());
+
+        // Check StakedIota object before test
+        let staked_iota: Vec<DelegatedStake> = client.get_stakes(address).await.unwrap();
+        assert!(staked_iota.is_empty());
+
+        let validator = client
+            .get_latest_iota_system_state()
+            .await
+            .unwrap()
+            .active_validators[0]
+            .iota_address;
+
+        let coin = objects.data[0].object().unwrap().object_id;
+        // Delegate some IOTA
+        let transaction_bytes: TransactionBlockBytes = client
+            .request_add_stake(
+                address,
+                vec![coin],
+                Some(1000000000.into()),
+                validator,
+                None,
+                100_000_000.into(),
+            )
+            .await
+            .unwrap();
+        let tx = cluster
+            .wallet
+            .sign_transaction(&transaction_bytes.to_data().unwrap());
+
+        let (tx_bytes, signatures) = tx.to_tx_bytes_and_signatures();
+
+        client
+            .execute_transaction_block(
+                tx_bytes,
+                signatures,
+                Some(IotaTransactionBlockResponseOptions::new()),
+                Some(ExecuteTransactionRequestType::WaitForLocalExecution),
+            )
+            .await
+            .unwrap();
+
+        cluster.force_new_epoch().await;
+        indexer_wait_for_latest_checkpoint(store, &cluster).await;
+
+        // Check DelegatedStake object after epoch transition
+        let staked_iota: Vec<DelegatedStake> = client.get_stakes(address).await.unwrap();
+        assert_eq!(1, staked_iota.len());
+        assert_eq!(1000000000, staked_iota[0].stakes[0].principal);
+        assert!(matches!(
+            staked_iota[0].stakes[0].status,
+            StakeStatus::Active {
+                estimated_reward: _
+            }
+        ));
     });
 }
 
 #[test]
-fn get_timelocked_stakes_by_id() {
+fn test_unstaking() {
+    let ApiTestSetup {
+        runtime,
+        store,
+        client,
+        cluster,
+    } = ApiTestSetup::get_or_init();
+
+    let indexer_client = client;
+
+    runtime.block_on(async move {
+        indexer_wait_for_checkpoint(store, 1).await;
+
+        let address = cluster.get_address_0();
+
+        let coins: CoinPage = indexer_client
+            .get_coins(address, None, None, None)
+            .await
+            .unwrap();
+        assert_eq!(5, coins.data.len());
+
+        // Check StakedIota object before test
+        let staked_iota: Vec<DelegatedStake> = indexer_client.get_stakes(address).await.unwrap();
+        assert!(staked_iota.is_empty());
+
+        let validator = indexer_client
+            .get_latest_iota_system_state()
+            .await
+            .unwrap()
+            .active_validators[0]
+            .iota_address;
+
+        // Delegate some IOTA
+        let transaction_bytes: TransactionBlockBytes = indexer_client
+            .request_add_stake(
+                address,
+                vec![coins.data[0].coin_object_id],
+                Some(1000000000.into()),
+                validator,
+                None,
+                100_000_000.into(),
+            )
+            .await
+            .unwrap();
+        let tx = cluster
+            .wallet
+            .sign_transaction(&transaction_bytes.to_data().unwrap());
+
+        let (tx_bytes, signatures) = tx.to_tx_bytes_and_signatures();
+
+        indexer_client
+            .execute_transaction_block(
+                tx_bytes,
+                signatures,
+                Some(IotaTransactionBlockResponseOptions::new()),
+                Some(ExecuteTransactionRequestType::WaitForLocalExecution),
+            )
+            .await
+            .unwrap();
+
+        cluster.force_new_epoch().await;
+        indexer_wait_for_latest_checkpoint(store, &cluster).await;
+
+        // Check DelegatedStake object
+        let staked_iota: Vec<DelegatedStake> = indexer_client.get_stakes(address).await.unwrap();
+        assert_eq!(1, staked_iota.len());
+        assert_eq!(1000000000, staked_iota[0].stakes[0].principal);
+        assert!(matches!(
+            staked_iota[0].stakes[0].status,
+            StakeStatus::Active {
+                estimated_reward: _
+            }
+        ));
+
+        let transaction_bytes: TransactionBlockBytes = indexer_client
+            .request_withdraw_stake(
+                address,
+                staked_iota[0].stakes[0].staked_iota_id,
+                None,
+                100_000_000.into(),
+            )
+            .await
+            .unwrap();
+        let tx = cluster
+            .wallet
+            .sign_transaction(&transaction_bytes.to_data().unwrap());
+
+        let _ = cluster.wallet.execute_transaction_must_succeed(tx).await;
+
+        cluster.force_new_epoch().await;
+        indexer_wait_for_latest_checkpoint(store, &cluster).await;
+
+        let node_response = cluster
+            .rpc_client()
+            .get_stakes_by_ids(vec![staked_iota[0].stakes[0].staked_iota_id])
+            .await
+            .unwrap();
+        assert_eq!(1, node_response.len());
+        assert!(matches!(
+            node_response[0].stakes[0].status,
+            StakeStatus::Unstaked
+        ));
+
+        let indexer_response = indexer_client
+            .get_stakes_by_ids(vec![staked_iota[0].stakes[0].staked_iota_id])
+            .await
+            .unwrap();
+        assert_eq!(0, indexer_response.len());
+
+        let staked_iota: Vec<DelegatedStake> = indexer_client.get_stakes(address).await.unwrap();
+        assert!(staked_iota.is_empty());
+    });
+}
+
+#[test]
+fn test_timelocked_staking() {
     let ApiTestSetup {
         runtime,
         store,
@@ -107,18 +242,19 @@ fn get_timelocked_stakes_by_id() {
         let context = &cluster.wallet;
 
         let gas_price = context.get_reference_gas_price().await.unwrap();
-        let mut gas_objects = context.get_all_gas_objects_owned_by_address(sender).await.unwrap();
+        let mut gas_objects = context
+            .get_all_gas_objects_owned_by_address(sender)
+            .await
+            .unwrap();
         assert!(gas_objects.len() >= 2);
         let iota_coin_ref = gas_objects.pop().unwrap();
-        println!("gas object id used: {:?}\n", iota_coin_ref.0);
-
-        let gas_data = context.gas_objects(sender).await.unwrap();
-        println!("gas_objects data: {:?}\n", gas_data);
 
         let pt = {
             let mut builder = ProgrammableTransactionBuilder::new();
 
-            let iota_coin_argument = builder.obj(ObjectArg::ImmOrOwnedObject(iota_coin_ref)).expect("valid obj");
+            let iota_coin_argument = builder
+                .input(CallArg::Object(ObjectArg::ImmOrOwnedObject(iota_coin_ref)))
+                .expect("valid obj");
 
             // Step 1: Get the IOTA balance from the coin object.
             let iota_balance = builder.programmable_move_call(
@@ -126,124 +262,54 @@ fn get_timelocked_stakes_by_id() {
                 Identifier::new("coin").unwrap(),
                 Identifier::new("into_balance").unwrap(),
                 vec![GAS::type_tag()],
-                vec![iota_coin_argument]);
+                vec![iota_coin_argument],
+            );
 
-            /*
             // Step 2: Timelock the IOTA balance.
-            let timelock_timestamp = builder.pure(1000).expect("valid pure");
+            let timelock_timestamp = builder.input(CallArg::from(u64::MAX)).unwrap();
             let timelocked_iota_balance = builder.programmable_move_call(
                 ObjectID::new(IOTA_FRAMEWORK_ADDRESS.into_bytes()),
                 Identifier::new("timelock").unwrap(),
                 Identifier::new("lock").unwrap(),
-                vec![GAS::type_tag()],
-                vec![iota_balance, timelock_timestamp]);
-             */
+                vec![TypeTag::Struct(Box::new(Balance::type_(GAS::type_tag())))],
+                vec![iota_balance, timelock_timestamp],
+            );
 
-            builder.transfer_arg(sender, iota_balance);
+            // Step 3: Delegate the timelocked IOTA balance.
+            let validator = client
+                .get_latest_iota_system_state()
+                .await
+                .unwrap()
+                .active_validators[0]
+                .iota_address;
+
+            let validator = builder
+                .input(CallArg::Pure(bcs::to_bytes(&validator).unwrap()))
+                .unwrap();
+            let state = builder.input(CallArg::IOTA_SYSTEM_MUT).unwrap();
+
+            let _ = builder.programmable_move_call(
+                ObjectID::new(IOTA_SYSTEM_ADDRESS.into_bytes()),
+                Identifier::new("timelocked_staking").unwrap(),
+                Identifier::new("request_add_stake").unwrap(),
+                vec![],
+                vec![state, timelocked_iota_balance, validator],
+            );
 
             builder.finish()
         };
 
         let tx_builder = TestTransactionBuilder::new(sender, gas_objects.pop().unwrap(), gas_price);
-        let txn = context.sign_transaction(
-            &tx_builder.programmable(pt).build(),
-        );
+        let txn = context.sign_transaction(&tx_builder.programmable(pt).build());
 
-        let resp = context.execute_transaction_must_succeed(txn).await;
+        let _ = context.execute_transaction_must_succeed(txn).await;
 
-        println!("resp: {:?}", resp);
+        cluster.force_new_epoch().await;
+        indexer_wait_for_latest_checkpoint(store, &cluster).await;
 
-        /*
-        let response = client
-            .get_timelocked_stakes_by_ids(vec![ObjectID::random()])
-            .await
-            .unwrap();
-        assert_eq!(response.len(), 0);
-         */
-    });
-}
+        let response = client.get_timelocked_stakes(sender).await.unwrap();
 
-#[test]
-fn get_timelocked_stakes() {
-    let ApiTestSetup {
-        runtime,
-        store,
-        client,
-        cluster,
-    } = ApiTestSetup::get_or_init();
-
-    runtime.block_on(async move {
-        indexer_wait_for_checkpoint(store, 1).await;
-
-        let validator_address = cluster
-            .swarm
-            .active_validators()
-            .next()
-            .unwrap()
-            .config
-            .iota_address();
-
-        let response = client
-            .get_timelocked_stakes(validator_address)
-            .await
-            .unwrap();
-
-        assert_eq!(response.len(), 0);
-    });
-}
-
-#[test]
-fn get_committee_info() {
-    let ApiTestSetup {
-        runtime,
-        store,
-        client,
-        ..
-    } = ApiTestSetup::get_or_init();
-
-    runtime.block_on(async move {
-        indexer_wait_for_checkpoint(store, 1).await;
-
-        // Test with no specified epoch
-        let indexer_response = client.get_committee_info(None).await.unwrap();
-
-        let (epoch_id, validators) = (indexer_response.epoch, indexer_response.validators);
-
-        assert!(epoch_id == 0);
-        assert_eq!(validators.len(), 4);
-
-        // Test with specified epoch 0
-        let indexer_response = client.get_committee_info(Some(0.into())).await.unwrap();
-
-        let (epoch_id, validators) = (indexer_response.epoch, indexer_response.validators);
-
-        assert!(epoch_id == 0);
-        assert_eq!(validators.len(), 4);
-
-        // Test with non-existent epoch
-        let response = client.get_committee_info(Some(1.into())).await;
-
-        rpc_call_error_msg_matches(
-            response,
-            r#"{"code":-32603,"message":"Invalid argument with error: `Missing epoch Some(1)`"}"#,
-        );
-    });
-}
-
-#[test]
-fn get_reference_gas_price() {
-    let ApiTestSetup {
-        runtime,
-        store,
-        client,
-        ..
-    } = ApiTestSetup::get_or_init();
-
-    runtime.block_on(async move {
-        indexer_wait_for_checkpoint(store, 1).await;
-
-        let response = client.get_reference_gas_price().await.unwrap();
-        assert_eq!(response, 1000.into());
+        assert_eq!(response.len(), 1);
     });
 }
 
@@ -263,6 +329,69 @@ fn get_latest_iota_system_state() {
         assert_eq!(response.epoch, 0);
         assert_eq!(response.protocol_version, 1);
         assert_eq!(response.system_state_version, 1);
+    });
+}
+
+#[test]
+fn get_committee_info() {
+    let ApiTestSetup {
+        runtime,
+        store,
+        client,
+        cluster,
+    } = ApiTestSetup::get_or_init();
+
+    runtime.block_on(async move {
+        indexer_wait_for_checkpoint(store, 1).await;
+
+        // Test with no specified epoch
+        let response = client.get_committee_info(None).await.unwrap();
+
+        let (epoch_id, validators) = (response.epoch, response.validators);
+
+        assert!(epoch_id == 0);
+        assert_eq!(validators.len(), 4);
+
+        // Test with specified epoch 0
+        let response = client.get_committee_info(Some(0.into())).await.unwrap();
+
+        let (epoch_id, validators) = (response.epoch, response.validators);
+
+        assert!(epoch_id == 0);
+        assert_eq!(validators.len(), 4);
+
+        // Test with non-existent epoch
+        let response = client.get_committee_info(Some(1.into())).await;
+
+        assert!(response.is_err());
+
+        // Sleep for 5 seconds
+        cluster.force_new_epoch().await;
+
+        // Test with specified epoch 1
+        let response = client.get_committee_info(Some(1.into())).await.unwrap();
+
+        let (epoch_id, validators) = (response.epoch, response.validators);
+
+        assert!(epoch_id == 1);
+        assert_eq!(validators.len(), 4);
+    });
+}
+
+#[test]
+fn get_reference_gas_price() {
+    let ApiTestSetup {
+        runtime,
+        store,
+        client,
+        ..
+    } = ApiTestSetup::get_or_init();
+
+    runtime.block_on(async move {
+        indexer_wait_for_checkpoint(store, 1).await;
+
+        let response = client.get_reference_gas_price().await.unwrap();
+        assert_eq!(response, 1000.into());
     });
 }
 
