b1199edca243fdcfeb8eb17bab5c75aa53147805
Chloe Martin <chloedaughterofmars@gmail.com>
2024-10-28 12:33:23 -0400

    chore(iota-sdk): Update doc comments

--- crates/iota-sdk/src/error.rs
+++ crates/iota-sdk/src/error.rs
@@ -29,7 +29,7 @@ pub enum Error {
         server_version: String,
     },
     #[error("Insufficient fund for address [{address}], requested amount: {amount}")]
-    InsufficientFund { address: IotaAddress, amount: u128 },
+    InsufficientFunds { address: IotaAddress, amount: u128 },
     #[error(transparent)]
     Json(#[from] serde_json::Error),
 }
