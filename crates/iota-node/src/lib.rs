e65544bafdb8087dd4669d0bb98d7d29a353c16b
muXxer <git@muxxer.de>
2024-10-29 12:20:24 +0100

    fix(node): rustfmt

--- crates/iota-node/src/lib.rs
+++ crates/iota-node/src/lib.rs
@@ -579,9 +579,7 @@ impl IotaNode {
             None
         };
 
-        let rest_index = if is_full_node
-            && config.enable_rest_api
-            && config.enable_index_processing
+        let rest_index = if is_full_node && config.enable_rest_api && config.enable_index_processing
         {
             Some(Arc::new(RestIndexStore::new(
                 config.db_path().join("rest_index"),
