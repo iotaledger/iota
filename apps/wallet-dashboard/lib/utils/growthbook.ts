8f37a95b34723fb9e0ce58c62236993d496650df
evavirseda <evirseda@boxfish.studio>
2024-10-30 09:58:48 +0100

    feat: remove comment

--- apps/wallet-dashboard/lib/utils/growthbook.ts
+++ apps/wallet-dashboard/lib/utils/growthbook.ts
@@ -5,7 +5,6 @@ import { GrowthBook } from '@growthbook/growthbook';
 import { getAppsBackend } from '@iota/iota-sdk/client';
 
 export const growthbook = new GrowthBook({
-    // If you want to develop locally, you can set the API host to this:
     apiHost: getAppsBackend(),
     clientKey: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     enableDevMode: process.env.NODE_ENV === 'development',
