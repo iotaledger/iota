8ac1d9f362424db12e09a7d7d64dfec1286de904
marc2332 <mespinsanz@gmail.com>
2024-11-11 13:07:01 +0100

    refactor(ci): Add nightly-only type of build

--- apps/wallet/configs/webpack/webpack.config.common.ts
+++ apps/wallet/configs/webpack/webpack.config.common.ts
@@ -25,10 +25,11 @@ const CONFIGS_ROOT = resolve(PROJECT_ROOT, 'configs');
 const SRC_ROOT = resolve(PROJECT_ROOT, 'src');
 const OUTPUT_ROOT = resolve(PROJECT_ROOT, 'dist');
 const TS_CONFIGS_ROOT = resolve(CONFIGS_ROOT, 'ts');
+const IS_NIGHTLY = process.env.NODE_ENV === 'nightly';
 const IS_DEV = process.env.NODE_ENV === 'development';
 const IS_PROD = process.env.NODE_ENV === 'production';
 const TS_CONFIG_FILE = resolve(TS_CONFIGS_ROOT, `tsconfig.${IS_DEV ? 'dev' : 'prod'}.json`);
-const APP_NAME = WALLET_RC ? 'IOTA Wallet (RC)' : IS_DEV ? 'IOTA Wallet (DEV)' : 'IOTA Wallet';
+const APP_NAME = WALLET_RC ? 'IOTA Wallet (RC)' : IS_DEV ? 'IOTA Wallet (DEV)' : IS_NIGHTLY ? 'IOTA Wallet (Nightly)' : 'IOTA Wallet';
 
 dotenv.config({
     path: [resolve(SDK_ROOT, '.env'), resolve(SDK_ROOT, '.env.defaults')],
