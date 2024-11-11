8e657861714bcf07c805aa003c3508770c5fc8ee
Valerii Reutov <valeriy.reutov@gmail.com>
2024-11-11 18:14:08 +0200

    feat: the latest cut should have the index 1

--- iota-execution/src/lib.rs
+++ iota-execution/src/lib.rs
@@ -24,9 +24,9 @@ pub fn executor(
     silent: bool,
     enable_profiler: Option<PathBuf>,
 ) -> IotaResult<Arc<dyn Executor + Send + Sync>> {
-    let version = protocol_config.execution_version_as_option().unwrap_or(0);
+    let version = protocol_config.execution_version_as_option().unwrap_or(1);
     Ok(match version {
-        0 => Arc::new(latest::Executor::new(
+        1 => Arc::new(latest::Executor::new(
             protocol_config,
             silent,
             enable_profiler,
@@ -41,10 +41,10 @@ pub fn verifier<'m>(
     for_signing: bool,
     metrics: &'m Arc<BytecodeVerifierMetrics>,
 ) -> Box<dyn Verifier + 'm> {
-    let version = protocol_config.execution_version_as_option().unwrap_or(0);
+    let version = protocol_config.execution_version_as_option().unwrap_or(1);
     let config = protocol_config.verifier_config(for_signing);
     match version {
-        0 => Box::new(latest::Verifier::new(config, metrics)),
+        1 => Box::new(latest::Verifier::new(config, metrics)),
         v => panic!("Unsupported execution version {v}"),
     }
 }
