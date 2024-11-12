c5f67204f85095c77e7562ce5fe13b873ca12dc9
Vivek Jain <vkj.vivekkumarjain@gmail.com>
2024-11-12 19:22:39 +0530

    Add browserify

--- docs/site/src/components/API/index.js
+++ docs/site/src/components/API/index.js
@@ -3,11 +3,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import React, { useState, useEffect } from "react";
-import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import Heading from "@theme/Heading";
 import RefNav from "./api-ref/refnav";
 import Methods from "./api-ref/method";
-
 import ScrollSpy from "react-ui-scrollspy";
 
 // TODO: Once the repo is public, we can use the old imports again and reactivate the ../../utils/getopenrpcspecs.js script
@@ -20,89 +19,87 @@ export function getRef(url) {
 }
 
 const Rpc = () => {
-  const [openrpc, setOpenRpc] = useState(() => {
-    if (ExecutionEnvironment.canUseDOM) {
-      const network = localStorage.getItem("RPC");
-      switch (network) {
-        case "mainnet":
-          return openrpc_mainnet;
-        case "testnet":
-          return openrpc_testnet;
-        case "devnet":
-          return openrpc_devnet;
-        default:
-          return openrpc_mainnet;
-      }
-    } else {
-      return openrpc_mainnet;
-    }
-  });
-  const [openDropdown, setOpenDropdown] = useState(null);
-
-  useEffect(() => {
-    const rpcswitch = () => {
-      if (localStorage.getItem("RPC")) {
-        switch (localStorage.getItem("RPC")) {
-          case "mainnet":
-            setOpenRpc(openrpc_mainnet);
-            break;
-          case "testnet":
-            setOpenRpc(openrpc_testnet);
-            break;
-          case "devnet":
-            setOpenRpc(openrpc_devnet);
-            break;
-          default:
-            setOpenRpc(openrpc_mainnet);
-        }
-      } else {
-        setOpenRpc(openrpc_mainnet);
-      }
-    };
-
-    window.addEventListener("storage", rpcswitch);
-
-    return () => {
-      window.removeEventListener("storage", rpcswitch);
-    };
-  }, [openrpc]);
-
-  const apis = [
-    ...new Set(openrpc["methods"].map((api) => api.tags[0].name)),
-  ].sort();
-  const schemas = openrpc.components.schemas;
-
-  if (!openrpc) {
-    return <p>Open RPC file not found.</p>;
-  }
-
-  let ids = [];
-  openrpc["methods"].map((method) => {
-    ids.push(method.name.replaceAll(/\s/g, "-").toLowerCase());
-  });
-
   return (
-    <div className="mx-4 flex flex-row">
-      <div className="pt-12 w-[290px] mb-24 flex-none max-h-screen overflow-y-auto sticky top-12" style={!openDropdown ? {borderRight:'1px solid #444950'}:{}}>
-        <RefNav json={openrpc} apis={apis} openDropdown={openDropdown} setOpenDropdown={setOpenDropdown}/>
-      </div>
+    <BrowserOnly fallback={<p>Loading...</p>}>
+      {() => {
+        const [openrpc, setOpenRpc] = useState(() => {
+          const network = localStorage.getItem("RPC");
+          switch (network) {
+            case "mainnet":
+              return openrpc_mainnet;
+            case "testnet":
+              return openrpc_testnet;
+            case "devnet":
+              return openrpc_devnet;
+            default:
+              return openrpc_mainnet;
+          }
+        });
 
-      <main className="flex-grow w-3/4">
-        <div className="mx-8">
-          <div className="">
-            <Heading as="h1" className=" w-full py-4 top-14">
-              IOTA JSON-RPC Reference - Version: {openrpc.info.version}
-            </Heading>
-            <ScrollSpy>
-              <div className="">
-                <p>{openrpc.info.description}</p>
-                <Methods json={openrpc} apis={apis} schemas={schemas} />
+        const [openDropdown, setOpenDropdown] = useState(null);
+
+        useEffect(() => {
+          const rpcswitch = () => {
+            const network = localStorage.getItem("RPC");
+            switch (network) {
+              case "mainnet":
+                setOpenRpc(openrpc_mainnet);
+                break;
+              case "testnet":
+                setOpenRpc(openrpc_testnet);
+                break;
+              case "devnet":
+                setOpenRpc(openrpc_devnet);
+                break;
+              default:
+                setOpenRpc(openrpc_mainnet);
+            }
+          };
+
+          window.addEventListener("storage", rpcswitch);
+          return () => {
+            window.removeEventListener("storage", rpcswitch);
+          };
+        }, []);
+
+        const apis = [
+          ...new Set(openrpc["methods"].map((api) => api.tags[0].name)),
+        ].sort();
+        const schemas = openrpc.components.schemas;
+
+        if (!openrpc) {
+          return <p>Open RPC file not found.</p>;
+        }
+
+        let ids = openrpc["methods"].map((method) =>
+          method.name.replaceAll(/\s/g, "-").toLowerCase()
+        );
+
+        return (
+          <div className="mx-4 flex flex-row">
+            <div className="pt-12 w-[290px] mb-24 flex-none max-h-screen overflow-y-auto sticky top-12" style={!openDropdown ? { borderRight: '1px solid #444950' } : {}}>
+              <RefNav json={openrpc} apis={apis} openDropdown={openDropdown} setOpenDropdown={setOpenDropdown} />
+            </div>
+
+            <main className="flex-grow w-3/4">
+              <div className="mx-8">
+                <div className="">
+                  <Heading as="h1" className=" w-full py-4 top-14">
+                    IOTA JSON-RPC Reference - Version: {openrpc.info.version}
+                  </Heading>
+                  <ScrollSpy>
+                    <div className="">
+                      <p>{openrpc.info.description}</p>
+                      <Methods json={openrpc} apis={apis} schemas={schemas} />
+                    </div>
+                  </ScrollSpy>
+                </div>
               </div>
-            </ScrollSpy>
+            </main>
           </div>
-        </div>
-      </main>
-    </div>
+        );
+      }}
+    </BrowserOnly>
   );
 };
 
