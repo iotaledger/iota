ec1f344e8a0935fad04b1f4a7055fb952647d1a5
cpl121 <cpeon@boxfish.studio>
2024-11-20 11:52:17 +0100

    fix(wallet-dashboard): lint

--- apps/wallet-dashboard/components/Dialogs/SendToken/constants/index.ts
+++ apps/wallet-dashboard/components/Dialogs/SendToken/constants/index.ts
@@ -1,4 +1,7 @@
-import { FormDataValues } from "../interfaces";
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { FormDataValues } from '../interfaces';
 
 export const INITIAL_VALUES: FormDataValues = {
     to: '',
