16292bc7d5bd1e60f2c2dc61ba52e2e084b4767c
evavirseda <evirseda@boxfish.studio>
2024-11-25 08:46:13 +0100

    feat(explorer): polish validators view and bring back actions in Table (#4153)
    * fix action in table and remove validators count
    
    * polish validators view
    
    * fix import
    
    ---------
    
    Co-authored-by: cpl121 <100352899+cpl121@users.noreply.github.com>

--- apps/explorer/src/pages/validators/Validators.tsx
+++ apps/explorer/src/pages/validators/Validators.tsx
@@ -11,7 +11,8 @@ import {
     InfoBox,
     InfoBoxStyle,
     InfoBoxType,
-    TableHeader,
+    Panel,
+    Title,
     TooltipPosition,
 } from '@iota/apps-ui-kit';
 import { useIotaClientQuery } from '@iota/dapp-kit';
@@ -155,26 +156,27 @@ function ValidatorPageResult(): JSX.Element {
                                 />
                             ))}
                         </div>
-                        <div>
-                            <ErrorBoundary>
-                                <TableHeader>All Validators</TableHeader>
-                                {(isPending || validatorsEventsLoading) && (
-                                    <PlaceholderTable
-                                        rowCount={20}
-                                        rowHeight="13px"
-                                        colHeadings={['Name', 'Address', 'Stake']}
-                                    />
-                                )}
-
-                                {isSuccess && tableData && tableColumns && (
-                                    <TableCard
-                                        data={tableData}
-                                        columns={tableColumns}
-                                        areHeadersCentered={false}
-                                    />
-                                )}
-                            </ErrorBoundary>
-                        </div>
+                        <Panel>
+                            <Title title="All Validators" />
+                            <div className="p-md">
+                                <ErrorBoundary>
+                                    {(isPending || validatorsEventsLoading) && (
+                                        <PlaceholderTable
+                                            rowCount={20}
+                                            rowHeight="13px"
+                                            colHeadings={['Name', 'Address', 'Stake']}
+                                        />
+                                    )}
+                                    {isSuccess && tableData && tableColumns && (
+                                        <TableCard
+                                            data={tableData}
+                                            columns={tableColumns}
+                                            areHeadersCentered={false}
+                                        />
+                                    )}
+                                </ErrorBoundary>
+                            </div>
+                        </Panel>
                     </div>
                 )
             }
