44c4461bb6b0b2c898cefd1bc2de7875c8493baa
evavirseda <evirseda@boxfish.studio>
2024-11-28 11:34:02 +0100

    polish

--- apps/wallet-dashboard/components/staking-overview/StartStaking.tsx
+++ apps/wallet-dashboard/components/staking-overview/StartStaking.tsx
@@ -1,10 +1,10 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { Button, ButtonSize, ButtonType, Panel } from '@iota/apps-ui-kit';
 import { StakeDialog } from '../Dialogs';
 import { Theme, useTheme } from '@iota/core';
 import { useStakeDialog } from '../Dialogs/Staking/hooks/useStakeDialog';
+import { Banner } from '../Banner';
 
 export function StartStaking() {
     const { theme } = useTheme();
@@ -25,36 +25,14 @@ export function StartStaking() {
             : 'https://files.iota.org/media/tooling/wallet-dashboard-staking-light.mp4';
 
     return (
-        <Panel bgColor="bg-secondary-90 dark:bg-secondary-10">
-            <div className="flex h-full w-full justify-between">
-                <div className="flex h-full w-full flex-col justify-between p-lg">
-                    <div className="flex flex-col gap-xxs">
-                        <span className="text-headline-sm text-neutral-10 dark:text-neutral-92">
-                            Start Staking
-                        </span>
-                        <span className="text-body-md text-neutral-40 dark:text-neutral-60">
-                            Earn Rewards
-                        </span>
-                    </div>
-                    <div>
-                        <Button
-                            onClick={handleNewStake}
-                            size={ButtonSize.Small}
-                            type={ButtonType.Outlined}
-                            text="Stake"
-                        />
-                    </div>
-                </div>
-                <div className="relative w-full overflow-hidden">
-                    <video
-                        src={videoSrc}
-                        autoPlay
-                        loop
-                        muted
-                        className="absolute -top-16 h-80 w-full"
-                    ></video>
-                </div>
-            </div>
+        <>
+            <Banner
+                videoSrc={videoSrc}
+                title="Start Staking"
+                subTitle="Earn Rewards"
+                onButtonClick={handleNewStake}
+                buttonText="Stake"
+            />
             {isDialogStakeOpen && stakeDialogView && (
                 <StakeDialog
                     stakedDetails={selectedStake}
@@ -66,6 +44,6 @@ export function StartStaking() {
                     setSelectedValidator={setSelectedValidator}
                 />
             )}
-        </Panel>
+        </>
     );
 }
