30285b6af3d4fc9f69a6b9365858b885ede40194
Andrew <i.am.andrew.cullen@gmail.com>
2024-11-29 11:20:42 +0000

    Revert "create v1 cut of adapter using script"
    This reverts commit 16d8c52b730319e3375fd0764eed96a0244dcf7c.

--- iota-execution/src/lib.rs
+++ iota-execution/src/lib.rs
@@ -15,7 +15,6 @@ pub mod executor;
 pub mod verifier;
 
 mod latest;
-mod v1;
 
 #[cfg(test)]
 mod tests;
@@ -27,9 +26,7 @@ pub fn executor(
 ) -> IotaResult<Arc<dyn Executor + Send + Sync>> {
     let version = protocol_config.execution_version_as_option().unwrap_or(1);
     Ok(match version {
-        1 => Arc::new(v1::Executor::new(protocol_config, silent, enable_profiler)?),
-
-        2 => Arc::new(latest::Executor::new(
+        1 => Arc::new(latest::Executor::new(
             protocol_config,
             silent,
             enable_profiler,
@@ -47,8 +44,7 @@ pub fn verifier<'m>(
     let version = protocol_config.execution_version_as_option().unwrap_or(1);
     let config = protocol_config.verifier_config(for_signing);
     match version {
-        1 => Box::new(v1::Verifier::new(config, metrics)),
-        2 => Box::new(latest::Verifier::new(config, metrics)),
+        1 => Box::new(latest::Verifier::new(config, metrics)),
         v => panic!("Unsupported execution version {v}"),
     }
 }
