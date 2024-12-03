ad72bb53f9ba8d7fffb17fbdef76301d17e2bcfb
jkrvivian <jkrvivian@users.noreply.github.com>
2024-12-03 15:25:01 +0000

    deploy: 1c2f8a60e37f8ace2236efc81a508710aed93844

---
+++ trait.impl/move_transactional_test_runner/framework/trait.MoveTestAdapter.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iota_transactional_test_runner",[["impl&lt;'a&gt; MoveTestAdapter&lt;'a&gt; for <a class=\"struct\" href=\"iota_transactional_test_runner/test_adapter/struct.IotaTestAdapter.html\" title=\"struct iota_transactional_test_runner::test_adapter::IotaTestAdapter\">IotaTestAdapter</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[285]}
