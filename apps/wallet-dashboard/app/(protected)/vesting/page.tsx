305f352a871462dd7c526bf10de89351825e0287
Panteleymonchuk <panteleymonchuk@gmail.com>
2024-12-04 20:23:43 +0200

    feat(dashboard): update UI after resolve conflicts.

--- apps/wallet-dashboard/app/(protected)/vesting/page.tsx
+++ apps/wallet-dashboard/app/(protected)/vesting/page.tsx
@@ -17,6 +17,22 @@ import {
 import { NotificationType } from '@/stores/notificationStore';
 import { useFeature } from '@growthbook/growthbook-react';
 import {
+    Panel,
+    Title,
+    TitleSize,
+    DisplayStats,
+    TooltipPosition,
+    Card,
+    CardImage,
+    CardAction,
+    CardActionType,
+    CardBody,
+    CardType,
+    ImageType,
+    ImageShape,
+    ButtonType,
+} from '@iota/apps-ui-kit';
+import {
     Theme,
     TIMELOCK_IOTA_TYPE,
     useFormatCoin,
@@ -28,28 +44,15 @@ import {
     useCountdownByTimestamp,
     Feature,
     ImageIcon,
+    ImageIconSize,
 } from '@iota/core';
 import { useCurrentAccount, useIotaClient, useSignAndExecuteTransaction } from '@iota/dapp-kit';
-import {
-    Panel,
-    Title,
-    DisplayStats,
-    Card,
-    CardImage,
-    CardBody,
-    CardAction,
-    CardActionType,
-    ImageShape,
-    CardType,
-    Button,
-    ButtonType,
-} from '@iota/apps-ui-kit';
 import { IotaValidatorSummary } from '@iota/iota-sdk/client';
+import { IOTA_TYPE_ARG } from '@iota/iota-sdk/utils';
 import { Calendar, StarHex } from '@iota/ui-icons';
 import { useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-import { IOTA_TYPE_ARG } from '@iota/iota-sdk/utils';
 
 function VestingDashboardPage(): JSX.Element {
     const account = useCurrentAccount();
@@ -209,72 +212,74 @@ function VestingDashboardPage(): JSX.Element {
     }, [router, supplyIncreaseVestingEnabled]);
 
     return (
-        <div className="flex w-full max-w-xl flex-col gap-lg justify-self-center">
-            <Panel>
-                <Title title="Vesting" size={TitleSize.Medium} />
-                <div className="flex flex-col gap-md p-lg pt-sm">
-                    <div className="flex h-24 flex-row gap-4">
-                        <DisplayStats
-                            label="Total Vested"
-                            value={formattedTotalVested}
-                            supportingLabel={vestedSymbol}
-                        />
-                        <DisplayStats
-                            label="Total Locked"
-                            value={formattedTotalLocked}
-                            supportingLabel={lockedSymbol}
-                            tooltipText="Total amount of IOTA that is still locked in your account."
-                            tooltipPosition={TooltipPosition.Right}
-                        />
+        <div className="flex w-full max-w-4xl items-stretch gap-xl justify-self-center">
+            <div className="flex w-1/2">
+                <Panel>
+                    <Title title="Vesting" size={TitleSize.Medium} />
+                    <div className="flex flex-col gap-md p-lg pt-sm">
+                        <div className="flex h-24 flex-row gap-4">
+                            <DisplayStats
+                                label="Total Vested"
+                                value={formattedTotalVested}
+                                supportingLabel={vestedSymbol}
+                            />
+                            <DisplayStats
+                                label="Total Locked"
+                                value={formattedTotalLocked}
+                                supportingLabel={lockedSymbol}
+                                tooltipText="Total amount of IOTA that is still locked in your account."
+                                tooltipPosition={TooltipPosition.Right}
+                            />
+                        </div>
+                        <Card type={CardType.Outlined}>
+                            <CardImage type={ImageType.BgSolid} shape={ImageShape.SquareRounded}>
+                                <StarHex className="h-5 w-5 text-primary-30 dark:text-primary-80" />
+                            </CardImage>
+                            <CardBody
+                                title={`${formattedAvailableClaiming} ${availableClaimingSymbol}`}
+                                subtitle="Available Rewards"
+                            />
+                            <CardAction
+                                type={CardActionType.Button}
+                                onClick={handleCollect}
+                                title="Collect"
+                                buttonType={ButtonType.Primary}
+                                buttonDisabled={
+                                    !vestingSchedule.availableClaiming ||
+                                    vestingSchedule.availableClaiming === 0
+                                }
+                            />
+                        </Card>
+                        <Card type={CardType.Outlined}>
+                            <CardImage type={ImageType.BgSolid} shape={ImageShape.SquareRounded}>
+                                <Calendar className="h-5 w-5 text-primary-30 dark:text-primary-80" />
+                            </CardImage>
+                            <CardBody
+                                title={`${formattedNextPayout} ${nextPayoutSymbol}`}
+                                subtitle={`Next payout ${
+                                    nextPayout?.expirationTimestampMs
+                                        ? formattedLastPayoutExpirationTime
+                                        : ''
+                                }`}
+                            />
+                            <CardAction
+                                type={CardActionType.Button}
+                                onClick={() => {
+                                    /*Open schedule dialog*/
+                                }}
+                                title="See All"
+                                buttonType={ButtonType.Secondary}
+                                buttonDisabled={
+                                    !vestingSchedule.availableStaking ||
+                                    vestingSchedule.availableStaking === 0
+                                }
+                            />
+                        </Card>
                     </div>
-                    <Card type={CardType.Outlined}>
-                        <CardImage type={ImageType.BgSolid} shape={ImageShape.SquareRounded}>
-                            <StarHex className="h-5 w-5 text-primary-30 dark:text-primary-80" />
-                        </CardImage>
-                        <CardBody
-                            title={`${formattedAvailableClaiming} ${availableClaimingSymbol}`}
-                            subtitle="Available Rewards"
-                        />
-                        <CardAction
-                            type={CardActionType.Button}
-                            onClick={handleCollect}
-                            title="Collect"
-                            buttonType={ButtonType.Primary}
-                            buttonDisabled={
-                                !vestingSchedule.availableClaiming ||
-                                vestingSchedule.availableClaiming === 0
-                            }
-                        />
-                    </Card>
-                    <Card type={CardType.Outlined}>
-                        <CardImage type={ImageType.BgSolid} shape={ImageShape.SquareRounded}>
-                            <Calendar className="h-5 w-5 text-primary-30 dark:text-primary-80" />
-                        </CardImage>
-                        <CardBody
-                            title={`${formattedNextPayout} ${nextPayoutSymbol}`}
-                            subtitle={`Next payout ${
-                                nextPayout?.expirationTimestampMs
-                                    ? formattedLastPayoutExpirationTime
-                                    : ''
-                            }`}
-                        />
-                        <CardAction
-                            type={CardActionType.Button}
-                            onClick={() => {
-                                /*Open schedule dialog*/
-                            }}
-                            title="See All"
-                            buttonType={ButtonType.Secondary}
-                            buttonDisabled={
-                                !vestingSchedule.availableStaking ||
-                                vestingSchedule.availableStaking === 0
-                            }
-                        />
-                    </Card>
-                </div>
-            </Panel>
-            {timelockedstakedMapped.length === 0 ? (
-                <>
+                </Panel>
+            </div>
+            <div className="flex w-1/2">
+                {timelockedstakedMapped.length === 0 ? (
                     <Banner
                         videoSrc={videoSrc}
                         title="Stake Vested Tokens"
@@ -284,88 +289,42 @@ function VestingDashboardPage(): JSX.Element {
                         }}
                         buttonText="Stake"
                     />
-                </>
-            ) : (
-                <div className="flex w-1/2 flex-col items-center justify-center space-y-4 pt-12">
-                    <h1>Staked Vesting</h1>
-                    <div className="flex flex-row space-x-4">
-                        <div className="flex flex-col items-center rounded-lg border p-4">
-                            <span>Your stake</span>
-                            <span>{vestingSchedule.totalStaked}</span>
-                        </div>
-                        <div className="flex flex-col items-center rounded-lg border p-4">
-                            <span>Total Unlocked</span>
-                            <span>{vestingSchedule.totalUnlocked}</span>
-                        </div>
-                    </div>
-                    <div className="flex w-full flex-col items-center justify-center space-y-4 pt-4">
-                        {timelockedStakedObjectsGrouped?.map((timelockedStakedObject) => {
-                            return (
-                                <div
-                                    key={
-                                        timelockedStakedObject.validatorAddress +
-                                        timelockedStakedObject.stakeRequestEpoch +
-                                        timelockedStakedObject.label
-                                    }
-                                    className="flex w-full flex-row items-center justify-center space-x-4"
-                                >
-                                    <span>
-                                        Validator:{' '}
-                                        {getValidatorByAddress(
-                                            timelockedStakedObject.validatorAddress,
-                                        )?.name || timelockedStakedObject.validatorAddress}
-                                    </span>
-                                    <span>
-                                        Stake Request Epoch:{' '}
-                                        {timelockedStakedObject.stakeRequestEpoch}
-                                    </span>
-                                    <span>Stakes: {timelockedStakedObject.stakes.length}</span>
-
-                                    <Button
-                                        onClick={() => handleUnstake(timelockedStakedObject)}
-                                        text="Unstake"
-                                    />
-                                </div>
-                            );
-                        })}
-                        <Panel>
-                            <Title title="Staked Vesting" />
-                            <div className="flex flex-col px-lg py-sm">
-                                <div className="flex flex-row gap-md">
-                                    <DisplayStats
-                                        label="Your stake"
-                                        value={`${totalStakedFormatted} ${totalStakedSymbol}`}
-                                    />
-                                    <DisplayStats
-                                        label="Total Unlocked"
-                                        value={`${totalUnlockedFormatted} ${totalUnlockedSymbol}`}
-                                    />
-                                </div>
+                ) : (
+                    <Panel>
+                        <Title title="Staked Vesting" />
+                        <div className="flex flex-col px-lg py-sm">
+                            <div className="flex flex-row gap-md">
+                                <DisplayStats
+                                    label="Your stake"
+                                    value={`${totalStakedFormatted} ${totalStakedSymbol}`}
+                                />
+                                <DisplayStats
+                                    label="Earned"
+                                    value={`${totalUnlockedFormatted} ${totalUnlockedSymbol}`}
+                                />
                             </div>
-                            <div className="flex flex-col px-lg py-sm">
-                                <div className="flex w-full flex-col items-center justify-center space-y-4 pt-4">
-                                    {timelockedStakedObjectsGrouped?.map(
-                                        (timelockedStakedObject) => {
-                                            return (
-                                                <TimelockedStakedObject
-                                                    key={
-                                                        timelockedStakedObject.validatorAddress +
-                                                        timelockedStakedObject.stakeRequestEpoch +
-                                                        timelockedStakedObject.label
-                                                    }
-                                                    getValidatorByAddress={getValidatorByAddress}
-                                                    timelockedStakedObject={timelockedStakedObject}
-                                                    handleUnstake={handleUnstake}
-                                                />
-                                            );
-                                        },
-                                    )}
-                                </div>
+                        </div>
+                        <div className="flex flex-col px-lg py-sm">
+                            <div className="flex w-full flex-col items-center justify-center space-y-4 pt-4">
+                                {timelockedStakedObjectsGrouped?.map((timelockedStakedObject) => {
+                                    return (
+                                        <TimelockedStakedObject
+                                            key={
+                                                timelockedStakedObject.validatorAddress +
+                                                timelockedStakedObject.stakeRequestEpoch +
+                                                timelockedStakedObject.label
+                                            }
+                                            getValidatorByAddress={getValidatorByAddress}
+                                            timelockedStakedObject={timelockedStakedObject}
+                                            handleUnstake={handleUnstake}
+                                        />
+                                    );
+                                })}
                             </div>
-                        </Panel>
-                    </div>
-                </div>
-            )}
+                        </div>
+                    </Panel>
+                )}
+            </div>
         </div>
     );
 }
