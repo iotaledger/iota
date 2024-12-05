f49f2d1bc7cd2670df4f4d07fdc8a3d34d0a9716
Dr-Electron <dr-electr0n@protonmail.com>
2024-12-05 19:16:16 +0100

    Update docs/site/configs/preContent.tsx
    Co-authored-by: Lucas Tortora <85233773+lucas-tortora@users.noreply.github.com>

--- docs/site/configs/preContent.tsx
+++ docs/site/configs/preContent.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import Admonition from '@theme/Admonition';
 
 export default {
-  '/identity*': <Admonition type='info'>IOTA Identity is currently in alpha and may still be subject to significant changes</Admonition>,
+  '/iota-identity*': <Admonition type='info'>IOTA Identity is currently in alpha and may still be subject to significant changes</Admonition>,
 };
