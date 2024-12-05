4fd9abc49e812035cc111ad628d779f46d940c77
Dr-Electron <dr-electr0n@protonmail.com>
2024-12-05 19:25:28 +0100

    feat(docs): Allow custom components before DocItems (#4144)

---
+++ docs/site/configs/preContent.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Admonition from '@theme/Admonition';
+
+export default {
+  '/iota-identity*': <Admonition type='info'>IOTA Identity is currently in alpha and may still be subject to significant changes</Admonition>,
+};
