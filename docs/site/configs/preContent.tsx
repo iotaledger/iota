8e39d5db60c2495fe50a2298f23d33d2f7a872ee
Eike Haß <eike-hass@web.de>
2024-12-05 19:31:12 +0100

    update Identity banner

--- docs/site/configs/preContent.tsx
+++ docs/site/configs/preContent.tsx
@@ -1,6 +1,8 @@
+//[object Object]
+//[object Object]
 import React from 'react';
 import Admonition from '@theme/Admonition';
 
 export default {
-  '/iota-identity*': <Admonition type='info'>IOTA Identity is currently in alpha and may still be subject to significant changes</Admonition>,
+  '/iota-identity*': <Admonition type='info'>IOTA Identity for Rebased is currently in alpha and may still be subject to significant changes</Admonition>,
 };
