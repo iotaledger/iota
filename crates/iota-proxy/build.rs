67373a32ebb6d4fa07655e651eb642a2bbece8c1
jkrvivian <jkrvivian@gmail.com>
2024-12-09 16:52:53 +0800

    chores: Add newline after headers

--- crates/iota-proxy/build.rs
+++ crates/iota-proxy/build.rs
@@ -1,6 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
+
 use std::io::Result;
 fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=build.rs");
