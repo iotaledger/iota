3331460b9b44f1ec84b82aa40b59a5c718ece974
nonast <29281463+nonast@users.noreply.github.com>
2024-12-17 18:14:01 +0800

    fix clippy issue

--- crates/iota/tests/cli_tests.rs
+++ crates/iota/tests/cli_tests.rs
@@ -163,6 +163,7 @@ async fn test_start() -> Result<(), anyhow::Error> {
             indexer_feature_args: IndexerFeatureArgs::for_testing(),
             local_migration_snapshots: vec![],
             remote_migration_snapshots: vec![],
+            delegator: None,
         }
         .execute(),
     )
