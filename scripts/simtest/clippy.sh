fe8672998a3bf41ebcf58053c95e9721f914f761
Thibault Martinez <thibault@iota.org>
2024-12-18 09:40:37 +0100

    remove echo (to allow colors)

--- scripts/simtest/clippy.sh
+++ scripts/simtest/clippy.sh
@@ -5,7 +5,7 @@
 # verify that git repo is clean
 if [[ -n $(git status -s) ]]; then
   echo "Working directory is not clean. Please commit all changes before running this script."
-  echo $(git status -s)
+  git status -s
   exit 1
 fi
 
