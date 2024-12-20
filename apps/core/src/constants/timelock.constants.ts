3dfce3318a8ef705d6d54270ce5a753a553a3d85
cpl121 <cpeon@boxfish.studio>
2024-12-20 10:17:51 +0100

    fix(core): linter

--- apps/core/src/constants/timelock.constants.ts
+++ apps/core/src/constants/timelock.constants.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { COIN_TYPE } from "./coins.constants";
+import { COIN_TYPE } from './coins.constants';
 
 export const TIMELOCK_IOTA_TYPE = `0x2::timelock::TimeLock<0x2::balance::Balance<${COIN_TYPE}>>`;
 export const TIMELOCK_STAKED_TYPE = '0x3::timelocked_staking::TimelockedStakedIota';
