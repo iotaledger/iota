eff6743c94ca246d9b6a0a0e211bb0f4415b133e
Valerii Reutov <valeriy.reutov@gmail.com>
2024-12-20 15:00:17 +0200

    fix: license headers

--- external-crates/move/crates/move-binary-format/src/compatibility_mode.rs
+++ external-crates/move/crates/move-binary-format/src/compatibility_mode.rs
@@ -1,3 +1,7 @@
+// Copyright (c) The Move Contributors
+// Modifications Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::compatibility::Compatibility;
 use crate::file_format::Visibility;
 use crate::normalized::{Enum, Function, Struct};
--- external-crates/move/crates/move-core-types/src/unit_tests/parsing_test.rs
+++ external-crates/move/crates/move-core-types/src/unit_tests/parsing_test.rs
@@ -1,3 +1,7 @@
+// Copyright (c) The Move Contributors
+// Modifications Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::{
     account_address::AccountAddress,
     identifier::Identifier,
