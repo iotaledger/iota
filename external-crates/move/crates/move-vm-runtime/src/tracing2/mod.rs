15f8373a6d379873a5ce06caaecc1c456cf09d2e
Valerii Reutov <valeriy.reutov@gmail.com>
2024-12-20 15:00:17 +0200

    fix: license headers

--- external-crates/move/crates/move-vm-runtime/src/tracing2/mod.rs
+++ external-crates/move/crates/move-vm-runtime/src/tracing2/mod.rs
@@ -1,3 +1,7 @@
+// Copyright (c) The Move Contributors
+// Modifications Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 pub(crate) mod tracer;
 
 #[cfg(feature = "tracing")]
