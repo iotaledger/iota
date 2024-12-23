cf069168f722007ece6e723fdeaa54d0057dd3ee
Thibault Martinez <thibault@iota.org>
2024-12-23 17:08:22 +0100

    chore: add rust examples to workspace

--- examples/custom-indexer/rust/local_reader.rs
+++ examples/custom-indexer/rust/local_reader.rs
@@ -2,16 +2,17 @@
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use tokio::sync::oneshot;
+use std::{env, path::PathBuf};
+
 use anyhow::Result;
 use async_trait::async_trait;
-use iota_types::full_checkpoint_content::CheckpointData;
 use iota_data_ingestion_core as sdic;
-use sdic::{Worker, WorkerPool, ReaderOptions};
-use sdic::{DataIngestionMetrics, FileProgressStore, IndexerExecutor};
+use iota_types::full_checkpoint_content::CheckpointData;
 use prometheus::Registry;
-use std::path::PathBuf;
-use std::env;
+use sdic::{
+    DataIngestionMetrics, FileProgressStore, IndexerExecutor, ReaderOptions, Worker, WorkerPool,
+};
+use tokio::sync::oneshot;
 
 struct CustomWorker;
 
@@ -19,29 +20,38 @@ struct CustomWorker;
 impl Worker for CustomWorker {
     async fn process_checkpoint(&self, checkpoint: CheckpointData) -> Result<()> {
         // custom processing logic
-        println!("Processing Local checkpoint: {}", checkpoint.checkpoint_summary.to_string());
+        println!(
+            "Processing Local checkpoint: {}",
+            checkpoint.checkpoint_summary.to_string()
+        );
         Ok(())
     }
 }
-        
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let concurrency = 5;
-    let (exit_sender, exit_receiver) = oneshot::channel();
+    let (_, exit_receiver) = oneshot::channel();
     let metrics = DataIngestionMetrics::new(&Registry::new());
     let backfill_progress_file_path =
         env::var("BACKFILL_PROGRESS_FILE_PATH").unwrap_or("/tmp/local_reader_progress".to_string());
     let progress_store = FileProgressStore::new(PathBuf::from(backfill_progress_file_path));
-    let mut executor = IndexerExecutor::new(progress_store, 1 /* number of workflow types */, metrics);
+    let mut executor = IndexerExecutor::new(
+        progress_store,
+        1, // number of workflow types
+        metrics,
+    );
     let worker_pool = WorkerPool::new(CustomWorker, "local_reader".to_string(), concurrency);
 
     executor.register(worker_pool).await?;
-    executor.run(
-        PathBuf::from("./chk".to_string()), // path to a local directory
-        None,
-        vec![], // optional remote store access options
-        ReaderOptions::default(),       /* remote_read_batch_size */
-        exit_receiver,
-        ).await?;
+    executor
+        .run(
+            PathBuf::from("./chk".to_string()), // path to a local directory
+            None,
+            vec![],                   // optional remote store access options
+            ReaderOptions::default(), // remote_read_batch_size
+            exit_receiver,
+        )
+        .await?;
     Ok(())
 }
