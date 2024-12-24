d3bf42f8db9f41b8f8684f71890ca8b19f1f35a8
DaughterOfMars <chloedaughterofmars@gmail.com>
2024-12-24 08:56:37 -0500

    Update crates/iota/src/completions.rs
    oop
    
    Co-authored-by: Thibault Martinez <thibault@iota.org>

--- crates/iota/src/completions.rs
+++ crates/iota/src/completions.rs
@@ -46,7 +46,7 @@ impl Generator for GenShell {
 #[derive(Debug, Parser)]
 pub struct CompletionCommand {
     /// The shell for which completions will be generated. By default all will
-    /// generated.
+    /// be generated.
     #[arg(long, short)]
     shell: Option<GenShell>,
     /// The output directory for the completions file.
