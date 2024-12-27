29660a13d4122a5e2d288836a8f426825d3106e1
Begoña Alvarez <balvarez@boxfish.studio>
2024-12-27 11:57:39 +0000

    refactor: add 0x check

--- apps/wallet-dashboard/lib/utils/migration/groupMigrationObjects.ts
+++ apps/wallet-dashboard/lib/utils/migration/groupMigrationObjects.ts
@@ -222,7 +222,9 @@ async function extractNativeTokensFromObject(
             result.push({
                 name: tokenName,
                 balance,
-                coinType: `0x${nativeTokenFields.name}`,
+                coinType: nativeTokenFields.name.startsWith('0x')
+                    ? nativeTokenFields.name
+                    : `0x${nativeTokenFields.name}`,
                 unlockConditionTimestamp: expirationKey,
                 commonObjectType: CommonMigrationObjectType.NativeToken,
                 output: object,
