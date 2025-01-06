52f788f8d17c4203c001cf28972397e515729833
jkrvivian <jkrvivian@gmail.com>
2025-01-06 14:47:02 +0800

    fix: Use python_venv_wrapper script to set up python environments

--- scripts/track_upstream_commits/run.sh
+++ scripts/track_upstream_commits/run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-
-# Check if either "python" or "python3" exists and use it
-if command -v python3 &>/dev/null; then
-    PYTHON_CMD="python3"
-elif command -v python &>/dev/null; then
-    PYTHON_CMD="python"
-else
-    echo "Neither python nor python3 binary is installed. Please install Python."
-    exit 1
-fi
+# OPTIONS:
+#   -h, --help            show this help message and exit
+#   --since SINCE         Start date for git log (e.g., "2024-09-05").
+#   --until UNTIL         End date for git log (e.g., "2024-10-26").
+#   --codeowner CODEOWNER
+#                         code owner of the crates (e.g., "core-node)
+#   --repo-url REPO_URL   The URL to the repository. Can also be a local folder.
+#   --repo-tag REPO_TAG   The tag to checkout in the repository.
+#   --version VERSION     The semantic version to filter overwrites/patches if not found in the repo-tag.
+#   --target-folder TARGET_FOLDER
+#                         The path to the target folder.
+#   --clone-source        Clone the upstream repository.
+#   --compare-source-folder COMPARE_SOURCE_FOLDER
+#                         The path to the source folder for comparison.
+source python_venv_wrapper.sh
 
 $PYTHON_CMD track_upstream_commits.py \
     --repo-tag "mainnet-v1.36.2" \
