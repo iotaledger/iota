1616a60bdd03262303887caff04833d0ef473347
Valerii Reutov <valeriy.reutov@gmail.com>
2025-01-06 17:02:18 +0200

    fix(iota-e2e-tests): fix the `basic_read_cmd_snapshot_tests` test

--- crates/iota-e2e-tests/tests/snapshot_tests.rs
+++ crates/iota-e2e-tests/tests/snapshot_tests.rs
@@ -63,10 +63,10 @@ async fn basic_read_cmd_snapshot_tests() -> Result<(), anyhow::Error> {
         "iota client objects 0x0000000000000000000000000000000000000000000000000000000000000000", /* empty addr */
         "iota client object 0x5",       // valid object
         "iota client object 0x5 --bcs", // valid object BCS
-        "iota client object 0x9135cb3b5aca99a1555b742bd11ddc45fba33343be182bdc161be69da2c41be1", /* valid object */
-        "iota client object 0x9135cb3b5aca99a1555b742bd11ddc45fba33343be182bdc161be69da2c41be1 --bcs", /* valid object BCS */
+        "iota client object 0xbeba8585ae10ad1f9fd71d8ae1932c09389e4c5f56f0c90eba1a67acd936b7e7", /* valid object */
+        "iota client object 0xbeba8585ae10ad1f9fd71d8ae1932c09389e4c5f56f0c90eba1a67acd936b7e7 --bcs", /* valid object BCS */
         "iota client object 0x0000000000000000000000000000000000000000000000000000000000000000", /* non-existent object */
-        "iota client tx-block E5Zp4QQ84PQEceSw4JRi4VTScSAQweKSgdwp9XH4aVPd", // valid tx digest
+        "iota client tx-block 21gG732sysdKxham2pmVdiy9FKwLapMPvhd6mbhjnYEb", // valid tx digest
         "iota client tx-block 11111111111111111111111111111111",             /* non-existent tx
                                                                               * digest */
     ];
