1d769126f254378d0269c03a25c403cfd3ba4f4f
Mario Sarcevic <mario.sarcevic@iota.org>
2025-01-07 14:23:37 +0100

    feat(wallet-dashboard): Unskip and fix broken tests in vesting.spec.ts

--- apps/wallet-dashboard/lib/utils/vesting/buildMockedObjectsWithDynamicDateUtils.ts
+++ apps/wallet-dashboard/lib/utils/vesting/buildMockedObjectsWithDynamicDateUtils.ts
@@ -1,14 +1,33 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { MILLISECONDS_PER_DAY } from '@iota/core/constants/time.constants';
+import { TimelockedObject } from '@/lib/interfaces';
+import { DAYS_PER_WEEK, MILLISECONDS_PER_DAY } from '@iota/core/constants/time.constants';
 import { DelegatedTimelockedStake } from '@iota/iota-sdk/client';
+
+/**
+ * Rebuilds the passed objects to spread the expiration dates from Date.now()
+ * to array length with a step of two weeks.
+ */
+export function getMockedSupplyIncreaseVestingTimelockedObjectsWithDynamicDate(
+    vestingObjects: TimelockedObject[],
+): TimelockedObject[] {
+    const twoWeeksMs = 2 * DAYS_PER_WEEK * MILLISECONDS_PER_DAY;
+    const twoWeeksFromNow = Date.now() + twoWeeksMs;
+
+    return structuredClone(vestingObjects)
+        .map((object, idx) => {
+            object.expirationTimestampMs = twoWeeksFromNow - idx * twoWeeksMs;
+            return object;
+        })
+        .reverse();
+}
 
 /**
  * Gets the objects in a distributed manner with half of the objects
  * being unlocked and the other half being locked.
  */
-export function getMockedTimelockedStakedObjectsWithDynamicDate(
+export function getMockedVestingTimelockedStakedObjectsWithDynamicDate(
     delegatedObjects: DelegatedTimelockedStake[],
 ): DelegatedTimelockedStake[] {
     const now = Date.now();
