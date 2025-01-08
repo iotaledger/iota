eec3b3b4184a6be8cc75985c80d125137eba3779
evavirseda <evirseda@boxfish.studio>
2025-01-08 12:08:45 +0100

    remvoe debris

--- apps/wallet/src/ui/app/helpers/index.ts
+++ apps/wallet/src/ui/app/helpers/index.ts
@@ -2,7 +2,6 @@
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-// export * from './getEventsSummary';
 export * from './getAmount';
 export * from './formatAccountName';
 export * from './accounts';
