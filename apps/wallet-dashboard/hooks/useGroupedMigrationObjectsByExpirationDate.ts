11662beb36048780fd4759df782f297cd935c0cc
evavirseda <evirseda@boxfish.studio>
2025-01-09 11:39:04 +0100

    fixes

--- apps/wallet-dashboard/hooks/useGroupedMigrationObjectsByExpirationDate.ts
+++ apps/wallet-dashboard/hooks/useGroupedMigrationObjectsByExpirationDate.ts
@@ -45,6 +45,7 @@ export function useGroupedMigrationObjectsByExpirationDate(
                 client,
                 address,
                 isTimelockUnlockCondition,
+                epochStartMs,
             );
 
             return sortStardustResolvedObjectsByExpiration(
