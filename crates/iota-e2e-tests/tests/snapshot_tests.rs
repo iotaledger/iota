6cb9ebf77267a6e20bed769b6a3cf003fce7f699
Valerii Reutov <valeriy.reutov@gmail.com>
2025-01-09 11:22:32 +0200

    feat: create the protocol version `3`

--- crates/iota-e2e-tests/tests/snapshot_tests.rs
+++ crates/iota-e2e-tests/tests/snapshot_tests.rs
@@ -63,10 +63,10 @@ async fn basic_read_cmd_snapshot_tests() -> Result<(), anyhow::Error> {
         "iota client objects 0x0000000000000000000000000000000000000000000000000000000000000000", /* empty addr */
         "iota client object 0x5",       // valid object
         "iota client object 0x5 --bcs", // valid object BCS
-        "iota client object 0x9135cb3b5aca99a1555b742bd11ddc45fba33343be182bdc161be69da2c41be1", /* valid object */
-        "iota client object 0x9135cb3b5aca99a1555b742bd11ddc45fba33343be182bdc161be69da2c41be1 --bcs", /* valid object BCS */
+        "iota client object 0x343fa277d590781d9f0b38c3f37c2edd3f995b25e5a48a830c565cae5cf21786", /* valid object */
+        "iota client object 0x343fa277d590781d9f0b38c3f37c2edd3f995b25e5a48a830c565cae5cf21786 --bcs", /* valid object BCS */
         "iota client object 0x0000000000000000000000000000000000000000000000000000000000000000", /* non-existent object */
-        "iota client tx-block E5Zp4QQ84PQEceSw4JRi4VTScSAQweKSgdwp9XH4aVPd", // valid tx digest
+        "iota client tx-block 4wRZTFvEK46pGYyJ19eSjtfoL9TuV8gx6hatLRdkyWL5", // valid tx digest
         "iota client tx-block 11111111111111111111111111111111",             /* non-existent tx
                                                                               * digest */
     ];
