45c32789fb935a51dce20c5059c46a39a48ac65b
cpl121 <cpeon@boxfish.studio>
2025-01-13 10:55:52 +0100

    fix(wallet): remove duplicate formatDate function

--- apps/wallet/src/ui/app/helpers/index.ts
+++ apps/wallet/src/ui/app/helpers/index.ts
@@ -2,7 +2,6 @@
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-export { default as formatDate } from './formatDate';
 export { default as notEmpty } from './notEmptyCheck';
 // export { getEventsSummary } from './getEventsSummary';
 export { getAmount } from './getAmount';
