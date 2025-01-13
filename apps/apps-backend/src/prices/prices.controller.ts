7e8290ed30707cf27c1e627f97a2f14cecfa1def
JCNoguera <88061365+VmMad@users.noreply.github.com>
2025-01-13 10:42:47 +0100

    fix: import

--- apps/apps-backend/src/prices/prices.controller.ts
+++ apps/apps-backend/src/prices/prices.controller.ts
@@ -5,7 +5,7 @@ import { Controller, Get, Inject, Param } from '@nestjs/common';
 import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
 import { CoinGeckoService } from '../coingecko/coingecko.service';
 import { TOKEN_PRICE_COINS, tokenPriceKey } from '../constants';
-import { FiatTokenName } from '@iota/core';
+import { FiatTokenName } from '@iota/core/enums/fiatTokenName.enums';
 
 const ONE_HOUR_IN_MS = 1000 * 60 * 60;
 
