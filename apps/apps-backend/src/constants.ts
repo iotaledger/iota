31d97cb96813037927b40a1d05388e0a506d7901
JCNoguera <88061365+VmMad@users.noreply.github.com>
2025-01-13 10:35:26 +0100

    fix: import error

--- apps/apps-backend/src/constants.ts
+++ apps/apps-backend/src/constants.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { FiatTokenName } from '@iota/core';
+import { FiatTokenName } from '@iota/core/enums/fiatTokenName.enums';
 
 export const tokenPriceKey = (coinName: string) => `tokenPrice${coinName}`;
 export const TOKEN_PRICE_CURRENCY = 'usd';
