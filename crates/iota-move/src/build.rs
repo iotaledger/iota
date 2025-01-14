8157e308939ae419d96ffb2990beb7af19da23cd
Thibault Martinez <thibault@iota.org>
2025-01-14 11:05:24 +0100

    fmt

--- crates/iota-move/src/build.rs
+++ crates/iota-move/src/build.rs
@@ -27,16 +27,15 @@ pub struct Build {
     #[clap(long, global = true)]
     pub dump_bytecode_as_base64: bool,
     /// Don't specialize the package to the active chain when dumping bytecode
-    /// as Base64. This allows building to proceed without a network
-    /// connection or active environment, but it will not be able to
-    /// automatically determine the addresses of its dependencies.
+    /// as Base64. This allows building to proceed without a network connection
+    /// or active environment, but it will not be able to automatically
+    /// determine the addresses of its dependencies.
     #[arg(long, global = true, requires = "dump_bytecode_as_base64")]
     pub ignore_chain: bool,
-    /// If true, generate struct layout schemas for
-    /// all struct types passed into `entry` functions declared by modules in
-    /// this package These layout schemas can be consumed by clients (e.g.,
-    /// the TypeScript SDK) to enable serialization/deserialization of
-    /// transaction arguments and events.
+    /// If true, generate struct layout schemas for all struct types passed into
+    /// `entry` functions declared by modules in this package These layout
+    /// schemas can be consumed by clients (e.g., the TypeScript SDK) to enable
+    /// serialization/deserialization of transaction arguments and events.
     #[clap(long, global = true)]
     pub generate_struct_layouts: bool,
     /// The chain ID, if resolved. Required when the dump_bytecode_as_base64 is
