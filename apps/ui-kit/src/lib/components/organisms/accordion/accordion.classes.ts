ec944077878b779df17f861fd523f5579486d7fd
Begoña Alvarez <balvarez@boxfish.studio>
2025-01-14 12:19:38 +0100

    test: ci

--- apps/ui-kit/src/lib/components/organisms/accordion/accordion.classes.ts
+++ apps/ui-kit/src/lib/components/organisms/accordion/accordion.classes.ts
@@ -1,4 +1,4 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-export const ICON_STYLE = 'dark:text-neutral-92 transition-transform ease-linear';
+export const ICON_STYLE = 'transition-transform ease-linear';
