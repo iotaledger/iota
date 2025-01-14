a8d33036b54d7a794c485d5feb9d9490490eb371
Bing-Yang Lin <bingyang.lin@iota.org>
2025-01-14 23:33:57 +0800

    Enhance tag

--- crates/iota-core/src/authority/authority_per_epoch_store.rs
+++ crates/iota-core/src/authority/authority_per_epoch_store.rs
@@ -131,7 +131,7 @@ pub(crate) type EncG = bls12381::G2Element;
 // retain a distinction anyway. If we need to support distributed object
 // storage, having this distinction will be useful, as we will most likely have
 // to re-implement a retry / write-ahead-log at that point.
-pub struct CertLockGuard(#[allow(unused)] MutexGuard);
+pub struct CertLockGuard(#[expect(unused)] MutexGuard);
 pub struct CertTxGuard(CertLockGuard);
 
 impl CertTxGuard {
