f3a43f219dd65fd2417d6ac2fbd723321d7bdfee
evavirseda <evirseda@boxfish.studio>
2025-01-14 09:22:50 +0100

    feat: add burnt and minted tokens

--- apps/explorer/src/pages/epochs/stats/EpochTopStats.tsx
+++ apps/explorer/src/pages/epochs/stats/EpochTopStats.tsx
@@ -5,11 +5,13 @@
 import { ProgressBar } from '~/components';
 import { EpochStatsGrid } from './EpochStats';
 import { LabelText, LabelTextSize } from '@iota/apps-ui-kit';
-import { formatDate } from '@iota/core';
+import { Feature, formatDate } from '@iota/core';
 import { TokenStats } from './TokenStats';
 import { getSupplyChangeAfterEpochEnd } from '~/lib';
 import { useEpochProgress } from '../utils';
-import type { EndOfEpochInfo } from '@iota/iota-sdk/src/client';
+import type { Network, EndOfEpochInfo } from '@iota/iota-sdk/client';
+import { useFeature } from '@growthbook/growthbook-react';
+import { useNetworkContext } from '~/contexts';
 
 interface EpochProgressProps {
     start: number;
@@ -25,7 +27,18 @@ export function EpochTopStats({
     endOfEpochInfo,
 }: EpochProgressProps): React.JSX.Element {
     const { progress, label } = useEpochProgress();
+    const [network] = useNetworkContext();
+
     const endTime = inProgress ? label : end ? formatDate(end) : undefined;
+
+    const featureBurntAndMintedTokensInEndedEpochsEnabled = useFeature<{
+        [key in Network]: boolean;
+    }>(Feature.BurntAndMintedTokensInEndedEpochs).value;
+
+    const isFeatureEnabledForCurrentNetwork =
+        featureBurntAndMintedTokensInEndedEpochsEnabled?.[
+            network as keyof typeof featureBurntAndMintedTokensInEndedEpochsEnabled
+        ];
 
     return (
         <div className="flex w-full flex-col gap-md--rs">
@@ -35,12 +48,30 @@ export function EpochTopStats({
                 <LabelText text={formatDate(start)} label="Start" />
                 {endTime ? <LabelText text={endTime} label="End" /> : null}
                 {endOfEpochInfo && (
-                    <TokenStats
-                        label="Supply Change"
-                        size={LabelTextSize.Large}
-                        amount={getSupplyChangeAfterEpochEnd(endOfEpochInfo)}
-                        showSign
-                    />
+                    <>
+                        {isFeatureEnabledForCurrentNetwork && (
+                            <>
+                                <TokenStats
+                                    label="Burnt Tokens"
+                                    size={LabelTextSize.Large}
+                                    amount={BigInt(endOfEpochInfo?.burntTokensAmount)}
+                                    showSign
+                                />
+                                <TokenStats
+                                    label="Minted Tokens"
+                                    size={LabelTextSize.Large}
+                                    amount={BigInt(endOfEpochInfo?.mintedTokensAmount)}
+                                    showSign
+                                />
+                            </>
+                        )}
+                        <TokenStats
+                            label="Supply Change"
+                            size={LabelTextSize.Large}
+                            amount={getSupplyChangeAfterEpochEnd(endOfEpochInfo)}
+                            showSign
+                        />
+                    </>
                 )}
             </EpochStatsGrid>
         </div>
