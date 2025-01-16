5c26c771f911abbeb9b8550f0361e272b439754e
cpl121 <cpeon@boxfish.studio>
2025-01-16 12:46:14 +0100

    fix(core): update icon for unstaking actions

--- apps/core/src/components/transaction/TransactionIcon.tsx
+++ apps/core/src/components/transaction/TransactionIcon.tsx
@@ -9,6 +9,7 @@ import {
     Migration,
     Person,
     Stake,
+    Unstake,
     Vesting,
 } from '@iota/ui-icons';
 import { TransactionAction } from '../../interfaces';
@@ -23,12 +24,12 @@ const icons = {
     [TransactionAction.Receive]: <ArrowBottomLeft className={ICON_COLORS.primary} />,
     [TransactionAction.Transaction]: <ArrowTopRight className={ICON_COLORS.primary} />,
     [TransactionAction.Staked]: <Stake className={ICON_COLORS.primary} />,
-    [TransactionAction.Unstaked]: <Stake className={ICON_COLORS.primary} />,
+    [TransactionAction.Unstaked]: <Unstake className={ICON_COLORS.primary} />,
     [TransactionAction.Rewards]: <IotaLogoMark className={ICON_COLORS.primary} />,
     [TransactionAction.Failed]: <Info className={ICON_COLORS.error} />,
     [TransactionAction.PersonalMessage]: <Person className={ICON_COLORS.primary} />,
     [TransactionAction.TimelockedStaked]: <Stake className={ICON_COLORS.primary} />,
-    [TransactionAction.TimelockedUnstaked]: <Stake className={ICON_COLORS.primary} />,
+    [TransactionAction.TimelockedUnstaked]: <Unstake className={ICON_COLORS.primary} />,
     [TransactionAction.Migration]: <Migration className={ICON_COLORS.primary} />,
     [TransactionAction.TimelockedCollect]: <Vesting className={ICON_COLORS.primary} />,
 };
