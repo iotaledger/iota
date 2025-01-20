afb3c0f464f9c97c31d279e3ba67f9234d3dd588
junwei0117 <v860117@gmail.com>
2025-01-20 07:25:43 +0000

    fix: fix clippy

--- crates/iota-tool/src/commands.rs
+++ crates/iota-tool/src/commands.rs
@@ -859,7 +859,7 @@ impl ToolCommand {
                         env::var("AWS_ARCHIVE_VIRTUAL_HOSTED_REQUESTS")
                             .ok()
                             .and_then(|b| b.parse().ok())
-                            .unwrap_or_else(|| matches!(network, Chain::Mainnet | Chain::Testnet));
+                            .unwrap_or(matches!(network, Chain::Mainnet | Chain::Testnet));
 
                     ObjectStoreConfig {
                         object_store: Some(ObjectStoreType::S3),
