b70b25df28cdaa565dd1278c28ff08d555f9e39e
muXxer <git@muxxer.de>
2025-01-21 14:34:20 +0100

    fix: rename `container-name` parameter to `image-tag`

--- docker/iota-rosetta/build.sh
+++ docker/iota-rosetta/build.sh
@@ -2,4 +2,4 @@
 # Copyright (c) Mysten Labs, Inc.
 # Modifications Copyright (c) 2024 IOTA Stiftung
 # SPDX-License-Identifier: Apache-2.0
-./../../docker/utils/build-script.sh --container-name "iotaledger/iota-rosetta"
+./../../docker/utils/build-script.sh --image-tag "iotaledger/iota-rosetta"
