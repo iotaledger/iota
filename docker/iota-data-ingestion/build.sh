7a89b79b76dab6913cf50a0f97787327c25a22de
Sergiu Popescu <sergiu.popescu@iota.org>
2025-01-21 14:25:04 +0100

    fixup! feat: Add build.sh script

--- docker/iota-data-ingestion/build.sh
+++ docker/iota-data-ingestion/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Copyright (c) 2024 IOTA Stiftung
 # SPDX-License-Identifier: Apache-2.0
-./../utils/build-script.sh --container-name "iotaledger/checkpoints-remote-store"
+./../utils/build-script.sh --container-name "iotaledger/iota-data-ingestion"
