5de35fd092fce5722ffa52578c7da6907a9be176
marc2332 <mespinsanz@gmail.com>
2025-01-24 15:33:31 +0100

    chore: Embedded sentry traces rate

--- apps/wallet-dashboard/sentry.client.config.ts
+++ apps/wallet-dashboard/sentry.client.config.ts
@@ -4,8 +4,6 @@
 
 import * as Sentry from '@sentry/nextjs';
 import { IS_PROD, SENTRY_DSN } from './sentry.common.config.mjs';
-import { growthbook } from './lib/utils';
-import { Feature } from '@iota/core';
 
 
 Sentry.init({
@@ -13,9 +11,7 @@ Sentry.init({
     dsn: SENTRY_DSN,
 
     // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-    tracesSampleRate: () => {
-        return growthbook.getFeatureValue(Feature.WalletSentryTracing, 0);
-    },
+    tracesSampleRate: 0.0025,
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
