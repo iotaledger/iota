9608a803f0e002c77c0c8385045f6c124e27f01e
Lucas Tortora <lucas.tortora@iota.org>
2025-01-30 12:04:37 -0300

    update admonition

--- docs/site/configs/preContent.tsx
+++ docs/site/configs/preContent.tsx
@@ -6,5 +6,5 @@ import Admonition from '@theme/Admonition';
 
 export default {
   '/iota-identity*': <Admonition type='info'>IOTA Identity for Rebased is currently in alpha and may still be subject to significant changes.</Admonition>,
-  '/iota-evm*': <Admonition type='info'>IOTA EVM is not available on the IOTA Rebased Tesnet at the moment.</Admonition>,
+  '/iota-evm*': <Admonition type='info'>IOTA EVM is not available on the IOTA Rebased Tesnet at the moment, please use the [IOTA EVM and Shimmer EVM Networks](https://wiki.iota.org/build/networks-endpoints/).</Admonition>,
 };
