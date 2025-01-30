39eca0004a930966f0727d641861e8a682745753
cpl121 <cpeon@boxfish.studio>
2025-01-30 11:22:27 +0100

    fix(dashboard): lint

--- apps/wallet-dashboard/prependLicense.mjs
+++ apps/wallet-dashboard/prependLicense.mjs
@@ -3,8 +3,7 @@
 
 import { readFile, writeFile } from 'fs/promises';
 
-const LICENSE =
-    '// Copyright (c) 2024 IOTA Stiftung\n// SPDX-License-Identifier: Apache-2.0\n\n';
+const LICENSE = '// Copyright (c) 2024 IOTA Stiftung\n// SPDX-License-Identifier: Apache-2.0\n\n';
 
 async function prependLicense(filename) {
     const content = await readFile(filename, 'utf8');
