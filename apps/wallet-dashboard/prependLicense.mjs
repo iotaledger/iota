1d330a1c1c635fd5e4d126da1f9b1ac00956beda
cpl121 <cpeon@boxfish.studio>
2025-01-30 10:21:37 +0100

    fix(dashboard): modify the license header in the prepend file

--- apps/wallet-dashboard/prependLicense.mjs
+++ apps/wallet-dashboard/prependLicense.mjs
@@ -1,11 +1,10 @@
-// Copyright (c) Mysten Labs, Inc.
-// Modifications Copyright (c) 2024 IOTA Stiftung
+// Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
 import { readFile, writeFile } from 'fs/promises';
 
 const LICENSE =
-    '// Modifications Copyright (c) 2024 IOTA Stiftung\n// SPDX-License-Identifier: Apache-2.0\n\n';
+    '// Copyright (c) 2024 IOTA Stiftung\n// SPDX-License-Identifier: Apache-2.0\n\n';
 
 async function prependLicense(filename) {
     const content = await readFile(filename, 'utf8');
