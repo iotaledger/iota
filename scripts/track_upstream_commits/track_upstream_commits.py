f5a83976144e5b07f4fa37baef888108892422dd
muXxer <git@muxxer.de>
2025-01-31 15:32:39 +0100

    scripts: refactor out `CodeOwners` and move to utils

--- scripts/track_upstream_commits/track_upstream_commits.py
+++ scripts/track_upstream_commits/track_upstream_commits.py
@@ -1,9 +1,8 @@
-import subprocess
-import os
-import argparse
-import re
-import pathlib
-import shutil
+# Copyright (c) 2024 IOTA Stiftung
+# SPDX-License-Identifier: Apache-2.0
+import os, sys, subprocess, argparse, re, pathlib, shutil
+sys.path.append('../utils')
+from codeowners import CodeOwners
 
 def is_commit_hash(ref):
     # A commit hash is a 40-character hexadecimal string
@@ -55,7 +54,7 @@ def clone_repo(repo_url, repo_tag, target_folder):
     # Change working directory to the cloned repo
     os.chdir(target_folder)
 
-def convert_iota_to_sui_folders(folders):
+def iota_to_sui_mapping_func(str):
     premap = {
         'crates/iota-common': 'crates/mysten-common',
         'crates/iota-metrics': 'crates/mysten-metrics',
@@ -64,38 +63,11 @@ def convert_iota_to_sui_folders(folders):
         'crates/iota-util-mem-derive': 'crates/mysten-util-mem-derive',
     }
 
-    mapped_folders = []
-    for folder in folders:
-        if folder in premap:
-            folder = premap[folder]
-        mapped_folders.append(folder.replace('iota', 'sui'))
+    for key in premap:
+        if key in str:
+            return str.replace(key, premap[key])
 
-    return mapped_folders
-
-# Parse the CODEOWNERS file and return the folders of the code owner
-def get_folders_for_code_owner(file_path, code_owner):
-    folder_to_owners = {}
-    matched_folders = []
-    with open(file_path, 'r') as file:
-        for line in file:
-            line = line.strip()
-            if line and not line.startswith('#'):
-                folder, *owners = line.split()
-                folder_to_owners[folder] = owners
-
-    for folder, owners in folder_to_owners.items():
-        if folder == "*":
-            # skip the fallback here, we want to check all other patterns first
-            continue
-
-        # Check if the pattern matches the relative path of the code owner
-        regex_pattern = '.*' + re.escape(code_owner) + '.*'
-        for owner in owners:
-            if re.match(regex_pattern, owner):
-                matched_folders.append(folder.strip('/'))
-                break
-    
-    return convert_iota_to_sui_folders(matched_folders)
+    return str.replace('iota', 'sui')
 
 # Get the commits of a folder in the specified range
 def get_folder_commits(folder, start_ref, end_ref):
@@ -105,18 +77,33 @@ def get_folder_commits(folder, start_ref, end_ref):
     if not is_commit_hash(end_ref):
         subprocess.run(["git", "fetch", "origin", "tag", end_ref], check=True)
     
+    if not os.path.exists(folder):
+        print(f"WARNING: Folder '{folder}' does not exist.")
+        return []
+    
     # Define the git log command
-    git_log_command = ["git", "log", f"{start_ref}..{end_ref}", "--format=format:https://github.com/MystenLabs/sui/commit/%H", "--", folder]
+    git_log_command = ["git", "log", f"{start_ref}..{end_ref}", "--format=%H", "--", folder]
+
     # Execute the git log command and collect the output
     result = subprocess.run(git_log_command, capture_output=True, text=True)
     git_log_output = result.stdout.strip().split('\n') if result.stdout.strip() else []
 
     return git_log_output
 
+# Run the git command to get the short commit message (title)
+def get_commit_short_message(commit_hash):
+    short_message = subprocess.check_output(
+        ['git', 'show', '-s', '--format=%s', commit_hash],
+        stderr=subprocess.STDOUT,
+        text=True
+    ).strip()
+    return short_message
+
 def analyze_folder_commits(start_ref, end_ref, folders):
     print(f"SINCE: {start_ref}")
     print(f"UNTIL: {end_ref}")
     print(f"FOLDERS: {', '.join(folders)}")
+
     # Only insert non-empty lists into crates_commits
     folders_commits = {}
     for folder in folders:
@@ -133,6 +120,9 @@ def analyze_folder_commits(start_ref, end_ref, folders):
         for commit in set(folders_commits[folder1]).intersection(folders_commits[folder2])
     )
 
+    def print_commit(commit_hash):
+        print(f"- https://github.com/MystenLabs/sui/commit/{commit_hash}: {get_commit_short_message(commit_hash)}")
+
     # Remove duplicate commits from each folder
     for folder in folders_commits:
         folders_commits[folder] = [commit for commit in folders_commits[folder] if commit not in duplicate_commits]
@@ -142,20 +132,19 @@ def analyze_folder_commits(start_ref, end_ref, folders):
         if commits:
             print(f"\n\n## {folder} ({len(commits)})")
             for commit in reversed(commits):
-                print(f"- {commit}")
-
+                print_commit(commit)
+    
     # Print the duplicate commits
     print(f"\n\n## Cross-folder commits ({len(duplicate_commits)})")
     for commit in reversed(list(duplicate_commits)):
-        print(f"- {commit}")
-
+        print_commit(commit)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Track upstream commits for specified folders.')
     parser.add_argument('--since', required=True, help='Start commit hash or the tag for git log (e.g., "bb778828e36d53a7d91a27e55109f2f45621badc", "mainnet-v1.32.2"), it is EXCLUDED from the results.')
     parser.add_argument('--until', required=True, help='End commit hash or the tag for git log (e.g., "3ada97c109cc7ae1b451cb384a1f2cfae49c8d3e", "mainnet-v1.36.2"), it is INCLUDED in the results.')
+    parser.add_argument('--codeowners', nargs='+', help='Code owners of the folders (e.g., "@iotaledger/node @iotaledger/consensus").')
     parser.add_argument('--folders', nargs='+', help='List of folders relative to the project root to track (e.g., "crates/iota-core crates/iota-node").')
-    parser.add_argument('--codeowner', help='code owner of the folders (e.g., "node")')
     parser.add_argument('--repo-url', default="git@github.com:MystenLabs/sui.git", help="The URL to the repository. Can also be a local folder.")
     parser.add_argument('--repo-tag', default=None, help="The tag to checkout in the repository.")
     parser.add_argument('--target-folder', default="result", help="The path to the target folder.")
@@ -169,25 +158,31 @@ if __name__ == '__main__':
     if args.clone_source and not args.repo_tag:
         parser.error("--repo-tag must be specified if --clone-source is true")
 
-    # Check if folders or code owner is specified
-    if not args.folders and not args.codeowner:
-        print("No crates or code owner specified.")
+    # Check if codeowners or folders is specified
+    if not args.codeowners and not args.folders:
+        print("No codeowners or folders specified.")
         exit(1)
 
-    folders = []
-    if args.folders:
-        folders = convert_iota_to_sui_folders(args.folders)
-
-    if args.codeowner:
+    code_owners = None
+    if args.codeowners:
         print("Parsing the CODEOWNERS file...")
         # Get crates of the code owner
         base_path = pathlib.Path("../../").absolute().resolve()
-        folders.extend(get_folders_for_code_owner(os.path.join(base_path, '.github', 'CODEOWNERS'), args.codeowner))
+
+        code_owners = CodeOwners(
+                file_path=os.path.join(base_path, '.github', 'CODEOWNERS'),
+                path_mapping_func=iota_to_sui_mapping_func,
+                pattern_mapping_func=iota_to_sui_mapping_func,
+            )
+
+    folders = set()
+    if args.folders:
+        folders.update(args.folders)        
 
     if args.clone_source:
-        # remove the target folder if it exists
+        # Check if the target folder already exists
         if os.path.exists(target_folder):
-            shutil.rmtree(target_folder)
+            raise Exception(f"Target folder '{target_folder}' already exists, please remove it manually or run without --clone-source.")
 
         # Clone the repository
         clone_repo(
@@ -198,6 +193,11 @@ if __name__ == '__main__':
     else:
         # Change working directory to the target folder
         os.chdir(target_folder)
+
+    if code_owners:
+        # Get folders of the code owner
+        paths = code_owners.match_paths_for_owners(target_folder, args.codeowners)
+        folders.update(paths)
 
     # Analyze the commits of the folders
     analyze_folder_commits(args.since, args.until, folders)
