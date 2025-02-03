30a1927127e33e7da0a9b3a255a7d3350531db20
Lucas Tortora <lucas.tortora@iota.org>
2025-02-03 13:57:10 -0300

    update path

--- docs/site/scripts/get-iota-references.sh
+++ docs/site/scripts/get-iota-references.sh
@@ -6,7 +6,7 @@ cd tmp
 
 # Download and copy docs
 curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/testnet.tar.gz | tar xzv
-cp -Rv ts ../../docs/content/references/ts-sdk/api
+cp -Rv ts ../../docs/content/ts-sdk/api
 cp -Rv graphql ../../docs/content/references/iota-api/iota-graphql
 cp -Rv rust ../../docs/content/references/framework
 
