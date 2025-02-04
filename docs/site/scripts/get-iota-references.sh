722931da179d8cc9771c7f34cbaff776ca266e65
Lucas Tortora <lucas.tortora@iota.org>
2025-02-04 18:23:31 -0300

    fix path

--- docs/site/scripts/get-iota-references.sh
+++ docs/site/scripts/get-iota-references.sh
@@ -6,9 +6,9 @@ cd tmp
 
 # Download and copy docs
 curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota/testnet.tar.gz | tar xzv
-cp -Rv generated-docs/ts/* ../../../content/ts-sdk/api
-cp -Rv generated-docs/graphql/* ../../../content/references/iota-api/iota-graphql
-cp -Rv generated-docs/framework/* ../../../content/references/framework
+cp -Rv generated-docs/ts/* ../../content/ts-sdk/api
+cp -Rv generated-docs/graphql/* ../../content/references/iota-api/iota-graphql
+cp -Rv generated-docs/framework/* ../../content/references/framework
 
 # Return to root and cleanup
 cd -
