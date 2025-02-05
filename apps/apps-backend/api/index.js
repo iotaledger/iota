e4f5a2a10744a71733afe93f4c1713b6b0a3a1d5
Mario Sarcevic <mario.sarcevic@iota.org>
2025-02-05 11:30:44 +0100

    feat: Remove duplicated copyright header

--- apps/apps-backend/api/index.js
+++ apps/apps-backend/api/index.js
@@ -1,9 +1,6 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-// Copyright (c) 2025 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from '../dist/apps-backend/src/app.module';
 
