45b0640371e89f9205a663693377de756e771303
Marc Espin <mespinsanz@gmail.com>
2025-02-05 18:50:28 +0100

    feat(wallet/wallet-dashboard/explorer): Build env for growthbook, amplitude & sentry (#5216)
    * feat(wallet/wallet-dashboard/explorer): Staging env for growthbook
    
    * fmt
    
    * rename WALLET_RC to IS_RC
    
    * add BUILD_ENV to turbo.json
    
    * feat: enable account finder in production
    
    * fix: Use `BUILD_ENV` instead of `NODE_ENV`
    
    * fix(explorer): Use `BUILD_ENV` instead of `import.meta.env.PROD`
    
    * fmt, ignore
    
    * `VITE_BUILD_ENV`
    
    * Use `NEXT_PUBLIC_BUILD_ENV` in dashboard amplitude and sentry
    
    * update env example
    
    * hook NEXT_PUBLIC_BUILD_ENV
    
    * revert enabling account finder in prod features
    
    * use staging build env in rc builds of the wallet
    
    * chore: disable production feature flags
    
    * fmt
    
    ---------
    
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>
    Signed-off-by: Lucas Tortora <lucass.tortora@iota.org>

--- apps/wallet/src/shared/experimentation/features.ts
+++ apps/wallet/src/shared/experimentation/features.ts
@@ -6,31 +6,31 @@ import { GrowthBook } from '@growthbook/growthbook';
 import { Network, getAppsBackend } from '@iota/iota-sdk/client';
 import Browser from 'webextension-polyfill';
 
-function getGrowthbookConfig(env: string) {
-    if (env === 'development') {
-        return {
-            clientKey: 'development',
-            enableDevMode: true,
-        };
-    }
-    if (env === 'production') {
-        return {
-            clientKey: 'production',
-            enableDevMode: false,
-        };
-    }
-
-    // return for local by default
-    return {
-        clientKey: 'development',
+const GROWTHBOOK_ENVIRONMENTS = {
+    production: {
+        clientKey: 'production',
+        enableDevMode: false,
+    },
+    rc: {
+        clientKey: 'staging',
+        enableDevMode: false,
+    },
+    nightly: {
+        clientKey: 'staging',
+        enableDevMode: false,
+    },
+    development: {
+        clientKey: 'staging',
         enableDevMode: true,
-    };
-}
+    },
+};
+
+const environment =
+    (process.env.BUILD_ENV as keyof typeof GROWTHBOOK_ENVIRONMENTS) || 'development';
 
 export const growthbook = new GrowthBook({
     apiHost: getAppsBackend(),
-    clientKey: getGrowthbookConfig(process.env.NODE_ENV as string).clientKey,
-    enableDevMode: getGrowthbookConfig(process.env.NODE_ENV as string).enableDevMode,
+    ...GROWTHBOOK_ENVIRONMENTS[environment],
 });
 
 export function setAttributes(network?: { network: Network; customRpc?: string | null }) {
@@ -43,7 +43,7 @@ export function setAttributes(network?: { network: Network; customRpc?: string |
     growthbook.setAttributes({
         network: activeNetwork,
         version: Browser.runtime.getManifest().version,
-        rc: process.env.WALLET_RC || false,
+        rc: process.env.IS_RC || false,
     });
 }
 
