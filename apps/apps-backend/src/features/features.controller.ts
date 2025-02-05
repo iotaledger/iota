0936750a511992864158f5083382e8bc74821382
Begoña Alvarez <balvarez@boxfish.studio>
2025-02-05 16:45:30 +0100

    feat: enable account finder in production

--- apps/apps-backend/src/features/features.controller.ts
+++ apps/apps-backend/src/features/features.controller.ts
@@ -150,7 +150,7 @@ export class FeaturesController {
                     defaultValue: true,
                 },
                 [Feature.AccountFinder]: {
-                    defaultValue: false,
+                    defaultValue: true,
                 },
                 [Feature.StardustMigration]: {
                     defaultValue: true,
