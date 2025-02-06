3717e967da90a588112baa1e9476038913febbf0
Thibault Martinez <thibault@iota.org>
2025-02-06 13:09:11 +0100

    fmt

--- crates/iota/src/iota_commands.rs
+++ crates/iota/src/iota_commands.rs
@@ -700,8 +700,9 @@ async fn start(
     // If this is set, then no data will be persisted between runs, and a new
     // genesis will be generated each run.
     if force_regenesis {
-        let committee_size = NonZeroUsize::new(committee_size.unwrap_or(DEFAULT_NUMBER_OF_AUTHORITIES))
-            .ok_or_else(|| anyhow!("Committee size must be at least 1."))?;
+        let committee_size =
+            NonZeroUsize::new(committee_size.unwrap_or(DEFAULT_NUMBER_OF_AUTHORITIES))
+                .ok_or_else(|| anyhow!("Committee size must be at least 1."))?;
 
         swarm_builder = swarm_builder.committee_size(committee_size);
         let mut genesis_config = GenesisConfig::custom_genesis(1, 100);
