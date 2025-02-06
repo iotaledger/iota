e4ee915c6da3af4b3a534e4186209ae8ef82bfed
marc2332 <mespinsanz@gmail.com>
2025-02-06 08:55:28 +0100

    fix(wallet): Proper formatting for the sum of balanace

--- apps/wallet/src/ui/app/components/accounts/AccountBalanceItem.tsx
+++ apps/wallet/src/ui/app/components/accounts/AccountBalanceItem.tsx
@@ -61,8 +61,8 @@ export function AccountBalanceItem({
             const balance = balances.reduce((acc, { totalBalance }) => {
                 return BigInt(acc) + BigInt(totalBalance);
             }, BigInt(0));
-            const [formatted, symbol] = formatBalance(balance, IOTA_COIN_METADATA.decimals);
-            return `${formatted} ${symbol}`;
+            const formattedAmount = formatBalance(balance, IOTA_COIN_METADATA.decimals);
+            return `${formattedAmount} ${IOTA_COIN_METADATA.symbol}`;
         },
         gcTime: 0,
         staleTime: 0,
