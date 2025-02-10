cc373f308c649ac1a425d7deedc5ad2728e943c6
Lucas Tortora <lucass.tortora@iota.org>
2025-02-10 16:28:27 -0300

    generate graphql from URL

--- docs/site/scripts/get-iota-references.sh
+++ docs/site/scripts/get-iota-references.sh
@@ -4,11 +4,25 @@
 mkdir tmp
 cd tmp
 
-# Download and copy docs
-curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota/testnet.tar.gz | tar xzv
-cp -Rv generated-docs/ts/* ../../content/ts-sdk/api
-cp -Rv generated-docs/graphql/* ../../content/references/iota-api/iota-graphql
-cp -Rv generated-docs/framework/* ../../content/references/framework
+# Download and copy Testnet docs
+curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota/devnet.tar.gz | tar xzv
+
+mkdir  ../../content/references/framework/testnet/
+cp -Rv generated-docs/framework/* ../../content/references/framework/testnet/
+
+mkdir ../../content/ts-sdk/api/testnet/
+cp -Rv generated-docs/ts/* ../../content/ts-sdk/api/testnet/
+
+# Download and copy Devnet docs
+rm -rf generated-docs
+curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota/devnet.tar.gz | tar xzv
+
+mkdir  ../../content/references/framework/devnet/
+cp -Rv generated-docs/framework/* ../../content/references/framework/devnet/
+
+mkdir  ../../content/ts-sdk/api/testnet/
+cp -Rv generated-docs/ts/* ../../content/ts-sdk/api/testnet/
+
 
 # Return to root and cleanup
 cd -
