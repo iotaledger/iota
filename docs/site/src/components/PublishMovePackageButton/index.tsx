5b392b99604c8297debfb333111bca8cf64d7d29
Dr-Electron <dr-electr0n@protonmail.com>
2025-02-10 15:48:19 +0100

    test publish

---
+++ docs/site/src/components/PublishMovePackageButton/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useCurrentAccount, useSignAndExecuteTransaction } from "@iota/dapp-kit";
+import { Transaction } from "@iota/iota-sdk/transactions";
+import { MovePackageJsonData } from "./types/types";
+
+export default function PublishMovePackageButton(
+    _contractJson,
+) {
+    const { mutate: signAndExecuteTransaction } = useSignAndExecuteTransaction();
+    const currentAccount = useCurrentAccount();
+
+    const contractJson: MovePackageJsonData = JSON.parse(_contractJson);
+
+    const onClick = () => {
+        const movePublishTx = new Transaction();
+
+        console.log('Publishing contract', contractJson);
+        console.log('Modules', contractJson.modules);
+        console.log('Dependencies', contractJson.dependencies);
+        const upgradeCap = movePublishTx.publish({
+            modules: contractJson.modules,
+            dependencies: contractJson.dependencies,
+        });
+        console.log('Published contract', contractJson);
+        movePublishTx.transferObjects([upgradeCap], movePublishTx.pure.address(currentAccount?.address));
+
+        signAndExecuteTransaction(
+            {
+                transaction: movePublishTx,
+            },
+            {
+                onSuccess: () => {
+                    console.log('Transaction succeeded');
+                }
+            },
+        );
+    };
+
+    return <button onClick={onClick}>Publish</button>
+}
