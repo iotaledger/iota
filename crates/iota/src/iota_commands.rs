c5b878942cbed10b23790a6a9b4e1152cc059d5e
/alex/ <alexander.schmidt@iota.org>
2025-02-11 15:04:42 +0100

    remove all useless clap arg renames

--- crates/iota/src/iota_commands.rs
+++ crates/iota/src/iota_commands.rs
@@ -244,7 +244,7 @@ pub enum IotaCommand {
         working_dir: Option<PathBuf>,
         #[arg(short, long, help = "Forces overwriting existing configuration")]
         force: bool,
-        #[arg(long = "epoch-duration-ms")]
+        #[arg(long]
         epoch_duration_ms: Option<u64>,
         #[arg(
             long,
