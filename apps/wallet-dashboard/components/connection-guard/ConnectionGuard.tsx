6bd20656c0d3ef1a6c2f83cc1d67bb4c97dc0ada
Panteleymonchuk <panteleymonchuk@gmail.com>
2025-02-12 14:33:34 +0200

    feat(wallet-dashboard): implement connection guard and redirect logic for account management

--- apps/wallet-dashboard/components/connection-guard/ConnectionGuard.tsx
+++ apps/wallet-dashboard/components/connection-guard/ConnectionGuard.tsx
@@ -3,40 +3,40 @@
 
 'use client';
 
-import { useAutoConnectWallet, useCurrentAccount, useCurrentWallet } from '@iota/dapp-kit';
+import { PropsWithChildren, useEffect } from 'react';
 import { redirect, usePathname } from 'next/navigation';
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { useAutoConnectWallet, useCurrentWallet } from '@iota/dapp-kit';
+import { LoadingIndicator } from '@iota/apps-ui-kit';
+import { CONNECT_ROUTE, HOMEPAGE_ROUTE } from '@/lib/constants/routes.constants';
 
 export function ConnectionGuard({ children }: PropsWithChildren) {
-    const [firstLoad, setFirstLoad] = useState<
-        'idle' | 'hasStartedConnecting' | 'finishedConnecting'
-    >('idle');
+    const { isConnected, isDisconnected } = useCurrentWallet();
 
-    const { isConnecting, isConnected, isDisconnected } = useCurrentWallet();
-    const account = useCurrentAccount();
     const pathname = usePathname();
     const autoConnect = useAutoConnectWallet();
 
-    const connected = isConnected && !!account;
-
     useEffect(() => {
-        if (autoConnect === 'idle' && firstLoad === 'idle') {
-            return; // wait until first load starts;
+        if (autoConnect !== 'attempted') return;
+        if (isConnected) {
+            // Redirect to home if on root ("/")
+            if (pathname === CONNECT_ROUTE.path) {
+                redirect(HOMEPAGE_ROUTE.path);
+            }
+        } else if (isDisconnected) {
+            // Redirect back to "/" if disconnected and trying to access another page
+            if (pathname !== CONNECT_ROUTE.path) {
+                redirect(CONNECT_ROUTE.path);
+            }
         }
+    }, [isConnected, isDisconnected, pathname, autoConnect]);
 
-        if (isConnecting) {
-            setFirstLoad('hasStartedConnecting');
-            return;
-        }
-        setFirstLoad('finishedConnecting');
-    }, [isDisconnected, isConnecting, firstLoad, autoConnect]);
+    if (autoConnect === 'idle') {
+        return (
+            <div className="flex h-screen w-full justify-center">
+                <LoadingIndicator size="w-16 h-16" />
+            </div>
+        );
+    }
 
-    useEffect(() => {
-        if (firstLoad !== 'finishedConnecting') return;
-        if (!connected && pathname !== '/') {
-            redirect('/');
-        }
-    }, [connected, pathname, firstLoad, isConnecting]);
-
-    return connected || pathname === '/' ? children : null;
+    return autoConnect === 'attempted' ? children : null;
 }
