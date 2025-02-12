b59e84ec5220b687abfc412d4ebfe50dc5835cd6
evavirseda <evirseda@boxfish.studio>
2025-02-12 12:11:37 +0100

    feat: polish grid views

--- apps/wallet-dashboard/app/(protected)/staking/page.tsx
+++ apps/wallet-dashboard/app/(protected)/staking/page.tsx
@@ -145,7 +145,7 @@ function StakingDashboardPage(): React.JSX.Element {
 
     return (
         <div className="flex justify-center">
-            <div className="w-3/4">
+            <div className="w-full md:w-3/4">
                 {(delegatedStakeData?.length ?? 0) > 0 ? (
                     <Panel>
                         <Title
