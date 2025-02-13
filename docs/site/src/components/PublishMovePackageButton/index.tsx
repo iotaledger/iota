18db172a3b582fb48a2444aee39d21a12adf662e
Dr-Electron <dr-electr0n@protonmail.com>
2025-02-13 01:23:19 +0100

    Give more feedback on Package publish

--- docs/site/src/components/PublishMovePackageButton/index.tsx
+++ docs/site/src/components/PublishMovePackageButton/index.tsx
@@ -1,21 +1,41 @@
-import React from "react";
-import { useCurrentAccount, useSignAndExecuteTransaction } from "@iota/dapp-kit";
+import React, { useState } from "react";
+import { useCurrentAccount, useIotaClient, useSignAndExecuteTransaction } from "@iota/dapp-kit";
 import { Transaction } from "@iota/iota-sdk/transactions";
 import { MovePackageJsonData } from "./types";
+import { Networks } from "../constant";
+import Link from "@docusaurus/Link";
 
 export default function PublishMovePackageButton(
     { contractJson }: { contractJson: MovePackageJsonData }
 ) {
-    const { mutate: signAndExecuteTransaction } = useSignAndExecuteTransaction();
+    const client = useIotaClient();
+    const { mutate: signAndExecuteTransaction } = useSignAndExecuteTransaction({
+        execute: async ({ bytes, signature }) =>
+            await client.executeTransactionBlock({
+                transactionBlock: bytes,
+                signature,
+                options: {
+                    // Raw effects are required so the effects can be reported back to the wallet
+                    showRawEffects: true,
+                    // Show effects and object changes so that the status and packageId can be extracted
+                    showEffects: true,
+                    showObjectChanges: true,
+                },
+            }),
+    });
     const currentAccount = useCurrentAccount();
+    const [isPublishing, setIsPublishing] = useState(false);
+    const [packageId, setPackageId] = useState<string | null>(null);
 
     const onClick = () => {
+        setIsPublishing(true);
         const movePublishTx = new Transaction();
 
         const upgradeCap = movePublishTx.publish({
             modules: contractJson.modules,
             dependencies: contractJson.dependencies,
         });
+        // Transfer the upgrade cap to the current account
         movePublishTx.transferObjects([upgradeCap], movePublishTx.pure.address(currentAccount.address));
 
         signAndExecuteTransaction(
@@ -23,8 +43,18 @@ export default function PublishMovePackageButton(
                 transaction: movePublishTx,
             },
             {
-                onSuccess: () => {
-                    console.log('Transaction succeeded');
+                onSuccess: (result) => {
+                    if (result.effects.status.status === "success") {
+                        console.log("Publish successful", result);
+                        // Extract the packageId from the object changes
+                        const publishedObject = result.objectChanges.find(
+                            (obj) => obj.type === "published"
+                        ) as {
+                            packageId: string;
+                        };
+                        setPackageId(publishedObject.packageId);
+                    }
+                    setIsPublishing(false);
                 }
             },
         );
@@ -32,7 +62,16 @@ export default function PublishMovePackageButton(
 
     return (
         currentAccount ? (
-            <button className='button button--primary' onClick={onClick}>Publish</button>
+            <div>
+                <button className='button button--primary' onClick={onClick} disabled={isPublishing}>
+                    {isPublishing ? "Publishing..." : "Publish"}
+                </button>
+                {packageId ? 
+                    <div className="margin-top--md">
+                        Congratulation to your first published package: <Link to={`${Networks.iota_move.explorerUrl}/object/${packageId}`}>{packageId}</Link>
+                    </div> : null
+                }
+            </div>
         ) : (
             <code>No Wallet connected</code>
         )
