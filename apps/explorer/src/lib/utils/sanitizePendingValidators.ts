a3ba69c3f71c7efa0a0808f29cb8b361db5bf18e
evavirseda <evirseda@boxfish.studio>
2025-02-17 09:44:46 +0100

    fix types

--- apps/explorer/src/lib/utils/sanitizePendingValidators.ts
+++ apps/explorer/src/lib/utils/sanitizePendingValidators.ts
@@ -1,9 +1,37 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import type { IotaObjectResponse, IotaValidatorSummary } from '@iota/iota-sdk/src/client';
+import type {
+    IotaObjectResponse,
+    IotaValidatorSummary,
+    MoveStruct,
+    MoveValue,
+} from '@iota/iota-sdk/client';
 
 export type IotaValidatorSummaryExtended = IotaValidatorSummary & { isPending?: boolean };
+
+function isMoveStructWithFields(
+    data: MoveStruct,
+): data is { fields: { [key: string]: MoveValue }; type: string } {
+    return (
+        typeof data === 'object' &&
+        data !== null &&
+        'fields' in data &&
+        typeof data.fields === 'object' &&
+        data.fields !== null
+    );
+}
+
+function getMoveFields(object: MoveStruct): { [key: string]: MoveValue } {
+    if (isMoveStructWithFields(object)) {
+        return object.fields as { [key: string]: MoveValue };
+    }
+    return {};
+}
+
+interface MoveStructFields {
+    fields: { [key: string]: MoveValue };
+}
 
 export function sanitizePendingValidators(
     allPendings: IotaObjectResponse[] | undefined,
@@ -13,53 +41,52 @@ export function sanitizePendingValidators(
             const fields =
                 (data &&
                     data.content &&
-                    'fields' in data.content &&
-                    'value' in (data.content.fields ?? {}) &&
-                    (data.content.fields.value?.fields ?? {})) ||
+                    data.content.dataType === 'moveObject' &&
+                    getMoveFields(data.content)) ||
+                {} ||
                 {};
-            const metadata = fields.metadata?.fields || {};
-            const stakingPool = fields.staking_pool?.fields || {};
-            const exchangeRates = stakingPool.exchange_rates?.fields || {};
+            const metadata = (fields.metadata as MoveStructFields)?.fields || {};
+            const stakingPool = (fields.staking_pool as MoveStructFields)?.fields || {};
+            const exchangeRates = (stakingPool.exchange_rates as MoveStructFields)?.fields || {};
 
             return {
                 isPending: true,
                 authorityPubkeyBytes: '',
-                commissionRate: fields.commission_rate,
-                description: metadata.description,
-                exchangeRatesId: exchangeRates.id?.id,
-                exchangeRatesSize: exchangeRates.size,
-                gasPrice: fields.gas_price,
-                imageUrl: metadata.image_url,
-                iotaAddress: metadata.iota_address,
-                name: metadata.name,
-                netAddress: metadata.net_address,
+                commissionRate: String(fields.commission_rate),
+                description: String(metadata.description),
+                exchangeRatesId: (
+                    exchangeRates.id as {
+                        id: string;
+                    }
+                )?.id,
+                exchangeRatesSize: String(exchangeRates.size),
+                gasPrice: String(fields.gas_price),
+                imageUrl: String(metadata.image_url),
+                iotaAddress: String(metadata.iota_address),
+                name: String(metadata.name),
+                netAddress: String(metadata.net_address),
                 networkPubkeyBytes: '',
-                nextEpochAuthorityPubkeyBytes: metadata.next_epoch_authority_pubkey_bytes || null,
-                nextEpochCommissionRate: fields.next_epoch_commission_rate,
-                nextEpochGasPrice: fields.next_epoch_gas_price,
-                nextEpochNetAddress: metadata.next_epoch_net_address || null,
-                nextEpochNetworkPubkeyBytes: metadata.next_epoch_network_pubkey_bytes || null,
-                nextEpochP2pAddress: metadata.next_epoch_p2p_address || null,
-                nextEpochPrimaryAddress: metadata.next_epoch_primary_address || null,
-                nextEpochProofOfPossession: metadata.next_epoch_proof_of_possession || null,
-                nextEpochProtocolPubkeyBytes: metadata.next_epoch_protocol_pubkey_bytes || null,
-                nextEpochStake: fields.next_epoch_stake,
-                operationCapId: fields.operation_cap_id,
-                p2pAddress: metadata.p2p_address,
-                pendingPoolTokenWithdraw: stakingPool.pending_pool_token_withdraw,
-                pendingStake: stakingPool.pending_stake,
-                pendingTotalIotaWithdraw: stakingPool.pending_total_iota_withdraw,
-                poolTokenBalance: stakingPool.pool_token_balance,
-                primaryAddress: metadata.primary_address,
-                projectUrl: metadata.project_url,
+                nextEpochCommissionRate: String(fields.next_epoch_commission_rate),
+                nextEpochGasPrice: String(fields.next_epoch_gas_price),
+                nextEpochStake: String(fields.next_epoch_stake),
+                operationCapId: String(fields.operation_cap_id),
+                p2pAddress: String(metadata.p2p_address),
+                pendingPoolTokenWithdraw: String(stakingPool.pending_pool_token_withdraw),
+                pendingStake: String(stakingPool.pending_stake),
+                pendingTotalIotaWithdraw: String(stakingPool.pending_total_iota_withdraw),
+                poolTokenBalance: String(stakingPool.pool_token_balance),
+                primaryAddress: String(metadata.primary_address),
+                projectUrl: String(metadata.project_url),
                 proofOfPossessionBytes: '',
                 protocolPubkeyBytes: '',
-                rewardsPool: stakingPool.rewards_pool,
-                stakingPoolActivationEpoch: stakingPool.activation_epoch || null,
-                stakingPoolDeactivationEpoch: stakingPool.deactivation_epoch || null,
-                stakingPoolId: stakingPool.id?.id,
-                stakingPoolIotaBalance: stakingPool.iota_balance,
-                votingPower: fields.voting_power,
+                rewardsPool: String(stakingPool.rewards_pool),
+                stakingPoolId: (
+                    stakingPool.id as {
+                        id: string;
+                    }
+                )?.id,
+                stakingPoolIotaBalance: String(stakingPool.iota_balance),
+                votingPower: String(fields.voting_power),
             };
         }) || []
     );
