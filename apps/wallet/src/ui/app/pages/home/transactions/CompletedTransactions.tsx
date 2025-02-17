56953cda0237fa2fc0b0a2bbe695f9981ee969b2
cpl121 <cpeon@boxfish.studio>
2025-02-17 17:21:35 +0100

    fix(wallet): build

--- apps/wallet/src/ui/app/pages/home/transactions/CompletedTransactions.tsx
+++ apps/wallet/src/ui/app/pages/home/transactions/CompletedTransactions.tsx
@@ -14,7 +14,7 @@ export function CompletedTransactions() {
         allTransactions: txns,
         isPending,
         error,
-    } = useQueryTransactionsByAddress(activeAddress);
+    } = useQueryTransactionsByAddress(activeAddress || '');
     if (error) {
         return (
             <div className="mb-2 flex h-full w-full items-center justify-center p-2">
