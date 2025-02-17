20df406966eb093891f6af1636712523acecbd1f
evavirseda <evirseda@boxfish.studio>
2025-02-17 13:18:07 +0100

    feat: remove timeouts

--- apps/wallet/tests/staking.spec.ts
+++ apps/wallet/tests/staking.spec.ts
@@ -5,17 +5,13 @@
 import { expect, test } from './fixtures';
 import { createWallet } from './utils/auth';
 
-const SHORT_TIMEOUT = 30 * 1000;
-const LONG_TIMEOUT = 80 * 1000;
 const STAKE_AMOUNT = 100;
 
 test('staking', async ({ page, extensionUrl }) => {
-    test.setTimeout(4 * LONG_TIMEOUT);
-
     await createWallet(page, extensionUrl);
 
     await page.getByText(/Request localnet tokens/i).click();
-    await expect(page.getByTestId('coin-balance')).not.toHaveText('0', { timeout: SHORT_TIMEOUT });
+    await expect(page.getByTestId('coin-balance')).not.toHaveText('0IOTA');
 
     await page.getByText(/Start Staking/).click();
     await page
@@ -26,37 +22,24 @@ test('staking', async ({ page, extensionUrl }) => {
     await page.getByPlaceholder('0 IOTA').fill(STAKE_AMOUNT.toString());
     await page.getByRole('button', { name: 'Stake' }).click();
 
-    await expect(page.getByTestId('overlay-title')).toHaveText('Transaction', {
-        timeout: SHORT_TIMEOUT,
-    });
-    await expect(page.getByText(/Successfully sent/)).toBeVisible({ timeout: SHORT_TIMEOUT });
-
-    await expect(page.getByTestId('loading-indicator')).not.toBeVisible({
-        timeout: LONG_TIMEOUT,
-    });
+    await expect(page.getByTestId('overlay-title')).toHaveText('Transaction');
+    await expect(page.getByText(/Successfully sent/)).toBeVisible();
+    await expect(page.getByTestId('loading-indicator')).not.toBeVisible();
 
     await page.getByTestId('close-icon').click();
 
-    await expect(page.getByText(`${STAKE_AMOUNT} IOTA`)).toBeVisible({
-        timeout: SHORT_TIMEOUT,
-    });
+    await expect(page.getByText(`${STAKE_AMOUNT} IOTA`)).toBeVisible();
     await page.getByText(`${STAKE_AMOUNT} IOTA`).click();
 
-    await expect(page.getByTestId('staked-card')).toBeVisible({ timeout: LONG_TIMEOUT });
+    await expect(page.getByTestId('staked-card')).toBeVisible();
     await page.getByTestId('staked-card').click();
     await page.getByText('Unstake').click();
     await page.getByRole('button', { name: 'Unstake' }).click();
 
-    await expect(page.getByTestId('overlay-title')).toHaveText('Transaction', {
-        timeout: SHORT_TIMEOUT,
-    });
-    await expect(page.getByText(/Successfully sent/)).toBeVisible({ timeout: SHORT_TIMEOUT });
-    await expect(page.getByTestId('loading-indicator')).not.toBeVisible({
-        timeout: LONG_TIMEOUT,
-    });
+    await expect(page.getByTestId('overlay-title')).toHaveText('Transaction');
+    await expect(page.getByText(/Successfully sent/)).toBeVisible();
+    await expect(page.getByTestId('loading-indicator')).not.toBeVisible();
 
     await page.getByTestId('close-icon').click();
-    await expect(page.getByText(`${STAKE_AMOUNT} IOTA`)).not.toBeVisible({
-        timeout: SHORT_TIMEOUT,
-    });
+    await expect(page.getByText(`${STAKE_AMOUNT} IOTA`)).not.toBeVisible();
 });
