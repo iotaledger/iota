301749cc90a748453316cc56c234611830c3faf5
evavirseda <evirseda@boxfish.studio>
2025-02-19 15:56:48 +0100

    cleanup

--- apps/explorer/src/lib/utils/sanitizePendingValidators.ts
+++ apps/explorer/src/lib/utils/sanitizePendingValidators.ts
@@ -45,18 +45,15 @@ export function sanitizePendingValidators(
                     getMoveFields(data.content)) ||
                 {} ||
                 {};
-            const metadata =
-                ((fields.value as MoveStructFields)?.fields?.metadata as MoveStructFields)
-                    ?.fields || {};
-            const stakingPool =
-                ((fields.value as MoveStructFields)?.fields?.staking_pool as MoveStructFields)
-                    ?.fields || {};
+            const value = fields.value as MoveStructFields;
+            const metadata = (value?.fields?.metadata as MoveStructFields)?.fields || {};
+            const stakingPool = (value?.fields?.staking_pool as MoveStructFields)?.fields || {};
             const exchangeRates = (stakingPool.exchange_rates as MoveStructFields)?.fields || {};
 
             return {
                 isPending: true,
                 authorityPubkeyBytes: '',
-                commissionRate: String((fields.value as MoveStructFields)?.fields.commission_rate),
+                commissionRate: String(value?.fields.commission_rate),
                 description: String(metadata.description),
                 exchangeRatesId: (
                     exchangeRates.id as {
@@ -64,20 +61,16 @@ export function sanitizePendingValidators(
                     }
                 )?.id,
                 exchangeRatesSize: String(exchangeRates.size),
-                gasPrice: String((fields.value as MoveStructFields)?.fields.gas_price),
+                gasPrice: String(value?.fields.gas_price),
                 imageUrl: String(metadata.image_url),
                 iotaAddress: String(metadata.iota_address),
                 name: String(metadata.name),
                 netAddress: String(metadata.net_address),
                 networkPubkeyBytes: '',
-                nextEpochCommissionRate: String(
-                    (fields.value as MoveStructFields)?.fields.next_epoch_commission_rate,
-                ),
-                nextEpochGasPrice: String(
-                    (fields.value as MoveStructFields)?.fields.next_epoch_gas_price,
-                ),
-                nextEpochStake: String((fields.value as MoveStructFields)?.fields.next_epoch_stake),
-                operationCapId: String((fields.value as MoveStructFields)?.fields.operation_cap_id),
+                nextEpochCommissionRate: String(value?.fields.next_epoch_commission_rate),
+                nextEpochGasPrice: String(value?.fields.next_epoch_gas_price),
+                nextEpochStake: String(value?.fields.next_epoch_stake),
+                operationCapId: String(value?.fields.operation_cap_id),
                 p2pAddress: String(metadata.p2p_address),
                 pendingPoolTokenWithdraw: String(stakingPool.pending_pool_token_withdraw),
                 pendingStake: String(stakingPool.pending_stake),
@@ -94,7 +87,7 @@ export function sanitizePendingValidators(
                     }
                 )?.id,
                 stakingPoolIotaBalance: String(stakingPool.iota_balance),
-                votingPower: String((fields.value as MoveStructFields)?.fields.voting_power),
+                votingPower: String(value?.fields.voting_power),
             };
         }) || []
     );
