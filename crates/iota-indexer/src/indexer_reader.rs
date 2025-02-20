46736134b8929a2ecf8327d5da8728f85662e226
samuel_rufi <samuel.rufinatscha@iota.org>
2025-02-20 11:14:17 +0100

    Revert "refactor: Remove TransactionFilter::TransactionKindIn filter"
    This reverts commit d993a49b070008e977cfff3059096d726686e1d5.

--- crates/iota-indexer/src/indexer_reader.rs
+++ crates/iota-indexer/src/indexer_reader.rs
@@ -933,6 +933,19 @@ impl IndexerReader {
             Some(TransactionFilter::TransactionKind(kind)) => {
                 ("tx_kinds".into(), format!("tx_kind = {kind}"))
             }
+            Some(TransactionFilter::TransactionKindIn(kind_vec)) => {
+                if kind_vec.is_empty() {
+                    return Err(IndexerError::InvalidArgument(
+                        "TransactionKindIn filter is empty".into(),
+                    ));
+                }
+                let kinds_str = kind_vec
+                    .iter()
+                    .map(|k| (*k as i16).to_string())
+                    .collect::<Vec<_>>()
+                    .join(", ");
+                ("tx_kinds".into(), format!("tx_kind IN ({})", kinds_str))
+            }
             None => {
                 // apply no filter
                 ("transactions".into(), "1 = 1".into())
