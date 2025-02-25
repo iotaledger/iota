6c92538af9a451b5d262296f81ad2c55ecbdf62f
Konstantinos Demartinos <konstantinos.demartinos@iota.org>
2025-02-25 13:24:38 +0200

    fixup! fix: update remaining tests to get_latest_iota_system_state_v2

--- crates/iota-indexer/tests/rpc-tests/governance_api.rs
+++ crates/iota-indexer/tests/rpc-tests/governance_api.rs
@@ -479,12 +479,14 @@ fn get_latest_iota_system_state_v2() {
         runtime,
         store,
         client,
-        ..
+        cluster,
     } = ApiTestSetup::get_or_init();
 
     runtime.block_on(async move {
         indexer_wait_for_checkpoint(store, 1).await;
 
+        // ensure that the system state is updated
+        cluster.force_new_epoch().await;
         let system_state = client.get_latest_iota_system_state_v2().await.unwrap();
         let IotaSystemStateSummary::V2(system_state_v2) = system_state else {
             panic!("expected IotaSystemStateSummaryV2");
@@ -493,7 +495,7 @@ fn get_latest_iota_system_state_v2() {
             system_state_v2.protocol_version,
             ProtocolVersion::MAX.as_u64()
         );
-        assert_eq!(system_state_v2.system_state_version, 1);
+        assert_eq!(system_state_v2.system_state_version, 2);
     });
 }
 
