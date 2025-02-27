bd6adc2d27637f9e1f8823fa4b40b94ff44dda60
Dkwcs <pavlo.botnar@gmail.com>
2025-02-27 17:02:13 +0100

    fields fox for protocol configs

--- crates/iota-protocol-config/src/lib.rs
+++ crates/iota-protocol-config/src/lib.rs
@@ -1689,6 +1689,8 @@ impl ProtocolConfig {
             cfg.vdf_hash_to_input_cost = Some(100);
 
             cfg.feature_flags.passkey_auth = true;
+
+            cfg.feature_flags.uncompressed_g1_group_elements = true;
         }
 
         for cur in 2..=version.0 {
@@ -1704,7 +1706,6 @@ impl ProtocolConfig {
                 }
                 5 => {
                     cfg.feature_flags.disallow_new_modules_in_deps_only_packages = true;
-                    cfg.feature_flags.uncompressed_g1_group_elements = true;
                     cfg.group_ops_bls12381_g1_to_uncompressed_g1_cost = Some(26);
                     cfg.group_ops_bls12381_uncompressed_g1_to_g1_cost = Some(52);
                     cfg.group_ops_bls12381_uncompressed_g1_sum_base_cost = Some(26);
