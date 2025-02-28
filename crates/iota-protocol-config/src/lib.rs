469e47aca1e09500e370164b634df95e68393ad2
Pavlo Botnar <pavlo.botnar@gmail.com>
2025-02-28 11:07:34 +0100

    feat(iota-framework, iota-move-natives): introduces a new uncompressed representation of BLS12381-G1 (#5630)
    * feat(iota-framework, iota-move-natives): introduces a new uncompressed representation of BLS12381-G1 elements and a function to add a vector of uncompressed elements

--- crates/iota-protocol-config/src/lib.rs
+++ crates/iota-protocol-config/src/lib.rs
@@ -195,6 +195,10 @@ struct FeatureFlags {
     #[serde(skip_serializing_if = "is_false")]
     relocate_event_module: bool,
 
+    // Enable uncompressed group elements in BLS123-81 G1
+    #[serde(skip_serializing_if = "is_false")]
+    uncompressed_g1_group_elements: bool,
+
     // Disallow adding new modules in `deps-only` packages.
     #[serde(skip_serializing_if = "is_false")]
     disallow_new_modules_in_deps_only_packages: bool,
@@ -846,6 +850,11 @@ pub struct ProtocolConfig {
     group_ops_bls12381_g2_msm_base_cost_per_input: Option<u64>,
     group_ops_bls12381_msm_max_len: Option<u32>,
     group_ops_bls12381_pairing_cost: Option<u64>,
+    group_ops_bls12381_g1_to_uncompressed_g1_cost: Option<u64>,
+    group_ops_bls12381_uncompressed_g1_to_g1_cost: Option<u64>,
+    group_ops_bls12381_uncompressed_g1_sum_base_cost: Option<u64>,
+    group_ops_bls12381_uncompressed_g1_sum_cost_per_term: Option<u64>,
+    group_ops_bls12381_uncompressed_g1_sum_max_terms: Option<u64>,
 
     // hmac::hmac_sha3_256
     hmac_hmac_sha3_256_cost_base: Option<u64>,
@@ -1085,6 +1094,10 @@ impl ProtocolConfig {
 
     pub fn relocate_event_module(&self) -> bool {
         self.feature_flags.relocate_event_module
+    }
+
+    pub fn uncompressed_g1_group_elements(&self) -> bool {
+        self.feature_flags.uncompressed_g1_group_elements
     }
 
     pub fn disallow_new_modules_in_deps_only_packages(&self) -> bool {
@@ -1519,6 +1532,11 @@ impl ProtocolConfig {
             group_ops_bls12381_g2_msm_base_cost_per_input: Some(52),
             group_ops_bls12381_msm_max_len: Some(32),
             group_ops_bls12381_pairing_cost: Some(52),
+            group_ops_bls12381_g1_to_uncompressed_g1_cost: None,
+            group_ops_bls12381_uncompressed_g1_to_g1_cost: None,
+            group_ops_bls12381_uncompressed_g1_sum_base_cost: None,
+            group_ops_bls12381_uncompressed_g1_sum_cost_per_term: None,
+            group_ops_bls12381_uncompressed_g1_sum_max_terms: None,
 
             // zklogin::check_zklogin_id
             check_zklogin_id_cost_base: Some(200),
@@ -1686,6 +1704,15 @@ impl ProtocolConfig {
                 }
                 5 => {
                     cfg.feature_flags.disallow_new_modules_in_deps_only_packages = true;
+                    cfg.group_ops_bls12381_g1_to_uncompressed_g1_cost = Some(26);
+                    cfg.group_ops_bls12381_uncompressed_g1_to_g1_cost = Some(52);
+                    cfg.group_ops_bls12381_uncompressed_g1_sum_base_cost = Some(26);
+                    cfg.group_ops_bls12381_uncompressed_g1_sum_cost_per_term = Some(13);
+                    cfg.group_ops_bls12381_uncompressed_g1_sum_max_terms = Some(2000);
+
+                    if chain != Chain::Mainnet && chain != Chain::Testnet {
+                        cfg.feature_flags.uncompressed_g1_group_elements = true;
+                    }
                 }
                 // Use this template when making changes:
                 //
