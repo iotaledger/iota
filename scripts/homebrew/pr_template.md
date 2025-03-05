9f2892677e51c011f7beafd455d20b8a15e14bde
Chloe Martin <chloedaughterofmars@gmail.com>
2025-03-05 10:16:26 -0500

    manually create PR

---
+++ scripts/homebrew/pr_template.md
@@ -0,0 +1,7 @@
+## Description
+
+A new release has been published on {{server_url}}/{{repository}}:
+
+### {{repository}} {{version}}
+
+From release: {{server_url}}/{{repository}}/actions/runs/{{run_id}}
---
+++ scripts/homebrew/update_formula.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+ROOT=$(git rev-parse --show-toplevel || realpath "$(dirname "$0")/../..")
+
+run_id=$1
+version=$2
+server_url=${3:-"https://github.com/iotaledger"}
+repository=${4:-"iota"}
+
+checksums=${ROOT}/checksum.txt
+cat ${checksums}
+
+macos_arm64_checksum=${sed -En 's/^macos-arm64.*([0-9a-f]{64})$/\1/p' ${checksums}}
+linux_x86_64_checksum=${sed -En 's/^linux-x86_64.*([0-9a-f]{64})$/\1/p' ${checksums}}
+
+git clone -b ${repository}-${version} ${repository}/homebrew-tap homebrew-tap
+cd homebrew-tap
+
+formula=Formula/${repository}.rb
+pr_description=${echo \
+    ${cat ${ROOT}/scripts/homebrew/pr_template.md} | \
+    sed 's/{{server_url}}/${server_url}/g' | \
+    sed 's/{{repository}}/${repository}/g' | \
+    sed 's/{{version}}/${version}/g' }
+
+cp -rf ${ROOT}/scripts/homebrew/template ${formula}
+
+sed -i 's/{{version}}/${version}/g' ${formula}
+sed -i 's/{{macos-arm64-checksum}}}/${macos_arm64_checksum}/g' ${formula}
+sed -i 's/{{linux-x86_64-checksum}}}/${linux_x86_64_checksum}/g' ${formula}
+
+title="Update brew formula for ${repository} ${version}"
+
+git add ${formula}
+git commit -m ${title}
+
+gh pr create --base main --title ${title} --body-file ${pr_description}
