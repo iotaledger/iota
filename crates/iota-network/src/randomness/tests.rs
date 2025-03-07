81f71e6b9e4a906c07459b6659fe6ca23a5fcdd8
Vlad Semenov <vlad.semenov@iota.org>
2025-03-07 15:55:13 +0300

    fix: use timeout function

--- crates/iota-network/src/randomness/tests.rs
+++ crates/iota-network/src/randomness/tests.rs
@@ -447,18 +447,15 @@ async fn test_byzantine_peer_handling() {
     let (rx2_mut, rx3_mut) = randomness_rxs.split_at_mut(3);
     let rnd2_fut = rx2_mut[2].recv();
     let rnd3_fut = rx3_mut[0].recv();
-    tokio::select! {
-        // Validators (2, 3) can communicate normally.
-        rnds = futures::future::join_all([rnd2_fut, rnd3_fut]) => {
-            for rnd in rnds {
-                let (epoch, round, bytes) = rnd
-                    .expect("Validators (2, 3) should receive randomness in epoch 0, round 0");
-                assert_eq!(0, epoch);
-                assert_eq!(0, round.0);
-                assert_ne!(0, bytes.len());
-            }
-        },
-        _ = tokio::time::sleep(timeout) => panic!("Timeout expired to receive randomness"),
+    // Validators (2, 3) can communicate normally.
+    let rnds = tokio::time::timeout(timeout, futures::future::join_all([rnd2_fut, rnd3_fut]))
+        .await
+        .expect("Honest peers (2, 3) should produce randomness in time");
+    for rnd in rnds {
+        let (epoch, round, bytes) = rnd.expect("Channel is not closed and randomness is produced");
+        assert_eq!(0, epoch, "Honest peers produce randomness in epoch 0");
+        assert_eq!(0, round.0, "Honest peers produce randomness in round 0");
+        assert_ne!(0, bytes.len(), "Honest peers produce non-empty randomness");
     }
     for rx in &mut randomness_rxs[..2] {
         // Validators (0, 1) are byzantine.
@@ -490,18 +487,19 @@ async fn test_byzantine_peer_handling() {
     let (rx0_mut, rx1_mut) = randomness_rxs.split_at_mut(1);
     let rnd0_fut = rx0_mut[0].recv();
     let rnd1_fut = rx1_mut[0].recv();
-    tokio::select! {
-        // Validators (0, 1) can communicate normally in new epoch.
-        rnds = futures::future::join_all([rnd0_fut, rnd1_fut]) => {
-            for rnd in rnds {
-                let (epoch, round, bytes) = rnd
-                    .expect("Validators (0, 1) should receive randomness in epoch 1, round 0");
-                assert_eq!(1, epoch);
-                assert_eq!(0, round.0);
-                assert_ne!(0, bytes.len());
-            }
-        },
-        _ = tokio::time::sleep(timeout) => panic!("Timeout expired to receive randomness"),
+    // Validators (0, 1) can communicate normally in new epoch.
+    let rnds = tokio::time::timeout(timeout, futures::future::join_all([rnd0_fut, rnd1_fut]))
+        .await
+        .expect("Byzantine peers (0, 1) should produce randomness in time");
+    for rnd in rnds {
+        let (epoch, round, bytes) = rnd.expect("Channel is not closed and randomness is produced");
+        assert_eq!(1, epoch, "Byzantine peers produce randomness in epoch 1");
+        assert_eq!(0, round.0, "Byzantine peers produce randomness in round 0");
+        assert_ne!(
+            0,
+            bytes.len(),
+            "Byzantine peers produce non-empty randomness"
+        );
     }
     for rx in &mut randomness_rxs[2..] {
         // Validators (2, 3) are still on old epoch.
