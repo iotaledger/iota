1666d916443e8b8ca8d26cb00323f37bca119074
Vlad Semenov <vlad.semenov@iota.org>
2025-03-07 21:41:07 +0300

    fix: increase timeout for tests only

--- crates/iota-network/src/randomness/mod.rs
+++ crates/iota-network/src/randomness/mod.rs
@@ -982,7 +982,15 @@ impl RandomnessEventLoop {
                     continue; // don't send partial sigs to self
                 }
                 let mut client = RandomnessClient::new(peer.clone());
-                const SEND_PARTIAL_SIGNATURES_TIMEOUT: Duration = Duration::from_secs(30);
+                // `test_byzantine_peer_handling` built in debug mode takes
+                // longer to verify invalid signatures and thus needs larger
+                // timeouts.
+                #[cfg(test)]
+                const SEND_PARTIAL_SIGNATURES_TIMEOUT: Duration = Duration::from_secs(100);
+                // In release signature verification should take less, so
+                // smaller timeout should be enough.
+                #[cfg(not(test))]
+                const SEND_PARTIAL_SIGNATURES_TIMEOUT: Duration = Duration::from_secs(10);
                 let full_sig = full_sig.get().cloned();
                 let request = anemo::Request::new(SendSignaturesRequest {
                     epoch,
