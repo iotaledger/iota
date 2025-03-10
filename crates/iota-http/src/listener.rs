7b354a5656886b7265f01a81ac06f6e32f35f4b1
ilyavorobev <vorobyev.i.v@yandex.ru>
2025-03-10 17:27:38 +0100

    update cargo.lock

--- crates/iota-http/src/listener.rs
+++ crates/iota-http/src/listener.rs
@@ -2,7 +2,9 @@
 // Modifications Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use std::net::SocketAddr;
 use std::time::Duration;
+use tokio::net;
 
 /// Types that can listen for connections.
 pub trait Listener: Send + 'static {
@@ -144,6 +146,7 @@ impl Listener for TcpListenerWithOptions {
     }
 }
 
+
 // Uncomment once we update tokio to >=1.41.0
 // #[cfg(unix)]
 // impl Listener for tokio::net::UnixListener {
@@ -164,6 +167,10 @@ impl Listener for TcpListenerWithOptions {
 //         Self::local_addr(self).map(Into::into)
 //     }
 // }
+
+
+
+
 
 /// Return type of [`ListenerExt::tap_io`].
 ///
