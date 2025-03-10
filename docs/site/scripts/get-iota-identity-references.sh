66c67df82979593fa7a920520138d4a737e6ceaa
Sebastian Wolfram <wulfraem@users.noreply.github.com>
2025-03-10 10:44:51 +0100

    update and re-enable wasm section
    - minor updates, fixes

--- docs/site/scripts/get-iota-identity-references.sh
+++ docs/site/scripts/get-iota-identity-references.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# Create temporaty directory to work in
+# Create temporary directory to work in
 mkdir tmp
 cd tmp
 
 # Download and copy docs
-curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota-identity/1.3/wasm.tar.gz | tar xzv
+curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota-identity/1.6/wasm.tar.gz  | tar xzv
 cp -Rv docs/* ../../content/references/iota-identity/
 
 # Return to root and cleanup
