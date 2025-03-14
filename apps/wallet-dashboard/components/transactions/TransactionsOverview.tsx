9ff27a08f9461ff3b1181a876b1d9275b069b3de
Panteleymonchuk <panteleymonchuk@gmail.com>
2025-03-14 15:58:58 +0200

    fix(wallet-dashboard): update layout to see end of the list TransactionsOverview component for xl screen

--- apps/wallet-dashboard/components/transactions/TransactionsOverview.tsx
+++ apps/wallet-dashboard/components/transactions/TransactionsOverview.tsx
@@ -9,7 +9,7 @@ export function TransactionsOverview() {
     return (
         <Panel>
             <Title title="Activity" />
-            <div className="h-[400px] px-sm pb-md pt-sm xl:h-[500px]">
+            <div className="flex-1 overflow-y-auto px-sm pb-md pt-sm">
                 <TransactionsList overflowClassName="overflow-y-auto" />
             </div>
         </Panel>
