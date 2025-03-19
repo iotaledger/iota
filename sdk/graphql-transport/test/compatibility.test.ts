61698616e49da73c034d18b313792ae3a7f0d005
Mario Sarcevic <mario.sarcevic@iota.org>
2025-03-19 11:12:08 +0100

    feat(graphql-transport): Revert compatibility test change

--- sdk/graphql-transport/test/compatibility.test.ts
+++ sdk/graphql-transport/test/compatibility.test.ts
@@ -355,24 +355,23 @@ describe('GraphQL IotaClient compatibility', () => {
     });
 
     test('queryTransactionBlocks', async () => {
-        const { nextCursor: cursor, ...rpcTransactions } =
-            await toolbox.client.queryTransactionBlocks({
-                filter: {
-                    FromAddress: toolbox.address(),
-                },
-                options: {
-                    showBalanceChanges: true,
-                    showEffects: true,
-                    showRawEffects: true,
-                    showEvents: true,
-                    // TODO inputs missing valueType
-                    showInput: false,
-                    showObjectChanges: true,
-                    showRawInput: true,
-                },
-            });
+        const { nextCursor: _, ...rpcTransactions } = await toolbox.client.queryTransactionBlocks({
+            filter: {
+                FromAddress: toolbox.address(),
+            },
+            options: {
+                showBalanceChanges: true,
+                showEffects: true,
+                showRawEffects: true,
+                showEvents: true,
+                // TODO inputs missing valueType
+                showInput: false,
+                showObjectChanges: true,
+                showRawInput: true,
+            },
+        });
 
-        const { nextCursor: cursor2, ...graphQLTransactions } =
+        const { nextCursor: __, ...graphQLTransactions } =
             await graphQLClient!.queryTransactionBlocks({
                 filter: {
                     FromAddress: toolbox.address(),
@@ -390,42 +389,6 @@ describe('GraphQL IotaClient compatibility', () => {
             });
 
         expect(graphQLTransactions).toEqual(rpcTransactions);
-
-        const { nextCursor: cursor3, ...rpcTransactions2 } =
-            await toolbox.client.queryTransactionBlocks({
-                filter: {
-                    TransactionKind: 'Genesis',
-                },
-                options: {
-                    showBalanceChanges: true,
-                    showEffects: true,
-                    showRawEffects: true,
-                    showEvents: true,
-                    // TODO inputs missing valueType
-                    showInput: false,
-                    showObjectChanges: true,
-                    showRawInput: true,
-                },
-            });
-
-        const { nextCursor: cursor4, ...graphQLTransactions2 } =
-            await graphQLClient!.queryTransactionBlocks({
-                filter: {
-                    FromAddress: toolbox.address(),
-                },
-                options: {
-                    showBalanceChanges: true,
-                    showEffects: true,
-                    showRawEffects: true,
-                    showEvents: true,
-                    // TODO inputs missing valueType
-                    showInput: false,
-                    showObjectChanges: true,
-                    showRawInput: true,
-                },
-            });
-
-        expect(graphQLTransactions2).toEqual(rpcTransactions2);
     });
 
     test('getTransactionBlock', async () => {
