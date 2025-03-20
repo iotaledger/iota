89d97613cffc1fee7dd3e9db5676c62cfa91f275
Thibault Martinez <thibault@iota.org>
2025-03-20 14:05:26 +0100

    feat: add `iota-names` crate (#106)
    * feat: add `iota-names` crate
    
    * move constants
    
    * reorder things
    
    * move registry entries
    
    * remove mainnet/testnet config
    
    * Modifications Copyright
    
    * cargo sort
    
    * remove unused error variant
    
    * use `NameServiceConfig` to compute dynamic field ID locally
    
    * add config module
    
    * add domain and error modules
    
    * add registry module
    
    * move TODO
    
    * Update crates/iota-names/Cargo.toml
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    * reenable parent_extraction
    
    * rename error to IotaNamesError
    
    * fix(iota-names): MAX_DOMAIN_LENGTH
    
    * fix(iota-names): add TODO
    
    * set CODEOWNERS for iota-names/
    
    * clippy
    
    * improve comment
    
    Co-authored-by: DaughterOfMars <chloedaughterofmars@gmail.com>
    
    * add iota-names to crates-filters.yml
    
    ---------
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    Co-authored-by: Thoralf Müller <thoralf.mue@gmail.com>
    Co-authored-by: DaughterOfMars <chloedaughterofmars@gmail.com>

---
+++ crates/iota-names/src/config.rs
@@ -0,0 +1,102 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use std::str::FromStr;
+
+use iota_types::{
+    TypeTag,
+    base_types::{IotaAddress, ObjectID},
+};
+use move_core_types::{ident_str, identifier::IdentStr};
+use serde::{Deserialize, Serialize};
+
+use crate::Domain;
+
+pub const NAME_SERVICE_DOMAIN_MODULE: &IdentStr = ident_str!("domain");
+pub const NAME_SERVICE_DOMAIN_STRUCT: &IdentStr = ident_str!("Domain");
+pub const LEAF_EXPIRATION_TIMESTAMP: u64 = 0;
+pub const DEFAULT_TLD: &str = "iota";
+pub const ACCEPTED_SEPARATORS: [char; 2] = ['.', '*'];
+pub const IOTA_NEW_FORMAT_SEPARATOR: char = '@';
+
+// Devnet values
+pub const IOTA_NAMES_PACKAGE: &str =
+    "0x20c890da38609db67e2713e6b33b4e4d3c6a8e9f620f9bb48f918d2337e31503";
+pub const IOTA_NAMES_OBJECT_ID: &str =
+    "0x55716ea4b9b7563537a1ef2705f1b06060b35f15f2ea00a20de29c547c319bef";
+pub const REGISTRATION_PACKAGE: &str =
+    "0x160581f35fb2a58a4964d513a96c70e0b64053a254936ae12b5f4d17087436f5";
+pub const UTILS_PACKAGE: &str =
+    "0xdea9e554fbee54e8dd0ac1d036d46047b5621b8f8739aa155258d656303af8cf";
+pub const IOTA_FRAMEWORK: &str = "0x2";
+pub const CLOCK_OBJECT_ID: &str = "0x6";
+pub const REGISTRY_TABLE_ID: &str =
+    "0xd48c0a882059036ca8c21dcc8d8bcaefc923aa678f225d3d515b79e3094e5616";
+pub const REVERSE_REGISTRY_TABLE_ID: &str =
+    "0x82139fa7c076816b67e2ff0927f2b30e4d6e2874a3a108649152a7b7d9eb25ac";
+
+#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
+#[serde(rename_all = "kebab-case")]
+pub struct NameServiceConfig {
+    pub package_address: IotaAddress,
+    pub registry_id: ObjectID,
+    pub reverse_registry_id: ObjectID,
+}
+
+impl Default for NameServiceConfig {
+    fn default() -> Self {
+        // TODO change to mainnet
+        Self::devnet()
+    }
+}
+
+impl NameServiceConfig {
+    pub fn new(
+        package_address: IotaAddress,
+        registry_id: ObjectID,
+        reverse_registry_id: ObjectID,
+    ) -> Self {
+        Self {
+            package_address,
+            registry_id,
+            reverse_registry_id,
+        }
+    }
+
+    pub fn record_field_id(&self, domain: &Domain) -> ObjectID {
+        let domain_type_tag = Domain::type_(self.package_address);
+        let domain_bytes = bcs::to_bytes(domain).unwrap();
+
+        iota_types::dynamic_field::derive_dynamic_field_id(
+            self.registry_id,
+            &TypeTag::Struct(Box::new(domain_type_tag)),
+            &domain_bytes,
+        )
+        .unwrap()
+    }
+
+    pub fn reverse_record_field_id(&self, address: &IotaAddress) -> ObjectID {
+        iota_types::dynamic_field::derive_dynamic_field_id(
+            self.reverse_registry_id,
+            &TypeTag::Address,
+            address.as_ref(),
+        )
+        .unwrap()
+    }
+
+    // TODO add mainnet() and testnet()
+
+    // Create a config based on the package and object ids published on devnet
+    pub fn devnet() -> Self {
+        const DEVNET_IOTA_NAMES_PACKAGE_ADDRESS: &str = IOTA_NAMES_PACKAGE;
+        const DEVNET_IOTA_NAMES_REGISTRY_ID: &str = REGISTRY_TABLE_ID;
+        const DEVNET_IOTA_NAMES_REVERSE_REGISTRY_ID: &str = REVERSE_REGISTRY_TABLE_ID;
+
+        let package_address = IotaAddress::from_str(DEVNET_IOTA_NAMES_PACKAGE_ADDRESS).unwrap();
+        let registry_id = ObjectID::from_str(DEVNET_IOTA_NAMES_REGISTRY_ID).unwrap();
+        let reverse_registry_id =
+            ObjectID::from_str(DEVNET_IOTA_NAMES_REVERSE_REGISTRY_ID).unwrap();
+
+        Self::new(package_address, registry_id, reverse_registry_id)
+    }
+}
