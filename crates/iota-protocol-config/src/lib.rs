f82e2a578cc5c901eccd8ea4016a245dd90770a7
VorobyevIlya <99146229+VorobyevIlya@users.noreply.github.com>
2025-03-22 09:35:44 +0100

    feat(consensus): get highest accepted rounds from round prober and use in ancestor selection (#6004)
    * get highest accepted rounds from round prober and use in ancestor selection
    
    * fix fmt
    
    * update snapshots
    
    * fix typos
    
    * formatting lib.rs
    
    * update version 6 description

--- crates/iota-protocol-config/src/lib.rs
+++ crates/iota-protocol-config/src/lib.rs
@@ -38,7 +38,7 @@ pub const MAX_PROTOCOL_VERSION: u64 = 6;
 //            Enable proper conversion of certain type argument errors in the
 //            execution layer.
 // Version 6: Variants as type nodes. Enable smart ancestor selection for
-// devnet.
+// devnet. Enable probing for accepted rounds in round prober for devnet.
 
 #[derive(Copy, Clone, Debug, Hash, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ProtocolVersion(u64);
@@ -245,6 +245,10 @@ struct FeatureFlags {
     // Use smart ancestor selection in consensus.
     #[serde(skip_serializing_if = "is_false")]
     consensus_smart_ancestor_selection: bool,
+
+    // Probe accepted rounds in round prober.
+    #[serde(skip_serializing_if = "is_false")]
+    consensus_round_prober_probe_accepted_rounds: bool,
 }
 
 fn is_true(b: &bool) -> bool {
@@ -1191,6 +1195,11 @@ impl ProtocolConfig {
     pub fn consensus_smart_ancestor_selection(&self) -> bool {
         self.feature_flags.consensus_smart_ancestor_selection
     }
+
+    pub fn consensus_round_prober_probe_accepted_rounds(&self) -> bool {
+        self.feature_flags
+            .consensus_round_prober_probe_accepted_rounds
+    }
 }
 
 #[cfg(not(msim))]
@@ -1900,6 +1909,9 @@ impl ProtocolConfig {
                     if !matches!(chain, Chain::Mainnet | Chain::Testnet) {
                         // Enable smart ancestor selection for devnet
                         cfg.feature_flags.consensus_smart_ancestor_selection = true;
+                        // Enable probing for accepted rounds in round prober.
+                        cfg.feature_flags
+                            .consensus_round_prober_probe_accepted_rounds = true;
                     }
                 }
                 // Use this template when making changes:
@@ -2037,6 +2049,11 @@ impl ProtocolConfig {
 
     pub fn set_consensus_linearize_subdag_v2_for_testing(&mut self, val: bool) {
         self.feature_flags.consensus_linearize_subdag_v2 = val;
+    }
+
+    pub fn set_consensus_round_prober_probe_accepted_rounds(&mut self, val: bool) {
+        self.feature_flags
+            .consensus_round_prober_probe_accepted_rounds = val;
     }
 }
 
