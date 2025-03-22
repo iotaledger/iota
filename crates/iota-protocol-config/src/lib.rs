60d0c226b0aecb690d470d4202eb25c4bbfad517
VorobyevIlya <99146229+VorobyevIlya@users.noreply.github.com>
2025-03-22 07:47:42 +0100

    feat(consensus): exclude low scoring ancestors in proposals with time based inclusion exclusion (#6003)
    * patch from upstream
    
    * update lib
    
    * fix formatting
    
    * fix errors
    
    * update snapshots
    
    * fix fmt
    
    * remove extra initialization
    
    * Update crates/iota-protocol-config/src/lib.rs
    
    Co-authored-by: DaughterOfMars <chloedaughterofmars@gmail.com>
    
    * update version 6 description
    
    ---------
    
    Co-authored-by: DaughterOfMars <chloedaughterofmars@gmail.com>

--- crates/iota-protocol-config/src/lib.rs
+++ crates/iota-protocol-config/src/lib.rs
@@ -30,7 +30,8 @@ pub const MAX_PROTOCOL_VERSION: u64 = 6;
 // maximal nodes which are allowed when converting to a type layout.
 // Version 5: Introduce fixed protocol-defined base fee, IotaSystemStateV2 and
 // SystemEpochInfoEventV2
-// Version 6: Variants as type nodes.
+// Version 6: Variants as type nodes. Enable smart ancestor selection for
+// devnet.
 
 #[derive(Copy, Clone, Debug, Hash, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ProtocolVersion(u64);
@@ -217,6 +218,10 @@ struct FeatureFlags {
     // Variants count as nodes
     #[serde(skip_serializing_if = "is_false")]
     variant_nodes: bool,
+
+    // Use smart ancestor selection in consensus.
+    #[serde(skip_serializing_if = "is_false")]
+    consensus_smart_ancestor_selection: bool,
 }
 
 fn is_true(b: &bool) -> bool {
@@ -1141,6 +1146,10 @@ impl ProtocolConfig {
     pub fn variant_nodes(&self) -> bool {
         self.feature_flags.variant_nodes
     }
+
+    pub fn consensus_smart_ancestor_selection(&self) -> bool {
+        self.feature_flags.consensus_smart_ancestor_selection
+    }
 }
 
 #[cfg(not(msim))]
@@ -1754,6 +1763,11 @@ impl ProtocolConfig {
                         .consensus_distributed_vote_scoring_strategy = true;
 
                     cfg.feature_flags.variant_nodes = true;
+
+                    if !matches!(chain, Chain::Mainnet | Chain::Testnet) {
+                        // Enable smart ancestor selection for devnet
+                        cfg.feature_flags.consensus_smart_ancestor_selection = true;
+                    }
                 }
                 // Use this template when making changes:
                 //
