24d632c7ea329b5ef304d3fb47ba8f8e2dae9c72
samuel_rufi <samuel.rufinatscha@iota.org>
2025-03-24 11:57:33 +0100

    feat: Add system package prefix

--- crates/iota-indexer/migrations/pg/2025-03-21-081552_participation_metrics/up.sql
+++ crates/iota-indexer/migrations/pg/2025-03-21-081552_participation_metrics/up.sql
@@ -1,8 +1,8 @@
 CREATE MATERIALIZED VIEW participation_metrics AS
 SELECT
-    COUNT(DISTINCT owner_id) AS total_addresses,
+    COUNT(DISTINCT owner_id) AS total_addresses
 FROM
     objects
 WHERE
-        object_type IN ('StakedIota', 'TimelockedStakedIota')
+        object_type IN ('0x0000000000000000000000000000000000000000000000000000000000000003::staking_pool::StakedIota', '0x0000000000000000000000000000000000000000000000000000000000000003::timelocked_staking::TimelockedStakedIota')
   AND owner_id IS NOT NULL;
