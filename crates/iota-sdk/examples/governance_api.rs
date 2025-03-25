34cf64ad91d7e698d0877b187755424c742ee284
Bing-Yang Lin <bingyang.lin@iota.org>
2025-03-25 16:25:39 +0800

    List active validators instead of committee

--- crates/iota-sdk/examples/governance_api.rs
+++ crates/iota-sdk/examples/governance_api.rs
@@ -40,19 +40,19 @@ async fn main() -> Result<(), anyhow::Error> {
     println!("{:?}", iota_system_state);
     println!(" *** IOTA System State ***\n");
 
-    // List all committee members
+    // List all active validators
 
-    let committee_members = iota_system_state.iter_committee_members();
+    let active_validators = iota_system_state.iter_active_validators();
 
-    println!(" *** List committee members *** ");
-    committee_members.for_each(|validator| {
+    println!(" *** List active validators *** ");
+    active_validators.for_each(|validator| {
         println!(
             "Name: {}, Description: {}, IotaAddress: {:?}",
             validator.name, validator.description, validator.iota_address
         )
     });
 
-    println!(" *** List committee members ***\n");
+    println!(" *** List active validators ***\n");
     // Reference Gas Price
     let reference_gas_price = client.governance_api().get_reference_gas_price().await?;
 
