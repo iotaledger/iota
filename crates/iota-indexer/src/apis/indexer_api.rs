888ede88e9af1e7b476715fc00ffc0fa6bc18cf1
samuel_rufi <samuel.rufinatscha@iota.org>
2025-03-26 09:15:31 +0100

    refactor: Fix typo

--- crates/iota-indexer/src/apis/indexer_api.rs
+++ crates/iota-indexer/src/apis/indexer_api.rs
@@ -250,6 +250,7 @@ impl IndexerApiServer for IndexerApi {
         .expect("deriving dynamic field id can't fail");
 
         let options = options.unwrap_or(IotaObjectDataOptions::full_content());
+
         match self.inner.get_object_read_in_blocking_task(id).await? {
             iota_types::object::ObjectRead::NotExists(_)
             | iota_types::object::ObjectRead::Deleted(_) => {}
