761eb9d560ca2fc1eeed13bc108b8a04cafc815c
Mario Sarcevic <mario.sarcevic@iota.org>
2025-03-28 13:58:04 +0100

    feat(tooling-apps): Remove unused hook

--- apps/core/src/hooks/index.ts
+++ apps/core/src/hooks/index.ts
@@ -35,7 +35,6 @@ export * from './useIotaAddressValidation';
 export * from './useUnlockTimelockedObjectsTransaction';
 export * from './useGetAllOwnedObjects';
 export * from './useGetTimelockedStakedObjects';
-export * from './useGetActiveValidatorsInfo';
 export * from './useGetStakingValidatorDetails';
 export * from './useCursorPagination';
 export * from './useTheme';
