cdf1e11cf222bdf1fc514c39c602b7e9153098c4
Nicolas M <marshall.nicolas@gmail.com>
2025-03-31 20:10:36 +0200

    scripts: fullnode systemd: setup script

--- setups/fullnode/systemd/setup.sh
+++ setups/fullnode/systemd/setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -x
 
-NETWORK=${NETWORK-"testnet"}
-CLONE_DEST=${CLONE_DEST-"./.cache/iota"}
+# INPUTS
+NETWORK=${NETWORK-"testnet"}; VALID_NETWORKS=("testnet" "devnet")
+CLONE_DIR=${CLONE_DIR-"$(git rev-parse --show-toplevel || echo ".")/.cache/iota-clone"}
+NODE_CONFIG_DIR=${NODE_CONFIG_DIR-"/opt/iota/config"}
+
+
+# TODO after 6017 remove once yaml templates merged
+HACK_ROOT="$(git rev-parse --show-toplevel || echo "$CLONE_DIR")"
 
 
 if [  "$(uname -s)" -ne "Linux" ]; then 
@@ -16,30 +22,10 @@ if ! command -v cargo &> /dev/null; then
     exit 1
 fi
 
-mkdir -p "$(dirname $CLONE_DEST)"
-if [ ! -d "$CLONE_DEST" ]; then
-    git clone https://github.com/iotaledger/iota.git "$CLONE_DEST"
-else
-    cd "$CLONE_DEST"
-    if [ "$(git remote get-url origin)" -ne "https://github.com/iotaledger/iota.git" ]; then
-        echo "Cloned repo does not have correct origin, please delete and re-clone"
-        exit 1
-    fi
+if [[ ! " ${VALID_NETWORKS[@]} " =~ " $NETWORK " ]]; then
+  echo "Invalid network selected: $NETWORK. Must be one of: ${VALID_NETWORKS[*]}"
+  exit 1
 fi
-cd "$CLONE_DEST"
-git checkout testnet
-git pull
-
-# TODO in 6017 extract min version from rust-toolchain.toml
-MIN_RUSTC_VERSION=1.85
-rustc_version=$(rustc --version | sed -n 's/rustc \([0-9]\+\.[0-9]\+\).*/\1/p')
-# checks that the min version is the smallest of both (by sorting)
-if [[ $(echo -e "$rustc_version\n$MIN_RUSTC_VERSION" | sort -V | head -n1) == "$rustc_version" ]]; then 
-    echo "Rust compiler version is "$rustc_version". Needs at least version "$MIN_RUSTC_VERSION". Upgrade with:"
-    echo "rustup update" # TODO in 6017 nightly ????
-    exit 1
-fi
-
 
 # Install system packages (libraries & other dependencies)
 sudo apt-get update \
@@ -58,8 +44,61 @@ sudo apt-get update \
     clang \
     cmake
 
-REPO_ROOT="$(git rev-parse --show-toplevel || echo "$CLONE_DEST")"
-# Install the binaries 
-# TODO in 6017 build with --release
-cargo build --bin iota-node
+mkdir -p "$(dirname $CLONE_DIR)"
+if [ ! -d "$CLONE_DIR" ]; then
+    git clone https://github.com/iotaledger/iota.git "$CLONE_DIR"
+else
+    cd "$CLONE_DIR"
+    if [ "$(git remote get-url origin)" -ne "https://github.com/iotaledger/iota.git" ]; then
+        echo "Cloned repo does not have correct origin, please delete and re-clone"
+        exit 1
+    fi
+fi
+cd "$CLONE_DIR"
+git checkout $NETWORK
+git pull
+
+# TODO in 6017 extract min version from rust-toolchain.toml
+MIN_RUSTC_VERSION=1.85
+rustc_version=$(rustc --version | sed -n 's/rustc \([0-9]\+\.[0-9]\+\).*/\1/p')
+# checks that the min version is the smallest of both (by sorting)
+if [[ $(echo -e "$rustc_version\n$MIN_RUSTC_VERSION" | sort -V | head -n1) == "$rustc_version" ]]; then 
+    echo "Rust compiler version is "$rustc_version". Needs at least version "$MIN_RUSTC_VERSION". Upgrade with:"
+    echo "rustup update" # TODO in 6017 nightly ????
+    exit 1
+fi
+
+
+# Build the binaries 
+cargo build --release --bin iota-node
+
+# Copy fullnode config
+mkdir -p "$NODE_CONFIG_DIR"
+# TODO after 6017 remove override once yaml files per network merged
+TEMPLATE=$( if [ -f "$CLONE_DIR/setups/fullnode/fullnode-template-$NETWORK.yaml" ]; 
+    then echo "$CLONE_DIR/setups/fullnode/fullnode-template-$NETWORK.yaml"; 
+    else echo "$HACK_ROOT/setups/fullnode/fullnode-template-$NETWORK.yaml";
+fi )
+
+# TODO in 6017 replace paths in yaml then pipe to file
+cat "$TEMPLATE" \
+    | sed "s|/opt/iota/config/genesis.blob|$NODE_CONFIG_DIR/genesis.blob|g" \
+    | sed "s|/opt/iota/config/migration.blob|$NODE_CONFIG_DIR/migration.blob|g" \
+    > "$NODE_CONFIG_DIR/fullnode.config.yaml"
+
+# Download genesis/migration blob for NETWORK
+curl -fLJ https://dbfiles.$NETWORK.iota.cafe/genesis.blob -o "$NODE_CONFIG_DIR/genesis.blob"
+if [ "$NETWORK" == "devnet" ]; then
+    curl -fLJ https://dbfiles.$NETWORK.iota.cafe/migration.blob -o "$NODE_CONFIG_DIR/migration.blob"
+fi
+
+
+# Finally run the node
+"$CLONE_DIR/target/release/iota-node" --config-path fullnode.yaml
+
+
+# TODO in 6017 add systemd service (config, start, enable)
+
+
+
 
