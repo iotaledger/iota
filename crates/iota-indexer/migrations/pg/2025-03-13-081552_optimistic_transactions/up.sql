a94a04a215b4032bfb2bb1b3e39361dd1a46f47b
Konstantinos Demartinos <konstantinos.demartinos@iota.org>
2025-04-01 16:46:46 +0300

    refactor(indexer,migration): define insertion_order sequence explicitly

--- crates/iota-indexer/migrations/pg/2025-03-13-081552_optimistic_transactions/up.sql
+++ crates/iota-indexer/migrations/pg/2025-03-13-081552_optimistic_transactions/up.sql
@@ -2,10 +2,17 @@
 -- checkpointed) is assigned when being indexed. It provides common
 -- ordering for optimistic and checkpointed transactions, whereas
 -- `tx_sequence_number` provides ordering only for checkpointed transactions.
+--
+-- In case of missing digests, the `tx_digests` table is used a fallback
+-- to resolve the transaction order. This is ok because optimistic transactions
+-- will be inserted only after creation of this table.
+CREATE SEQUENCE tx_insertion_order_seq;
 CREATE TABLE tx_insertion_order (
     tx_digest                   BYTEA        PRIMARY KEY,
-    insertion_order             BIGSERIAL    NOT NULL
+    insertion_order             BIGINT       NOT NULL DEFAULT nextval('tx_insertion_order_seq')
 );
+ALTER SEQUENCE tx_insertion_order_seq OWNED BY tx_insertion_order.insertion_order;
+SELECT setval('tx_insertion_order_seq', (SELECT MAX(tx_sequence_number) FROM tx_digests));
 CREATE UNIQUE INDEX tx_insertion_order_insertion_order ON tx_insertion_order (insertion_order);
 
 -- Main table storing data about optimistically indexed transactions
