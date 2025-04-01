e578c3aa54e274aef0df2cb93ddbab43f9e09ef4
Konstantinos Demartinos <konstantinos.demartinos@iota.org>
2025-04-01 09:41:51 +0300

    refactor(mainnet-unlocks): add aggregated data as a const str

--- crates/iota-mainnet-unlocks/src/lib.rs
+++ crates/iota-mainnet-unlocks/src/lib.rs
@@ -1,6 +1,7 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+mod aggregated_data;
 /// Provides functionality for loading and querying token unlock data over time.
 ///
 /// This module defines a `MainnetUnlocksStore` which maintains an in-memory
