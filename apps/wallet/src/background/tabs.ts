9917c7c1d0a4e434757410ec48756707e0db9926
Panteleymonchuk <panteleymonchuk@gmail.com>
2025-04-07 17:45:32 +0300

    refactor(wallet): removing redundant code

--- apps/wallet/src/background/tabs.ts
+++ apps/wallet/src/background/tabs.ts
@@ -5,23 +5,6 @@
 import { BehaviorSubject, filter, fromEventPattern, merge, share, Subject } from 'rxjs';
 import Browser from 'webextension-polyfill';
 import type { Tabs as BrowserTabs } from 'webextension-polyfill';
-
-const onTabActivated = fromEventPattern<BrowserTabs.OnActivatedActiveInfoType>(
-    (handler) => Browser.tabs.onActivated.addListener(handler),
-    (handler) => Browser.tabs.onActivated.removeListener(handler),
-).pipe(share());
-
-const onWindowFocusChanged = fromEventPattern<number>(
-    (handler) => Browser.windows.onFocusChanged.addListener(handler),
-    (handler) => Browser.windows.onFocusChanged.removeListener(handler),
-).pipe(share());
-
-// For window management (works with activeTab)
-export const onWindowRemovedStream = fromEventPattern<number>(
-    (handler) => Browser.windows.onRemoved.addListener(handler),
-    (handler) => Browser.windows.onRemoved.removeListener(handler),
-).pipe(share());
-
 // With activeTab, we can still track URL changes in the active tab
 const onActiveTabUpdated = fromEventPattern<
     [number, BrowserTabs.OnUpdatedChangeInfoType, BrowserTabs.Tab]
@@ -33,6 +16,16 @@ const onActiveTabUpdated = fromEventPattern<
     filter(([_, __, tab]) => tab.active === true),
     share(),
 );
+
+const onTabActivated = fromEventPattern<BrowserTabs.OnActivatedActiveInfoType>(
+    (handler) => Browser.tabs.onActivated.addListener(handler),
+    (handler) => Browser.tabs.onActivated.removeListener(handler),
+).pipe(share());
+
+const onWindowFocusChanged = fromEventPattern<number>(
+    (handler) => Browser.windows.onFocusChanged.addListener(handler),
+    (handler) => Browser.windows.onFocusChanged.removeListener(handler),
+).pipe(share());
 
 type TabInfo = {
     id: number;
@@ -160,82 +153,6 @@ class Tabs {
      */
     public get activeOrigin() {
         return this._onActiveOrigin.asObservable();
-    }
-
-    public async openUrlIfNotActive(
-        option: { windowID?: number } & (
-            | {
-                  url: string;
-                  match?: (values: { url: string; inAppRedirectUrl?: string }) => boolean;
-              }
-            | { tabID: number }
-        ),
-    ): Promise<boolean> {
-        try {
-            // With activeTab permission, we can only check the current active tab
-            // or create new tabs, not query or highlight other tabs
-
-            // If tabID is specified, we can only handle it if it's the current active tab
-            if ('tabID' in option) {
-                if (this.activeTab && this.activeTab.id === option.tabID) {
-                    // It's already the active tab
-                    return true;
-                } else {
-                    // We can't check or activate other tabs with activeTab permission
-                    return false;
-                }
-            }
-
-            // If URL is specified, check if current active tab matches
-            else {
-                // Refresh to make sure we have current info
-                await this.refreshActiveTab();
-
-                if (this.activeTab && this.activeTab.url) {
-                    const activeUrl = this.activeTab.url;
-
-                    // Direct URL match
-                    if (activeUrl === option.url) {
-                        return true;
-                    }
-
-                    // Check for locked URL pattern
-                    try {
-                        const tabURL = new URL(activeUrl);
-                        const targetUrlPart = option.url.split('#')[1] || '';
-
-                        if (tabURL.hash.startsWith('#/locked?url=')) {
-                            const inAppRedirectUrl = decodeURIComponent(
-                                tabURL.hash.replace('#/locked?url=', ''),
-                            );
-                            if (inAppRedirectUrl === targetUrlPart) {
-                                return true;
-                            }
-                        }
-
-                        // Use custom matcher if provided
-                        if (
-                            option.match &&
-                            option.match({
-                                url: activeUrl,
-                                inAppRedirectUrl: undefined,
-                            })
-                        ) {
-                            return true;
-                        }
-                    } catch (e) {
-                        // URL parsing error
-                    }
-                }
-
-                // No match found - create a new tab with the URL
-                // This is allowed with activeTab permission
-                await Browser.tabs.create({ url: option.url });
-                return true;
-            }
-        } catch (error) {
-            return false;
-        }
     }
 }
 
