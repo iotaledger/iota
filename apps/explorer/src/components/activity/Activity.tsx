51475d780c249bdaa2acd9f33d9b15ed4ecd5261
Jose Luis Torres Corbalan <jltorres@boxfish.studio>
2025-04-10 11:32:41 +0200

    fix(explorer): change div

--- apps/explorer/src/components/activity/Activity.tsx
+++ apps/explorer/src/components/activity/Activity.tsx
@@ -85,22 +85,17 @@ export function Activity({ initialLimit, disablePagination }: ActivityProps): JS
                         />
                     ))}
                 </SegmentedButton>
-                <div className="absolute inset-y-0 -top-1 right-sm flex items-center gap-sm text-2xl">
-                    {pollingTxnTableEnabled &&
-                        selectedCategory === ActivityCategory.Transactions && (
-                            <div>
-                                <Toggle
-                                    label="Show System Transactions"
-                                    isToggled={showSystemTransactions}
-                                    size={ToggleSize.Small}
-                                    onChange={() =>
-                                        setshowSystemTransactions(!showSystemTransactions)
-                                    }
-                                />
-                                <PlayPause paused={paused} onChange={handlePauseChange} />
-                            </div>
-                        )}
-                </div>
+                {pollingTxnTableEnabled && selectedCategory === ActivityCategory.Transactions && (
+                    <div className="absolute inset-y-0 -top-1 right-sm flex items-center gap-sm text-2xl">
+                        <Toggle
+                            label="Show System Transactions"
+                            isToggled={showSystemTransactions}
+                            size={ToggleSize.Small}
+                            onChange={() => setshowSystemTransactions(!showSystemTransactions)}
+                        />
+                        <PlayPause paused={paused} onChange={handlePauseChange} />
+                    </div>
+                )}
             </div>
             <div className="p-md">
                 {selectedCategory === ActivityCategory.Transactions && (
