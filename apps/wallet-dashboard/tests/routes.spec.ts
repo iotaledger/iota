3ddde2a04399e95ce24fda40276bde41d78192a7
Mario Sarcevic <mario.sarcevic@iota.org>
2025-04-17 12:14:38 +0200

    feat(wallet-dashboard): fix commented tests. add apps-backend run to playwright

--- apps/wallet-dashboard/tests/routes.spec.ts
+++ apps/wallet-dashboard/tests/routes.spec.ts
@@ -1,22 +1,9 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { Feature } from '@iota/core';
 import { expect, test } from './fixtures';
 import { connectWallet, createWallet } from './utils';
-import { growthbook } from '@/lib/utils';
-import { BrowserContext, Page } from '@playwright/test';
-
-// Todo fix
-// Configure the feature flags for the tests
-const setupFeatureFlags = async (context: BrowserContext, page: Page) => {
-    await context.addInitScript(() => {
-        growthbook.getFeatureValue(Feature.StardustMigration, true);
-        growthbook.getFeatureValue(Feature.SupplyIncreaseVesting, true);
-    });
-
-    await page.reload();
-};
+import { Page } from '@playwright/test';
 
 test.describe.serial('Protected Routes', () => {
     test.setTimeout(20_000);
@@ -32,7 +19,6 @@ test.describe.serial('Protected Routes', () => {
         await dashboardPage.goto('/');
 
         await connectWallet(dashboardPage, context, extensionName);
-        await setupFeatureFlags(context, dashboardPage);
 
         page = dashboardPage;
     });
@@ -58,19 +44,17 @@ test.describe.serial('Protected Routes', () => {
         });
     });
 
-    // Todo fix
+    test('Migration route', async () => {
+        await page.getByTestId('sidebar-migration').click();
+        await expect(page.getByRole('heading', { name: 'Migration' })).toBeVisible({
+            timeout: 30_000,
+        });
+    });
 
-    // test('Migration route', async () => {
-    //     await page.getByTestId('sidebar-migration').click();
-    //     await expect(page.getByRole('heading', { name: 'Migration' })).toBeVisible({
-    //         timeout: 30_000,
-    //     });
-    // });
-    //
-    // test('Vesting route', async () => {
-    //     await page.getByTestId('sidebar-vesting').click();
-    //     await expect(page.getByRole('heading', { name: 'Vesting' })).toBeVisible({
-    //         timeout: 30_000,
-    //     });
-    // });
+    test('Vesting route', async () => {
+        await page.getByTestId('sidebar-vesting').click();
+        await expect(page.getByRole('heading', { name: 'Vesting' })).toBeVisible({
+            timeout: 30_000,
+        });
+    });
 });
