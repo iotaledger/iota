2d10e6c018a8f615bd607378e45e3e9c92f0ec55
Dr-Electron <dr-electr0n@protonmail.com>
2025-04-20 23:27:33 +0200

    Format

--- docs/examples/rust/exchange/derive-address.rs
+++ docs/examples/rust/exchange/derive-address.rs
@@ -4,7 +4,8 @@ use hex;
 fn main() {
     // Example of deriving an Ed25519 address from a public key
     let mut hasher = iota_types::crypto::DefaultHash::default();
-    let example_public_key: &str = "0xc5b4eb7d3c7efdeb7f3546fee9971536a24298d0f6cac7a48619c343b3999e11";
+    let example_public_key: &str =
+        "0xc5b4eb7d3c7efdeb7f3546fee9971536a24298d0f6cac7a48619c343b3999e11";
     hasher.update(example_public_key);
     let arr = hasher.finalize();
     let iota_address_string = hex::encode(arr);
@@ -14,7 +15,8 @@ fn main() {
     let flag = 0x01; // 0x01 = Secp256k1, 0x02 = Secp256r1, 0x03 = multiSig
     // Hash the [flag, public key] bytearray using Blake2b
     let mut hasher = iota_types::crypto::DefaultHash::default();
-    let example_public_key: &str = "0xc5b4eb7d3c7efdeb7f3546fee9971536a24298d0f6cac7a48619c343b3999e11";
+    let example_public_key: &str =
+        "0xc5b4eb7d3c7efdeb7f3546fee9971536a24298d0f6cac7a48619c343b3999e11";
     hasher.update([flag]);
     hasher.update(example_public_key);
     let arr = hasher.finalize();
