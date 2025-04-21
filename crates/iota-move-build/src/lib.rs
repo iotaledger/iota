f69e7e3f069f444afcee1bcdbf3366e4b42ab2ea
Dkwcs <pavlo.botnar@gmail.com>
2025-04-21 10:45:21 +0200

    Minor fix

--- crates/iota-move-build/src/lib.rs
+++ crates/iota-move-build/src/lib.rs
@@ -397,8 +397,8 @@ impl CompiledPackage {
     /// Return the set of Object IDs corresponding to this package's transitive
     /// dependencies' storage package IDs (where to load those packages
     /// on-chain).
-    pub fn get_dependency_storage_package_ids(&self) -> Vec<ObjectID> {
-        self.dependency_ids.published.values().cloned().collect()
+    pub fn get_dependency_storage_package_ids(&self) -> impl Iterator<Item = ObjectID> {
+        self.dependency_ids.published.values().copied().collect()
     }
 
     pub fn get_package_digest(&self, with_unpublished_deps: bool) -> [u8; 32] {
