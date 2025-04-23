d3c68b4dc806e42e86caaacfd2e870c1d4356db8
Nicolas M <marshall.nicolas@gmail.com>
2025-04-23 20:02:11 +0100

    ci: simplify

--- setups/fullnode/docker/prepare_devnet.sh
+++ setups/fullnode/docker/prepare_devnet.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 WORKDIR="$( dirname "${BASH_SOURCE[0]}" )"
-DATA_DIR="$WORKDIR/data"
+DOCKER_DIR="$WORKDIR/docker"
+DATA_DIR="$DOCKER_DIR/data"
 CONFIG_DIR="$DATA_DIR/config"
 
 # check if the "data" folder exists
@@ -14,7 +15,6 @@ mkdir -p "$CONFIG_DIR"
 
 # download the genesis file
 curl -fLJ https://dbfiles.devnet.iota.cafe/genesis.blob -o "$CONFIG_DIR/genesis.blob"
-
 # download the migration file
 curl -fLJ https://dbfiles.devnet.iota.cafe/migration.blob -o "$CONFIG_DIR/migration.blob"
 
