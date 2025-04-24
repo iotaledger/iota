a3ea4c8987a9c7a70d55e2952dd17365bb422f44
evavirseda <evirseda@boxfish.studio>
2025-04-24 10:28:55 +0200

    update inactive validator message

--- apps/wallet-dashboard/components/dialogs/staking/views/DetailsView.tsx
+++ apps/wallet-dashboard/components/dialogs/staking/views/DetailsView.tsx
@@ -126,8 +126,8 @@ export function DetailsView({
                     ) : !isAnActiveValidator ? (
                         <InfoBox
                             type={InfoBoxType.Error}
-                            title="Disabled Validator is not earning rewards"
-                            supportingText="This validator is flagged as a bad actor. Continue staking at your own discretion."
+                            title="Inactive Validator is not earning rewards"
+                            supportingText="This validator is currently inactive and not earning rewards. Future rewards depend on it becoming active again. Stake at your own risk."
                             icon={<Warning />}
                             style={InfoBoxStyle.Elevated}
                         />
