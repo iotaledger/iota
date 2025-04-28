dbd09820a3c7f54a3cc6d06aaf7efc95ad6e582c
Ilya Vorobyev <99146229+VorobyevIlya@users.noreply.github.com>
2025-04-28 21:34:57 +0200

    cleanup(consensus): remove gc in starfish (#6456)
    # Description of change
    
    PR removes garbage collection(gc) feature from Starfish. It may be
    reimplemented later.
    
    
    ## Links to any relevant issues
    
    fixes #6420.
    
    ## Type of change
    
    Choose a type of change, and delete any options that are not relevant.
    
    - Cleanup
    
    
    ## How the change has been tested
    
    All tests from consensus crate except the gc-related ones are passed
    
    Make sure to provide instructions for the maintainer as well as any
    relevant configurations.
    
    ## Change checklist
    
    Tick the boxes that are relevant to your changes, and delete any items
    that are not.
    
    - [ ] I have followed the contribution guidelines for this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have checked that new and existing unit tests pass locally with
    my changes

--- crates/starfish/core/src/linearizer.rs
+++ crates/starfish/core/src/linearizer.rs
@@ -4,13 +4,11 @@
 
 use std::{collections::HashSet, sync::Arc};
 
-use itertools::Itertools;
 use parking_lot::RwLock;
 use starfish_config::AuthorityIndex;
 
 use crate::{
-    Round,
-    block::{BlockAPI, BlockRef, VerifiedBlock},
+    block::{BlockAPI, BlockRef, GENESIS_ROUND, VerifiedBlock},
     commit::{Commit, CommittedSubDag, TrustedCommit, sort_sub_dag_blocks},
     context::Context,
     dag_state::DagState,
@@ -23,10 +21,6 @@ use crate::{
 pub(crate) trait BlockStoreAPI {
     fn get_blocks(&self, refs: &[BlockRef]) -> Vec<Option<VerifiedBlock>>;
 
-    fn gc_round(&self) -> Round;
-
-    fn gc_enabled(&self) -> bool;
-
     fn set_committed(&mut self, block_ref: &BlockRef) -> bool;
 
     fn is_committed(&self, block_ref: &BlockRef) -> bool;
@@ -37,14 +31,6 @@ impl BlockStoreAPI
 {
     fn get_blocks(&self, refs: &[BlockRef]) -> Vec<Option<VerifiedBlock>> {
         DagState::get_blocks(self, refs)
-    }
-
-    fn gc_round(&self) -> Round {
-        DagState::gc_round(self)
-    }
-
-    fn gc_enabled(&self) -> bool {
-        DagState::gc_enabled(self)
     }
 
     fn set_committed(&mut self, block_ref: &BlockRef) -> bool {
@@ -145,23 +131,15 @@ impl Linearizer {
         last_committed_rounds: Vec<u32>,
         dag_state: &mut impl BlockStoreAPI,
     ) -> Vec<VerifiedBlock> {
-        let gc_enabled = dag_state.gc_enabled();
-        // The GC round here is calculated based on the last committed round of the
-        // leader block. The algorithm will attempt to commit blocks up to this
-        // GC round. Once this commit has been processed and written to DagState, then
-        // gc round will update and on the processing of the next commit we'll
-        // have it already updated, so no need to do any gc_round recalculations here.
-        // We just use whatever is currently in DagState.
-        let gc_round: Round = dag_state.gc_round();
         let leader_block_ref = leader_block.reference();
         let mut buffer = vec![leader_block];
 
         let mut to_commit = Vec::new();
 
         // The new logic will perform the recursion without stopping at the highest
-        // round round that has been committed per authority. Instead it will
+        // round that has been committed per authority. Instead it will
         // allow to commit blocks that are lower than the highest committed round for an
-        // authority but higher than gc_round.
+        // authority.
         if context.protocol_config.consensus_linearize_subdag_v2() {
             assert!(
                 dag_state.set_committed(&leader_block_ref),
@@ -178,7 +156,7 @@ impl Linearizer {
                             .iter()
                             .copied()
                             .filter(|ancestor| {
-                                ancestor.round > gc_round && !dag_state.is_committed(ancestor)
+                                ancestor.round > GENESIS_ROUND && !dag_state.is_committed(ancestor)
                             })
                             .collect::<Vec<_>>(),
                     )
@@ -212,19 +190,8 @@ impl Linearizer {
                             .filter(|ancestor| {
                                 // We skip the block if we already committed it or we reached a
                                 // round that we already committed.
-                                // TODO: for Fast Path we need to amend the recursion rule here and
-                                // allow us to commit blocks all the way up to the `gc_round`.
-                                // Some additional work will be needed to make sure that we keep the
-                                // uncommitted blocks up to the `gc_round` across commits.
                                 !committed.contains(ancestor)
                                     && last_committed_rounds[ancestor.author] < ancestor.round
-                            })
-                            .filter(|ancestor| {
-                                // Keep the block if GC is not enabled or it is enabled and the
-                                // block is above the gc_round. We do this
-                                // to stop the recursion early and avoid going to deep when it's
-                                // unnecessary.
-                                !gc_enabled || ancestor.round > gc_round
                             })
                             .collect::<Vec<_>>(),
                     )
@@ -239,17 +206,6 @@ impl Linearizer {
                     assert!(committed.insert(ancestor.reference()));
                 }
             }
-        }
-
-        // The above code should have not yielded any blocks that are <= gc_round, but
-        // just to make sure that we'll never commit anything that should be
-        // garbage collected we attempt to prune here as well.
-        if gc_enabled {
-            assert!(
-                to_commit.iter().all(|block| block.round() > gc_round),
-                "No blocks <= {gc_round} should be committed. Leader round {}, blocks {to_commit:?}.",
-                leader_block_ref
-            );
         }
 
         // Sort the blocks of the sub-dag blocks
@@ -292,8 +248,6 @@ impl Linearizer {
             let (sub_dag, commit) =
                 self.collect_sub_dag_and_commit(leader_block, reputation_scores_desc);
 
-            self.update_blocks_pruned_metric(&sub_dag);
-
             // Buffer commit in dag state for persistence later.
             // This also updates the last committed rounds.
             self.dag_state.write().add_commit(commit.clone());
@@ -311,60 +265,10 @@ impl Linearizer {
 
         committed_sub_dags
     }
-
-    // Try to measure the number of blocks that get pruned due to GC. This is not
-    // very accurate, but it can give us a good enough idea. We consider a block
-    // as pruned when it is an ancestor of a block that has been committed as part
-    // of the provided `sub_dag`, but it has not been committed as part of
-    // previous commits. Right now we measure this via checking that highest
-    // committed round for the authority as we don't an efficient look up
-    // functionality to check if a block has been committed or not.
-    fn update_blocks_pruned_metric(&self, sub_dag: &CommittedSubDag) {
-        let (last_committed_rounds, gc_round) = {
-            let dag_state = self.dag_state.read();
-            (dag_state.last_committed_rounds(), dag_state.gc_round())
-        };
-
-        for block_ref in sub_dag
-            .blocks
-            .iter()
-            .flat_map(|block| block.ancestors())
-            .filter(
-                |ancestor_ref| {
-                    ancestor_ref.round <= gc_round
-                        && last_committed_rounds[ancestor_ref.author] != ancestor_ref.round
-                }, /* If the last committed round is the same as the pruned block's round, then
-                    * we know for sure that it has been committed and it doesn't count here
-                    * as pruned block. */
-            )
-            .unique()
-        {
-            let hostname = &self.context.committee.authority(block_ref.author).hostname;
-
-            // If the last committed round from this authority is lower than the pruned
-            // ancestor in question, then we know for sure that it has not been committed.
-            let label_values = if last_committed_rounds[block_ref.author] < block_ref.round {
-                &[hostname, "uncommitted"]
-            } else {
-                // If last committed round is higher for this authority, then we don't really
-                // know it's status, but we know that there is a higher committed block from
-                // this authority.
-                &[hostname, "higher_committed"]
-            };
-
-            self.context
-                .metrics
-                .node_metrics
-                .blocks_pruned_on_commit
-                .with_label_values(label_values)
-                .inc();
-        }
-    }
 }
 
 #[cfg(test)]
 mod tests {
-    use rstest::rstest;
 
     use super::*;
     use crate::{
@@ -571,9 +475,6 @@ mod tests {
         let (mut context, _) = Context::new_for_test(num_authorities);
         context
             .protocol_config
-            .set_consensus_gc_depth_for_testing(0);
-        context
-            .protocol_config
             .set_consensus_linearize_subdag_v2_for_testing(false);
 
         let context = Arc::new(context);
@@ -679,23 +580,18 @@ mod tests {
         }
     }
 
-    /// This test will run the linearizer with GC disabled (gc_depth = 0) and gc
-    /// enabled (gc_depth = 3) and make sure that for the exact same DAG the
-    /// linearizer will commit different blocks according to the rules.
-    #[rstest]
+    /// This test will make sure that the linearizer will commit blocks
+    /// according to the rules.
     #[tokio::test]
-    async fn test_handle_commit_with_gc_simple(#[values(0, 3)] gc_depth: u32) {
+    async fn test_handle_commit_simple() {
         telemetry_subscribers::init_for_testing();
 
         let num_authorities = 4;
         let (mut context, _keys) = Context::new_for_test(num_authorities);
-        context.protocol_config.set_gc_depth_for_testing(gc_depth);
-
-        if gc_depth == 0 {
-            context
-                .protocol_config
-                .set_consensus_linearize_subdag_v2_for_testing(false);
-        }
+        context.protocol_config.set_gc_depth_for_testing(0);
+        context
+            .protocol_config
+            .set_consensus_linearize_subdag_v2_for_testing(false);
 
         let context = Arc::new(context);
         let dag_state = Arc::new(RwLock::new(DagState::new(
@@ -713,8 +609,7 @@ mod tests {
         // 3 we create blocks that connect to all the other authorities.
         // On round 5 we finally make the other authorities see the blocks of authority
         // 3. Practically we "simulate" here a long chain created by authority 3
-        // that is visible in round 5, but due to GC blocks of only round >=2 will
-        // be committed, when GC is enabled. When GC is disabled all blocks will be
+        // that is visible in round 5. All blocks will be
         // committed for rounds >= 1.
         let dag_str = "DAG {
                 Round 0 : { 4 },
@@ -768,140 +663,13 @@ mod tests {
                 //   "sub dag" of leader of round 3, which will be another 2 blocks
                 assert_eq!(subdag.blocks.len(), 6);
             } else {
-                // GC is enabled, so we expect to see only blocks of round >= 2
-                if gc_depth > 0 {
-                    // Now it's going to be the first time that a leader will see the blocks of
-                    // authority 3 and will attempt to commit the long chain.
-                    // However, due to GC it will only commit blocks of round > 1. That's because it
-                    // will commit blocks up to previous leader's round (round =
-                    // 4) minus the gc_depth = 3, so that will be gc_round = 4 - 3 = 1. So we expect
-                    // to see on the sub dag committed blocks of round >= 2.
-                    assert_eq!(subdag.blocks.len(), 5);
-
-                    assert!(
-                        subdag.blocks.iter().all(|block| block.round() >= 2),
-                        "Found blocks that are of round < 2."
-                    );
-
-                    // Also ensure that gc_round has advanced with the latest committed leader
-                    assert_eq!(dag_state.read().gc_round(), subdag.leader.round - gc_depth);
-                } else {
-                    // GC is disabled, so we expect to see all blocks of round >= 1
-                    assert_eq!(subdag.blocks.len(), 6);
-                    assert!(
-                        subdag.blocks.iter().all(|block| block.round() >= 1),
-                        "Found blocks that are of round < 1."
-                    );
-
-                    // GC round should never have moved
-                    assert_eq!(dag_state.read().gc_round(), 0);
-                }
-            }
-            for block in subdag.blocks.iter() {
-                assert!(block.round() <= leaders[idx].round());
-            }
-            assert_eq!(subdag.commit_ref.index, idx as CommitIndex + 1);
-        }
-    }
-
-    #[rstest]
-    #[tokio::test]
-    async fn test_handle_commit_below_highest_committed_round(#[values(3)] gc_depth: u32) {
-        telemetry_subscribers::init_for_testing();
-
-        let num_authorities = 4;
-        let (mut context, _keys) = Context::new_for_test(num_authorities);
-        context
-            .protocol_config
-            .set_consensus_gc_depth_for_testing(gc_depth);
-        context
-            .protocol_config
-            .set_consensus_linearize_subdag_v2_for_testing(true);
-
-        let context = Arc::new(context);
-        let dag_state = Arc::new(RwLock::new(DagState::new(
-            context.clone(),
-            Arc::new(MemStore::new()),
-        )));
-        let leader_schedule = Arc::new(LeaderSchedule::new(
-            context.clone(),
-            LeaderSwapTable::default(),
-        ));
-        let mut linearizer = Linearizer::new(context.clone(), dag_state.clone(), leader_schedule);
-
-        // Authority D will create an "orphaned" block on round 1 as it won't reference
-        // to it on the block of round 2. Similar, no other authority will reference to
-        // it on round 2. Then on round 3 the authorities A, B & C will link to
-        // block D1. Once the DAG gets committed we should see the block D1 getting
-        // committed as well. Normally ,as block D2 would have been committed
-        // first block D1 should be omitted. With the new logic this is no longer true.
-        let dag_str = "DAG {
-                Round 0 : { 4 },
-                Round 1 : { * },
-                Round 2 : {
-                    A -> [-D1],
-                    B -> [-D1],
-                    C -> [-D1],
-                    D -> [-D1],
-                },
-                Round 3 : {
-                    A -> [A2, B2, C2, D1],
-                    B -> [A2, B2, C2, D1],
-                    C -> [A2, B2, C2, D1],
-                    D -> [A2, B2, C2, D2]
-                },
-                Round 4 : { * },
-            }";
-
-        let (_, dag_builder) = parse_dag(dag_str).expect("Invalid dag");
-        dag_builder.print();
-        dag_builder.persist_all_blocks(dag_state.clone());
-
-        let leaders = dag_builder
-            .leader_blocks(1..=4)
-            .into_iter()
-            .flatten()
-            .collect::<Vec<_>>();
-
-        let commits = linearizer.handle_commit(leaders.clone());
-        for (idx, subdag) in commits.into_iter().enumerate() {
-            tracing::info!("{subdag:?}");
-            assert_eq!(subdag.leader, leaders[idx].reference());
-            assert_eq!(subdag.timestamp_ms, leaders[idx].timestamp_ms());
-            if idx == 0 {
-                // First subdag includes the leader block only B1
-                assert_eq!(subdag.blocks.len(), 1);
-            } else if idx == 1 {
-                // We commit:
-                // * 1 block on round 2, the leader block C2
-                // * 2 blocks on round 1, A1, C1
-                assert_eq!(subdag.blocks.len(), 3);
-            } else if idx == 2 {
-                // We commit:
-                // * 1 block on round 3, the leader block D3
-                // * 3 blocks on round 2, A2, B2, D2
-                assert_eq!(subdag.blocks.len(), 4);
-
+                // we expect to see all blocks of round >= 1
+                assert_eq!(subdag.blocks.len(), 6);
                 assert!(
-                    subdag.blocks.iter().any(|block| block.round() == 2
-                        && block.author() == AuthorityIndex::new_for_test(3)),
-                    "Block D2 should have been committed."
+                    subdag.blocks.iter().all(|block| block.round() >= 1),
+                    "Found blocks that are of round < 1."
                 );
-            } else if idx == 3 {
-                // We commit:
-                // * 1 block on round 4, the leader block A4
-                // * 3 blocks on round 3, A3, B3, C3
-                // * 1 block of round 1, D1
-                assert_eq!(subdag.blocks.len(), 5);
-                assert!(
-                    subdag.blocks.iter().any(|block| block.round() == 1
-                        && block.author() == AuthorityIndex::new_for_test(3)),
-                    "Block D1 should have been committed."
-                );
-            } else {
-                panic!("Unexpected subdag with index {:?}", idx);
             }
-
             for block in subdag.blocks.iter() {
                 assert!(block.round() <= leaders[idx].round());
             }
