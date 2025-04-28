c3a4119257c621eabbe790e5165cc2af7cf5ff9a
Thibault Martinez <thibault@iota.org>
2025-04-28 15:03:50 +0200

    Update crates/iota/src/displays/dry_run_tx_block.rs
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

--- crates/iota/src/displays/dry_run_tx_block.rs
+++ crates/iota/src/displays/dry_run_tx_block.rs
@@ -108,7 +108,7 @@ impl Display for Pretty<'_, DryRunTransactionBlockResponse> {
         )?;
 
         if let Some(err) = &response.execution_error_source {
-            writeln!(f, "Execution error: {}", err)?;
+            writeln!(f, "Execution error: {err}")?;
         }
 
         Ok(())
