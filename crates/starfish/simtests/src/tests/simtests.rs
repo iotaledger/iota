8af9b22d4faf7b95764735c2d9690f7ed1c6d4b8
Ilya Vorobyev <99146229+VorobyevIlya@users.noreply.github.com>
2025-05-06 10:32:07 +0200

    cleanup(consensus): remove linearizer_v2 (#6551)
    # Description of change
    
    This PR removes linearizer_v2.
    Linearizer_v2 works only when gc is enabled. We removed gc, so
    linearizer v2 is also removed.
    
    ## Links to any relevant issues
    
    Be sure to reference any related issues by adding `fixes #(issue)`.
    
    ## Type of change
    
    Cleanup
    
    ## How the change has been tested
    
    CI
    
    ## Change checklist
    
    Tick the boxes that are relevant to your changes, and delete any items
    that are not.
    
    - [x] I have followed the contribution guidelines for this project
    - [x] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [x] I have checked that new and existing unit tests pass locally with
    my changes
    
    ### Release Notes
    
    - [ ] Protocol:
    - [ ] Nodes (Validators and Full nodes):
    - [ ] Indexer:
    - [ ] JSON-RPC:
    - [ ] GraphQL:
    - [ ] CLI:
    - [ ] Rust SDK:
    - [ ] REST API:

--- crates/starfish/simtests/src/tests/simtests.rs
+++ crates/starfish/simtests/src/tests/simtests.rs
@@ -50,7 +50,6 @@ mod test {
         const NUM_OF_AUTHORITIES: usize = 10;
         let (committee, keypairs) = local_committee_and_keys(0, [1; NUM_OF_AUTHORITIES].to_vec());
         let mut protocol_config = ProtocolConfig::get_for_max_version_UNSAFE();
-        protocol_config.set_consensus_linearize_subdag_v2_for_testing(false);
 
         let mut authorities = Vec::with_capacity(committee.size());
         let mut transaction_clients = Vec::with_capacity(committee.size());
