7edcd318997184051f8584759b3d62782a3fdd35
Adam Welc <adam@mystenlabs.com>
2025-05-08 11:11:58 -0700

    fix(trace-view): replace platform-dependent zstd library with platform-independent one Port upstream Sui PR #22076 Partial fix for #10821

--- external-crates/move/crates/move-analyzer/trace-debug/src/extension.ts
+++ external-crates/move/crates/move-analyzer/trace-debug/src/extension.ts
@@ -13,8 +13,7 @@ import {
     TextDocument,
     Position
 } from 'vscode';
-import { decompress } from '@mongodb-js/zstd';
-import { trace } from 'console';
+import { decompress } from 'fzstd';
 
 /**
  * Log level for the debug adapter.
@@ -656,7 +655,7 @@ async function pickTraceFileToDebug(
 async function decompressTraceFile(traceFilePath: string): Promise<string> {
     const buf = fs.readFileSync(traceFilePath);
     const decompressed = await decompress(buf);
-    return decompressed.toString('utf8').trimEnd();
+    return new TextDecoder().decode(decompressed).trimEnd();
 }
 
 /**
