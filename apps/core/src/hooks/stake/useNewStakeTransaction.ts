44b1224117823318007e948ea54963dad3475c02
Jose Luis Torres Corbalan <jltorres@boxfish.studio>
2025-05-08 13:55:51 +0200

    fix typo

--- apps/core/src/hooks/stake/useNewStakeTransaction.ts
+++ apps/core/src/hooks/stake/useNewStakeTransaction.ts
@@ -10,17 +10,9 @@ export function useNewStakeTransaction(validator: string, amount: bigint, sender
     const client = useIotaClient();
     return useQuery({
         // eslint-disable-next-line @tanstack/query/exhaustive-deps
-        queryKey: [
-            'stake-transaction',
-            '0xd17a0271bb4ae0a12af1a6d458805b6220603bb73a025d8da6d5c4cc848395e1',
-            amount.toString(),
-            senderAddress,
-        ],
+        queryKey: ['stake-transaction', validator, amount.toString(), senderAddress],
         queryFn: async () => {
-            const transaction = createStakeTransaction(
-                amount,
-                '0xd17a0271bb4ae0a12af1a6d458805b6220603bb73a025d8da6d5c4cc848395e1',
-            );
+            const transaction = createStakeTransaction(amount, validator);
             transaction.setSender(senderAddress);
             const txBytes = await transaction.build({ client });
             const txDryRun = await client.dryRunTransactionBlock({
