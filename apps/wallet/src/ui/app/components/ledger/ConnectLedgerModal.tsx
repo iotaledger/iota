300f6a7d7ba0dac332b37e8f04075f4ce348112b
evavirseda <evirseda@boxfish.studio>
2025-05-08 17:02:20 +0200

    fix typo

--- apps/wallet/src/ui/app/components/ledger/ConnectLedgerModal.tsx
+++ apps/wallet/src/ui/app/components/ledger/ConnectLedgerModal.tsx
@@ -63,7 +63,7 @@ export function ConnectLedgerModal({ onClose, onConfirm, onError }: ConnectLedge
                         </span>
                         <InfoBox
                             title="Update your IOTA Ledger app"
-                            supportingText="Last version 0.9.3 (deprecated devices like Ledger Nano S is  0.9.2)"
+                            supportingText="Latest version 0.9.3 (deprecated devices like Ledger Nano S is  0.9.2)"
                             icon={<Info />}
                             type={InfoBoxType.Default}
                             style={InfoBoxStyle.Elevated}
