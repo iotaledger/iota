480050cff873569af53538f032cd5c9bdd774601
Tim Zakian <2895723+tzakian@users.noreply.github.com>
2025-05-20 15:17:12 -0700

    fix(execution): protocol gate unbounded system objects
    Port upstream Sui PR #22188
    Partial fix for #10836

--- crates/iota-types/src/object.rs
+++ crates/iota-types/src/object.rs
@@ -75,7 +75,16 @@ impl MoveObject {
         protocol_config: &ProtocolConfig,
         system_mutation: bool,
     ) -> Result<Self, ExecutionError> {
-        let bound = if system_mutation && protocol_config.allow_unbounded_system_objects() {
+        let bound = if protocol_config.allow_unbounded_system_objects() && system_mutation {
+            if contents.len() as u64 > protocol_config.max_move_object_size() {
+                debug_fatal!(
+                    "System created object (ID = {:?}) of type {:?} and size {} exceeds normal max size {}",
+                    MoveObject::id_opt(&contents).ok(),
+                    type_,
+                    contents.len(),
+                    protocol_config.max_move_object_size()
+                );
+            }
             u64::MAX
         } else {
             protocol_config.max_move_object_size()
@@ -220,14 +229,23 @@ impl MoveObject {
         &mut self,
         new_contents: Vec<u8>,
         protocol_config: &ProtocolConfig,
-    ) {
+    ) -> Result<(), ExecutionError> {
         if new_contents.len() as u64 > protocol_config.max_move_object_size() {
-            debug_fatal!(
-                "Safe mode object update (ID = {}) of size {} exceeds normal max size {}",
-                self.id(),
-                new_contents.len(),
-                protocol_config.max_move_object_size()
-            );
+            if protocol_config.allow_unbounded_system_objects() {
+                debug_fatal!(
+                    "Safe mode object update (ID = {}) of size {} exceeds normal max size {}",
+                    self.id(),
+                    new_contents.len(),
+                    protocol_config.max_move_object_size()
+                )
+            } else {
+                return Err(ExecutionError::from_kind(
+                    ExecutionErrorKind::MoveObjectTooBig {
+                        object_size: new_contents.len() as u64,
+                        max_object_size: protocol_config.max_move_object_size(),
+                    },
+                ));
+            }
         }
 
         #[cfg(debug_assertions)]
@@ -237,6 +255,8 @@ impl MoveObject {
         // Update should not modify ID
         #[cfg(debug_assertions)]
         debug_assert_eq!(self.id(), old_id);
+
+        Ok(())
     }
 
     /// Sets the version of this object to a new value which is assumed to be
