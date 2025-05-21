c01a761b1a15f8aabb72cadd0bee8d1460a764fd
Nicolas M <marshall.nicolas@gmail.com>
2025-05-21 11:16:37 +0200

    feat(core): Update outdated event id doc

--- sdk/typescript/src/client/types/generated.ts
+++ sdk/typescript/src/client/types/generated.ts
@@ -390,8 +390,7 @@ export type IotaEventFilter =
           Or: [IotaEventFilter, IotaEventFilter];
       };
 /**
- * Unique ID of an IOTA Event, the ID is a combination of tx seq number and event seq number, the ID is
- * local to this particular fullnode and will be different from other fullnode.
+ * Unique ID of a IOTA Event, the ID is a combination of transaction digest and event seq number.
  */
 export interface EventId {
     eventSeq: string;
