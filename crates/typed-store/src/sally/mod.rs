d774e5f39e26a0a8a7208158bacc4432f7980a84
miker83z <mirko.zichichi@iota.org>
2025-05-22 18:08:23 +0200

    fix fmt

--- crates/typed-store/src/sally/mod.rs
+++ crates/typed-store/src/sally/mod.rs
@@ -48,7 +48,9 @@
 //!     let mut table = ExampleTable::init(SallyDBOptions::TestDB);
 //!     insert_key_vals(&table).await;
 //!     // switch to rocksdb backend
-//!     let primary_path = tempfile::tempdir().expect("Failed to open db path").into_path();
+//!     let primary_path = tempfile::tempdir()
+//!         .expect("Failed to open db path")
+//!         .into_path();
 //!     table = ExampleTable::init(SallyDBOptions::RocksDB((
 //!         primary_path,
 //!         MetricConf::default(),
