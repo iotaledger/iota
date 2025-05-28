c82db52156f3a6a618f50d299f5cdb574a905830
DaughterOfMars <chloedaughterofmars@gmail.com>
2025-05-28 10:35:34 +0200

    Update crates/iota-names-indexer/src/metrics.rs
    Co-authored-by: Thibault Martinez <thibault@iota.org>

--- crates/iota-names-indexer/src/metrics.rs
+++ crates/iota-names-indexer/src/metrics.rs
@@ -39,7 +39,7 @@ impl PrometheusServer {
 
     pub(crate) async fn start(&self, token: CancellationToken) -> anyhow::Result<()> {
         let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 9184);
-        info!("Starting prometheus metrics at {}", addr);
+        info!("Starting prometheus metrics at {addr}");
 
         let app = Router::new()
             .route(METRICS_ROUTE, get(iota_metrics::metrics))
