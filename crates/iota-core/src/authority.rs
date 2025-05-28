adc9ef1a734f701ed234d1be6e609bbe24a6ea2f
Nicolas Marshall <marshall.nicolas@gmail.com>
2025-05-28 08:56:32 +0200

    upstream(node): Add system_invariant_violation macro (#6974)
    ## Port
    [MystenLabs/sui@7c517e7](https://github.com/MystenLabs/sui/commit/7c517e7e5ce7193d1f51ef7f20a31b56f0e66797):
    Add system_invariant_violation macro
    Upstream range: [1.36.2, 1.37.4) 
    ### Upstream PR(s): 
    - https://github.com/MystenLabs/sui/pull/20105  
    
    ## Description 
    
    Introduce a system invariant violation macro.  
    We can then set up alerts whenever the counter value changes.  
    Used it in one place as an example.

--- crates/iota-core/src/authority.rs
+++ crates/iota-core/src/authority.rs
@@ -26,6 +26,7 @@ use fastcrypto::{
     hash::MultisetHash,
 };
 use iota_archival::reader::ArchiveReaderBalancer;
+use iota_common::debug_fatal;
 use iota_config::{
     NodeConfig,
     genesis::Genesis,
@@ -296,8 +297,6 @@ pub struct AuthorityMetrics {
 
     /// bytecode verifier metrics for tracking timeouts
     pub bytecode_verifier_metrics: Arc<BytecodeVerifierMetrics>,
-
-    pub authenticator_state_update_failed: IntCounter,
 
     /// Count of zklogin signatures
     pub zklogin_sig_count: IntCounter,
@@ -716,12 +715,6 @@ impl AuthorityMetrics {
             ).unwrap(),
             limits_metrics: Arc::new(LimitsMetrics::new(registry)),
             bytecode_verifier_metrics: Arc::new(BytecodeVerifierMetrics::new(registry)),
-            authenticator_state_update_failed: register_int_counter_with_registry!(
-                "authenticator_state_update_failed",
-                "Number of failed authenticator state updates",
-                registry,
-            )
-            .unwrap(),
             zklogin_sig_count: register_int_counter_with_registry!(
                 "zklogin_sig_count",
                 "Count of zkLogin signatures",
@@ -1414,10 +1407,8 @@ impl AuthorityState {
             certificate.data().transaction_data().kind()
         {
             if let Some(err) = &execution_error_opt {
-                error!("Authenticator state update failed: {err}");
-                self.metrics.authenticator_state_update_failed.inc();
+                debug_fatal!("Authenticator state update failed: {:?}", err);
             }
-            debug_assert!(execution_error_opt.is_none());
             epoch_store.update_authenticator_state(auth_state);
 
             // double check that the signature verifier always matches the authenticator
