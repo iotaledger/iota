6db4ef1d6ac283746ac8882bed483d2571c35406
Roman Overko <roman.overko@iota.org>
2025-05-28 16:16:21 +0200

    test: add test_multi_commit_congestion_info_updates

--- crates/iota-core/src/authority/shared_object_congestion_info.rs
+++ crates/iota-core/src/authority/shared_object_congestion_info.rs
@@ -27,7 +27,7 @@ pub(crate) enum SharedObjectTransactionResult {
 }
 
 /// Holds shared object congestion data for a single shared object.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) struct PerObjectCongestionInfo {
     /// List of gas prices of SCHEDULED transactions operating on a shared
     /// object.
@@ -56,6 +56,7 @@ impl Default for PerObjectCongestionInfo {
 }
 
 /// Holds shared object congestion data for a single consensus commit round.
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) struct PerCommitCongestionInfo {
     /// Consensus commit round number
     commit_round: CommitRound,
@@ -128,6 +129,7 @@ impl PerCommitCongestionInfo {
 /// consensus commit rounds, so that if the buffer capacity is reached, the
 /// data for the oldest commit will be dropped and the data for the new
 /// consensus commit round will be added.
+#[derive(Debug, PartialEq, Eq)]
 pub(crate) struct MultiCommitCongestionInfo {
     /// Shared object congestion data from multiple consensus commit rounds.
     commits_data: VecDeque<PerCommitCongestionInfo>,
@@ -191,26 +193,253 @@ impl Default for MultiCommitCongestionInfo {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::{HashMap, VecDeque};
+
+    use iota_types::{
+        base_types::{CommitRound, ObjectID},
+        executable_transaction::VerifiedExecutableTransaction,
+    };
     use parking_lot::Mutex;
 
-    struct Data {
-        x: Mutex<u32>,
+    use super::{MultiCommitCongestionInfo, SharedObjectTransactionResult};
+    use crate::authority::{
+        shared_object_congestion_info::{PerCommitCongestionInfo, PerObjectCongestionInfo},
+        shared_object_congestion_tracker::{
+            ExecutionTime, shared_object_test_utils::build_transaction,
+        },
+    };
+
+    const MAX_NUM_COMMITS_TO_STORE_DATA: u32 = 2;
+
+    // Dummy `AuthorityPerEpochStore` used to test `MultiCommitCongestionInfo`
+    pub struct DummyAuthorityPerEpochStore {
+        congestion_info: Mutex<MultiCommitCongestionInfo>,
     }
 
-    impl Data {
-        fn write(&self) {
-            let mut x = self.x.lock();
-            *x = 5;
+    impl DummyAuthorityPerEpochStore {
+        fn new() -> Self {
+            Self {
+                congestion_info: Mutex::new(MultiCommitCongestionInfo::new(
+                    MAX_NUM_COMMITS_TO_STORE_DATA as usize,
+                )),
+            }
+        }
+
+        fn add_new_per_commit_congestion_info(&self, commit_round: CommitRound) {
+            let mut congestion_info = self.congestion_info.lock();
+            (*congestion_info).add_new_per_commit_congestion_info(commit_round);
+        }
+
+        /// Update per-commit congestion info for a single consensus certificate
+        /// in the current consensus commit round.
+        fn update_per_commit_congestion_info_for_consensus_certificate(
+            &self,
+            certificate: &VerifiedExecutableTransaction,
+            estimated_execution_duration: ExecutionTime,
+            scheduling_result: SharedObjectTransactionResult,
+        ) {
+            let mut congestion_info = self.congestion_info.lock();
+            (*congestion_info).update_per_commit_congestion_info_for_consensus_certificate(
+                certificate,
+                estimated_execution_duration,
+                scheduling_result,
+            );
         }
     }
 
     #[test]
-    fn test() {
-        let data = Data { x: Mutex::new(0) };
+    fn test_multi_commit_congestion_info_updates() {
+        let dummy_epoch_store = DummyAuthorityPerEpochStore::new();
 
-        data.write();
+        // Add the first consensus commit round
+        let commit_round_1 = 1;
+        dummy_epoch_store.add_new_per_commit_congestion_info(commit_round_1);
 
-        let x = data.x.lock();
-        assert_eq!(*x, 1);
+        // Now there should be empty data for a single commit round, i.e., round 1
+        {
+            let multi_commit_congestion_info = dummy_epoch_store.congestion_info.lock();
+
+            assert_eq!(
+                *multi_commit_congestion_info,
+                MultiCommitCongestionInfo {
+                    commits_data: VecDeque::from([PerCommitCongestionInfo::new(commit_round_1)]),
+                }
+            );
+        }
+
+        let object_1 = ObjectID::random();
+        let object_2 = ObjectID::random();
+
+        // Create a certificate (scheduled)
+        let objects_1 = vec![(object_1, true), (object_2, false)];
+        let gas_price_1 = 10;
+        let gas_budget_1 = 100;
+        let certificate_1 = build_transaction(&objects_1, gas_budget_1, gas_price_1);
+        // and process this certificate to update the multi-commit congestion info
+        dummy_epoch_store.update_per_commit_congestion_info_for_consensus_certificate(
+            &certificate_1,
+            gas_budget_1,
+            SharedObjectTransactionResult::Schedule,
+        );
+
+        // Create another certificate (deferred)
+        let objects_2 = vec![(object_1, true)];
+        let gas_price_2 = 5;
+        let gas_budget_2 = 50;
+        let certificate_2 = build_transaction(&objects_2, gas_budget_2, gas_price_2);
+        // and process this certificate to update the multi-commit congestion info
+        dummy_epoch_store.update_per_commit_congestion_info_for_consensus_certificate(
+            &certificate_2,
+            gas_budget_2,
+            SharedObjectTransactionResult::Defer,
+        );
+
+        // Expected congestion info for commit 1 looks as follows:
+        let per_commit_congestion_info_1 = PerCommitCongestionInfo {
+            commit_round: commit_round_1,
+            objects_data: HashMap::from([
+                (
+                    object_1,
+                    PerObjectCongestionInfo {
+                        // NOTE: we do not store gas prices of deferred transactions
+                        scheduled_txs_gas_prices: vec![gas_price_1],
+                        txs_estimated_exec_durations: vec![gas_budget_1, gas_budget_2],
+                    },
+                ),
+                (
+                    object_2,
+                    PerObjectCongestionInfo {
+                        scheduled_txs_gas_prices: vec![gas_price_1],
+                        txs_estimated_exec_durations: vec![gas_budget_1],
+                    },
+                ),
+            ]),
+        };
+        // Verify that multi-commit congestion info contains only
+        // `per_commit_congestion_info_1`
+        {
+            let multi_commit_congestion_info = dummy_epoch_store.congestion_info.lock();
+
+            assert_eq!(
+                *multi_commit_congestion_info,
+                MultiCommitCongestionInfo {
+                    commits_data: VecDeque::from([per_commit_congestion_info_1.clone()]),
+                }
+            );
+        }
+
+        // Add the second consensus commit round
+        let commit_round_2 = 2;
+        dummy_epoch_store.add_new_per_commit_congestion_info(commit_round_2);
+
+        // Now there should be non-empty data for the first commit round,
+        // and empty data for the second one
+        {
+            let multi_commit_congestion_info = dummy_epoch_store.congestion_info.lock();
+
+            assert_eq!(
+                *multi_commit_congestion_info,
+                MultiCommitCongestionInfo {
+                    commits_data: VecDeque::from([
+                        per_commit_congestion_info_1.clone(),
+                        PerCommitCongestionInfo::new(commit_round_2)
+                    ]),
+                }
+            );
+        }
+
+        // Create a certificate (scheduled)
+        let objects_1 = vec![(object_1, true), (object_2, false)];
+        let gas_price_1 = 10;
+        let gas_budget_1 = 100;
+        let certificate_1 = build_transaction(&objects_1, gas_budget_1, gas_price_1);
+        // and process this certificate to update the multi-commit congestion info
+        dummy_epoch_store.update_per_commit_congestion_info_for_consensus_certificate(
+            &certificate_1,
+            gas_budget_1,
+            SharedObjectTransactionResult::Schedule,
+        );
+
+        // Create another certificate (deferred)
+        let objects_2 = vec![(object_1, true)];
+        let gas_price_2 = 5;
+        let gas_budget_2 = 50;
+        let certificate_2 = build_transaction(&objects_2, gas_budget_2, gas_price_2);
+        // and process this certificate to update the multi-commit congestion info
+        dummy_epoch_store.update_per_commit_congestion_info_for_consensus_certificate(
+            &certificate_2,
+            gas_budget_2,
+            SharedObjectTransactionResult::Defer,
+        );
+
+        // Create another certificate (scheduled)
+        let objects_3 = vec![(object_2, false)];
+        let gas_price_3 = 1;
+        let gas_budget_3 = 10;
+        let certificate_3 = build_transaction(&objects_3, gas_budget_3, gas_price_3);
+        // and process this certificate to update the multi-commit congestion info
+        dummy_epoch_store.update_per_commit_congestion_info_for_consensus_certificate(
+            &certificate_3,
+            gas_budget_3,
+            SharedObjectTransactionResult::Schedule,
+        );
+
+        // Expected congestion info for commit 1 looks as follows:
+        let per_commit_congestion_info_2 = PerCommitCongestionInfo {
+            commit_round: commit_round_2,
+            objects_data: HashMap::from([
+                (
+                    object_1,
+                    PerObjectCongestionInfo {
+                        // NOTE: we do not store gas prices of deferred transactions
+                        scheduled_txs_gas_prices: vec![gas_price_1],
+                        txs_estimated_exec_durations: vec![gas_budget_1, gas_budget_2],
+                    },
+                ),
+                (
+                    object_2,
+                    PerObjectCongestionInfo {
+                        scheduled_txs_gas_prices: vec![gas_price_1, gas_price_3],
+                        txs_estimated_exec_durations: vec![gas_budget_1, gas_budget_3],
+                    },
+                ),
+            ]),
+        };
+        // Verify that multi-commit congestion info contains both
+        // `per_commit_congestion_info_1` and `per_commit_congestion_info_2`
+        {
+            let multi_commit_congestion_info = dummy_epoch_store.congestion_info.lock();
+
+            assert_eq!(
+                *multi_commit_congestion_info,
+                MultiCommitCongestionInfo {
+                    commits_data: VecDeque::from([
+                        per_commit_congestion_info_1,
+                        per_commit_congestion_info_2.clone()
+                    ]),
+                }
+            );
+        }
+
+        // Add the third consensus commit round
+        let commit_round_3 = 3;
+        dummy_epoch_store.add_new_per_commit_congestion_info(commit_round_3);
+
+        // Now there should be non-empty data for the second commit round,
+        // and empty data for the third one. Data for the first commit round
+        // should be dropped because `MAX_NUM_COMMITS_TO_STORE_DATA` is set to 2.
+        {
+            let multi_commit_congestion_info = dummy_epoch_store.congestion_info.lock();
+
+            assert_eq!(
+                *multi_commit_congestion_info,
+                MultiCommitCongestionInfo {
+                    commits_data: VecDeque::from([
+                        per_commit_congestion_info_2,
+                        PerCommitCongestionInfo::new(commit_round_3)
+                    ]),
+                }
+            );
+        }
     }
 }
