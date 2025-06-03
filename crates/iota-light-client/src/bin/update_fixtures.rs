07f5fda5a00b75d01116c6e31b733ef13c3a26e1
/alex/ <alexander.schmidt@iota.org>
2025-06-03 12:26:11 +0200

    download fixtures from object store

---
+++ crates/iota-light-client/src/bin/update_fixtures.rs
@@ -0,0 +1,103 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use std::path::{Path, PathBuf};
+
+use anyhow::{Context, Result, ensure};
+use iota_light_client::{
+    checkpoint::{
+        download_summaries_from_checkpoint_store, sync_checkpoint_list_to_latest_from_archive,
+        write_checkpoint_list,
+    },
+    config::Config,
+    object_store::CheckpointStore,
+};
+use iota_types::full_checkpoint_content::CheckpointData;
+use tokio::fs::create_dir_all;
+use tracing::info;
+
+const FIXTURES_DIR: &str = "tests/fixtures";
+const CHECKPOINT_LIST_INDEXES: &[usize] = &[0, 1];
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    let _guard = telemetry_subscribers::TelemetryConfig::new()
+        .with_log_level("info")
+        .with_env()
+        .init();
+
+    let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(FIXTURES_DIR);
+    if !path.exists() {
+        create_dir_all(path.clone()).await?;
+    }
+
+    // TODO: switch to mainnet config once endpoints available
+    let mut config = Config::devnet();
+    config.checkpoints_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(FIXTURES_DIR);
+    config.validate().expect("invalid config");
+
+    let mut checkpoint_list = sync_checkpoint_list_to_latest_from_archive(&config)
+        .await
+        .context("failed to sync checkpoints")?;
+
+    // only keep necessary indexes
+    let checkpoints = &mut checkpoint_list.checkpoints;
+    let _ = checkpoints.split_off(*CHECKPOINT_LIST_INDEXES.last().unwrap() + 1);
+    write_checkpoint_list(&config, &checkpoint_list)?;
+
+    let checkpoints = CHECKPOINT_LIST_INDEXES
+        .iter()
+        .map(|i| *checkpoint_list.checkpoints.get(*i).unwrap())
+        .collect::<Vec<_>>();
+
+    ensure!(checkpoints.len() > 1, "not enough checkpoints");
+
+    download_summaries_from_checkpoint_store(&config, checkpoints.clone()).await?;
+    download_checkpoints_from_checkpoint_store(&config, checkpoints).await?;
+
+    Ok(())
+}
+
+pub async fn download_checkpoints_from_checkpoint_store(
+    config: &Config,
+    checkpoints: Vec<u64>,
+) -> anyhow::Result<()> {
+    info!("Downloading checkpoints from checkpoint store.");
+
+    ensure!(
+        config.archive_store_config.is_some(),
+        "missing archive store config"
+    );
+
+    let checkpoint_store = CheckpointStore::new(config)?;
+    for seq in checkpoints {
+        info!("Downloading checkpoint: {seq}.chk");
+
+        let checkpoint = checkpoint_store
+            .fetch_full_checkpoint(seq)
+            .await
+            .context(format!(
+                "Failed to download checkpoint '{seq}' from checkpoint store"
+            ))?;
+        write_full_checkpoint(config, &checkpoint)?;
+    }
+
+    Ok(())
+}
+
+pub fn write_full_checkpoint(config: &Config, checkpoint: &CheckpointData) -> Result<()> {
+    let path = full_checkpoint_file_path(config, *checkpoint.checkpoint_summary.sequence_number());
+    bcs::serialize_into(
+        &mut std::fs::File::create(&path).context(format!(
+            "error writing checkpoint file '{}'",
+            path.display()
+        ))?,
+        &checkpoint,
+    )
+    .expect("error serializing to bcs");
+    Ok(())
+}
+
+pub fn full_checkpoint_file_path(config: &Config, seq: u64) -> PathBuf {
+    Path::new(&config.checkpoints_dir).join(format!("{seq}.chk"))
+}
--- crates/iota-light-client/tests/check_proof.rs
+++ crates/iota-light-client/tests/check_proof.rs
@@ -27,17 +27,19 @@ use iota_types::{
 const FIXTURES_DIR: &str = "tests/fixtures";
 
 async fn read_test_data() -> (Committee, CheckpointData) {
-    let mut config = Config::mainnet();
+    // TODO: switch to mainnet config once endpoints available
+    let mut config = Config::devnet();
     config.checkpoints_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(FIXTURES_DIR);
 
     let checkpoint_list =
         read_checkpoint_list(&config).expect("reading the checkpoints.yaml should not fail");
+
     let committee_seq = checkpoint_list
-        .checkpoints()
+        .checkpoints
         .first()
         .expect("there should be a first checkpoint in the checkpoints.yaml");
     let seq = checkpoint_list
-        .checkpoints()
+        .checkpoints
         .get(1)
         .expect("there should be a second checkpoint in the checkpoints.yaml");
 
