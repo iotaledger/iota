ddc89162f720b5473beb13dee7f6d93c668c2abb
Jort <jordan.jennings@mystenlabs.com>
2025-06-05 14:03:23 -0700

    fix(graphql): add error code to graphql clever error Port upstream Sui PR #21941 Partial fix for #10835

--- crates/iota-package-resolver/src/lib.rs
+++ crates/iota-package-resolver/src/lib.rs
@@ -119,6 +119,8 @@ pub struct CleverError {
     pub error_info: ErrorConstants,
     /// The line number in the source file where the error occurred.
     pub source_line_number: u16,
+    /// The error code of the abort
+    pub error_code: Option<u8>,
 }
 
 /// The `ErrorConstants` enum is used to represent the different kinds of error
@@ -637,6 +639,7 @@ impl<S: PackageStore> Resolver<S> {
         let package = self.package_store.fetch(*module_id.address()).await.ok()?;
         let module = package.module(module_id.name().as_str()).ok()?.bytecode();
         let source_line_number = bitset.line_number()?;
+        let error_code = bitset.error_code();
 
         // We only have a line number in our clever error, so return early.
         if bitset.identifier_index().is_none() && bitset.constant_index().is_none() {
@@ -644,6 +647,7 @@ impl<S: PackageStore> Resolver<S> {
                 module_id,
                 error_info: ErrorConstants::None,
                 source_line_number,
+                error_code,
             });
         } else if bitset.identifier_index().is_none() || bitset.constant_index().is_none() {
             return None;
@@ -683,6 +687,7 @@ impl<S: PackageStore> Resolver<S> {
             module_id,
             error_info,
             source_line_number,
+            error_code,
         })
     }
 }
