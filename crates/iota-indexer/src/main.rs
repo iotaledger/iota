a1dc64433bee873e7fd7818659ac7a6d6cbd9abe
Tomasz Pastusiak <tomasz.pastusiak@iota.org>
2025-06-11 10:58:15 +0200

    Simplifi parsing the old config, reduce level of indentation

--- crates/iota-indexer/src/main.rs
+++ crates/iota-indexer/src/main.rs
@@ -4,7 +4,7 @@
 
 use std::env;
 
-use clap::{CommandFactory, FromArgMatches};
+use clap::{CommandFactory, FromArgMatches, Parser};
 use iota_indexer::{
     config::{Command, IndexerConfig, deprecated::OldIndexerConfig},
     db::{
@@ -32,10 +32,7 @@ async fn main() -> Result<(), IndexerError> {
         "WARNING: IOTA indexer is still experimental and we expect occasional breaking changes that require backfills."
     );
 
-    let old_conf = OldIndexerConfig::command()
-        .version(VERSION)
-        .try_get_matches()
-        .and_then(|mut arg_matches| OldIndexerConfig::from_arg_matches_mut(&mut arg_matches));
+    let old_conf = OldIndexerConfig::try_parse();
 
     let opts = match old_conf {
         Ok(old_conf) => old_conf.try_into()?,
@@ -49,65 +46,66 @@ async fn main() -> Result<(), IndexerError> {
     iota_metrics::init_metrics(&registry);
     let indexer_metrics = IndexerMetrics::new(&registry);
 
-    if matches!(opts.command, Command::HelpDeprecated) {
+    if let Command::HelpDeprecated = opts.command {
         OldIndexerConfig::command().print_help().map_err(|e| {
             IndexerError::Generic(format!("Failed printing deprecated CLI help: {e}"))
         })?;
-    } else {
-        let connection_pool = new_connection_pool(
-            opts.database_url
-                .ok_or(IndexerError::InvalidArgument(
-                    "--database-url argument is mandatory for this command".into(),
-                ))?
-                .as_str(),
-            &opts.connection_pool_config,
-        )?;
-        spawn_connection_pool_metric_collector(indexer_metrics.clone(), connection_pool.clone());
+        return Ok(());
+    }
 
-        match opts.command {
-            Command::Indexer {
-                ingestion_config,
+    let connection_pool = new_connection_pool(
+        opts.database_url
+            .ok_or(IndexerError::InvalidArgument(
+                "--database-url argument is mandatory for this command".into(),
+            ))?
+            .as_str(),
+        &opts.connection_pool_config,
+    )?;
+    spawn_connection_pool_metric_collector(indexer_metrics.clone(), connection_pool.clone());
+
+    match opts.command {
+        Command::Indexer {
+            ingestion_config,
+            snapshot_config,
+            pruning_options,
+            reset_db,
+        } => {
+            {
+                // Make sure to run all migrations on startup, and also serve as a compatibility
+                // check.
+                let mut pool_conn = get_pool_connection(&connection_pool)?;
+                if reset_db {
+                    reset_database(&mut pool_conn)?;
+                } else {
+                    run_migrations(&mut pool_conn)?;
+                }
+            }
+
+            let store = PgIndexerStore::new(connection_pool, indexer_metrics.clone());
+            Indexer::start_writer_with_config(
+                &ingestion_config,
+                store,
+                indexer_metrics,
                 snapshot_config,
                 pruning_options,
-                reset_db,
-            } => {
-                {
-                    // Make sure to run all migrations on startup, and also serve as a compatibility
-                    // check.
-                    let mut pool_conn = get_pool_connection(&connection_pool)?;
-                    if reset_db {
-                        reset_database(&mut pool_conn)?;
-                    } else {
-                        run_migrations(&mut pool_conn)?;
-                    }
-                }
-
-                let store = PgIndexerStore::new(connection_pool, indexer_metrics.clone());
-                Indexer::start_writer_with_config(
-                    &ingestion_config,
-                    store,
-                    indexer_metrics,
-                    snapshot_config,
-                    pruning_options,
-                    CancellationToken::new(),
-                )
-                .await?;
-            }
-            Command::JsonRpcService(json_rpc_config) => {
-                {
-                    // Run compatibility check
-                    let mut pool_conn = get_pool_connection(&connection_pool)?;
-                    check_db_migration_consistency(&mut pool_conn)?;
-                }
-
-                Indexer::start_reader(&json_rpc_config, &registry, connection_pool).await?;
-            }
-            Command::AnalyticalWorker => {
-                let store = PgIndexerAnalyticalStore::new(connection_pool);
-                return Indexer::start_analytical_worker(store, indexer_metrics.clone()).await;
-            }
-            Command::HelpDeprecated => unreachable!("This case is handled earlier"),
+                CancellationToken::new(),
+            )
+            .await?;
         }
+        Command::JsonRpcService(json_rpc_config) => {
+            {
+                // Run compatibility check
+                let mut pool_conn = get_pool_connection(&connection_pool)?;
+                check_db_migration_consistency(&mut pool_conn)?;
+            }
+
+            Indexer::start_reader(&json_rpc_config, &registry, connection_pool).await?;
+        }
+        Command::AnalyticalWorker => {
+            let store = PgIndexerAnalyticalStore::new(connection_pool);
+            return Indexer::start_analytical_worker(store, indexer_metrics.clone()).await;
+        }
+        Command::HelpDeprecated => unreachable!("This case is handled earlier"),
     }
 
     Ok(())
