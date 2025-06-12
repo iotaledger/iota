78795ccf0b065d4d368d8c806a81391e3ee4a3e0
Juligs <julicodes29@gmail.com>
2025-06-12 20:48:09 +0200

    feat(apps-ui-kit): update atom components to apply new palettes

--- apps/ui-kit/src/lib/components/atoms/info-box/infoBox.classes.ts
+++ apps/ui-kit/src/lib/components/atoms/info-box/infoBox.classes.ts
@@ -4,16 +4,15 @@
 import { InfoBoxType } from './infoBox.enums';
 
 export const ICON_COLORS: Record<InfoBoxType, string> = {
-    [InfoBoxType.Default]: 'bg-primary-90 dark:bg-primary-10 text-primary-20 dark:text-primary-90',
-    [InfoBoxType.Error]: 'bg-error-90 dark:bg-error-10 text-error-20 dark:text-error-90',
-    [InfoBoxType.Success]:
-        'bg-tertiary-90 dark:bg-tertiary-10 text-tertiary-20 dark:text-tertiary-90',
-    [InfoBoxType.Warning]: 'bg-warning-90 dark:bg-warning-20 text-warning-10 dark:text-warning-90',
+    [InfoBoxType.Default]: 'bg-on-default text-default-surface',
+    [InfoBoxType.Error]: 'bg-on-error text-error-surface',
+    [InfoBoxType.Success]: 'bg-on-success text-success-surface',
+    [InfoBoxType.Warning]: 'bg-on-warning text-warning-surface',
 };
 
 export const BACKGROUND_COLORS: Record<InfoBoxType, string> = {
-    [InfoBoxType.Default]: 'bg-primary-90 dark:bg-primary-10',
-    [InfoBoxType.Error]: 'bg-error-90 dark:bg-error-10',
-    [InfoBoxType.Success]: 'bg-tertiary-90 dark:bg-tertiary-10',
-    [InfoBoxType.Warning]: 'bg-warning-90 dark:bg-warning-20',
+    [InfoBoxType.Default]: 'bg-default-surface',
+    [InfoBoxType.Error]: 'bg-error-surface',
+    [InfoBoxType.Success]: 'bg-success-surface',
+    [InfoBoxType.Warning]: 'bg-warning-surface',
 };
