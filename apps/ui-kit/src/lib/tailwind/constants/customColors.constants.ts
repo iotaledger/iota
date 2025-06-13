1850fbf685cce626287d47c9f2c651ee29fd9425
Juliana <115430927+Juligs@users.noreply.github.com>
2025-06-13 11:25:52 +0200

    refactor(apps-ui-kit):  update atoms to apply new palettes (Badge-LabelText) (#7355)
    fixes #7344
    
    ---------
    
    Co-authored-by: evavirseda <evirseda@boxfish.studio>

--- apps/ui-kit/src/lib/tailwind/constants/customColors.constants.ts
+++ apps/ui-kit/src/lib/tailwind/constants/customColors.constants.ts
@@ -2,5 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export const THEMED_CUSTOM_COLORS = {
-    primary: 'var(--color-primary)',
+    'default-surface': 'var(--default-surface)',
+    'on-default': 'var(--on-default)',
+    'warning-surface': 'var(--warning-surface)',
+    'on-warning': 'var(--on-warning)',
+    'error-surface': 'var(--error-surface)',
+    'on-error': 'var(--on-error)',
+    'success-surface': 'var(--success-surface)',
+    'on-success': 'var(--on-success)',
 } as const;
