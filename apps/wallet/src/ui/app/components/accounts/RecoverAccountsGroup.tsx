deea975fa1d3558964c082404b8d61c703c2549b
evavirseda <evirseda@boxfish.studio>
2025-06-13 13:55:39 +0200

    fix class

--- apps/wallet/src/ui/app/components/accounts/RecoverAccountsGroup.tsx
+++ apps/wallet/src/ui/app/components/accounts/RecoverAccountsGroup.tsx
@@ -46,7 +46,7 @@ export function RecoverAccountsGroup({
             </div>
             <div className="flex flex-col gap-xs">
                 {accounts.map((anAccount) => (
-                    <div className="border-shader-iota-neutral-light-8 rounded-xl border">
+                    <div className="rounded-xl border border-shader-neutral-light-8">
                         <AccountListItem key={anAccount.id} account={anAccount} icon={<Key />} />
                     </div>
                 ))}
