92262c367f772921ea78f7b3da5dbea603e5a9e3
vivekjain23 <vkj.vivekkumarjain@gmail.com>
2025-06-17 15:14:25 +0530

    Feat(Docs): Restructure docs:- created users moved wallet

--- docs/site/sidebars.js
+++ docs/site/sidebars.js
@@ -7,9 +7,11 @@ const developer = require("../content/sidebars/developer.js");
 const aboutIota = require("../content/sidebars/about-iota.js");
 const operator = require("../content/sidebars/operator.js");
 const references = require("../content/sidebars/references.js");
-const tsSDK = require("../content/sidebars/ts-sdk.js")
-const identity = require("../content/sidebars/identity.js");
-const iotaEvm = require("../content/sidebars/iota-evm.js");
+// const tsSDK = require("../content/sidebars/ts-sdk.js")
+// const identity = require("../content/sidebars/identity.js");
+// const iotaEvm = require("../content/sidebars/iota-evm.js");
+const users = require("../content/sidebars/users.js");
+
 
 const sidebars = {
   //whyIOTASidebar: why_iota,
@@ -17,9 +19,10 @@ const sidebars = {
   operatorSidebar: operator,
   aboutIotaSidebar: aboutIota,
   referencesSidebar: references,
-  tsSDK: tsSDK,
-  identity: identity,
-  iotaEvm: iotaEvm,
+  // tsSDK: tsSDK,
+  // identity: identity,
+  // iotaEvm: iotaEvm,
+  usersSidebar: users,
 };
 
 module.exports = sidebars;
