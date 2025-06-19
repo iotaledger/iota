267093c2cae922194eab0058184f22a613a236c6
JCNoguera <88061365+VmMad@users.noreply.github.com>
2025-06-19 16:33:56 +0200

    feat: make background of ObjectVideoImage similar to panels in explorer
    Co-authored-by: evavirseda <evirseda@boxfish.studio>

--- apps/explorer/src/components/ui/ObjectVideoImage.tsx
+++ apps/explorer/src/components/ui/ObjectVideoImage.tsx
@@ -100,6 +100,7 @@ export function ObjectVideoImage({
             />
             <div
                 className={clsx(
+                    'bg-neutral-96 dark:bg-neutral-10',
                     imageStyles({ variant, disablePreview, rounded }),
                     rounded && 'overflow-hidden',
                 )}
