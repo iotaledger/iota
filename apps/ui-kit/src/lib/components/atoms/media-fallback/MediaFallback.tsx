c6e36692051fd48e3c72bf564b76db203eeb31b4
Juligs <julicodes29@gmail.com>
2025-06-24 14:02:41 +0200

    chore: revert unnecessary changes without prettier

--- apps/ui-kit/src/lib/components/atoms/media-fallback/MediaFallback.tsx
+++ apps/ui-kit/src/lib/components/atoms/media-fallback/MediaFallback.tsx
@@ -5,8 +5,8 @@ import { PlaceholderReplace } from '@iota/apps-ui-icons';
 
 export function MediaFallback() {
     return (
-        <div className="bg-neutral-96 dark:bg-neutral-10 flex h-full w-full items-center justify-center">
-            <PlaceholderReplace className="text-neutral-40 dark:text-neutral-60 h-4 w-4" />
+        <div className="flex h-full w-full items-center justify-center bg-neutral-96 dark:bg-neutral-10">
+            <PlaceholderReplace className="h-4 w-4 text-neutral-40 dark:text-neutral-60" />
         </div>
     );
 }
