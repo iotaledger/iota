753eb306493a10e971573905b657169961e9651f
samuel_rufi <samuel.rufinatscha@iota.org>
2025-06-25 01:44:12 +0200

    fix: compatibility version

--- crates/iota-json-rpc-api/src/indexer.rs
+++ crates/iota-json-rpc-api/src/indexer.rs
@@ -46,7 +46,7 @@ pub trait IndexerApi {
 
     /// Return list of transactions for a specified query criteria.
     #[rustfmt::skip]
-    #[method(name = "queryTransactionBlocks", version <= "1.0.0")]
+    #[method(name = "queryTransactionBlocks", version <= "1.2.3")]
     async fn query_transaction_blocks(
         &self,
         /// the transaction query criteria.
