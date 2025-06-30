e2ca855e01eaeb78d410e151ff1c1630c0aa6bf3
Dr-Electron <dr-electr0n@protonmail.com>
2025-06-30 12:04:19 +0200

    Update docusaurus.config.js

--- docs/site/docusaurus.config.js
+++ docs/site/docusaurus.config.js
@@ -201,7 +201,14 @@ const config = {
           }
         }
       }
-    ]
+    ],
+    [
+      '@docusaurus/plugin-google-gtag',
+      {
+        trackingID: 'G-SEE2W8WK21',
+        anonymizeIP: true,
+      },
+    ],
   ],
   presets: [
     [
