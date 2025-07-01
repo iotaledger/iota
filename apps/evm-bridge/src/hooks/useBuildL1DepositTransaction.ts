f371002bbae8f1daad4a1dcf66a611e82d0d3bc7
Branko <brankobosnic1@gmail.com>
2025-07-01 12:15:29 +0200

    feat: add native tokens to L1 to L2 transaction

--- apps/evm-bridge/src/hooks/useBuildL1DepositTransaction.ts
+++ apps/evm-bridge/src/hooks/useBuildL1DepositTransaction.ts
@@ -11,16 +11,21 @@ import {
 } from '@iota/isc-sdk';
 import { IOTA_TYPE_ARG } from '@iota/iota-sdk/utils';
 import { useNetworkVariables } from '../config';
+import { CoinStruct } from '@iota/iota-sdk/client';
 
 interface UseBuildL1DepositTransactionProps {
     amount: bigint; // Amount in nanos
     receivingAddress: string;
+    coins: CoinStruct[];
+    coinType?: string;
     refetchInterval?: number;
 }
 
 export function useBuildL1DepositTransaction({
     receivingAddress,
     amount,
+    coins,
+    coinType = IOTA_TYPE_ARG,
     refetchInterval,
 }: UseBuildL1DepositTransactionProps) {
     const currentAccount = useCurrentAccount();
@@ -34,13 +39,50 @@ export function useBuildL1DepositTransaction({
             if (!receivingAddress) {
                 throw Error('Invalid input: receivingAddress is missing');
             }
-
-            const amountToPlace = amount + L2_FROM_L1_GAS_BUDGET;
-
             const iscTx = new IscTransaction(variables.chain);
             const bag = iscTx.newBag();
+
+            const isIotaCoinType = coinType === IOTA_TYPE_ARG;
+            // If the coin type is IOTA, we need to add the L2 gas budget to the amount
+            const amountToPlace = isIotaCoinType
+                ? amount + L2_FROM_L1_GAS_BUDGET
+                : L2_FROM_L1_GAS_BUDGET;
+
+            // add iota coins to the bag
             const coin = iscTx.coinFromAmount({ amount: amountToPlace });
             iscTx.placeCoinInBag({ coin, bag });
+
+            // If the coin type is not IOTA, we need to add the coins to the bag
+            if (!isIotaCoinType) {
+                const totalCoinBalance = coins.reduce((acc, { balance }) => {
+                    return BigInt(acc) + BigInt(balance);
+                }, BigInt(0));
+                const isTransferAllObjects = totalCoinBalance === amount;
+
+                const tx = iscTx.transaction();
+
+                const [primaryCoin, ...mergeCoins] = coins.filter(
+                    (coin) => coin.coinType === coinType,
+                );
+                const primaryCoinInput = tx.object(primaryCoin.coinObjectId);
+
+                if (mergeCoins.length) {
+                    tx.mergeCoins(
+                        primaryCoinInput,
+                        mergeCoins.map((coin) => tx.object(coin.coinObjectId)),
+                    );
+                }
+                const coin = isTransferAllObjects
+                    ? primaryCoinInput
+                    : tx.splitCoins(primaryCoinInput, [amount]);
+
+                iscTx.placeCoinInBag({
+                    bag,
+                    coin,
+                    coinType,
+                });
+            }
+
             iscTx.createAndSendToEvm({
                 bag,
                 transfers: [[IOTA_TYPE_ARG, amount]],
