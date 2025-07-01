0ba471fecb2d507e901a771d8b2bd87119961fca
muXxer <git@muxxer.de>
2025-07-01 12:03:13 +0200

    fix: increase re-execute/re-built timeouts during validator start

--- crates/iota-node/src/lib.rs
+++ crates/iota-node/src/lib.rs
@@ -1580,7 +1580,7 @@ impl IotaNode {
         // clients in some circumstances. However, the transactions are still in
         // pending_consensus_certificates, so we cannot lose any finality guarantees.
         if tokio::time::timeout(
-            std::time::Duration::from_secs(10),
+            std::time::Duration::from_secs(120),
             state
                 .get_transaction_cache_reader()
                 .notify_read_executed_effects_digests(&digests),
