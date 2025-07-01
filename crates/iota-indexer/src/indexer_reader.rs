022c251f89bffc9af47b8827e49701c2ffee746e
samuel_rufi <samuel.rufinatscha@iota.org>
2025-07-01 15:45:08 +0200

    refactor: Fix ci-clippy

--- crates/iota-indexer/src/indexer_reader.rs
+++ crates/iota-indexer/src/indexer_reader.rs
@@ -1999,7 +1999,7 @@ impl IndexerReader {
     where
         T: TryFrom<Object, Error = IotaError>,
     {
-        let cache_key = format!("{}", tag.to_canonical_string(/* with_prefix */ true));
+        let cache_key = tag.to_canonical_string(/* with_prefix */ true);
 
         let mut cache = self
             .package_obj_type_cache
