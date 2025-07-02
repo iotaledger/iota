aa90e77e171eef85ba8f23de55c73b63c621a8e4
Marc Espin <marc@mespin.me>
2025-07-02 11:50:18 +0200

    Update useSignAndExecuteTransaction.ts

--- sdk/dapp-kit/src/hooks/wallet/useSignAndExecuteTransaction.ts
+++ sdk/dapp-kit/src/hooks/wallet/useSignAndExecuteTransaction.ts
@@ -29,7 +29,7 @@ type UseSignAndExecuteTransactionArgs = PartialBy<
     'account' | 'chain'
 > & {
     transaction: Transaction | string;
-    wait?: boolean;
+    waitForTransaction?: boolean;
 };
 
 type UseSignAndExecuteTransactionResult = IotaSignAndExecuteTransactionOutput;
@@ -159,7 +159,7 @@ export function useSignAndExecuteTransaction<
 
             reportTransactionEffects({ effects, account: signerAccount, chain });
 
-            if (signTransactionArgs.wait && 'digest' in result) {
+            if (signTransactionArgs.waitForTransaction && 'digest' in result) {
                 await client.waitForTransaction({
                     digest: result.digest,
                 });
