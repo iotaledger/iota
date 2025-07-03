3724fa02049ca25ddc31a731a458f3173e426275
Konstantinos Demartinos <konstantinos.demartinos@iota.org>
2025-07-03 15:23:20 +0300

    feat(indexer): mark checkpoint transactions as indexed (#7573)
    # Description of change
    
    This patch introduces an `IndexStatus` for checkpoint transactions that
    transitions from `Started -> Completed` after a transactions has been
    indexed. This is mediated through
    `tx_global_order.optimistic_sequence_number`.
    
    ## Links to any relevant issues
    
    Fixes #7430, #7432
    
    ## How the change has been tested
    
    - [x] Basic tests (linting, compilation, formatting, unit/integration
    tests)
    - [x] Patch-specific tests (correctness, functionality coverage)
    
    
    ### Infrastructure QA
    
    This patch adds an additional db operation on top of existing indexing
    operations. As such it tested only through integration tests.
    
    - [ ] Synchronization of the indexer from genesis for a network
    including migration objects.
    - [ ] Restart of indexer synchronization locally without resetting the
    database.
    - [ ] Restart of indexer synchronization on a production-like database.
    - [ ] Deployment of services using Docker.
    - [ ] Verification of API backward compatibility.

--- crates/iota-indexer/src/handlers/committer.rs
+++ crates/iota-indexer/src/handlers/committer.rs
@@ -10,10 +10,7 @@ use tokio_util::sync::CancellationToken;
 use tracing::{error, info, instrument};
 
 use super::{CheckpointDataToCommit, EpochToCommit};
-use crate::{
-    metrics::IndexerMetrics, models::transactions::TxGlobalOrder, store::IndexerStore,
-    types::IndexerResult,
-};
+use crate::{metrics::IndexerMetrics, store::IndexerStore, types::IndexerResult};
 
 pub(crate) const CHECKPOINT_COMMIT_BATCH_SIZE: usize = 100;
 
@@ -124,10 +121,6 @@ async fn commit_checkpoints<S>(
     let guard = metrics.checkpoint_db_commit_latency.start_timer();
     let tx_batch = tx_batch.into_iter().flatten().collect::<Vec<_>>();
 
-    let tx_global_order_batch = tx_batch
-        .iter()
-        .map(Into::into)
-        .collect::<Vec<TxGlobalOrder>>();
     let tx_indices_batch = tx_indices_batch.into_iter().flatten().collect::<Vec<_>>();
     let events_batch = events_batch.into_iter().flatten().collect::<Vec<_>>();
     let event_indices_batch = event_indices_batch
@@ -147,7 +140,6 @@ async fn commit_checkpoints<S>(
         let mut persist_tasks = vec![
             state.persist_transactions(tx_batch),
             state.persist_tx_indices(tx_indices_batch),
-            state.persist_tx_global_order(tx_global_order_batch),
             state.persist_events(events_batch),
             state.persist_event_indices(event_indices_batch),
             state.persist_displays(display_updates_batch),
