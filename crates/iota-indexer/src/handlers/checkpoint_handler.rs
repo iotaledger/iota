aa76a653a2018c0e1307f88ad21c32ecbb7a23d1
Tomasz Pastusiak <tomasz.pastusiak@iota.org>
2025-07-03 16:10:05 +0200

    feat(indexer): Make optimistic indexing commit all data in single transaction (#7520)
    This PR modifies optimistic indexing logic so that it commits all data
    related to execution of single TX inside of one DB transaction.
    This means that the fact of optimistically indexing a TX is atomic and
    combined with unique columns check in tx_global_order table will ensure
    that given TX will either be indexed on optimistic path, or checkpoint
    path, but not both.
    
    fixes https://github.com/iotaledger/iota/issues/7431
    
    `cargo test --profile dev-nodebug --package iota-indexer --features
    pg_integration -- --test-threads=1`
    `cargo test --profile dev-nodebug --package iota-indexer --test
    rpc-tests --all-features`
    
    The change was manually tested by adding artificial delays inside of
    optimistic indexing logic:
    - inside optimistic indexing DB transaction, but **before** tx global
    order is assigned - results in unique constraint violation, and
    optimistic indexing is aborted - this is as expected
    - inside optimistic indexing DB transaction, but **after** tx global
    order is assigned - results in checkpoint indexing waiting until
    optimistic indexing for given TX finishes - this is correct from data
    consistency perspective, although a bit unexpected, still this should be
    a rather rare case
    
    - [x] Basic tests (linting, compilation, formatting, unit/integration
    tests)
    - [ ] Patch-specific tests (correctness, functionality coverage)
    
    @iotaledger/infrastructure)
    
    - [ ] Synchronization of the indexer from genesis for a network
    including migration objects.
    - [ ] Restart of indexer synchronization locally without resetting the
    database.
    - [ ] Restart of indexer synchronization on a production-like database.
    - [ ] Deployment of services using Docker.
    - [ ] Verification of API backward compatibility.
    
    - [ ] Protocol:
    - [ ] Nodes (Validators and Full nodes):
    - [ ] Indexer:
    - [ ] JSON-RPC:
    - [ ] GraphQL:
    - [ ] CLI:
    - [ ] Rust SDK:
    - [ ] REST API:
    
    ---------
    
    Co-authored-by: Konstantinos Demartinos <konstantinos.demartinos@iota.org>

--- crates/iota-indexer/src/handlers/checkpoint_handler.rs
+++ crates/iota-indexer/src/handlers/checkpoint_handler.rs
@@ -90,6 +90,14 @@ pub struct CheckpointHandler {
     indexed_checkpoint_sender: iota_metrics::metered_channel::Sender<CheckpointDataToCommit>,
 }
 
+pub type IndexedTransactionComponents = (
+    IndexedTransaction,
+    TxIndex,
+    Vec<IndexedEvent>,
+    Vec<EventIndex>,
+    BTreeMap<String, StoredDisplay>,
+);
+
 #[async_trait]
 impl Worker for CheckpointHandler {
     type Message = ();
@@ -387,13 +395,7 @@ impl CheckpointHandler {
         checkpoint_seq: CheckpointSequenceNumber,
         checkpoint_timestamp_ms: u64,
         metrics: &IndexerMetrics,
-    ) -> IndexerResult<(
-        IndexedTransaction,
-        TxIndex,
-        Vec<IndexedEvent>,
-        Vec<EventIndex>,
-        BTreeMap<String, StoredDisplay>,
-    )> {
+    ) -> IndexerResult<IndexedTransactionComponents> {
         let CheckpointTransaction {
             transaction: sender_signed_data,
             effects: fx,
