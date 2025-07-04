2ab7a7d2cd588026b468fdf9ec7be1f44297cadd
NaitsabesMue <51112618+NaitsabesMue@users.noreply.github.com>
2025-07-04 13:12:34 +0200

    feat(consensus): Improve synchronizer (#7371)
    # Description of change
    
    This PR improves block sync performance when a single/few missing block
    blocks may suspend many others.
    
    Problem:
    Random selection in the periodic synchronizer often takes too long to
    locate blocks that are held by only a few authorities, leaving large
    portions of the DAG stalled.
    
    Fix:
    Add a knowledge-based fetch path to the periodic synchronizer:
    
    - Leverage extended missing_blocks map from BlockManager, which now
    tracks for each missing block a set of authorities that are known to
    have it.
    - Prioritize “knowledge-based” peers in the scheduler: when requesting
    missing blocks, first query exactly those authorities that should
    already hold them, rather than picking peers at random.
    - Issue random‐peer requests alongside the knowledge‐based queries
    
    This change avoids long delays in recovering blocks that live on only a
    few nodes by directing periodic sync requests to the right peers up
    front.
    
    
    
    ## Links to any relevant issues
    
    closes #7370
    
    ## How the change has been tested
    
    - X Basic tests (linting, compilation, formatting, unit/integration
    tests)
    - X Patch-specific tests (correctness, functionality coverage)
    - X I have added tests that prove my fix is effective or that my feature
    works
    - X I have checked that new and existing unit tests pass locally with my
    changes
    
    ### Release Notes
    
    - [ ] Protocol:
    - [ ] Nodes (Validators and Full nodes):
    - [ ] Indexer:
    - [ ] JSON-RPC:
    - [ ] GraphQL:
    - [ ] CLI:
    - [ ] Rust SDK:
    - [ ] REST API:
    
    ---------
    
    Co-authored-by: Nikita Polianskii <nikita.polianskii@iota.org>

--- consensus/core/src/authority_service.rs
+++ consensus/core/src/authority_service.rs
@@ -749,7 +749,11 @@ async fn make_recv_future<T: Clone>(
 
 #[cfg(test)]
 mod tests {
-    use std::{collections::BTreeSet, sync::Arc, time::Duration};
+    use std::{
+        collections::{BTreeMap, BTreeSet},
+        sync::Arc,
+        time::Duration,
+    };
 
     use async_trait::async_trait;
     use bytes::Bytes;
@@ -819,7 +823,9 @@ mod tests {
             Ok(())
         }
 
-        async fn get_missing_blocks(&self) -> Result<BTreeSet<BlockRef>, CoreError> {
+        async fn get_missing_blocks(
+            &self,
+        ) -> Result<BTreeMap<BlockRef, BTreeSet<AuthorityIndex>>, CoreError> {
             Ok(Default::default())
         }
 
