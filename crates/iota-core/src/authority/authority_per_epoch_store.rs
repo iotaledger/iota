3200c657e47f8a9889bf8413180a262ee4f66f72
Roman Overko <63564739+roman1e2f5p8s@users.noreply.github.com>
2025-07-07 15:31:02 +0200

    Update crates/iota-core/src/authority/authority_per_epoch_store.rs
    Co-authored-by: muXxer <git@muxxer.de>

--- crates/iota-core/src/authority/authority_per_epoch_store.rs
+++ crates/iota-core/src/authority/authority_per_epoch_store.rs
@@ -229,7 +229,7 @@ pub enum ConsensusCertificateResult {
     /// The transaction should be re-processed at a future commit, specified by
     /// `deferral_key`. `suggested_gas_price_as_opt` indicates a gas price that
     /// the certificate would need to pay to be scheduled in a consensus commit.
-    /// For certificates deferred due to randomness not available, the
+    /// For certificates deferred due to "randomness not available", the
     /// `suggested_gas_price_as_opt` price field will be set to `None`.
     Deferred {
         deferral_key: DeferralKey,
