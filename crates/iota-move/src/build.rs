6339005e210b0e940124b727aefeb72e2743bf87
István Davorin Ács <istvan.acs@iota.org>
2025-07-22 09:35:00 +0200

    feat(CLI): Tree shaking for package dependencies (#7883)
    # Description of change
    
    This PR introduces tree shaking for package dependencies. The package
    dependencies that are not referenced in code (directly or via trans
    deps) are removed before publishing/upgrading a package.
    
    ## Links to any relevant issues
    
    fixes #7855 
    
    ## How the change has been tested
    
    ```
    cargo nextest run --no-fail-fast test_tree_shaking
    ```
    
    - [x] Basic tests (linting, compilation, formatting, unit/integration
    tests)
    - [ ] Patch-specific tests (correctness, functionality coverage)
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have checked that new and existing unit tests pass locally with
    my changes
    
    ### Release Notes
    
    - [ ] Protocol:
    - [ ] Nodes (Validators and Full nodes):
    - [ ] Indexer:
    - [ ] JSON-RPC:
    - [ ] GraphQL:
    - [x] CLI: `iota client publish/upgrade` will now by default remove
    dependencies that are not referenced in source code from being published
    on-chain.
    - [ ] Rust SDK:
    - [ ] REST API:

--- crates/iota-move/src/build.rs
+++ crates/iota-move/src/build.rs
@@ -71,7 +71,7 @@ impl Build {
         generate_struct_layouts: bool,
         chain_id: Option<String>,
     ) -> anyhow::Result<()> {
-        let pkg = BuildConfig {
+        let mut pkg = BuildConfig {
             config,
             run_bytecode_verifier: true,
             print_diags_to_stderr: true,
@@ -84,6 +84,7 @@ impl Build {
                 check_unpublished_dependencies(&pkg.dependency_ids.unpublished)?;
             }
 
+            pkg.tree_shake(with_unpublished_deps)?;
             println!(
                 "{}",
                 json!({
