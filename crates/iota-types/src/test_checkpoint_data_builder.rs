258d594610b64e2e15e12317e587e4de42bd7814
jkrvivian <jkrvivian@gmail.com>
2025-07-28 18:14:50 +0800

    upstream(node): Improve assert message in create_coin_object_with_owner

--- crates/iota-types/src/test_checkpoint_data_builder.rs
+++ crates/iota-types/src/test_checkpoint_data_builder.rs
@@ -203,7 +203,10 @@ impl TestCheckpointDataBuilder {
     ) -> Self {
         let tx_builder = self.checkpoint_builder.next_transaction.as_mut().unwrap();
         let object_id = Self::derive_object_id(object_idx);
-        assert!(!self.live_objects.contains_key(&object_id));
+        assert!(
+            !self.live_objects.contains_key(&object_id),
+            "Object already exists: {object_id}. Please use a different object index.",
+        );
         let move_object = MoveObject::new_coin(
             Coin::type_(coin_type).into(),
             // version doesn't matter since we will set it to the lamport version when we finalize
