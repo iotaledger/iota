2a7edd8d7960ff7811d7be1e88ad2135b0c52bdc
JCNoguera <88061365+VmMad@users.noreply.github.com>
2025-07-28 19:41:05 +0200

    fix: type

--- apps/wallet-dashboard/components/dialogs/send-token/interfaces/index.ts
+++ apps/wallet-dashboard/components/dialogs/send-token/interfaces/index.ts
@@ -1,8 +1,8 @@
 // Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
 
 import type { ReceiverInputFormValues } from '@iota/core';
 
-// SPDX-License-Identifier: Apache-2.0
 export interface FormDataValues extends ReceiverInputFormValues {
     amount: string;
 }
