80f99e885a9a878a3ced5ecb9cdfd58886e8ed83
Branko <brankobosnic1@gmail.com>
2025-07-29 18:06:47 +0200

    test: parallel execution

--- apps/evm-bridge/playwright.config.ts
+++ apps/evm-bridge/playwright.config.ts
@@ -22,23 +22,11 @@ export default defineConfig({
     },
     projects: [
         {
-            name: 'send-max-tests',
-            testMatch: ['**/*sendMax*.spec.ts'], // Match any sendMax test files
+            name: 'chromium',
             use: {
                 ...devices['Desktop Chrome'],
                 userAgent: 'Playwright',
-                contextOptions: {
-                    permissions: ['clipboard-read'],
-                },
-            },
-        },
-        {
-            name: 'deposit-tests',
-            testMatch: ['**/*deposit*.spec.ts'], // Match any deposit test files
-            dependencies: ['send-max-tests'], // This makes it wait for send-max-tests
-            use: {
-                ...devices['Desktop Chrome'],
-                userAgent: 'Playwright',
+                // Remove after we don't need copy address anymore (tests/utils/auth.ts > line with clipboard)
                 contextOptions: {
                     permissions: ['clipboard-read'],
                 },
