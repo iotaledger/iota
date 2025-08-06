8cd74129d33e17f279187f0a6c051cb9d32e1016
Roman Overko <roman.overko@iota.org>
2025-08-06 20:32:07 +0200

    fix: more merge conflicts and clippy

--- crates/iota-core/src/authority.rs
+++ crates/iota-core/src/authority.rs
@@ -1893,16 +1893,15 @@ impl AuthorityState {
                 suggested_gas_price: self
                     .congestion_tracker
                     .get_prediction_suggested_gas_price(&transaction),
-                input: IotaTransactionBlockData::try_from(
-                    transaction.clone(),
-                    &module_cache,
-                    tx_digest,
-                )
-                .map_err(|e| IotaError::TransactionSerialization {
-                    error: format!(
-                        "Failed to convert transaction to IotaTransactionBlockData: {e}",
-                    ),
-                })?, // TODO: replace the underlying try_from to IotaError. This one goes deep
+                objects_hotness: self
+                    .congestion_tracker
+                    .get_hotness_for_transaction(&transaction),
+                input: IotaTransactionBlockData::try_from(transaction, &module_cache, tx_digest)
+                    .map_err(|e| IotaError::TransactionSerialization {
+                        error: format!(
+                            "Failed to convert transaction to IotaTransactionBlockData: {e}",
+                        ),
+                    })?, // TODO: replace the underlying try_from to IotaError. This one goes deep
                 effects: effects.clone().try_into()?,
                 events: IotaTransactionBlockEvents::try_from(
                     inner_temp_store.events.clone(),
@@ -1912,9 +1911,6 @@ impl AuthorityState {
                 )?,
                 object_changes,
                 balance_changes,
-                objects_hotness: self
-                    .congestion_tracker
-                    .get_hotness_for_transaction(&transaction),
             },
             written_with_kind,
             effects,
