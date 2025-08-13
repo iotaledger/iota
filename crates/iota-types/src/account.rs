992e6ca14b595359938d7200943a92a66a547596
Valerii Reutov <valeriy.reutov@gmail.com>
2025-08-13 13:45:55 +0300

    refactor: code cleanup

--- crates/iota-types/src/account.rs
+++ crates/iota-types/src/account.rs
@@ -1,4 +1,7 @@
-use crate::{base_types::ObjectID, transaction::CallArg};
+use crate::{
+    base_types::{ObjectID, ObjectRef},
+    transaction::CallArg,
+};
 
 /// Temporary created structures.
 /// This part will be removed once the real types are implemented.
@@ -10,4 +13,13 @@ pub struct AuthenticatorInfo {
     pub package: ObjectID,
     pub module: String,
     pub function: String,
+}
+
+impl MoveAuthenticator {
+    pub fn receiving_objects(&self) -> Vec<ObjectRef> {
+        self.inputs
+            .iter()
+            .flat_map(|arg| arg.receiving_objects())
+            .collect()
+    }
 }
