cd8301f59018973ca0aee81c0649440316299b83
marc2332 <mespinsanz@gmail.com>
2025-08-14 16:28:32 +0200

    clean up

--- docs/site/docusaurus.config.js
+++ docs/site/docusaurus.config.js
@@ -187,10 +187,6 @@ const config = {
             {
               from: '/about-iota/iota-wallet/how-to/integrate-ledger',
               to: '/users/iota-wallet/how-to/import/ledger'
-            },
-            {
-              from: '/about-iota/iota-wallet/how-to/integrate-ledger#manually-for-deprecated-devices-such-as-ledger-nano-s',
-              to: '/users/iota-wallet/how-to/import/ledger?install-method=manual#install-the-iota-app-on-your-ledger-device'
             }
           ];
           let paths = [];
