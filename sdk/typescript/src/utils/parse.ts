4709e7bef1df40db64eedccc20a9ea61e9a657f8
JCNoguera <88061365+VmMad@users.noreply.github.com>
2025-08-27 11:15:55 +0200

    fix(ts-sdk): remove unnecessary param from parse function

--- sdk/typescript/src/utils/parse.ts
+++ sdk/typescript/src/utils/parse.ts
@@ -3,18 +3,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import BigNumber from 'bignumber.js';
+import { IOTA_DECIMALS } from './constants.js';
 
-export function parseIotaToNanos(amount: string, coinDecimals: number) {
+export function parseIotaToNanos(amount: string) {
     try {
-        return BigInt(new BigNumber(amount).shiftedBy(coinDecimals).integerValue().toString());
+        return BigInt(new BigNumber(amount).shiftedBy(IOTA_DECIMALS).integerValue().toString());
     } catch (e) {
         return BigInt(0);
     }
 }
 
-export function safeParseIotaToNanos(amount: string, coinDecimals: number) {
+export function safeParseIotaToNanos(amount: string) {
     try {
-        return BigInt(new BigNumber(amount).shiftedBy(coinDecimals).integerValue().toString());
+        return BigInt(new BigNumber(amount).shiftedBy(IOTA_DECIMALS).integerValue().toString());
     } catch (e) {
         return null;
     }
