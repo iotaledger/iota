a33fe02fcea94455dcaf27623a077edff7ee3c3e
Vlad Semenov <vlad.semenov@iota.org>
2025-08-28 09:00:19 +0300

    fix(core): typo

--- crates/iota-common/src/try_iterator_ext.rs
+++ crates/iota-common/src/try_iterator_ext.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 IOTA Stiftung
+// Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
 pub trait TryIteratorExt<T, E>: Iterator<Item = Result<T, E>> + Sized {
