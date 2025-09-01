a3ec1c92ea9fc8f818bf572199f10e4b5779c779
Ching-Hua (Vivian) Lin <jkrvivian@gmail.com>
2025-09-01 22:11:33 +0800

    upstream(core): Add execution error to response (#8190)
    # Description of change
    
    - Upstream range: [v1.44.3, v1.45.3)
    - Port commit: 
    -
    https://github.com/MystenLabs/sui/commit/e98fe17811feaafb7e75abf97818085595b58b73
    - Description:
    
    Return source information in dry run transactions
    
    ## Links to any relevant issues
    
    Part of #6153 
    
    ## How the change has been tested
    
    - [x] Basic tests (linting, compilation, formatting, unit/integration
    tests)
    - [ ] Patch-specific tests (correctness, functionality coverage)
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have checked that new and existing unit tests pass locally with
    my changes

--- crates/iota-json-rpc-types/src/iota_transaction.rs
+++ crates/iota-json-rpc-types/src/iota_transaction.rs
@@ -1158,6 +1158,7 @@ pub struct DryRunTransactionBlockResponse {
     #[schemars(with = "Option<BigInt<u64>>")]
     #[serde_as(as = "Option<BigInt<u64>>")]
     pub suggested_gas_price: Option<u64>,
+    pub execution_error_source: Option<String>,
 }
 
 #[derive(Eq, PartialEq, Clone, Debug, Default, Serialize, Deserialize, JsonSchema)]
