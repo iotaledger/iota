93f88bec59dc5a99e48c7858bc090a66b6fd96cf
msarcev <mario.sarcevic@iota.org>
2025-09-12 17:30:02 +0200

    feat(wallet): Improve keystone views UX (#8535)
    # Description of change
    
    ⚠️  Based of https://github.com/iotaledger/iota/pull/8518
    Closes https://github.com/iotaledger/iota/issues/8512
    
    ---------
    
    Co-authored-by: marc2332 <mespinsanz@gmail.com>

--- apps/wallet/src/ui/app/pages/accounts/ImportKeystone.tsx
+++ apps/wallet/src/ui/app/pages/accounts/ImportKeystone.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AccountsFormType, useAccountsFormContext, PageTemplate, AccountList } from '_components';
 import { AnimatedQRScanner } from '@keystonehq/animated-qr';
 import { Button, ButtonType, InfoBox, InfoBoxStyle, InfoBoxType } from '@iota/apps-ui-kit';
@@ -12,7 +12,7 @@ import { fromHex } from '@iota/iota-sdk/utils';
 import { toast } from '@iota/core';
 import { useState } from 'react';
 import { useAccounts } from '../../hooks';
-import { Warning } from '@iota/apps-ui-icons';
+import { ImportPass, IotaLogoMark, QrCode, Warning } from '@iota/apps-ui-icons';
 
 type Step =
     | {
@@ -76,26 +76,96 @@ export function ImportKeystone() {
     const disableFinish = step.type === 'select-accounts' && step.selectedAccounts.size === 0;
 
     return (
-        <PageTemplate title="Import Keystone">
+        <PageTemplate title="Import Keystone" isTitleCentered showBackButton>
             <div className="flex h-full w-full flex-col items-center ">
                 <div className="w-full grow">
-                    <div className="flex h-full flex-col justify-between gap-2 ">
+                    <div className="flex h-full flex-col justify-between gap-1">
                         {step.type === 'scan-qr' ? (
                             <>
-                                <div className="flex flex-col gap-sm">
-                                    <AnimatedQRScanner
-                                        handleScan={onSucceed}
-                                        handleError={onError}
-                                        urTypes={[URType.CryptoMultiAccounts]}
-                                    />
+                                <div className="relative flex flex-col items-center justify-center gap-xs">
+                                    <div className="box-border flex h-[220px] w-[220px] items-center justify-center overflow-hidden rounded-lg">
+                                        <div className="flex-shrink-0">
+                                            <AnimatedQRScanner
+                                                handleScan={onSucceed}
+                                                handleError={onError}
+                                                urTypes={[URType.CryptoMultiAccounts]}
+                                                options={{
+                                                    blur: true,
+                                                    width: '230px',
+                                                    height: '230px',
+                                                }}
+                                            />
+                                        </div>
+                                    </div>
+                                    <span className="mb-sm text-center text-body-sm text-iota-neutral-40 dark:text-iota-neutral-60">
+                                        Camera is blurred for security reasons
+                                    </span>
+                                    <div className="input-border-color flex w-full flex-col gap-xs rounded-2lg border border-solid p-4 no-underline">
+                                        <div className="flex">
+                                            <div className="mr-4 flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-iota-primary-90 [&_svg]:h-4 [&_svg]:w-4 [&_svg]:text-black">
+                                                <IotaLogoMark />
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <span className="text-iota-neutral-40 dark:text-iota-neutral-60">
+                                                    Step 1
+                                                </span>
+                                                <span className="font-semibold text-iota-neutral-40 dark:text-iota-neutral-60">
+                                                    Open the IOTA Wallet app in Keystone
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div className="flex">
+                                            <div className="mr-4 flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-iota-primary-90 [&_svg]:h-4 [&_svg]:w-4 [&_svg]:text-black">
+                                                <QrCode />
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <span className="text-iota-neutral-40 dark:text-iota-neutral-60">
+                                                    Step 2
+                                                </span>
+                                                <span className="font-semibold text-iota-neutral-40 dark:text-iota-neutral-60">
+                                                    Point the QR code to your camera
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div className="flex">
+                                            <div className="mr-4 flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-iota-primary-90 [&_svg]:h-4 [&_svg]:w-4 [&_svg]:text-black">
+                                                <ImportPass />
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <span className="text-iota-neutral-40 dark:text-iota-neutral-60">
+                                                    Step 3
+                                                </span>
+                                                <span className="font-semibold text-iota-neutral-40 dark:text-iota-neutral-60">
+                                                    Import wallets
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="flex flex-row justify-stretch gap-2.5">
-                                    <Button
-                                        type={ButtonType.Secondary}
-                                        text="Cancel"
-                                        onClick={() => navigate(-1)}
-                                        fullWidth
-                                    />
+
+                                <div className="flex flex-col">
+                                    <div className="mb-2 flex items-center justify-center gap-x-1">
+                                        <span className="text-body-md text-iota-neutral-40 dark:text-iota-neutral-60">
+                                            Need more help?
+                                        </span>
+                                        <Link
+                                            // TODO: Add tutorial docs links when available - https://github.com/iotaledger/iota/issues/8511
+                                            to=""
+                                            className="text-body-md text-iota-primary-30 no-underline dark:text-iota-primary-80"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            View tutorial.
+                                        </Link>
+                                    </div>
+                                    <div className="flex flex-row justify-stretch gap-2">
+                                        <Button
+                                            type={ButtonType.Secondary}
+                                            text="Back"
+                                            onClick={() => navigate(-1)}
+                                            fullWidth
+                                        />
+                                    </div>
                                 </div>
                             </>
                         ) : (
