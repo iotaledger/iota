49e1b3b011daab338e79882c1a8ed41f9c69a673
Marc Espin <mespinsanz@gmail.com>
2025-09-12 09:35:22 +0200

    feat(wallet): Keystone Account Sources (#8494)
    Based off https://github.com/iotaledger/iota/pull/8473
    
    Closes https://github.com/iotaledger/iota/issues/8478

--- apps/wallet/src/background/accounts/keystoneAccount.ts
+++ apps/wallet/src/background/accounts/keystoneAccount.ts
@@ -1,8 +1,6 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { decrypt, encrypt } from '_src/shared/cryptography/keystore';
-
 import {
     Account,
     AccountType,
@@ -10,18 +8,18 @@ import {
     type SerializedAccount,
     type SerializedUIAccount,
 } from './account';
+import { KeystoneAccountSource } from '../account-sources/keystoneAccountSource';
 
 export interface KeystoneAccountSerialized extends SerializedAccount {
     type: AccountType.KeystoneDerived;
     derivationPath: string;
-    // just used for authentication nothing is stored here at the moment
-    encrypted: string;
-    masterFingerprint: string;
+    sourceID: string;
 }
 
 export interface KeystoneAccountSerializedUI extends SerializedUIAccount {
     type: AccountType.KeystoneDerived;
     derivationPath: string;
+    sourceID: string;
     masterFingerprint: string;
 }
 
@@ -44,27 +42,24 @@ export class KeystoneAccount
     static async createNew({
         address,
         publicKey,
-        password,
+        sourceID,
         derivationPath,
-        masterFingerprint,
     }: {
         address: string;
         publicKey: string | null;
-        password: string;
         derivationPath: string;
-        masterFingerprint: string;
+        sourceID: string;
     }): Promise<Omit<KeystoneAccountSerialized, 'id'>> {
         return {
             type: AccountType.KeystoneDerived,
             address,
             publicKey,
-            encrypted: await encrypt(password, {}),
             derivationPath,
             lastUnlockedOn: null,
             selected: false,
             nickname: null,
             createdAt: Date.now(),
-            masterFingerprint,
+            sourceID,
         };
     }
 
@@ -74,6 +69,14 @@ export class KeystoneAccount
 
     constructor({ id, cachedData }: { id: string; cachedData?: KeystoneAccountSerialized }) {
         super({ type: AccountType.KeystoneDerived, id, cachedData });
+    }
+
+    get derivationPath() {
+        return this.getCachedData().then(({ derivationPath }) => derivationPath);
+    }
+
+    get sourceID() {
+        return this.getCachedData().then(({ sourceID }) => sourceID);
     }
 
     async lock(allowRead = false): Promise<void> {
@@ -86,23 +89,28 @@ export class KeystoneAccount
     }
 
     async passwordUnlock(password?: string): Promise<void> {
-        if (!password) {
+        const keystoneSource = await this.#getKeystoneSource();
+        if ((await keystoneSource.isLocked()) && !password) {
             throw new Error('Missing password to unlock the account');
         }
-        const { encrypted } = await this.getStoredData();
-        await decrypt<string>(password, encrypted);
-        await this.setEphemeralValue({ unlocked: true });
+        if (password) {
+            await keystoneSource.unlock(password);
+        }
+        await this.setEphemeralValue({
+            unlocked: true,
+        });
         await this.onUnlocked();
     }
 
     async verifyPassword(password: string): Promise<void> {
-        const { encrypted } = await this.getStoredData();
-        await decrypt<string>(password, encrypted);
+        const keystoneSource = await this.#getKeystoneSource();
+        await keystoneSource.verifyPassword(password);
     }
 
     async toUISerialized(): Promise<KeystoneAccountSerializedUI> {
-        const { address, type, publicKey, derivationPath, selected, nickname, masterFingerprint } =
+        const { address, type, publicKey, derivationPath, selected, nickname, sourceID } =
             await this.getStoredData();
+        const masterFingerprint = await (await this.#getKeystoneSource()).masterFingerprint;
         return {
             id: this.id,
             type,
@@ -115,7 +123,12 @@ export class KeystoneAccount
             nickname,
             isPasswordUnlockable: true,
             isKeyPairExportable: false,
+            sourceID,
             masterFingerprint,
         };
+    }
+
+    async #getKeystoneSource() {
+        return new KeystoneAccountSource((await this.getStoredData()).sourceID);
     }
 }
