222f5dd2f3878889e0ba69a113140b6f435f45a4
Thoralf Müller <thoralf.mue@gmail.com>
2025-09-29 10:29:40 +0200

    fix(iota-grpc-api): use --experimental_allow_proto3_optional

--- crates/iota-grpc-api/build.rs
+++ crates/iota-grpc-api/build.rs
@@ -3,6 +3,7 @@
 
 fn main() {
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
             &[
                 "proto/common.proto",
