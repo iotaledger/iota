65cfd1241c2b365007712bacacd27e50b116d84f
Konstantinos Demartinos <konstantinos.demartinos@iota.org>
2025-10-02 11:38:53 +0300

    fixup! fixup! fixup! refactor(indexer): insert object deletion and mutation chunks concurrently

--- crates/iota-indexer/src/rolling/mod.rs
+++ crates/iota-indexer/src/rolling/mod.rs
@@ -2,14 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //! Rolling version of the library.
-//!
-//! This is to phase in breaking changes that may or may not
-//! include refactoring of the existing API.
-//!
-//! This will remain private until the versioning scheme is stabilized.
-//!
-//! In cases where the [`rolling`] module is used in other modules above, it
-//! MUST be used only in private APIs, or in inner implementations.
 
 pub(crate) mod error;
 pub(crate) mod extract;
