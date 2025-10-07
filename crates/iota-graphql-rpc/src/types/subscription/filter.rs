88404957b420ec0b0e1e6857d4223a66ddd04270
Sergiu Popescu <44298302+sergiupopescu199@users.noreply.github.com>
2025-10-07 12:39:53 +0200

    feat(graphql): add subscriptions support for events and transactions (#8698)
    # Description of change
    
    This PR adds the feature to add subscription support for `events` and
    `transactions` for GraphQL.
    
    For filtering capabilities it leverages the
    [`@oneOf`](https://graphql.org/blog/2025-09-04-multioption-inputs-with-oneof/)
    directive which does not allow more than one filter at a time for the
    input filter object.
    
    ## Links to any relevant issues
    
    fixes #8561
    
    ## How the change has been tested
    
    - [x] Basic tests (linting, compilation, formatting, unit/integration
    tests)
    - [ ] Patch-specific tests (correctness, functionality coverage)
    
    - ran graphql tests
    ```shell
    cargo nextest run -p iota-graphql-rpc --features pg_integration --no-fail-fast --test-threads 1
    ```
    - used the iota binary to spin up an indexer and graphql instance and
    test the subscription queries
    ```
    cargo r --features indexer -- start --force-regenesis   --with-indexer --with-graphql
    ```
    - used the integrated graphiQL ui to issue subscription queries
    - used a third party graphql client (Postman) to issue the same queries
    
    > [!NOTE]
    > At approximately 1k subscribers, the local machine reached its
    operational limit. The sheer intensity and concurrent demand of managing
    the network, indexer, GraphQL server, and a high volume of WebSocket
    connections prevented reliable testing.
    
    In future iteration we could add **Prometheus** metrics to track the
    total amount of active subscribers, how many subscribers experience
    lags, ect...
    
    ### Infrastructure QA (only required for crates that are maintained by
    @iotaledger/infrastructure)
    
    - [ ] Synchronization of the indexer from genesis for a network
    including migration objects.
    - [ ] Restart of indexer synchronization locally without resetting the
    database.
    - [ ] Restart of indexer synchronization on a production-like database.
    - [ ] Deployment of services using Docker.
    - [ ] Verification of API backward compatibility.
    
    > [!NOTE]
    > The following patch does not affect the normal operation of the
    indexer, thus tests were skipped.
    
    ### Release Notes
    - [x] GraphQL: Add `Subscription` support for `Event` and
    `TransactionBlock`.

---
+++ crates/iota-graphql-rpc/src/types/subscription/filter.rs
@@ -0,0 +1,114 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use async_graphql::OneofObject;
+use iota_indexer::stream::{ModuleFunction, StreamEventFilter, StreamTransactionFilter};
+use iota_json_rpc_types::IotaTransactionKind;
+use iota_types::base_types::ObjectID;
+
+use crate::types::{
+    iota_address::IotaAddress,
+    transaction_block::TransactionBlockKindInput,
+    type_filter::{FqNameFilter, ModuleFilter},
+};
+
+/// Filter incoming events in a subscription.
+#[derive(OneofObject, Clone, Debug, Eq, PartialEq)]
+pub(crate) enum SubscriptionEventFilter {
+    /// Filter incoming events by emitting module.
+    ///
+    /// - Filter by package: "0x02"
+    /// - Filter by module: "0x02::coin"
+    EmittingModule(ModuleFilter),
+}
+
+impl From<SubscriptionEventFilter> for StreamEventFilter {
+    fn from(value: SubscriptionEventFilter) -> Self {
+        use SubscriptionEventFilter::*;
+        match value {
+            EmittingModule(ModuleFilter::ByPackage(package)) => {
+                StreamEventFilter::EmittingPackage(package.into())
+            }
+            EmittingModule(ModuleFilter::ByModule(package, module)) => {
+                StreamEventFilter::EmittingModule {
+                    package: package.into(),
+                    module,
+                }
+            }
+        }
+    }
+}
+
+/// Filter incoming transactions in a subscription.
+#[derive(OneofObject, Clone, Debug, Eq, PartialEq)]
+pub(crate) enum SubscriptionTransactionFilter {
+    /// Filter incoming transactions by kind.
+    Kind(TransactionBlockKindInput),
+    /// Filter incoming transactions by signing address.
+    SigningAddress(IotaAddress),
+    /// Filter incoming transactions by package, module, or function name.
+    ///
+    /// - Filter by package: "0x03"
+    /// - Filter by module: "0x03::iota_system"
+    /// - Filter by function: "0x03::iota_system::request_add_stake"
+    Function(FqNameFilter),
+}
+
+impl From<SubscriptionTransactionFilter> for StreamTransactionFilter {
+    fn from(value: SubscriptionTransactionFilter) -> Self {
+        use SubscriptionTransactionFilter::*;
+        match value {
+            Kind(kind) => StreamTransactionFilter::Kind(kind.into()),
+            SigningAddress(address) => StreamTransactionFilter::SigningAddress(address.into()),
+            Function(name) => {
+                let (package, module) = name.into();
+                StreamTransactionFilter::Function { package, module }
+            }
+        }
+    }
+}
+
+impl From<TransactionBlockKindInput> for IotaTransactionKind {
+    fn from(value: TransactionBlockKindInput) -> Self {
+        match value {
+            TransactionBlockKindInput::SystemTx => IotaTransactionKind::SystemTransaction,
+            TransactionBlockKindInput::ProgrammableTx => {
+                IotaTransactionKind::ProgrammableTransaction
+            }
+            TransactionBlockKindInput::Genesis => IotaTransactionKind::Genesis,
+            TransactionBlockKindInput::ConsensusCommitPrologueV1 => {
+                IotaTransactionKind::ConsensusCommitPrologueV1
+            }
+            TransactionBlockKindInput::AuthenticatorStateUpdateV1 => {
+                IotaTransactionKind::AuthenticatorStateUpdateV1
+            }
+            TransactionBlockKindInput::RandomnessStateUpdate => {
+                IotaTransactionKind::RandomnessStateUpdate
+            }
+            TransactionBlockKindInput::EndOfEpochTx => IotaTransactionKind::EndOfEpochTransaction,
+        }
+    }
+}
+
+impl From<FqNameFilter> for (ObjectID, Option<ModuleFunction>) {
+    fn from(value: FqNameFilter) -> Self {
+        use FqNameFilter::*;
+        match value {
+            ByModule(ModuleFilter::ByPackage(package)) => (package.into(), None),
+            ByModule(ModuleFilter::ByModule(package, module_name)) => (
+                package.into(),
+                Some(ModuleFunction {
+                    module_name,
+                    function_name: None,
+                }),
+            ),
+            ByFqName(package, module_name, function_name) => (
+                package.into(),
+                Some(ModuleFunction {
+                    module_name,
+                    function_name: Some(function_name),
+                }),
+            ),
+        }
+    }
+}
---
+++ crates/iota-graphql-rpc/src/types/subscription/mod.rs
@@ -0,0 +1,182 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use std::sync::Arc;
+
+use async_graphql::{Context, OutputType, SimpleObject, Subscription, Union};
+use futures::{Stream, StreamExt, TryStreamExt, future};
+use iota_indexer::{
+    indexer_reader::IndexerReader,
+    stream::{IndexerStreamer, StreamEventFilter, StreamTransactionFilter},
+};
+use iota_json_rpc_types::Filter;
+use tokio_stream::wrappers::errors::BroadcastStreamRecvError;
+use tracing::warn;
+
+use crate::{
+    error::Error,
+    types::{
+        event::Event,
+        subscription::filter::{SubscriptionEventFilter, SubscriptionTransactionFilter},
+        transaction_block::{TransactionBlock, TransactionBlockInner},
+    },
+};
+
+mod filter;
+
+/// Notifies that the subscription consumer has fallen behind the live
+/// subscription stream and missed one or more payloads.
+#[derive(SimpleObject, Clone)]
+pub(crate) struct Lagged {
+    /// Number of missed payloads since the previous emitted one.
+    count: u64,
+}
+
+/// Possible responses from a subscription.
+///
+/// It could be one of the following:
+/// - A successful payload from the subscription stream.
+/// - A notice that the subscription has been lagged behind the network with the
+///   number of lost payloads.
+#[derive(Union, Clone)]
+#[graphql(concrete(name = "EventSubscriptionPayload", params(Event)))]
+#[graphql(concrete(name = "TransactionBlockSubscriptionPayload", params(TransactionBlock)))]
+pub(crate) enum SubscriptionItem<T: OutputType> {
+    /// Successfully received payload from the subscription stream.
+    Payload(T),
+    /// A notice that the subscription has been lagged behind the network.
+    Lagged(Lagged),
+}
+
+/// Subscribe to events and transactions from the IOTA network.
+pub struct Subscription;
+
+#[Subscription]
+impl Subscription {
+    /// Subscribe to incoming transactions from the IOTA network.
+    ///
+    /// If no filter is provided, all transactions will be returned.
+    async fn transactions(
+        &self,
+        ctx: &Context<'_>,
+        filter: Option<SubscriptionTransactionFilter>,
+    ) -> impl Stream<Item = Result<SubscriptionItem<TransactionBlock>, Error>> {
+        let streams = ctx.data_unchecked::<GraphQLStream>().clone();
+        streams.subscribe_transactions(filter.map(Into::<StreamTransactionFilter>::into))
+    }
+
+    /// Subscribe to incoming events from the IOTA network.
+    ///
+    /// If no filter is provided, all events will be returned.
+    async fn events(
+        &self,
+        ctx: &Context<'_>,
+        filter: Option<SubscriptionEventFilter>,
+    ) -> impl Stream<Item = Result<SubscriptionItem<Event>, Error>> {
+        let streams = ctx.data_unchecked::<GraphQLStream>().clone();
+        streams.subscribe_events(filter.map(Into::<StreamEventFilter>::into))
+    }
+}
+
+/// Provides real-time data streams for the GraphQL subscription feature.
+///
+/// It wraps the low-level [`IndexerStreamer`] and handles necessary
+/// data processing, filtering, and subscription-specific error handling before
+/// yielding items to GraphQL.
+///
+/// It ensures that when a critical data error occurs during item conversion,
+/// the resulting stream is gracefully terminated by the server.
+#[derive(Clone)]
+pub(crate) struct GraphQLStream {
+    streamer: Arc<IndexerStreamer>,
+}
+
+impl GraphQLStream {
+    pub(crate) async fn new(db_url: &str, indexer_reader: IndexerReader) -> Result<Self, Error> {
+        let streamer = IndexerStreamer::new(db_url, indexer_reader)
+            .await
+            .map_err(|e| Error::Internal(format!("failed to connect to postgres: {e}")))?;
+        Ok(Self {
+            streamer: Arc::new(streamer),
+        })
+    }
+
+    /// Checks if the provided filter matches the item.
+    ///
+    /// If no filter is provided, the item is **always** considered a match, and
+    /// the function returns `true`.
+    fn matches_filter<T, F>(filter: Option<&F>, item: &T) -> bool
+    where
+        F: Filter<T>,
+    {
+        filter.as_ref().map(|f| f.matches(item)).unwrap_or(true)
+    }
+
+    /// Subscribe to transactions from IOTA Network.
+    pub(crate) fn subscribe_transactions(
+        &self,
+        filter: Option<StreamTransactionFilter>,
+    ) -> impl Stream<Item = Result<SubscriptionItem<TransactionBlock>, Error>> {
+        self.streamer
+            .subscribe_transactions()
+            .try_filter(move |stored| future::ready(Self::matches_filter(filter.as_ref(), stored)))
+            .then(|stored| {
+                let subscription_item = match stored {
+                    Ok(stored) => {
+                        let checkpoint_viewed_at = stored.checkpoint_sequence_number as u64;
+                        TransactionBlockInner::try_from(stored).map(|inner| {
+                            SubscriptionItem::Payload(TransactionBlock {
+                                inner,
+                                checkpoint_viewed_at,
+                            })
+                        })
+                    }
+                    Err(BroadcastStreamRecvError::Lagged(count)) => {
+                        warn!("subscriber lagging by {count} messages");
+                        Ok(SubscriptionItem::Lagged(Lagged { count }))
+                    }
+                };
+                future::ready(subscription_item)
+            })
+            // intercept the error, send it to the client and terminate the stream.
+            .scan(false, |should_terminate_stream, subscription_item| {
+                if *should_terminate_stream {
+                    return future::ready(None);
+                }
+                future::ready(Some(
+                    subscription_item.inspect_err(|_| *should_terminate_stream = true),
+                ))
+            })
+    }
+
+    /// Subscribe to events from IOTA Network.
+    pub(crate) fn subscribe_events(
+        &self,
+        filter: Option<StreamEventFilter>,
+    ) -> impl Stream<Item = Result<SubscriptionItem<Event>, Error>> {
+        self.streamer
+            .subscribe_events()
+            .try_filter(move |stored| future::ready(Self::matches_filter(filter.as_ref(), stored)))
+            .then(|stored| {
+                let subscription_item = match stored {
+                    Ok(stored) => {
+                        Event::try_from_stored_event(stored, 0).map(SubscriptionItem::Payload)
+                    }
+                    Err(BroadcastStreamRecvError::Lagged(count)) => {
+                        warn!("subscriber lagging by {count} messages");
+                        Ok(SubscriptionItem::Lagged(Lagged { count }))
+                    }
+                };
+                future::ready(subscription_item)
+            })
+            // intercept the error, send it to the client and terminate the stream.
+            .scan(false, |should_terminate_stream, subscription_item| {
+                if *should_terminate_stream {
+                    return future::ready(None);
+                }
+                future::ready(Some(
+                    subscription_item.inspect_err(|_| *should_terminate_stream = true),
+                ))
+            })
+    }
+}
