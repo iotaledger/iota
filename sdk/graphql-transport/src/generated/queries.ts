e745b9ec58b2a5fee8936d7d3ec55515aba6daf8
Piotr Macek <4007944+piotrm50@users.noreply.github.com>
2025-10-08 11:24:22 +0200

    fix(sdk): Regenerate autogenerated GraphQL files.  (#8793)
    # Description of change
    
    Regenerate autogenerated GraphQL files. 
    
    ## Links to any relevant issues
    
    Partly resolves https://github.com/iotaledger/iota/issues/7814
    
    ## How the change has been tested
    
    - [ ] Basic tests (linting, compilation, formatting, unit/integration
    tests)
    - [ ] Patch-specific tests (correctness, functionality coverage)
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have checked that new and existing unit tests pass locally with
    my changes
    
    ---------
    
    Co-authored-by: Mario Sarcevic <mario.sarcevic@iota.org>

--- sdk/graphql-transport/src/generated/queries.ts
+++ sdk/graphql-transport/src/generated/queries.ts
@@ -551,6 +551,11 @@ export type ChangeEpochTransactionV2 = {
    * epoch (in NANOS).
    */
   computationChargeBurned: Scalars['BigInt']['output'];
+  /**
+   * The list of active validators eligible for committee selection for the
+   * next epoch.
+   */
+  eligibleActiveValidators?: Maybe<Array<Scalars['BigInt']['output']>>;
   /** The next (to become) epoch. */
   epoch?: Maybe<Epoch>;
   /**
@@ -1498,7 +1503,12 @@ export type Epoch = {
    * epoch.
    */
   transactionBlocks: TransactionBlockConnection;
-  /** Validator related properties, including the active validators. */
+  /**
+   * Validator related properties, including the active validators.
+   *
+   * For epochs other than the current the data provided refer to the start
+   * of the epoch.
+   */
   validatorSet?: Maybe<ValidatorSet>;
 };
 
@@ -5562,14 +5572,17 @@ export type Validator = {
   operationCap?: Maybe<MoveObject>;
   /**
    * Pending pool token withdrawn during the current epoch, emptied at epoch
-   * boundaries.
+   * boundaries. Zero for past epochs.
    */
   pendingPoolTokenWithdraw?: Maybe<Scalars['BigInt']['output']>;
-  /** Pending stake amount for this epoch. */
+  /**
+   * Pending stake amount for the current epoch, emptied at epoch boundaries.
+   * Zero for past epochs.
+   */
   pendingStake?: Maybe<Scalars['BigInt']['output']>;
   /**
    * Pending stake withdrawn during the current epoch, emptied at epoch
-   * boundaries.
+   * boundaries. Zero for past epochs.
    */
   pendingTotalIotaWithdraw?: Maybe<Scalars['BigInt']['output']>;
   /** Total number of pool tokens issued by the pool. */
