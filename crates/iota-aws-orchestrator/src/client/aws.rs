2fa426be6c3e9220fe0550869f2c74f13a0cee16
Filip Dulic <filip.dulic@gmail.com>
2025-10-17 16:10:32 +0200

    feat: update image to 24.04, fix dependancies

--- crates/iota-aws-orchestrator/src/client/aws.rs
+++ crates/iota-aws-orchestrator/src/client/aws.rs
@@ -52,7 +52,7 @@ impl Display for AwsClient {
 
 impl AwsClient {
     const UBUNTU_NAME_PATTERN: &'static str =
-        "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*";
+        "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*";
     const CANONICAL_OWNER_ID: &'static str = "099720109477";
 
     /// Make a new AWS client.
@@ -142,7 +142,7 @@ impl AwsClient {
     async fn find_image_id(&self, client: &aws_sdk_ec2::Client) -> CloudProviderResult<String> {
         // Use a more general filter that doesn't depend on specific build dates
         let filters = [
-            // Filter for Ubuntu 22.04 LTS
+            // Filter for Ubuntu 24.04 LTS
             FilterBuilder::default()
                 .name("name")
                 .values(Self::UBUNTU_NAME_PATTERN)
@@ -174,7 +174,7 @@ impl AwsClient {
         // Select the newest image
         let image = images
             .first()
-            .ok_or_else(|| CloudProviderError::Request("Cannot find Ubuntu 22.04 image".into()))?;
+            .ok_or_else(|| CloudProviderError::Request("Cannot find Ubuntu 24.04 image".into()))?;
 
         image
             .image_id
