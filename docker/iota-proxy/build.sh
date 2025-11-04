8da4e205c6bd1ab2db453787b46f3c3771dcd4ba
Roman Overko <roman.overko@iota.org>
2025-11-04 18:37:24 +0100

    fix: determine docker build script location to resolve relative paths
    Signed-off-by: Roman Overko <roman.overko@iota.org>

--- docker/iota-proxy/build.sh
+++ docker/iota-proxy/build.sh
@@ -2,4 +2,9 @@
 # Copyright (c) Mysten Labs, Inc.
 # Modifications Copyright (c) 2024 IOTA Stiftung
 # SPDX-License-Identifier: Apache-2.0
+
+# Determine script's location to resolve the relative path correctly
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE}")" >/dev/null && pwd -P)
+cd "$SCRIPT_DIR"
+
 ./../utils/build-script.sh --image-tag "iotaledger/iota-proxy"
--- docker/iota-rest-kv/build.sh
+++ docker/iota-rest-kv/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Copyright (c) 2025 IOTA Stiftung
 # SPDX-License-Identifier: Apache-2.0
+
+# Determine script's location to resolve the relative path correctly
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE}")" >/dev/null && pwd -P)
+cd "$SCRIPT_DIR"
+
 ./../utils/build-script.sh --image-tag "iotaledger/iota-rest-kv"
