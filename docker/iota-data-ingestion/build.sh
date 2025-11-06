a64ce46dc6cfb941e0557db858319e0d7cc71fa9
Roman Overko <roman.overko@iota.org>
2025-11-06 18:22:57 +0100

    fix: add [0] to BASH_SOURCE and wrap script dir in quotes
    Signed-off-by: Roman Overko <roman.overko@iota.org>

--- docker/iota-data-ingestion/build.sh
+++ docker/iota-data-ingestion/build.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Determine script's location to resolve the relative path correctly
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE}")" >/dev/null && pwd -P)
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd -P)"
 cd "$SCRIPT_DIR"
 
 ./../utils/build-script.sh --image-tag "iotaledger/iota-data-ingestion"
--- docker/iota-graphql-rpc/build.sh
+++ docker/iota-graphql-rpc/build.sh
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Determine script's location to resolve the relative path correctly
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE}")" >/dev/null && pwd -P)
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd -P)"
 cd "$SCRIPT_DIR"
 
 ./../utils/build-script.sh --image-tag "iotaledger/iota-graphql-rpc"
