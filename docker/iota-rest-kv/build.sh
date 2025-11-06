2447a89452e1989cc2c253db93e6d087f5ffa9d8
Roman Overko <roman.overko@iota.org>
2025-11-06 19:43:16 +0100

    feat: avoid using cd in docker build scripts
    Signed-off-by: Roman Overko <roman.overko@iota.org>

--- docker/iota-rest-kv/build.sh
+++ docker/iota-rest-kv/build.sh
@@ -4,6 +4,7 @@
 
 # Determine script's location to resolve the relative path correctly
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd -P)"
-cd "$SCRIPT_DIR"
+# The name of last directory in script's path is used for tag
+BASE_NAME="$(basename "$SCRIPT_DIR")"
 
-./../utils/build-script.sh --image-tag "iotaledger/iota-rest-kv"
+"$SCRIPT_DIR/../utils/build-script.sh" --image-tag "iotaledger/$BASE_NAME"
