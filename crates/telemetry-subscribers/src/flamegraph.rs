75fdacc4af211fd695b9a5a6621978f6c9285c4f
Uladzislau Siamionau <vlad.semenov@iota.org>
2025-11-06 07:55:40 +0100

    feat: add grafana flamegraph feature to telemetry-subscribers

---
+++ crates/telemetry-subscribers/src/flamegraph.rs
@@ -0,0 +1,10 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+mod arena;
+mod callgraph;
+mod flame;
+mod grafana;
+mod metric;
+mod tracing;
+pub use tracing::FlameSub;
