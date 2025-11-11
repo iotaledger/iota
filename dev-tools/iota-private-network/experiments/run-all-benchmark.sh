3766fe60cac2a9fa1a026358426245e0827e43a5
NaitsabesMue <51112618+NaitsabesMue@users.noreply.github.com>
2025-11-11 20:25:51 +0100

    fix: typo

--- dev-tools/iota-private-network/experiments/run-all-benchmark.sh
+++ dev-tools/iota-private-network/experiments/run-all-benchmark.sh
@@ -293,7 +293,7 @@ log "Grafana URL: http://localhost:3000/dashboards"
 cd - >/dev/null
 
 # --- 5) Launch combined latency + fuzz watcher in background ---
-./network-benchmark \
+./network-benchmark.sh \
     -n "$NUM_VALIDATORS" \
     -s "$SEED" \
     -b "$PERCENT_BLOCK" \
