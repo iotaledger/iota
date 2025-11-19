a0d35b4efaac16252941f5efc9cd7e3fb7d0c9f2
oliviasaa <olivia.saa@iota.org>
2025-11-19 16:40:14 +0000

    feat(iota-types, iota-execution): Introduction of new basic types (ChangeEpochTransactionV4) (#9159)
    ```mermaid
    ---
    config:
      gitGraph:
        {parallelCommits: true,
        mainBranchName: 'Feature',
        rotateCommitLabel: true}
    ---
    gitGraph
       commit id: "Initial commit"
       commit id: "Old Scorer" tag: "PR8530"
       checkout Feature
          branch new-types
       commit id: "add ChangeEpochV4"
       checkout Feature
        branch misbehavior-reports
       commit id: "add MisbehaviorReports"
        checkout misbehavior-reports
        branch scoring-metrics
       commit id: "add ScoringMetrics"
        checkout scoring-metrics
       branch scorer
       commit id: "add Scorer"
        checkout scorer
       branch scoring-metrics-store
       commit id: "add ScoringMetricsStore"
     checkout Feature
        merge new-types tag: "THIS PR"
    ```
    # Description of change
    
    This PR introduces: 
    - a new variant of `EndOfEpochTransactionKind` which includes the
    `scores` field. This transaction is used when score based rewards are
    enabled (i.e., whenever the newly added feature flag
    `score_based_rewards` is set to true).
    - a placeholder for the new `advance_epoch_v4` function, currently with
    the same logic as `advance_epoch_v3`
    - related changes in `iota-graphql-rpc` crate, as the
    `ChangeEpochTransactionV4` type, and a new variant of
    `IotaEndOfEpochTransactionKind`.
    
    Note: the changes in the PR are built on top of commit
    [c7eeee2](https://github.com/iotaledger/iota-rust-sdk/tree/feat/add-scores)
    of `iota-rust-sdk`, which is currently not even merged to `develop`.
    Thus, any changes present in this PR should be merged to `iota`'s
    `develop` branch together with the changes in `iota-rust-sdk` repo.
    
    ## How the change has been tested
    
    - [x] Basic tests (linting, compilation, formatting, unit/integration
    tests)
    - [ ] Patch-specific tests (correctness, functionality coverage)
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] I have checked that new and existing unit tests pass locally with
    my changes
    
    ### Release Notes
    
    - [ ] Protocol:
    - [x] Nodes (Validators and Full nodes): Introduce type
    ChangeEpochTransactionV4
    - [ ] Indexer:
    - [ ] JSON-RPC:
    - [ ] GraphQL:
    - [ ] CLI:
    - [ ] Rust SDK:
    - [ ] REST API:
    
    ---------
    
    Co-authored-by: Konstantinos Demartinos <konstantinos.demartinos@iota.org>

--- crates/iota-types/src/iota_sdk_types_conversions.rs
+++ crates/iota-types/src/iota_sdk_types_conversions.rs
@@ -13,7 +13,7 @@
 use fastcrypto::traits::ToFromBytes;
 use iota_sdk2::types::{
     object::{MovePackage, MoveStruct},
-    transaction::{ChangeEpoch, ChangeEpochV2, ChangeEpochV3},
+    transaction::{ChangeEpoch, ChangeEpochV2, ChangeEpochV3, ChangeEpochV4},
     *,
 };
 use move_core_types::language_storage::ModuleId;
@@ -643,6 +643,30 @@ impl From<crate::transaction::EndOfEpochTransactionKind> for EndOfEpochTransacti
                     eligible_active_validators: change_epoch_v3.eligible_active_validators,
                 })
             }
+
+            crate::transaction::EndOfEpochTransactionKind::ChangeEpochV4(change_epoch_v4) => {
+                EndOfEpochTransactionKind::ChangeEpochV4(ChangeEpochV4 {
+                    epoch: change_epoch_v4.epoch,
+                    protocol_version: change_epoch_v4.protocol_version.as_u64(),
+                    storage_charge: change_epoch_v4.storage_charge,
+                    computation_charge: change_epoch_v4.computation_charge,
+                    computation_charge_burned: change_epoch_v4.computation_charge_burned,
+                    storage_rebate: change_epoch_v4.storage_rebate,
+                    non_refundable_storage_fee: change_epoch_v4.non_refundable_storage_fee,
+                    epoch_start_timestamp_ms: change_epoch_v4.epoch_start_timestamp_ms,
+                    system_packages: change_epoch_v4
+                        .system_packages
+                        .into_iter()
+                        .map(|(version, modules, dependencies)| SystemPackage {
+                            version: version.value(),
+                            modules,
+                            dependencies: dependencies.into_iter().map(Into::into).collect(),
+                        })
+                        .collect(),
+                    eligible_active_validators: change_epoch_v4.eligible_active_validators,
+                    scores: change_epoch_v4.scores,
+                })
+            }
             crate::transaction::EndOfEpochTransactionKind::AuthenticatorStateCreate => {
                 EndOfEpochTransactionKind::AuthenticatorStateCreate
             }
@@ -728,6 +752,31 @@ impl From<EndOfEpochTransactionKind> for crate::transaction::EndOfEpochTransacti
                         })
                         .collect(),
                     eligible_active_validators: change_epoch_v3.eligible_active_validators,
+                })
+            }
+            EndOfEpochTransactionKind::ChangeEpochV4(change_epoch_v4) => {
+                Self::ChangeEpochV4(crate::transaction::ChangeEpochV4 {
+                    epoch: change_epoch_v4.epoch,
+                    protocol_version: change_epoch_v4.protocol_version.into(),
+                    storage_charge: change_epoch_v4.storage_charge,
+                    computation_charge: change_epoch_v4.computation_charge,
+                    computation_charge_burned: change_epoch_v4.computation_charge_burned,
+                    storage_rebate: change_epoch_v4.storage_rebate,
+                    non_refundable_storage_fee: change_epoch_v4.non_refundable_storage_fee,
+                    epoch_start_timestamp_ms: change_epoch_v4.epoch_start_timestamp_ms,
+                    system_packages: change_epoch_v4
+                        .system_packages
+                        .into_iter()
+                        .map(|package| {
+                            (
+                                package.version.into(),
+                                package.modules,
+                                package.dependencies.into_iter().map(Into::into).collect(),
+                            )
+                        })
+                        .collect(),
+                    eligible_active_validators: change_epoch_v4.eligible_active_validators,
+                    scores: change_epoch_v4.scores,
                 })
             }
             EndOfEpochTransactionKind::AuthenticatorStateCreate => Self::AuthenticatorStateCreate,
