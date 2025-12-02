2602b2fa3e1279d7642477ce9a27785f92fa1a57
ilyavorobev <vorobyev.i.v@yandex.ru>
2025-12-02 11:08:21 +0100

    test

--- crates/starfish/core/src/commit.rs
+++ crates/starfish/core/src/commit.rs
@@ -15,6 +15,7 @@ use bytes::Bytes;
 use enum_dispatch::enum_dispatch;
 use fastcrypto::hash::{Digest, HashFunction as _};
 use serde::{Deserialize, Serialize};
+use tracing::info;
 use starfish_config::{AuthorityIndex, DIGEST_LENGTH, DefaultHashFunction};
 
 use crate::{
@@ -74,6 +75,7 @@ impl Commit {
         committed_transactions: Vec<GenericTransactionRef>,
     ) -> Self {
         if context.protocol_config.consensus_transaction_ref() {
+            info!("Creating CommitV2 as consensus_transaction_ref is enabled");
             // Extract TransactionRefs from GenericTransactionRef
             let transaction_refs: Vec<TransactionRef> = committed_transactions
                 .into_iter()
@@ -93,6 +95,7 @@ impl Commit {
                 committed_transactions: transaction_refs,
             })
         } else {
+            info!("Creating CommitV1 as consensus_transaction_ref is disabled");
             // Extract BlockRefs from GenericTransactionRef
             let block_refs: Vec<BlockRef> = committed_transactions
                 .into_iter()
