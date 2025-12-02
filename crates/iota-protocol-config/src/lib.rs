0080b31543a817c3fdfc4d360896041a6cc8acc7
Mirko Zichichi <mirko.zichichi@iota.org>
2025-12-02 13:58:59 +0100

    feat(move): Add custom attributes (#9453)
    # Description of change
    This PR cherry picks 2 commits in common to enable authenticator and
    move view attributes at the move level.
    Changes:
    1. Added a generic FlavoredAttribute which can then allow us to define
    new attributes within the iota_mode compiler part.
    2. Introduces a new protocol feature flag (iota_metadata_module_bytes)
    to allow the insertion of metadata in the serialized compiled modules
    (bytecode). This kind of operation was disabled before this PR, with the
    flag no_extraneous_module_bytes. Instead, iota_metadata_module_bytes
    allows to fill this metadata field during the execution of
    build_from_resolution_graph. At the module deserializer level, the only
    performed check is that metadata have only 1 field and the metadata key
    is hardcoded. During the execution of the iota-verifier, the metadata
    value is checked for having well formed metadata struct (later
    authenticate and view functions checks will be performed here).
    
    ### Release Notes
    
    - [x] Protocol: Added metadata_in_module_bytes flag to allow the
    insertion of IOTA specific metadata in the serialized compiled modules
    (bytecode).
    - [ ] Nodes (Validators and Full nodes):
    - [ ] Indexer:
    - [ ] JSON-RPC:
    - [ ] GraphQL:
    - [ ] CLI:
    - [ ] Rust SDK:
    - [ ] REST API:

--- crates/iota-protocol-config/src/lib.rs
+++ crates/iota-protocol-config/src/lib.rs
@@ -19,7 +19,7 @@ use tracing::{info, warn};
 
 /// The minimum and maximum protocol versions supported by this build.
 const MIN_PROTOCOL_VERSION: u64 = 1;
-pub const MAX_PROTOCOL_VERSION: u64 = 16;
+pub const MAX_PROTOCOL_VERSION: u64 = 17;
 
 // Record history of protocol version allocations here:
 //
@@ -91,6 +91,8 @@ pub const MAX_PROTOCOL_VERSION: u64 = 16;
 //             AuthorityCapabilities notification.
 //             Enable committing transactions only for traversed headers in
 //             Starfish.
+// Version 17: Allow metadata bytes indexed with a dedicated key in compiled
+//             Move modules in devnet.
 #[derive(Copy, Clone, Debug, Hash, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ProtocolVersion(u64);
 
@@ -372,6 +374,11 @@ struct FeatureFlags {
     // If true, then transactions are committed only for traversed headers
     #[serde(skip_serializing_if = "is_false")]
     consensus_commit_transactions_only_for_traversed_headers: bool,
+
+    // If true, it allows metadata bytes indexed with a dedicated key in a compiled module.
+    // This flag is used to provide the correct MoveVM configuration for clients.
+    #[serde(skip_serializing_if = "is_false")]
+    metadata_in_module_bytes: bool,
 }
 
 fn is_true(b: &bool) -> bool {
@@ -1441,9 +1448,14 @@ impl ProtocolConfig {
         );
         res
     }
+
     pub fn consensus_commit_transactions_only_for_traversed_headers(&self) -> bool {
         self.feature_flags
             .consensus_commit_transactions_only_for_traversed_headers
+    }
+
+    pub fn metadata_in_module_bytes(&self) -> bool {
+        self.feature_flags.metadata_in_module_bytes
     }
 }
 
@@ -2314,6 +2326,11 @@ impl ProtocolConfig {
                     // Enable committing transactions only for traversed headers in Starfish
                     cfg.feature_flags
                         .consensus_commit_transactions_only_for_traversed_headers = true;
+                }
+                17 => {
+                    if chain != Chain::Mainnet && chain != Chain::Testnet {
+                        cfg.feature_flags.metadata_in_module_bytes = true;
+                    }
                 }
                 // Use this template when making changes:
                 //
