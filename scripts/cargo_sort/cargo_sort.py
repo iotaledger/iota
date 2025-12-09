7e00f265dae6ab3cc42143e1b9fe8213c30b160d
muXxer <git@muxxer.de>
2025-12-09 13:48:02 +0100

    feat(scripts): add `iota-sdk-transaction-builder` to `cargo_sort.py`

--- scripts/cargo_sort/cargo_sort.py
+++ scripts/cargo_sort/cargo_sort.py
@@ -348,6 +348,7 @@ if __name__ == '__main__':
     
     # add special cases
     internal_crates_dict["iota-sdk-types"] = None
+    internal_crates_dict["iota-sdk-transaction-builder"] = None
 
     # ignored folders
     ignored_folders = []
