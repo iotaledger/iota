babf38c9d3ecf390ecbbb8256c5d846cd75d7478
muXxer <git@muxxer.de>
2025-12-15 20:06:09 +0100

    fix(stress): increase `BENCHMARK_GAS_AMOUNT` for higher workloads

--- crates/iota-swarm-config/src/genesis_config.rs
+++ crates/iota-swarm-config/src/genesis_config.rs
@@ -300,7 +300,7 @@ impl GenesisConfig {
     /// Port offset for benchmarks' genesis configs.
     pub const BENCHMARKS_PORT_OFFSET: u16 = 2000;
     /// The gas amount for each genesis gas object.
-    const BENCHMARK_GAS_AMOUNT: u64 = 50_000_000_000_000_000;
+    const BENCHMARK_GAS_AMOUNT: u64 = 1_000_000_000_000_000_000;
     /// Trigger epoch change every hour minutes.
     const BENCHMARK_EPOCH_DURATION_MS: u64 = 60 * 60 * 1000;
 
