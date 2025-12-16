b71cac69584347c7342d90893dfb4d8bf6f8a351
muXxer <git@muxxer.de>
2025-12-16 11:54:19 +0100

    fix(aws): set network buffer sizes on all instances

--- crates/iota-aws-orchestrator/src/orchestrator.rs
+++ crates/iota-aws-orchestrator/src/orchestrator.rs
@@ -235,6 +235,11 @@ impl<P: ProtocolCommands<T> + ProtocolMetrics, T: BenchmarkType> Orchestrator<P,
             "sudo sysctl -p",
             // Set limits for current session
             "ulimit -n 1048576 || true",
+            // set network buffer sizes
+            "sudo sysctl -w net.core.rmem_max=104857600",
+            "sudo sysctl -w net.core.wmem_max=104857600",
+            "sudo sysctl -w net.ipv4.tcp_rmem=\"8192 262144 104857600\"",
+            "sudo sysctl -w net.ipv4.tcp_wmem=\"8192 262144 104857600\"",
             // Create the working directory.
             working_dir_cmd.as_str(),
             // Clone the repo.
