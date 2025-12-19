c2b902d058da658d0d6083e2940238efecce0df5
Panteleymonchuk <panteleymonchuk@gmail.com>
2025-12-19 12:37:11 +0200

    fix(explorer): update link color for disclaimer

--- apps/explorer/src/index.tsx
+++ apps/explorer/src/index.tsx
@@ -42,7 +42,13 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                             agree with our{' '}
                             {LEGAL_LINKS.map(({ title, href }, index) => (
                                 <React.Fragment key={title}>
-                                    <Link href={href}>{title}</Link>
+                                    <Link
+                                        className="text-iota-primary-30 dark:text-iota-primary-80"
+                                        variant="mono"
+                                        href={href}
+                                    >
+                                        {title}
+                                    </Link>
                                     {index < LEGAL_LINKS.length - 1 ? ', ' : ''}
                                 </React.Fragment>
                             ))}
