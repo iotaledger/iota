ad529a2bfdd93ddc8e49891aa039f489b34d9805
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2026-01-05 21:36:49 -0300

    feat(explorer): implement identity wasm web initialization fn

---
+++ apps/explorer/src/lib/utils/trust-framework/identity.ts
@@ -0,0 +1,23 @@
+// Copyright (c) Mysten Labs, Inc.
+// Modifications Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import * as identity from '@iota/identity-wasm/web';
+
+let initPromise: Promise<void> | null = null;
+
+/**
+ * Idempotent initialization of WASM module of Identity.
+ *
+ * Use it everytime you need to call any identity API.
+ */
+export const initIdentityWasmWeb = async (): Promise<void> => {
+    if (!initPromise) {
+        initPromise = identity.init().catch((e) => {
+            console.error('failed to load identity wasm (web version)', e);
+            initPromise = null; // allow retry
+            throw e;
+        });
+    }
+    return initPromise;
+};
