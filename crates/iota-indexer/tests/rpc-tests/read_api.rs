2f6fe03e57f95eed7bb8b933674042edb8eeb9f2
Chloe Martin <chloedaughterofmars@gmail.com>
2026-01-20 21:55:06 +0100

    fix test

--- crates/iota-indexer/tests/rpc-tests/read_api.rs
+++ crates/iota-indexer/tests/rpc-tests/read_api.rs
@@ -1953,6 +1953,10 @@ async fn failed_stored_tx_into_transaction_block() {
             .await
             .is_ok()
     );
+    // We have to drop the package resolver before dropping the test db because it
+    // holds a reference to the db pool and the active connection will prevent the
+    // db from being dropped.
+    drop(package_resolver);
     test_db.drop_if_exists();
 }
 
