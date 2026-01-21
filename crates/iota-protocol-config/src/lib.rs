875cb032bd5a5b5bf3d4b069bca5ab5242220833
Piotr Macek <4007944+piotrm50@users.noreply.github.com>
2026-01-21 22:19:43 +0100

    Fix protocol config and regenrate snapshots and related files.

--- crates/iota-protocol-config/src/lib.rs
+++ crates/iota-protocol-config/src/lib.rs
@@ -19,7 +19,7 @@ use tracing::{info, warn};
 
 /// The minimum and maximum protocol versions supported by this build.
 const MIN_PROTOCOL_VERSION: u64 = 1;
-pub const MAX_PROTOCOL_VERSION: u64 = 19;
+pub const MAX_PROTOCOL_VERSION: u64 = 20;
 
 // Record history of protocol version allocations here:
 //
