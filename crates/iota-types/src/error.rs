52017da5cff59e774d739b5ba11f585ca2dd1249
miker83z <mirko.zichichi@iota.org>
2026-01-22 17:23:07 +0100

    Revert "fix(core): tx input loader rejects receiving objects owned by AA accounts"
    This reverts commit 5b3798071cfbfd24dcbdad661580905a2efeb980.

--- crates/iota-types/src/error.rs
+++ crates/iota-types/src/error.rs
@@ -381,13 +381,6 @@ pub enum UserInputError {
         "Mutable shared object {object_id:?} is in the `MoveAuthenticator` input that is unsupported"
     )]
     MutableSharedIsInMoveAuthenticatorInput { object_id: ObjectID },
-    #[error(
-        "Receiving object {receiving_object_id:?} is not allowed for Account Object {account_object_id:?}"
-    )]
-    ReceivingObjectForAccountObject {
-        receiving_object_id: ObjectID,
-        account_object_id: ObjectID,
-    },
 }
 
 #[derive(
