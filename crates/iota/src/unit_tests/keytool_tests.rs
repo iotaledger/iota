49a321eab63532f6923c9b2d8f6bbbb8738644df
Thoralf Müller <thoralf.mue@gmail.com>
2026-01-22 14:43:55 +0100

    rename var

--- crates/iota/src/unit_tests/keytool_tests.rs
+++ crates/iota/src/unit_tests/keytool_tests.rs
@@ -636,8 +636,8 @@ async fn test_sign_command() -> Result<(), anyhow::Error> {
 async fn test_sign_raw_command() -> Result<(), anyhow::Error> {
     // Add a keypair
     let mut keystore = Keystore::from(InMemKeystore::new_insecure_for_tests(1));
-    let binding = keystore.addresses();
-    let sender = binding.first().unwrap();
+    let addresses = keystore.addresses();
+    let sender = addresses.first().unwrap();
     let alias = keystore.get_alias_by_address(sender).unwrap();
 
     let raw_data = Hex::encode_with_format("IOTA");
