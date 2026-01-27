9e2ff314a973fd9f1d2c5a0aa584407473a768a4
Chloe Martin <chloedaughterofmars@gmail.com>
2026-01-27 11:51:40 +0100

    chore: Use Display impls in error implementations instead of Debug

--- crates/iota-types/src/error.rs
+++ crates/iota-types/src/error.rs
@@ -267,7 +267,7 @@ pub enum UserInputError {
     #[error("Immutable parameter provided, mutable parameter expected")]
     MutableParameterExpected { object_id: ObjectID },
 
-    #[error("Address {address:?} is denied for coin {coin_type}")]
+    #[error("Address {address} is denied for coin {coin_type}")]
     AddressDeniedForCoin {
         address: IotaAddress,
         coin_type: String,
@@ -357,10 +357,10 @@ pub enum IotaObjectResponseError {
     Eq, PartialEq, Clone, Debug, Serialize, Deserialize, Error, Hash, AsRefStr, IntoStaticStr,
 )]
 pub enum IotaError {
-    #[error("Error checking transaction input objects: {:?}", error)]
+    #[error("Error checking transaction input objects: {}", error)]
     UserInput { error: UserInputError },
 
-    #[error("Error checking transaction object: {:?}", error)]
+    #[error("Error checking transaction object: {}", error)]
     IotaObjectResponse { error: IotaObjectResponseError },
 
     #[error("Expecting a single owner, shared ownership found")]
