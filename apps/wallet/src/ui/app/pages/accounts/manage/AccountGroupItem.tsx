b23116622b22e534af82f31c3d19ec2adaa895c3
evavirseda <evirseda@boxfish.studio>
2026-01-28 12:59:06 +0100

    fix format

--- apps/wallet/src/ui/app/pages/accounts/manage/AccountGroupItem.tsx
+++ apps/wallet/src/ui/app/pages/accounts/manage/AccountGroupItem.tsx
@@ -152,7 +152,7 @@ export function AccountGroupItem({
                             top: dropdownPosition.y,
                         }}
                         className={clsx(
-                            `dark:bg-iota-neutral-6 absolute right-0 z-[99] rounded-lg bg-iota-neutral-100 shadow-md`,
+                            `absolute right-0 z-[99] rounded-lg bg-iota-neutral-100 shadow-md dark:bg-iota-neutral-6`,
                             showDropdownOptionsBottom ? '-translate-y-full' : '',
                         )}
                     >
@@ -202,7 +202,7 @@ function AccountAvatar({ account }: { account: SerializedUIAccount }) {
     }
     return (
         <div
-            className={`flex h-8 w-8 items-center justify-center rounded-full [&_svg]:h-5 [&_svg]:w-5 [&_svg]:text-iota-neutral-100 ${account.isLocked ? 'dark:bg-iota-neutral-20 [&_svg]:dark:text-iota-neutral-50 bg-iota-neutral-90' : 'bg-iota-primary-30 '}`}
+            className={`flex h-8 w-8 items-center justify-center rounded-full [&_svg]:h-5 [&_svg]:w-5 [&_svg]:text-iota-neutral-100 ${account.isLocked ? 'bg-iota-neutral-90 dark:bg-iota-neutral-20 [&_svg]:dark:text-iota-neutral-50' : 'bg-iota-primary-30 '}`}
         >
             {logo}
         </div>
