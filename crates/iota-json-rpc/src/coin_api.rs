1ac27760cfa14d7c7722ff4be556051291a48e19
Chloe Martin <chloedaughterofmars@gmail.com>
2026-01-29 13:57:19 +0100

    one more

--- crates/iota-json-rpc/src/coin_api.rs
+++ crates/iota-json-rpc/src/coin_api.rs
@@ -697,7 +697,7 @@ mod tests {
         let object_id = if let Some(literal) = id_hex_literal {
             ObjectID::from_prefixed_short_hex(literal).unwrap()
         } else {
-            ObjectID::from_short_hex(default_hex).unwrap()
+            ObjectID::from_prefixed_short_hex(default_hex).unwrap()
         };
 
         Coin {
