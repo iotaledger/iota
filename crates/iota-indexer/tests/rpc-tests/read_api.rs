71c48714671b00a76fdec8e6d61421fbe0d2d569
Chloe Martin <chloedaughterofmars@gmail.com>
2026-02-02 18:55:27 +0100

    fix another test

--- crates/iota-indexer/tests/rpc-tests/read_api.rs
+++ crates/iota-indexer/tests/rpc-tests/read_api.rs
@@ -372,7 +372,7 @@ fn get_checkpoint_by_digest_not_found() {
 
         assert!(rpc_call_error_msg_matches(
             result,
-            r#"{"code":-32603,"message":"Invalid argument with error: `Checkpoint Digest(CheckpointDigest(11111111111111111111111111111111)) not found`"}"#,
+            r#"{"code":-32603,"message":"Invalid argument with error: `Checkpoint Digest(Digest(\"11111111111111111111111111111111\")) not found`"}"#,
         ));
     });
 }
