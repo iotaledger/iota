a23c1662893722016013eb9564d694d14d682cbc
/alex/ <alexander.schmidt@iota.org>
2026-02-02 16:59:24 +0100

    convert between sdk and rpc type

--- crates/iota-json-rpc-types/src/balance_changes.rs
+++ crates/iota-json-rpc-types/src/balance_changes.rs
@@ -2,6 +2,7 @@
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use core::str::FromStr;
 use std::fmt::{Display, Formatter, Result};
 
 use iota_types::{iota_serde::IotaTypeTag, object::Owner};
@@ -34,5 +35,43 @@ impl Display for BalanceChange {
             " ┌──\n │ Owner: {} \n │ CoinType: {} \n │ Amount: {}\n └──",
             self.owner, self.coin_type, self.amount
         )
+    }
+}
+
+pub fn from_sdk_balance_change(
+    balance_change: iota_sdk_types::BalanceChange,
+    is_object_owner: bool,
+) -> BalanceChange {
+    BalanceChange {
+        owner: if is_object_owner {
+            Owner::ObjectOwner(balance_change.address)
+        } else {
+            Owner::AddressOwner(balance_change.address)
+        },
+        // TODO use SDK TypeTag
+        coin_type: TypeTag::from_str(&balance_change.coin_type.to_canonical_string(false)).unwrap(),
+        amount: balance_change.amount,
+    }
+}
+
+pub fn try_into_sdk_balance_change(
+    balance_change: BalanceChange,
+) -> anyhow::Result<iota_sdk_types::BalanceChange> {
+    match balance_change.owner {
+        Owner::AddressOwner(address) | Owner::ObjectOwner(address) => {
+            Ok(iota_sdk_types::BalanceChange {
+                address,
+                // TODO use SDK TypeTag
+                coin_type: iota_sdk_types::TypeTag::from_str(
+                    &balance_change.coin_type.to_canonical_string(false),
+                )
+                .unwrap(),
+                amount: balance_change.amount,
+            })
+        }
+        _ => anyhow::bail!(
+            "Expected address or object owner, got {}",
+            balance_change.owner
+        ),
     }
 }
