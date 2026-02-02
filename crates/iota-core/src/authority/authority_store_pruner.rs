bf6bd9175da45d2aeb8998a48b6f86b681450e35
Chloe Martin <chloedaughterofmars@gmail.com>
2026-02-02 17:30:41 +0100

    use next_lexicographical_opt

--- crates/iota-core/src/authority/authority_store_pruner.rs
+++ crates/iota-core/src/authority/authority_store_pruner.rs
@@ -281,14 +281,13 @@ impl AuthorityStorePruner {
             effect_digests.push(effects_digest);
 
             if let Some(event_digest) = effects.events_digest() {
-                // TODO: Maybe we need a next_lexicographical which returns an option? How
-                // important is this?
-                let next_digest = event_digest.next_lexicographical();
-                perpetual_batch.schedule_delete_range(
-                    &perpetual_db.events,
-                    &(*event_digest, 0),
-                    &(next_digest, 0),
-                )?;
+                if let Some(next_digest) = event_digest.next_lexicographical_opt() {
+                    perpetual_batch.schedule_delete_range(
+                        &perpetual_db.events,
+                        &(*event_digest, 0),
+                        &(next_digest, 0),
+                    )?;
+                }
             }
         }
         perpetual_batch.delete_batch(&perpetual_db.effects, effect_digests)?;
