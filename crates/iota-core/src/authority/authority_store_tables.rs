135cdb4545776f1d4179dbcb857ba8dda4efc004
Chloe Martin <chloedaughterofmars@gmail.com>
2026-02-05 16:06:05 +0100

    clippy

--- crates/iota-core/src/authority/authority_store_tables.rs
+++ crates/iota-core/src/authority/authority_store_tables.rs
@@ -269,7 +269,7 @@ impl AuthorityPerpetualTables {
         let StoreObject::Value(store_object) = store_object.migrate().into_inner() else {
             return Ok(None);
         };
-        Ok(Some(self.construct_object(object_key, store_object)?))
+        Ok(Some(self.construct_object(object_key, *store_object)?))
     }
 
     pub fn object_reference(
@@ -279,7 +279,7 @@ impl AuthorityPerpetualTables {
     ) -> Result<ObjectRef, IotaError> {
         let obj_ref = match store_object.migrate().into_inner() {
             StoreObject::Value(object) => self
-                .construct_object(object_key, object)?
+                .construct_object(object_key, *object)?
                 .compute_object_reference(),
             StoreObject::Deleted => (object_key.0, object_key.1, ObjectDigest::OBJECT_DELETED),
             StoreObject::Wrapped => (object_key.0, object_key.1, ObjectDigest::OBJECT_WRAPPED),
@@ -596,7 +596,7 @@ impl LiveSetIter<'_> {
             StoreObject::Value(object) => {
                 let object = self
                     .tables
-                    .construct_object(&object_key, object)
+                    .construct_object(&object_key, *object)
                     .expect("Constructing object from store cannot fail");
                 Some(LiveObject::Normal(object))
             }
