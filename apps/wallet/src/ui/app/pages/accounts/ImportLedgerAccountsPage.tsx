bdcc55da36bf952cb81d0f5fea0f0536ae03c9d4
marc2332 <mespinsanz@gmail.com>
2026-02-06 10:15:22 +0100

    ;

--- apps/wallet/src/ui/app/pages/accounts/ImportLedgerAccountsPage.tsx
+++ apps/wallet/src/ui/app/pages/accounts/ImportLedgerAccountsPage.tsx
@@ -159,7 +159,6 @@ export function ImportLedgerAccountsPage() {
                             fullWidth
                         />
                     )}
-                    ;
                 </div>
             </div>
         </Overlay>
