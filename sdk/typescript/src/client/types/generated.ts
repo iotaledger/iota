4a95dacf7a07d33bc2b4fd28798bce4d8124eef4
Thibault Martinez <thibault@iota.org>
2026-02-09 13:33:35 +0100

    TS

--- sdk/typescript/src/client/types/generated.ts
+++ sdk/typescript/src/client/types/generated.ts
@@ -154,7 +154,7 @@ export interface DevInspectArgs {
     /** The gas budget for the transaction. */
     gasBudget?: string | null;
     /** The gas objects used to pay for the transaction. */
-    gasObjects?: [string, string, string][] | null;
+    gasObjects?: ObjectReference[] | null;
     /** The sponsor of the gas for the transaction, might be different from the sender. */
     gasSponsor?: string | null;
     /** Whether to return the raw transaction data and effects. */
@@ -1370,6 +1370,7 @@ export interface IotaObjectRef {
     /** Object version. */
     version: string;
 }
+export type ObjectReference = StructObjectReference | [string, string, string];
 export type ObjectResponseError =
     | {
           code: 'notExists';
@@ -1632,6 +1633,12 @@ export type StakeObject =
           stakedIotaId: string;
           status: 'Unstaked';
       };
+export interface StructObjectReference {
+    digest: string;
+    objectId: string;
+    /** Radix-10 encoded 64-bit unsigned integer */
+    version: string;
+}
 export interface CoinSupply {
     value: string;
 }
