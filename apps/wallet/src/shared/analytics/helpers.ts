34e1533daff0686e946ad2117b63ff72f937c4ee
Panteleymonchuk <panteleymonchuk@gmail.com>
2026-02-11 11:16:29 +0200

    feat(wallet): add "account deleted" event

---
+++ apps/wallet/src/shared/analytics/helpers.ts
@@ -0,0 +1,43 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+import { type AddedAccountsProperties } from '_src/shared/analytics/ampli';
+import { AccountType } from '_src/background/accounts/account';
+import { AccountsFormType } from '_components/accounts';
+
+enum AmpliAccountType {
+    Derived = 'Derived',
+    PrivateKey = 'Private Key',
+    Passkey = 'Passkey',
+    Ledger = 'Ledger',
+    Keystone = 'Keystone',
+    Mnemonic = 'Mnemonic',
+    Seed = 'Seed',
+}
+
+export const ACCOUNT_FORM_TYPE_TO_AMPLI_ACCOUNT_TYPE: Record<
+    AccountsFormType,
+    AddedAccountsProperties['accountType']
+> = {
+    [AccountsFormType.NewMnemonic]: AmpliAccountType.Derived,
+    [AccountsFormType.ImportMnemonic]: AmpliAccountType.Derived,
+    [AccountsFormType.ImportSeed]: AmpliAccountType.Derived,
+    [AccountsFormType.MnemonicSource]: AmpliAccountType.Derived,
+    [AccountsFormType.SeedSource]: AmpliAccountType.Derived,
+    [AccountsFormType.ImportPrivateKey]: AmpliAccountType.PrivateKey,
+    [AccountsFormType.Passkey]: AmpliAccountType.Passkey,
+    [AccountsFormType.ImportPasskey]: AmpliAccountType.Passkey,
+    [AccountsFormType.ImportLedger]: AmpliAccountType.Ledger,
+    [AccountsFormType.ImportKeystone]: AmpliAccountType.Keystone,
+};
+
+export const ACCOUNT_TYPE_TO_AMPLI_ACCOUNT_TYPE: Record<
+    AccountType,
+    AddedAccountsProperties['accountType']
+> = {
+    [AccountType.MnemonicDerived]: AmpliAccountType.Mnemonic,
+    [AccountType.SeedDerived]: AmpliAccountType.Seed,
+    [AccountType.PrivateKeyDerived]: AmpliAccountType.PrivateKey,
+    [AccountType.PasskeyDerived]: AmpliAccountType.Passkey,
+    [AccountType.LedgerDerived]: AmpliAccountType.Ledger,
+    [AccountType.KeystoneDerived]: AmpliAccountType.Keystone,
+};
