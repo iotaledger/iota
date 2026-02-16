765a31fba7a196fc8b92a9badfa9c0d374f9b5b4
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2026-02-16 09:15:54 +0100

    feat(iota-types)!: replace Event (#10038)
    Replace Event by the external SDK Event type
    
    Fixes https://github.com/iotaledger/iota/issues/9841

--- crates/iota-types/src/display.rs
+++ crates/iota-types/src/display.rs
@@ -2,12 +2,10 @@
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use iota_sdk_types::{StructTag, TypeTag};
 use serde::Deserialize;
 
 use crate::{
     collection_types::VecMap,
-    event::Event,
     id::{ID, UID},
 };
 
@@ -27,33 +25,4 @@ pub struct DisplayVersionUpdatedEvent {
     pub id: ID,
     pub version: u16,
     pub fields: VecMap<String, String>,
-}
-
-impl DisplayVersionUpdatedEvent {
-    // Checks if the provided `StructTag` is a DisplayVersionUpdatedEvent<T> and
-    // returns a reference to the inner type T if so.
-    pub fn inner_type(inner: &StructTag) -> Option<&StructTag> {
-        if !inner.is_display_version_updated() {
-            return None;
-        }
-
-        match inner.type_params() {
-            [TypeTag::Struct(struct_type)] => Some(struct_type),
-            _ => None,
-        }
-    }
-
-    pub fn try_from_event(event: &Event) -> Option<(&StructTag, Self)> {
-        let inner_type = Self::inner_type(&event.type_)?;
-
-        bcs::from_bytes(&event.contents)
-            .ok()
-            .map(|event| (inner_type, event))
-    }
-}
-
-#[derive(Deserialize, Debug)]
-pub struct DisplayCreatedEvent {
-    // The Object ID of Display Object
-    pub id: ID,
 }
