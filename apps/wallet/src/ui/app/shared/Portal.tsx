66ad1bb842d8b467ca2b39c98457f5e6d95050b4
marc2332 <mespinsanz@gmail.com>
2026-02-18 14:26:19 +0100

    fix lint

--- apps/wallet/src/ui/app/shared/Portal.tsx
+++ apps/wallet/src/ui/app/shared/Portal.tsx
@@ -2,7 +2,6 @@
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 interface PortalProps {
