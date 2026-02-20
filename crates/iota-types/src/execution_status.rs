4009adffd6b91a4f693d5af5b591bcb32af68906
Thibault Martinez <thibault@iota.org>
2026-02-20 10:41:36 +0100

    remove comment

--- crates/iota-types/src/execution_status.rs
+++ crates/iota-types/src/execution_status.rs
@@ -6,10 +6,3 @@ pub use iota_sdk_types::{
     CommandArgumentError, ExecutionError as ExecutionFailureStatus, ExecutionStatus, MoveLocation,
     PackageUpgradeError, TypeArgumentError,
 };
-
-#[cfg(test)]
-#[path = "unit_tests/execution_status_tests.rs"]
-mod execution_status_tests;
-
-// #[derive(Eq, PartialEq, Clone, Debug, Serialize, Deserialize,
-// EnumVariantOrder)] pub enum ExecutionFailureStatus {}
