2b2c3f54845f7279f95e837e367f5cc06414acd3
Thoralf Müller <thoralf.mue@gmail.com>
2026-02-25 10:51:28 +0100

    newline

--- crates/iota-rest-api/src/transactions/resolve.rs
+++ crates/iota-rest-api/src/transactions/resolve.rs
@@ -1,6 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // Modifications Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
+
 use std::collections::{BTreeMap, HashMap};
 
 use axum::{
