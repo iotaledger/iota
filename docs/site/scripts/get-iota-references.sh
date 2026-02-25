07bf632c36326771ae8ee6d71f03dc87f00802b4
marc2332 <mespinsanz@gmail.com>
2026-02-25 13:01:13 +0100

    revert unnecessary changes

--- docs/site/scripts/get-iota-references.sh
+++ docs/site/scripts/get-iota-references.sh
@@ -6,24 +6,24 @@ packages="typescript graphql-transport kiosk ledgerjs-hw-app-iota wallet-standar
 networks="testnet devnet"
 
 # Copy framework docs
-mkdir -p "./docs/content/developer/references/framework/"
-cp -Rv ./docs/generated-docs/framework/* "./docs/content/developer/references/framework/"
+mkdir -p "./../content/developer/references/framework/"
+cp -Rv ../generated-docs/framework/* "./../content/developer/references/framework/"
 
 for network in $networks; do
     # Download and extract the docs for the current network
     curl -sL "https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota/${network}.tar.gz" | tar xzv
 
     # Copy framework docs
-    mkdir -p "./docs/content/developer/references/framework/${network}"
-    cp -Rv ./docs/generated-docs/framework/* "./docs/content/developer/references/framework/${network}"
+    mkdir -p "./../content/developer/references/framework/${network}"
+    cp -Rv ../generated-docs/framework/* "./../content/developer/references/framework/${network}"
 done
 
 for package in $packages; do
     # Fix Sidebar for new route
-    sed -i -e "s|../generated-docs/ts-sdk/${package}|developer/ts-sdk/${package}/api|g" ./docs/generated-docs/ts-sdk/${package}/typedoc-sidebar.cjs
+    sed -i -e "s|../generated-docs/ts-sdk/${package}|developer/ts-sdk/${package}/api|g" ../generated-docs/ts-sdk/${package}/typedoc-sidebar.cjs
 
     # Copy package docs
-    mkdir -p "./docs/content/developer/ts-sdk/${package}/api/"
-    cp -Rv ./docs/generated-docs/ts-sdk/${package}/* "./docs/content/developer/ts-sdk/${package}/api/"
+    mkdir -p "./../content/developer/ts-sdk/${package}/api/"
+    cp -Rv ../generated-docs/ts-sdk/${package}/* "./../content/developer/ts-sdk/${package}/api/"
 done
 
