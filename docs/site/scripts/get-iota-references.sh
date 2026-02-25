aa8c0d73f21e2642f3b223e27d35c96d7418f6fc
marc2332 <mespinsanz@gmail.com>
2026-02-25 15:22:14 +0100

    build ts-docs instead of downloading from s3

--- docs/site/scripts/get-iota-references.sh
+++ docs/site/scripts/get-iota-references.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
 
-# Define the packages to process
-packages="typescript graphql-transport kiosk ledgerjs-hw-app-iota wallet-standard dapp-kit bcs isc-sdk signers kiosk"
+
+# Define main network 
+main_network="mainnet"
 
 networks="testnet devnet"
 
+# Create temporary directory to work in
+mkdir -p tmp
+cd tmp || exit
+
+# Download and extract the docs for the current network
+curl -sL "https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota/${main_network}.tar.gz" | tar xzv
+
 # Copy framework docs
-mkdir -p "./../content/developer/references/framework/"
-cp -Rv ../generated-docs/framework/* "./../content/developer/references/framework/"
+mkdir -p "../../content/developer/references/framework/"
+cp -Rv docs/generated-docs/framework/* "../../content/developer/references/framework/"
 
 for network in $networks; do
     # Download and extract the docs for the current network
     curl -sL "https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota/${network}.tar.gz" | tar xzv
 
     # Copy framework docs
-    mkdir -p "./../content/developer/references/framework/${network}"
-    cp -Rv ../generated-docs/framework/* "./../content/developer/references/framework/${network}"
+    mkdir -p "../../content/developer/references/framework/${network}"
+    cp -Rv docs/generated-docs/framework/* "../../content/developer/references/framework/${network}"
 done
 
-for package in $packages; do
-    # Fix Sidebar for new route
-    sed -i -e "s|../generated-docs/ts-sdk/${package}|developer/ts-sdk/${package}/api|g" ../generated-docs/ts-sdk/${package}/typedoc-sidebar.cjs
-
-    # Copy package docs
-    mkdir -p "./../content/developer/ts-sdk/${package}/api/"
-    cp -Rv ../generated-docs/ts-sdk/${package}/* "./../content/developer/ts-sdk/${package}/api/"
-done
-
+# Return to root and cleanup
+cd - || exit
+rm -rf tmp
