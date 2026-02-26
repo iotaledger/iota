ed8d87432a9c7ede30f27affc759bcdba4d29aea
Mirko Zichichi <mirko.zichichi@iota.org>
2026-02-26 14:11:22 +0100

    feat(types): create AuthContextCommand and AuthContextCallArg (#10466)
    # Description of change
    
    This PR makes it so that `AuthContext` fields use dedicated types
    instead of reusing the `PTB::Command` and `PTB::CallArg` types.
    `AuthContextCommand` and `AuthContextCallArg` are created in such a way
    that `AuthContext` de/serialization from/to Move is straightforward.

--- iota-execution/latest/iota-move-natives/src/auth_context.rs
+++ iota-execution/latest/iota-move-natives/src/auth_context.rs
@@ -3,19 +3,17 @@
 
 use std::collections::VecDeque;
 
-use iota_types::{
-    digests::MoveAuthenticatorDigest,
-    transaction::{CallArg, Command},
-};
 use move_binary_format::errors::{PartialVMError, PartialVMResult};
 use move_core_types::{
     gas_algebra::InternalGas, runtime_value::MoveTypeLayout, vm_status::StatusCode,
 };
 use move_vm_runtime::{native_charge_gas_early_exit, native_functions::NativeContext};
 use move_vm_types::{
-    loaded_data::runtime_types::Type, natives::function::NativeResult, pop_arg, values::Value,
+    loaded_data::runtime_types::Type,
+    natives::function::NativeResult,
+    pop_arg,
+    values::{StructRef, Value},
 };
-use serde::{Serialize, de::DeserializeOwned};
 use smallvec::smallvec;
 
 use crate::{
@@ -30,7 +28,8 @@ pub struct AuthContextDigestCostParams {
 
 /// ****************************************************************************
 /// native fun native_digest
-/// Implementation of the Move native function `fun native_digest(): vector<u8>`
+/// Implementation of the Move native function `fun native_digest():
+/// &vector<u8>`
 /// ****************************************************************************
 pub fn native_digest(
     context: &mut NativeContext,
@@ -48,10 +47,16 @@ pub fn native_digest(
         auth_context_digest_cost_params.auth_context_digest_cost_base
     );
 
-    let auth_context: &AuthenticationContext = get_extension!(context)?;
+    let auth_context: &mut AuthenticationContext = get_extension_mut!(context)?;
 
-    let digest = Value::vector_u8(auth_context.digest().into_inner());
-    Ok(NativeResult::ok(context.gas_used(), smallvec![digest]))
+    let digest_ref = auth_context
+        .struct_with_digest()?
+        .borrow_global()
+        .inspect_err(|err| assert!(err.major_status() != StatusCode::MISSING_DATA))?
+        .value_as::<StructRef>()?
+        .borrow_field(0)?;
+
+    Ok(NativeResult::ok(context.gas_used(), smallvec![digest_ref]))
 }
 
 #[derive(Clone)]
@@ -61,8 +66,8 @@ pub struct AuthContextTxCommandsCostParams {
 
 /// ****************************************************************************
 /// native fun native_tx_commands
-/// Implementation of the Move native function `fun native_tx_commands<C>():
-/// vector<C>`
+/// Implementation of the Move native function `fun native_tx_commands():
+/// &vector<Command>`
 /// ****************************************************************************
 pub fn native_tx_commands(
     context: &mut NativeContext,
@@ -82,15 +87,18 @@ pub fn native_tx_commands(
 
     let command_type = ty_args.pop().unwrap();
     let command_move_layout = resolve_move_layout(context, &command_type)?;
-    let commands_move_layout = MoveTypeLayout::Vector(Box::new(command_move_layout));
 
-    let auth_context: &AuthenticationContext = get_extension!(context)?;
-
-    let commands_value = to_value(&auth_context.tx_commands(), &commands_move_layout)?;
+    let auth_context: &mut AuthenticationContext = get_extension_mut!(context)?;
+    let tx_commands_ref = auth_context
+        .struct_with_tx_commands(command_move_layout)?
+        .borrow_global()
+        .inspect_err(|err| assert!(err.major_status() != StatusCode::MISSING_DATA))?
+        .value_as::<StructRef>()?
+        .borrow_field(0)?;
 
     Ok(NativeResult::ok(
         context.gas_used(),
-        smallvec![commands_value],
+        smallvec![tx_commands_ref],
     ))
 }
 
@@ -122,15 +130,19 @@ pub fn native_tx_inputs(
 
     let input_type = ty_args.pop().unwrap();
     let input_move_layout = resolve_move_layout(context, &input_type)?;
-    let inputs_move_layout = MoveTypeLayout::Vector(Box::new(input_move_layout));
 
-    let auth_context: &AuthenticationContext = get_extension!(context)?;
+    let auth_context: &mut AuthenticationContext = get_extension_mut!(context)?;
 
-    let inputs_value = to_value(&auth_context.tx_inputs(), &inputs_move_layout)?;
+    let tx_inputs_ref = auth_context
+        .struct_with_tx_inputs(input_move_layout)?
+        .borrow_global()
+        .inspect_err(|err| assert!(err.major_status() != StatusCode::MISSING_DATA))?
+        .value_as::<StructRef>()?
+        .borrow_field(0)?;
 
     Ok(NativeResult::ok(
         context.gas_used(),
-        smallvec![inputs_value],
+        smallvec![tx_inputs_ref],
     ))
 }
 
@@ -141,8 +153,8 @@ pub struct AuthContextReplaceCostParams {
 
 /// ****************************************************************************
 /// native fun replace
-/// Implementation of the Move native function `fun native_replace<I,
-/// C>(auth_digest: vector<u8>, tx_inputs: vector<I>, tx_commands: vector<C>)`
+/// Implementation of the Move native function `fun native_replace(auth_digest:
+/// vector<u8>,tx_inputs: vector<CallArg>,tx_commands: vector<Command>)`
 /// ****************************************************************************
 pub fn native_replace(
     context: &mut NativeContext,
@@ -162,59 +174,25 @@ pub fn native_replace(
 
     let command_type = ty_args.pop().unwrap();
     let command_move_layout = resolve_move_layout(context, &command_type)?;
-
-    let tx_commands = pop_arg!(args, Vec<Value>)
-        .into_iter()
-        .map(|value| from_value(value, &command_move_layout))
-        .collect::<PartialVMResult<Vec<Command>>>()?;
+    let tx_commands_value = pop_arg!(args, Vec<Value>);
 
     let input_type = ty_args.pop().unwrap();
     let input_move_layout = resolve_move_layout(context, &input_type)?;
+    let tx_inputs_value = pop_arg!(args, Vec<Value>);
 
-    let tx_inputs = pop_arg!(args, Vec<Value>)
-        .into_iter()
-        .map(|value| from_value(value, &input_move_layout))
-        .collect::<PartialVMResult<Vec<CallArg>>>()?;
-
-    let auth_digest = MoveAuthenticatorDigest::try_from(pop_arg!(args, Vec<u8>).as_slice())
-        .map_err(|err| {
-            PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
-                .with_message(err.to_string())
-        })?;
+    let auth_digest_value = pop_arg!(args, Vec<u8>);
 
     let auth_context: &mut AuthenticationContext = get_extension_mut!(context)?;
 
-    auth_context.replace(auth_digest, tx_inputs, tx_commands)?;
+    auth_context.replace(
+        auth_digest_value,
+        tx_inputs_value,
+        input_move_layout,
+        tx_commands_value,
+        command_move_layout,
+    )?;
 
     Ok(NativeResult::ok(context.gas_used(), smallvec![]))
-}
-
-fn to_value<T: ?Sized + Serialize>(
-    input: &T,
-    input_move_layout: &MoveTypeLayout,
-) -> PartialVMResult<Value> {
-    let bytes = bcs::to_bytes(input).map_err(|err| {
-        PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
-            .with_message(format!("Failed to serialize an input: {err}"))
-    })?;
-    Value::simple_deserialize(&bytes, input_move_layout).ok_or_else(|| {
-        PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
-            .with_message("Failed to deserialize an input to a Move value".to_string())
-    })
-}
-
-fn from_value<T: DeserializeOwned>(
-    value: Value,
-    value_move_layout: &MoveTypeLayout,
-) -> PartialVMResult<T> {
-    let bytes = value.simple_serialize(value_move_layout).ok_or_else(|| {
-        PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
-            .with_message("Failed to serialize a value".to_string())
-    })?;
-    bcs::from_bytes::<T>(&bytes).map_err(|err| {
-        PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
-            .with_message(format!("Failed to deserialize a value: {err}"))
-    })
 }
 
 fn resolve_move_layout(context: &NativeContext, ty: &Type) -> PartialVMResult<MoveTypeLayout> {
