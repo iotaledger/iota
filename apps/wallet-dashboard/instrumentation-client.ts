0ba7e746804163baf2ee8f32f9eecb4a0c694590
marc2332 <mespinsanz@gmail.com>
2026-02-27 09:59:39 +0100

    license header

--- apps/wallet-dashboard/instrumentation-client.ts
+++ apps/wallet-dashboard/instrumentation-client.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2026 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 // This file configures the initialization of Sentry on the client.
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
