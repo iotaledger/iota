fd2427e54e7c0bba6a2b62686ee39724ab2aaa3f
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2026-02-27 02:07:16 +0000

    refactor: rename directory to identity-result

--- apps/explorer/src/pages/index.tsx
+++ apps/explorer/src/pages/index.tsx
@@ -15,7 +15,7 @@ import { TransactionResult } from './transaction-result/TransactionResult';
 import { ValidatorDetails } from './validator/ValidatorDetails';
 import { ValidatorPageResult } from './validators/Validators';
 import { Layout } from '~/components';
-import { DidResult } from './trust-framework/did-result/DidResult';
+import { DidResult } from './trust-framework/identity-result/DidResult';
 
 interface RedirectWithIdProps {
     base: string;
