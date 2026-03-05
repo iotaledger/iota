62c7f48d1bc3b0e9fa8bfc35ab2f224095eaa801
Jose Luis Torres Corbalan <jltorres@boxfish.studio>
2026-03-05 09:58:48 +0100

    missing

--- sdk/typescript/test/unit/object-inputs.test.ts
+++ sdk/typescript/test/unit/object-inputs.test.ts
@@ -226,6 +226,7 @@ describe('Transaction inputs', () => {
           {
             "$kind": "UnresolvedObject",
             "UnresolvedObject": {
+              "initialSharedVersion": 1,
               "objectId": "0x0000000000000000000000000000000000000000000000000000000000000005",
             },
           },
