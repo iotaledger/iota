fdb232f5e3c6e73ca2259524efa7e2ab3b12ba8f
/alex/ <alexander.schmidt@iota.org>
2026-03-09 14:35:29 +0100

    remove breaking change in openrpc.json

--- sdk/typescript/src/client/types/generated.ts
+++ sdk/typescript/src/client/types/generated.ts
@@ -41,19 +41,6 @@ export interface BalanceChange {
     /** Owner of the balance change */
     owner: ObjectOwner;
 }
-/**
- * A transaction that was cancelled
- *
- * # BCS
- *
- * The BCS serialized form for this type is defined by the following ABNF:
- *
- * `text cancelled-transaction = digest (vector version-assignment) `
- */
-export interface CancelledTransaction {
-    digest: string;
-    versionAssignments: VersionAssignment[];
-}
 export interface Checkpoint {
     /** Commitments to checkpoint state */
     checkpointCommitments: CheckpointCommitment[];
@@ -102,13 +89,10 @@ export interface CommitteeInfo {
     epoch: string;
     validators: [string, string][];
 }
-export type ConsensusDeterminedVersionAssignments =
-    /** Cancelled transaction version assignment. */
-    {
-        CancelledTransactions: {
-            CancelledTransactions: CancelledTransaction[];
-        };
-    };
+/** Uses an enum to allow for future expansion of the ConsensusDeterminedVersionAssignments. */
+export type ConsensusDeterminedVersionAssignments = {
+    CancelledTransactions: [string, [string, string][]][];
+};
 export type IotaParsedData =
     | {
           dataType: 'moveObject';
@@ -1845,18 +1829,4 @@ export interface ValidatorApy {
 export interface ValidatorsApy {
     apys: ValidatorApy[];
     epoch: string;
-}
-/**
- * Object version assignment from consensus
- *
- * # BCS
- *
- * The BCS serialized form for this type is defined by the following ABNF:
- *
- * `text version-assignment = object-id u64 `
- */
-export interface VersionAssignment {
-    objectId: string;
-    /** Radix-10 encoded 64-bit unsigned integer */
-    version: string;
 }
