3ada103f22b7332c384316fbf9554e755c661c28
Panteley <panteleymonchuk@gmail.com>
2026-03-10 16:39:42 +0200

    feat(dashboard): add external link event

--- apps/wallet-dashboard/components/ExplorerLink.tsx
+++ apps/wallet-dashboard/components/ExplorerLink.tsx
@@ -1,9 +1,12 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+'use client';
+
 import { useExplorerLinkGetter } from '@/hooks';
 import { getExplorerLink } from '@iota/core';
 import Link from 'next/link';
+import { ampli } from '@/lib/utils/analytics/ampli';
 
 type GetExplorerLinkArgs = Parameters<typeof getExplorerLink>[0];
 
@@ -19,8 +22,14 @@ export function ExplorerLink({
     const getExplorerLink = useExplorerLinkGetter();
     const href = getExplorerLink(getLinkProps) ?? '#';
 
+    function handleClick() {
+        if (getLinkProps.type) {
+            ampli.externalLinkOpened({ type: getLinkProps.type });
+        }
+    }
+
     return (
-        <Link href={href} target="_blank" rel="noopener noreferrer">
+        <Link href={href} target="_blank" rel="noopener noreferrer" onClick={handleClick}>
             {children}
         </Link>
     );
