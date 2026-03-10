7ceab6e6c260a0ab99f99c065513c5e3520d46f4
Panteley <panteleymonchuk@gmail.com>
2026-03-10 18:18:08 +0200

    feat(dashboard): add `theme changed` event

--- apps/wallet-dashboard/app/(protected)/components/top-nav/TopNav.tsx
+++ apps/wallet-dashboard/app/(protected)/components/top-nav/TopNav.tsx
@@ -8,6 +8,7 @@ import { Network } from '@iota/iota-sdk/client';
 import { toTitleCase, ThemeSwitcher, Feature, useFeatureEnabledByNetwork } from '@iota/core';
 import { Settings } from '@iota/apps-ui-icons';
 import { usePersistedNetwork } from '@/hooks';
+import { ampli } from '@/lib/utils/analytics/ampli';
 
 export function TopNav() {
     const { persistedNetwork } = usePersistedNetwork();
@@ -41,7 +42,7 @@ export function TopNav() {
                 view={settingsDialogView}
                 setView={setSettingsDialogView}
             />
-            <ThemeSwitcher />
+            <ThemeSwitcher onThemeChange={(theme) => ampli.themeChanged({ theme })} />
             <Button
                 icon={<Settings />}
                 type={ButtonType.Ghost}
