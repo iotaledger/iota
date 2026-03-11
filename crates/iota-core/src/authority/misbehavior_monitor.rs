cfa73b80790e6c4533920e2a029944c3ed85f021
Olivia Saa <olivia.saa@iota.org>
2026-03-11 14:35:30 +0100

    feat: decouple current_local_metrics_count from node and consensus

--- crates/iota-core/src/authority/misbehavior_monitor.rs
+++ crates/iota-core/src/authority/misbehavior_monitor.rs
@@ -1,8 +1,6 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use std::sync::Arc;
-
 use iota_protocol_config::ProtocolConfig;
 use iota_types::scoring_metrics::VersionedScoringMetrics;
 
@@ -10,7 +8,7 @@ use iota_types::scoring_metrics::VersionedScoringMetrics;
 pub struct MisbehaviorMonitor {
     // The current metrics counts collected by the authority, i.e., the local view of the node
     // about the behaviour of the rest of the committee, according to the blocks received.
-    pub(crate) current_local_metrics_count: Arc<VersionedScoringMetrics>,
+    pub(crate) current_local_metrics_count: VersionedScoringMetrics,
 }
 
 impl MisbehaviorMonitor {
@@ -19,10 +17,8 @@ impl MisbehaviorMonitor {
         match protocol_config.scorer_version_as_option() {
             None | Some(1) => {
                 // Local metrics count are always initialized as zero.
-                let current_local_metrics_count = Arc::new(VersionedScoringMetrics::new(
-                    committee_size,
-                    protocol_config,
-                ));
+                let current_local_metrics_count =
+                    VersionedScoringMetrics::new(committee_size, protocol_config);
 
                 Self {
                     current_local_metrics_count,
