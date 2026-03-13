b4ab3cca9f74c51b61d261a873e5099098cffc77
vivekjain23 <vkj.vivekkumarjain@gmail.com>
2026-03-13 15:36:44 +0530

    fix: update wallet connection handling in MintLeapFrogNFT component

--- docs/site/src/components/CTF/mint-leap-frog-nft.tsx
+++ docs/site/src/components/CTF/mint-leap-frog-nft.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import {
-  useCurrentWallet,
+  useConnectWallet,
+  useCurrentAccount,
   useSignAndExecuteTransaction,
+  useWallets,
 } from '@iota/dapp-kit';
 import clsx from 'clsx';
-import { useCurrentAccount, useWallets } from '@iota/dapp-kit';
 import PopIn from './pop-in';
 import { handleMintLeapFrogSubmit } from "../../utils/ctf-utils"
 
@@ -14,7 +15,6 @@ const MintLeapFrogNFT: React.FC = () => {
     name:'',
     description:'',
     url:'',
-    address: account?.address || '',
   });
   const [coins, setCoins] = useState<string | null>(null);
   const [showPopIn, setShowPopIn] = useState<boolean>(false);
@@ -34,8 +34,13 @@ const MintLeapFrogNFT: React.FC = () => {
   const { mutate } = useConnectWallet();
   const { mutate: signAndExecuteTransaction } = useSignAndExecuteTransaction();
   const handleSubmit = () => {
+    if (!account?.address) {
+      alert("Please connect your wallet first");
+      return;
+    }
+
     handleMintLeapFrogSubmit({
-      nft,
+      nft: { ...nft, address: account.address },
       wallets,
       mutate,
       signAndExecuteTransaction,
