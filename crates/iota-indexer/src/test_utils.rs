d7b2dc495a951cbe0b89073e3d1314c7a25e3d81
Sergiu Popescu <sergiu.popescu@iota.org>
2026-03-17 16:45:14 +0100

    refactor(iota-indexer): remove rpc-client-url arg as it was unused for the ingestion path

--- crates/iota-indexer/src/test_utils.rs
+++ crates/iota-indexer/src/test_utils.rs
@@ -180,7 +180,6 @@ pub async fn start_test_indexer_impl(
                 .is_none()
                 .then_some(fullnode_rpc_url.clone());
             ingestion_config.sources.data_ingestion_path = data_ingestion_path;
-            ingestion_config.sources.rpc_client_url = Some(fullnode_rpc_url);
 
             tokio::spawn(async move {
                 Indexer::start_writer_with_config(
