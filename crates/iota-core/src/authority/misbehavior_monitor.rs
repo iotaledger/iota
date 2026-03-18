995facd1f4e326dbe1e2a05e85a997e0120c911c
Olivia Saa <olivia.saa@iota.org>
2026-03-18 11:09:10 +0000

    feat: create node specific VersionedScoringMetrics

--- crates/iota-core/src/authority/misbehavior_monitor.rs
+++ crates/iota-core/src/authority/misbehavior_monitor.rs
@@ -2,13 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use iota_protocol_config::ProtocolConfig;
-use iota_types::scoring_metrics::VersionedScoringMetrics;
+
+use crate::authority::authority_per_epoch_store::scorer::NodeVersionedScoringMetrics;
 
 /// Holds all information related to scoring of authorities in the committee.
 pub struct MisbehaviorMonitor {
     // The current metrics counts collected by the authority, i.e., the local view of the node
     // about the behaviour of the rest of the committee, according to the blocks received.
-    pub(crate) current_local_metrics_count: VersionedScoringMetrics,
+    pub(crate) current_local_metrics_count: NodeVersionedScoringMetrics,
 }
 
 impl MisbehaviorMonitor {
@@ -18,7 +19,7 @@ impl MisbehaviorMonitor {
             None | Some(1) => {
                 // Local metrics count are always initialized as zero.
                 let current_local_metrics_count =
-                    VersionedScoringMetrics::new(committee_size, protocol_config);
+                    NodeVersionedScoringMetrics::new(committee_size, protocol_config);
 
                 Self {
                     current_local_metrics_count,
