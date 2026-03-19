ed16cc65f0e803ab361b81cfa747f8a070d753ee
Branko <brankobosnic1@gmail.com>
2026-03-19 12:04:56 +0100

    refactor(core): streamline commission calculation by consolidating logic in getValidatorCommission function

--- apps/core/src/utils/stake/getValidatorCommission.ts
+++ apps/core/src/utils/stake/getValidatorCommission.ts
@@ -4,16 +4,9 @@
 import { IotaValidatorSummary } from '@iota/iota-sdk/client';
 import { formatPercentageDisplay } from '../formatPercentageDisplay';
 
-export function getValidatorCommission(
-    validatorData?: IotaValidatorSummary | null,
-    hasEffectiveCommissionRate?: boolean,
-) {
-    const showEffectiveCommissionRate = !!hasEffectiveCommissionRate;
+export function getValidatorCommission(validatorData?: IotaValidatorSummary | null) {
     const commission = validatorData
-        ? showEffectiveCommissionRate
-            ? Math.max(Number(validatorData.commissionRate), Number(validatorData.votingPower)) /
-              100
-            : Number(validatorData.commissionRate) / 100
+        ? Number(validatorData.effectiveCommissionRate ?? validatorData.commissionRate) / 100
         : 0;
     return formatPercentageDisplay(commission, '--');
 }
