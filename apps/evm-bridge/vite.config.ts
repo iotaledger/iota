88a103b791546e4ddcc60aafa737f5dab88e9e92
Begoña Alvarez <balvarez@boxfish.studio>
2026-03-19 12:55:09 +0100

    fix(evm-bridge): access to envs to vite during build

--- apps/evm-bridge/vite.config.ts
+++ apps/evm-bridge/vite.config.ts
@@ -1,38 +1,39 @@
-import { defineConfig } from 'vite';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 import { execSync } from 'child_process';
-import { sentryVitePlugin } from '@sentry/vite-plugin';
+import { defineConfig, loadEnv } from 'vite';
 
 const COMMIT_REV = execSync('git rev-parse HEAD').toString().trim().toString();
 
 process.env.VITE_VERCEL_ENV = process.env.VERCEL_ENV || 'development';
-process.env.VITE_BUILD_ENV = process.env.BUILD_ENV || 'development';
-
-const sentryAuthToken = process.env.SENTRY_AUTH_TOKEN;
-const IS_PROD = process.env.BUILD_ENV === 'production';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        react(),
-        sentryVitePlugin({
-            org: 'iota-foundation-eu',
-            project: 'iota-evm-bridge',
-            authToken: sentryAuthToken,
-            sourcemaps: {
-                assets: './dist/**',
-            },
-            disable: !IS_PROD || !sentryAuthToken,
-            silent: !process.env.CI,
-            release: {
-                name: COMMIT_REV,
-            },
-        }),
-    ],
-    build: {
-        sourcemap: true,
-    },
-    define: {
-        COMMIT_REV: JSON.stringify(COMMIT_REV),
-    },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const sentryAuthToken = env.SENTRY_AUTH_TOKEN;
+    const IS_PROD = env.VITE_BUILD_ENV === 'production';
+    return {
+        plugins: [
+            react(),
+            sentryVitePlugin({
+                org: 'iota-foundation-eu',
+                project: 'iota-evm-bridge',
+                authToken: sentryAuthToken,
+                sourcemaps: {
+                    assets: './dist/**',
+                },
+                disable: !IS_PROD || !sentryAuthToken,
+                silent: !env.CI,
+                release: {
+                    name: COMMIT_REV,
+                },
+            }),
+        ],
+        build: {
+            sourcemap: true,
+        },
+        define: {
+            COMMIT_REV: JSON.stringify(COMMIT_REV),
+        },
+    };
 });
