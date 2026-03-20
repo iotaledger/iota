328503b665bfac1968616d6ce51db00244e92c05
evavirseda <evirseda@boxfish.studio>
2026-03-20 09:47:22 +0100

    update overviewhint to show action

--- apps/wallet/src/ui/app/pages/home/tokens/OverviewHint.tsx
+++ apps/wallet/src/ui/app/pages/home/tokens/OverviewHint.tsx
@@ -7,19 +7,25 @@ interface OverviewHintProps {
     onClick: () => void;
     icon: ElementType;
     title: string;
+    subtitle?: string;
 }
 
-export function OverviewHint({ onClick, icon, title }: OverviewHintProps) {
+export function OverviewHint({ onClick, icon, title, subtitle }: OverviewHintProps) {
     const IconComponent = icon;
     return (
         <div
-            className="state-layer relative flex w-full cursor-pointer items-center gap-3 rounded-xl border border-transparent bg-iota-warning-90 p-xs px-sm py-xs dark:bg-iota-warning-20"
+            className="state-layer dark:bg-iota-warning-20 relative flex w-full cursor-pointer items-center gap-3 rounded-xl border border-transparent bg-iota-warning-90 p-xs px-sm py-xs"
             onClick={onClick}
         >
-            <IconComponent className="h-5 w-5 text-iota-warning-10 dark:text-iota-warning-90" />
-            <span className="text-label-sm text-iota-neutral-10 dark:text-iota-neutral-92">
-                {title}
-            </span>
+            <IconComponent className="dark:text-iota-warning-90 h-5 w-5 text-iota-warning-10" />
+            <div className="flex flex-col text-label-sm">
+                <span className="dark:text-iota-neutral-92 text-iota-neutral-10">{title}</span>
+                {subtitle && (
+                    <span className="dark:text-iota-neutral-60 text-iota-neutral-40">
+                        {subtitle}
+                    </span>
+                )}
+            </div>
         </div>
     );
 }
