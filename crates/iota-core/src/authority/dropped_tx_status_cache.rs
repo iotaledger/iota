f3a60dc01d257c021a3177d922a8652d37507b46
Piotr Macek <4007944+piotrm50@users.noreply.github.com>
2026-03-20 16:57:29 +0100

    chore: fix copyright year to 2026

--- crates/iota-core/src/authority/dropped_tx_status_cache.rs
+++ crates/iota-core/src/authority/dropped_tx_status_cache.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 IOTA Stiftung
+// Copyright (c) 2026 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
 //! In-memory bounded cache for transactions dropped by white-flag conflict
