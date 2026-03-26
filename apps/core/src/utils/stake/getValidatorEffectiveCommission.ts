70ec322f827e5628e41569be2bd361f5e49fb789
Branko <brankobosnic1@gmail.com>
2026-03-26 11:25:03 +0100

    refactor: simplify effective commission calculation in getValidatorEffectiveCommission function

--- apps/core/src/utils/stake/getValidatorEffectiveCommission.ts
+++ apps/core/src/utils/stake/getValidatorEffectiveCommission.ts
@@ -5,8 +5,7 @@ import { IotaValidatorSummary } from '@iota/iota-sdk/client';
 import { formatPercentageDisplay } from '../formatPercentageDisplay';
 
 export function getValidatorEffectiveCommission(validatorData?: IotaValidatorSummary | null) {
-    const effectiveCommission = validatorData
-        ? Number(validatorData.effectiveCommissionRate) / 100
-        : 0;
+    const rate = validatorData?.effectiveCommissionRate;
+    const effectiveCommission = rate != null ? Number(rate) / 100 : 0;
     return formatPercentageDisplay(effectiveCommission, '--');
 }
