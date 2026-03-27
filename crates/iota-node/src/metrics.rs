fd2c8c2011669ae62dbaf488c96d9b7bfd55be69
Ching-Hua (Vivian) Lin <jkrvivian@gmail.com>
2026-03-27 15:05:21 +0800

    upstream(core): Export current, max supported and max configured protocol config versions as metrics (#10910)
    ## Description of change
    
    - Upstream range: [v1.47.1, v1.48.4)
    - Port commit:
    -
    [MystenLabs/sui@e8f522a](https://github.com/MystenLabs/sui/commit/e8f522a3c05d0f06774e4c8b2a5ddb301c2803e5)
    - Description:
    
    Export current, max supported and max configured protocol config
    versions as Prometheus metrics for debugging and setting up alerts.
    
    ## Links to any relevant issues
    
    Part of #10909
    
    ## How the change has been tested
    
    - [x] Basic tests (linting, compilation, formatting, unit/integration
    tests)
    - [ ] Patch-specific tests (correctness, functionality coverage)
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have checked that new and existing unit tests pass locally with
    my changes

--- crates/iota-node/src/metrics.rs
+++ crates/iota-node/src/metrics.rs
@@ -9,8 +9,9 @@ use iota_metrics::RegistryService;
 use iota_network::tonic::Code;
 use iota_network_stack::metrics::MetricsCallbackProvider;
 use prometheus::{
-    HistogramVec, IntCounterVec, IntGaugeVec, Registry, register_histogram_vec_with_registry,
-    register_int_counter_vec_with_registry, register_int_gauge_vec_with_registry,
+    HistogramVec, IntCounterVec, IntGauge, IntGaugeVec, Registry,
+    register_histogram_vec_with_registry, register_int_counter_vec_with_registry,
+    register_int_gauge_vec_with_registry, register_int_gauge_with_registry,
 };
 
 /// Starts a task to periodically push metrics to a configured endpoint if a
@@ -74,6 +75,10 @@ pub struct IotaNodeMetrics {
     pub total_jwks: IntCounterVec,
     pub invalid_jwks: IntCounterVec,
     pub unique_jwks: IntCounterVec,
+
+    pub current_protocol_version: IntGauge,
+    pub binary_max_protocol_version: IntGauge,
+    pub configured_max_protocol_version: IntGauge,
 }
 
 impl IotaNodeMetrics {
@@ -111,6 +116,24 @@ impl IotaNodeMetrics {
                 "unique_jwks",
                 "Total number of unique JWKs",
                 &["provider"],
+                registry,
+            )
+            .unwrap(),
+            current_protocol_version: register_int_gauge_with_registry!(
+                "iota_current_protocol_version",
+                "Current protocol version in this epoch",
+                registry,
+            )
+            .unwrap(),
+            binary_max_protocol_version: register_int_gauge_with_registry!(
+                "iota_binary_max_protocol_version",
+                "Max protocol version supported by this binary",
+                registry,
+            )
+            .unwrap(),
+            configured_max_protocol_version: register_int_gauge_with_registry!(
+                "iota_configured_max_protocol_version",
+                "Max protocol version configured in the node config",
                 registry,
             )
             .unwrap(),
