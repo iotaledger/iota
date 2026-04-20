b4f3cc627a4b2775cdf7d058b19ce5f0a5d2a1f1
evavirseda <evirseda@boxfish.studio>
2026-04-20 07:33:59 +0200

    improve layout

--- apps/wallet/src/ui/app/index.tsx
+++ apps/wallet/src/ui/app/index.tsx
@@ -41,6 +41,7 @@ import {
     NFTDetailsPage,
     NftTransferPage,
     KioskDetailsPage,
+    NetworkPage,
     ReceiptPage,
     TransactionBlocksPage,
     TransferCoinPage,
@@ -175,6 +176,7 @@ export function App() {
                 <Route path="send" element={<TransferCoinPage />} />
                 <Route path="send/select" element={<CoinsSelectorPage />} />
                 <Route path="stake/*" element={<StakingPage />} />
+                <Route path="network" element={<NetworkPage />} />
                 <Route path="tokens/*" element={<TokenDetailsPage />} />
                 <Route path="transactions/:status?" element={<TransactionBlocksPage />} />
                 <Route path="*" element={<Navigate to="/tokens" replace={true} />} />
