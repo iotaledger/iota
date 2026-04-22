dbe102e278eb7208eb237efaa0a18891f0b521bc
marc2332 <mespinsanz@gmail.com>
2026-04-22 13:19:50 +0200

    bring back prettier

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+// Copyright (c) Mysten Labs, Inc.
+// Modifications Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+module.exports = {
+    printWidth: 100,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    useTabs: false,
+    overrides: [
+        {
+            files: 'external-crates/move/documentation/book/**/*',
+            options: {
+                proseWrap: 'always',
+            },
+        },
+    ],
+};
