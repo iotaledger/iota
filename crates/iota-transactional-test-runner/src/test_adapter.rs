eb0015a159cb4d5cc61e625d8f7c29b86a20ef5d
Lorenzo Benetollo <38067715+lollobene@users.noreply.github.com>
2026-05-12 16:45:37 +0200

    feat(move): Move view functions (compiler implementation) (#11271)
    # Description of change
    
    Adding the Move view functions implementation in the complier
    
    - Linter warning in future PR
    - Attribute removal in future PR
    - PackageMetadata in future PR
    - IOTA verifier in future PR 
    
    ## Links to any relevant issues
    
    fixes #8254 
    
    ## How the change has been tested
    
    - [x] Basic tests (linting, compilation, formatting, unit/integration
    tests)
    - [ ] Patch-specific tests (correctness, functionality coverage)
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [x] I have checked that new and existing unit tests pass locally with
    my changes

--- crates/iota-transactional-test-runner/src/test_adapter.rs
+++ crates/iota-transactional-test-runner/src/test_adapter.rs
@@ -2944,6 +2944,12 @@ fn fill_metadata(modules: &mut [MaybeNamedCompiledModule]) {
                         IotaAttribute::authenticator_attribute(version),
                     );
                 }
+                if info.attributes.is_view() {
+                    runtime_metadata.add_function_attribute(
+                        name.as_str().to_owned(),
+                        IotaAttribute::view_attribute(),
+                    );
+                }
             }
         }
 
