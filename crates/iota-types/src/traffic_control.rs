42cbe4edf3fd1258d5069374cedce5a08482a361
jkrvivian <jkrvivian@gmail.com>
2026-05-17 15:31:30 +0800

    upstream(core): Increase the default spam threshold

--- crates/iota-types/src/traffic_control.rs
+++ crates/iota-types/src/traffic_control.rs
@@ -303,7 +303,7 @@ impl PolicyConfig {
         Self {
             client_id_source: ClientIdSource::SocketAddr,
             spam_policy_type: PolicyType::FreqThreshold(FreqThresholdConfig {
-                client_threshold: 500,
+                client_threshold: 1000,
                 window_size_secs: 5,
                 update_interval_secs: 1,
                 ..FreqThresholdConfig::default()
