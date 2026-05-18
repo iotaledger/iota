e79478cd40952b5c6b956cc045b0c057136f326a
chrisgitiota <christof.gerritsma@iota.org>
2026-05-18 18:56:19 +0200

    Several fixes and rewritten texts for combined Single Notarization and Audit Trail docs

--- docs/content/developer/iota-notarization/audit-trail/references/wasm/api_ref.md
+++
@@ -1,8 +0,0 @@
----
-title: WASM API Reference
-description: IOTA Audit Trail WASM API Reference
----
-
-# WASM API Reference
-
-Auto-generated API documentation for the `@iota/audit-trail` WASM package.
--- docs/site/scripts/get-iota-notarization-references.sh
+++ docs/site/scripts/get-iota-notarization-references.sh
@@ -5,8 +5,8 @@ mkdir tmp
 cd tmp
 
 # Create the target directory structure if it doesn't exist
-mkdir -p ../../content/developer/iota-notarization/references/wasm/single-notarization
-mkdir -p ../../content/developer/iota-notarization/references/wasm/audit-trail
+mkdir -p ../../content/developer/iota-notarization/single-notarization/references/wasm
+mkdir -p ../../content/developer/iota-notarization/audit-trail/references/wasm
 
 # Create additional temporary directories for single-notarization and audit-trail
 mkdir single-notarization
@@ -15,12 +15,15 @@ mkdir audit-trail
 # Download and copy single-notarization docs
 cd single-notarization
 curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota-notarization/0.1/wasm.tar.gz | tar xzv
-cp -Rv ./docs/wasm/* ../../../content/developer/iota-notarization/references/wasm/single-notarization/
+cp -Rv ./docs/wasm/* ../../../content/developer/iota-notarization/single-notarization/references/wasm/
 
 # Download and copy audit-trail docs
 cd ../audit-trail
-curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota-notarization/0.1/audit-trail-wasm.tar.gz | tar xzv
-cp -Rv ./docs/wasm/* ../../../content/developer/iota-notarization/references/wasm/audit-trail/
+curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota-audit-trail/0.1/audit-trail-wasm.tar.gz | tar xzv
+# For unknown reasons the path to the audit-trail docs in the extracted tar archive has an additional folder `audit-trail-docs`:
+# * Path for single-notarization: ./docs/wasm/*
+# * Path for audit-trail:         ./audit-trail-docs/docs/wasm/*
+cp -Rv ./audit-trail-docs/docs/wasm/* ../../../content/developer/iota-notarization/audit-trail/references/wasm/
 
 # Return to root and cleanup
 cd ../..
