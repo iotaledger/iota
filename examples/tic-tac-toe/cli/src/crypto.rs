ba2f430414e8e9004970429f6a03919bb309d383
Thibault Martinez <thibault@iota.org>
2026-05-21 11:29:02 +0200

    cleanup

--- examples/tic-tac-toe/cli/src/crypto.rs
+++ examples/tic-tac-toe/cli/src/crypto.rs
@@ -22,20 +22,19 @@ pub(crate) fn public_key_from_base64(base64: &str) -> Result<PublicKey> {
 
 /// Combine public keys into a MultiSig. Keys are deduplicated before generation
 /// as multisigs cannot contain the same public key twice.
+/// TODO remove conversion https://github.com/iotaledger/iota/issues/11590
 pub(crate) fn combine_keys(keys: impl IntoIterator<Item = PublicKey>) -> Result<MultiSigPublicKey> {
-    let dedupped: Vec<_> =
-        BTreeMap::from_iter(keys.into_iter().map(|key| (key.encode_base64(), key)))
-            .into_values()
-            .collect();
-
-    let members = dedupped
+    let members: Vec<_> = keys
         .into_iter()
-        .map(|pk| {
-            let member_pk = SdkPublicKey::from_base64(&pk.encode_base64())
+        .map(|key| {
+            let b64 = key.encode_base64();
+            let pk = SdkPublicKey::from_base64(&b64)
                 .map_err(|e| anyhow!("Failed to convert public key: {e}"))?;
-            Ok(MultisigMember::new(member_pk, 1))
+            Ok((b64, MultisigMember::new(pk, 1)))
         })
-        .collect::<Result<Vec<_>>>()?;
+        .collect::<Result<BTreeMap<_, _>>>()?
+        .into_values()
+        .collect();
 
     Ok(MultiSigPublicKey::new(members, 1)?)
 }
