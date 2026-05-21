6606a9065dc92f069b1e2c573f814cee3fc906f4
Thibault Martinez <thibault@iota.org>
2026-05-21 10:27:38 +0200

    add more crates to the update rev script

--- scripts/update_sdk_types_rev.sh
+++ scripts/update_sdk_types_rev.sh
@@ -61,6 +61,8 @@ update_file() {
     local file="$1"
     update_rev iota-sdk-types "$file"
     update_rev iota-sdk-crypto "$file"
+    update_rev iota-sdk-grpc-types "$file"
+    update_rev iota-sdk-grpc-client "$file"
 }
 
 echo "New rev: $NEW_REV"
