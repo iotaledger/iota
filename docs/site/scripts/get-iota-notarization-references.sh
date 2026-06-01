6edee8cc153cf4609354da74279e31b4c72d3a6d
chrisgitiota <christof.gerritsma@iota.org>
2026-06-01 15:11:48 +0200

    Renamed several folder and page names from `audit-trail` to `audit-trails` and updated the banner icon

--- docs/site/scripts/get-iota-notarization-references.sh
+++ docs/site/scripts/get-iota-notarization-references.sh
@@ -6,24 +6,24 @@ cd tmp
 
 # Create the target directory structure if it doesn't exist
 mkdir -p ../../content/developer/iota-notarization/single-notarization/references/wasm
-mkdir -p ../../content/developer/iota-notarization/audit-trail/references/wasm
+mkdir -p ../../content/developer/iota-notarization/audit-trails/references/wasm
 
-# Create additional temporary directories for single-notarization and audit-trail
+# Create additional temporary directories for single-notarization and audit-trails
 mkdir single-notarization
-mkdir audit-trail
+mkdir audit-trails
 
 # Download and copy single-notarization docs
 cd single-notarization
 curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota-notarization/0.1/wasm.tar.gz | tar xzv
 cp -Rv ./docs/wasm/* ../../../content/developer/iota-notarization/single-notarization/references/wasm/
 
-# Download and copy audit-trail docs
-cd ../audit-trail
+# Download and copy audit-trails docs
+cd ../audit-trails
 curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota-audit-trail/0.1/audit-trail-wasm.tar.gz | tar xzv
-# For unknown reasons the path to the audit-trail docs in the extracted tar archive has an additional folder `audit-trail-docs`:
+# For unknown reasons the path to the audit-trails docs in the extracted tar archive has an additional folder `audit-trail-docs`:
 # * Path for single-notarization: ./docs/wasm/*
 # * Path for audit-trail:         ./audit-trail-docs/docs/wasm/*
-cp -Rv ./audit-trail-docs/docs/wasm/* ../../../content/developer/iota-notarization/audit-trail/references/wasm/
+cp -Rv ./audit-trail-docs/docs/wasm/* ../../../content/developer/iota-notarization/audit-trails/references/wasm/
 
 # Return to root and cleanup
 cd ../..
