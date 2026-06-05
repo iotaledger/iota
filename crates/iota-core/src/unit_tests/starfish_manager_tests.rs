deaadc0153d57e4f1983e6b7f72f94410c9fd9d5
jkrvivian <jkrvivian@gmail.com>
2026-06-05 17:54:25 +0800

    upstream(core): Wait for replaying consensus commits before checkpoint builder starts

--- crates/iota-core/src/unit_tests/starfish_manager_tests.rs
+++ crates/iota-core/src/unit_tests/starfish_manager_tests.rs
@@ -54,7 +54,7 @@ pub fn checkpoint_service_for_testing(state: Arc<AuthorityState>) -> Arc<Checkpo
         3,
         100_000,
     );
-    checkpoint_service.spawn().now_or_never().unwrap();
+    checkpoint_service.spawn(None).now_or_never().unwrap();
     checkpoint_service
 }
 
