a9a64f7533ec89705598bdefe8d3f85abfe1d62b
Tomasz Pastusiak <tomasz.pastusiak@iota.org>
2026-06-09 11:02:32 +0200

    feat(indexer): Remove unused impls

--- crates/iota-indexer/src/models/obj_indices.rs
+++ crates/iota-indexer/src/models/obj_indices.rs
@@ -4,11 +4,7 @@
 
 use diesel::prelude::*;
 
-use crate::{
-    errors::IndexerError,
-    schema::objects_version,
-    types::{IndexedDeletedObject, IndexedObject},
-};
+use crate::schema::objects_version;
 
 /// Model types related to tables that support efficient execution of queries
 /// on the `objects` and related object tables.
@@ -30,30 +26,4 @@ pub struct StoredObjectVersion {
     pub object_id: Vec<u8>,
     pub object_version: i64,
     pub cp_sequence_number: i64,
-}
-
-impl TryFrom<&IndexedObject> for StoredObjectVersion {
-    type Error = IndexerError;
-
-    fn try_from(o: &IndexedObject) -> Result<Self, Self::Error> {
-        Ok(Self {
-            object_id: o.object.id().as_bytes().to_vec(),
-            object_version: o.object.version().as_u64() as i64,
-            cp_sequence_number: o.checkpoint_sequence_number.ok_or_else(|| {
-                IndexerError::InvalidArgument(
-                    "checkpoint_sequence_number is required for StoredObjectVersion".to_string(),
-                )
-            })? as i64,
-        })
-    }
-}
-
-impl From<&IndexedDeletedObject> for StoredObjectVersion {
-    fn from(o: &IndexedDeletedObject) -> Self {
-        Self {
-            object_id: o.object_id.as_bytes().to_vec(),
-            object_version: o.object_version as i64,
-            cp_sequence_number: o.checkpoint_sequence_number as i64,
-        }
-    }
 }
