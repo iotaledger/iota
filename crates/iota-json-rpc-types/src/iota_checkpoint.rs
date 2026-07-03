8a83cad21d1df8da8ae2461a6c53afa182515e5c
Chloe Martin <chloedaughterofmars@gmail.com>
2026-07-03 09:34:28 +0200

    clippy

--- crates/iota-json-rpc-types/src/iota_checkpoint.rs
+++ crates/iota-json-rpc-types/src/iota_checkpoint.rs
@@ -356,8 +356,8 @@ impl From<CheckpointDigest> for CheckpointId {
 impl std::fmt::Display for CheckpointId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            CheckpointId::SequenceNumber(seq) => write!(f, "SequenceNumber({})", seq),
-            CheckpointId::Digest(digest) => write!(f, "Digest({})", digest),
+            CheckpointId::SequenceNumber(seq) => write!(f, "SequenceNumber({seq})"),
+            CheckpointId::Digest(digest) => write!(f, "Digest({digest})"),
         }
     }
 }
