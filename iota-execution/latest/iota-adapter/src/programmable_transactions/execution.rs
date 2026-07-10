21766e2121141c06879ad53c1f1b06b138161d56
Thibault Martinez <thibault@iota.org>
2026-07-10 14:49:20 +0200

    remove comma

--- iota-execution/latest/iota-adapter/src/programmable_transactions/execution.rs
+++ iota-execution/latest/iota-adapter/src/programmable_transactions/execution.rs
@@ -1283,7 +1283,7 @@ mod checked {
         else {
             return Err(ExecutionError::new_with_source(
                 ExecutionErrorKind::FunctionNotFound,
-                format!("Could not resolve function '{function}' in module {module_id}",),
+                format!("Could not resolve function '{function}' in module {module_id}"),
             ));
         };
 
