1f3c14abd1cb46a15ddf8ee1b83ac2deebb20d99
jkrvivian <jkrvivian@gmail.com>
2026-07-30 13:46:34 +0800

    fix(doc): Fix snapshot restore link anchor and stale wording

--- crates/iota-storage/src/object_store/util.rs
+++ crates/iota-storage/src/object_store/util.rs
@@ -62,41 +62,6 @@ impl EpochMetadata {
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct PerEpochManifest {
-    pub lines: Vec<String>,
-}
-
-impl PerEpochManifest {
-    pub fn new(lines: Vec<String>) -> Self {
-        PerEpochManifest { lines }
-    }
-
-    pub fn serialize_as_newline_delimited(&self) -> String {
-        self.lines.join("\n")
-    }
-
-    pub fn deserialize_from_newline_delimited(s: &str) -> PerEpochManifest {
-        PerEpochManifest {
-            lines: s.lines().map(String::from).collect(),
-        }
-    }
-
-    // Method to filter lines by a given prefix
-    pub fn filter_by_prefix(&self, prefix: &str) -> PerEpochManifest {
-        let filtered_lines = self
-            .lines
-            .iter()
-            .filter(|line| line.starts_with(prefix))
-            .cloned()
-            .collect();
-
-        PerEpochManifest {
-            lines: filtered_lines,
-        }
-    }
-}
-
 pub async fn get<S: ObjectStoreGetExt>(store: &S, src: &Path) -> Result<Bytes> {
     let bytes = retry(backoff::ExponentialBackoff::default(), || async {
         store.get_bytes(src).await.map_err(|e| {
@@ -169,36 +134,6 @@ pub async fn copy_files<S: ObjectStoreGetExt, D: ObjectStorePutExt>(
         })
         .await;
     Ok(results.into_iter().collect())
-}
-
-/// Copies all files in the directory from the source store to the destination
-/// store.
-pub async fn copy_recursively<S: ObjectStoreGetExt + ObjectStoreListExt, D: ObjectStorePutExt>(
-    dir: &Path,
-    src_store: &S,
-    dest_store: &D,
-    concurrency: NonZeroUsize,
-) -> Result<Vec<()>> {
-    let mut input_paths = vec![];
-    let mut output_paths = vec![];
-    let mut paths = src_store.list_objects(Some(dir)).await;
-    while let Some(res) = paths.next().await {
-        if let Ok(object_metadata) = res {
-            input_paths.push(object_metadata.location.clone());
-            output_paths.push(object_metadata.location);
-        } else {
-            return Err(res.err().unwrap().into());
-        }
-    }
-    copy_files(
-        &input_paths,
-        &output_paths,
-        src_store,
-        dest_store,
-        concurrency,
-        None,
-    )
-    .await
 }
 
 pub async fn delete_files<S: ObjectStoreDeleteExt>(
@@ -417,43 +352,6 @@ pub fn get_path(prefix: &str) -> Path {
     Path::from(prefix)
 }
 
-// Snapshot MANIFEST file is very simple. Just a newline delimited list of all
-// paths in the snapshot directory this simplicity enables easy parsing for
-// scripts to download snapshots
-pub async fn write_snapshot_manifest<S: ObjectStoreListExt + ObjectStorePutExt>(
-    dir: &Path,
-    store: &S,
-    epoch_prefix: String,
-) -> Result<()> {
-    let mut file_names = vec![];
-    let mut paths = store.list_objects(Some(dir)).await;
-    while let Some(res) = paths.next().await {
-        if let Ok(object_metadata) = res {
-            // trim the "epoch_XX/" dir prefix here
-            let mut path_str = object_metadata.location.to_string();
-            if path_str.starts_with(&epoch_prefix) {
-                path_str = String::from(&path_str[epoch_prefix.len()..]);
-                file_names.push(path_str);
-            } else {
-                warn!("{path_str}, should be coming from the files in the {epoch_prefix} dir",)
-            }
-        } else {
-            return Err(res.err().unwrap().into());
-        }
-    }
-
-    let epoch_manifest = PerEpochManifest::new(file_names);
-    let bytes = Bytes::from(epoch_manifest.serialize_as_newline_delimited());
-    put(
-        store,
-        &Path::from(format!("{dir}/{MANIFEST_FILENAME}")),
-        bytes,
-    )
-    .await?;
-
-    Ok(())
-}
-
 #[cfg(test)]
 mod tests {
     use std::{fs, num::NonZeroUsize};
@@ -462,102 +360,7 @@ mod tests {
     use object_store::path::Path;
     use tempfile::TempDir;
 
-    use crate::object_store::util::{
-        MANIFEST_FILENAME, copy_recursively, delete_recursively, write_snapshot_manifest,
-    };
-
-    #[tokio::test]
-    pub async fn test_copy_recursively() -> anyhow::Result<()> {
-        let input = TempDir::new()?;
-        let input_path = input.path();
-        let child = input_path.join("child");
-        fs::create_dir(&child)?;
-        let file1 = child.join("file1");
-        fs::write(file1, b"Lorem ipsum")?;
-        let grandchild = child.join("grand_child");
-        fs::create_dir(&grandchild)?;
-        let file2 = grandchild.join("file2");
-        fs::write(file2, b"Lorem ipsum")?;
-
-        let output = TempDir::new()?;
-        let output_path = output.path();
-
-        let input_store = ObjectStoreConfig {
-            object_store: Some(ObjectStoreType::File),
-            directory: Some(input_path.to_path_buf()),
-            ..Default::default()
-        }
-        .make()?;
-
-        let output_store = ObjectStoreConfig {
-            object_store: Some(ObjectStoreType::File),
-            directory: Some(output_path.to_path_buf()),
-            ..Default::default()
-        }
-        .make()?;
-
-        copy_recursively(
-            &Path::from("child"),
-            &input_store,
-            &output_store,
-            NonZeroUsize::new(1).unwrap(),
-        )
-        .await?;
-
-        assert!(output_path.join("child").exists());
-        assert!(output_path.join("child").join("file1").exists());
-        assert!(output_path.join("child").join("grand_child").exists());
-        assert!(
-            output_path
-                .join("child")
-                .join("grand_child")
-                .join("file2")
-                .exists()
-        );
-        let content = fs::read_to_string(output_path.join("child").join("file1"))?;
-        assert_eq!(content, "Lorem ipsum");
-        let content =
-            fs::read_to_string(output_path.join("child").join("grand_child").join("file2"))?;
-        assert_eq!(content, "Lorem ipsum");
-        Ok(())
-    }
-
-    #[tokio::test]
-    pub async fn test_write_snapshot_manifest() -> anyhow::Result<()> {
-        let input = TempDir::new()?;
-        let input_path = input.path();
-        let epoch_0 = input_path.join("epoch_0");
-        fs::create_dir(&epoch_0)?;
-        let file1 = epoch_0.join("file1");
-        fs::write(file1, b"Lorem ipsum")?;
-        let file2 = epoch_0.join("file2");
-        fs::write(file2, b"Lorem ipsum")?;
-        let grandchild = epoch_0.join("grand_child");
-        fs::create_dir(&grandchild)?;
-        let file3 = grandchild.join("file2.tar.gz");
-        fs::write(file3, b"Lorem ipsum")?;
-
-        let input_store = ObjectStoreConfig {
-            object_store: Some(ObjectStoreType::File),
-            directory: Some(input_path.to_path_buf()),
-            ..Default::default()
-        }
-        .make()?;
-
-        write_snapshot_manifest(
-            &Path::from("epoch_0"),
-            &input_store,
-            String::from("epoch_0/"),
-        )
-        .await?;
-
-        assert!(input_path.join("epoch_0").join(MANIFEST_FILENAME).exists());
-        let content = fs::read_to_string(input_path.join("epoch_0").join(MANIFEST_FILENAME))?;
-        assert!(content.contains("file2"));
-        assert!(content.contains("file1"));
-        assert!(content.contains("grand_child/file2.tar.gz"));
-        Ok(())
-    }
+    use crate::object_store::util::delete_recursively;
 
     #[tokio::test]
     pub async fn test_delete_recursively() -> anyhow::Result<()> {
