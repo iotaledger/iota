601e8d51864a3536e590c70f20b1ce759a313140
Thibault Martinez <thibault@iota.org>
2026-08-10 12:11:14 +0200

    remove another pub ire-export

--- crates/iota-types/src/unit_tests/utils.rs
+++ crates/iota-types/src/unit_tests/utils.rs
@@ -223,9 +223,9 @@ pub fn make_sponsored_regular_sig_tx() -> (TransactionEnvelope, Address, Address
 mod move_authenticator {
     use fastcrypto::hash::HashFunction;
     use iota_sdk_types::{
-        Address, Digest, SenderSignedTransaction, SharedObjectReference, UserSignature,
+        Address, Digest, MoveAuthenticator, MoveAuthenticatorV1, SenderSignedTransaction,
+        SharedObjectReference, UserSignature,
     };
-    pub use iota_sdk_types::{MoveAuthenticator, MoveAuthenticatorV1};
 
     use crate::{
         crypto::DefaultHash,
