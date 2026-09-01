4956e10fc57e39df75aebbd8883eca31216a6138
muXxer <git@muxxer.de>
2026-09-01 18:55:44 +0200

    refactor(iota-core): share the effects-derived store updates in TransactionOutputs

--- crates/iota-core/src/transaction_outputs.rs
+++ crates/iota-core/src/transaction_outputs.rs
@@ -9,9 +9,10 @@ use std::{
 
 use iota_common::debug_fatal;
 use iota_sdk_types::{
-    ObjectId, ObjectReference, ObjectVersion, TransactionEffects, TransactionEvents,
+    ObjectId, ObjectReference, ObjectVersion, Owner, TransactionEffects, TransactionEvents, Version,
 };
 use iota_types::{
+    base_types::VersionDigest,
     effects::{TransactionEffectsAPI, TransactionEffectsExt},
     inner_temporary_store::{InnerTemporaryStore, WrittenObjects},
     object::{Object, ObjectSet},
@@ -61,87 +62,20 @@ impl TransactionOutputs {
         } = inner_temporary_store;
 
         let tx_digest = *transaction.digest();
-
-        let deleted: HashMap<_, _> = effects.all_tombstones().into_iter().collect();
-
         let modified_at_versions = effects.modified_at_versions();
 
-        // Get the actual set of objects that have been received -- any received
-        // object will show up in the modified-at set.
-        let modified_at: HashSet<_> = modified_at_versions
-            .iter()
-            .map(|modified| (modified.object_id, modified.version))
-            .collect();
-        let possible_to_receive = transaction.transaction().receiving_objects();
-        let received_objects = possible_to_receive
-            .into_iter()
-            .filter(|obj_ref| modified_at.contains(&(obj_ref.object_id, obj_ref.version)));
-
-        // We record any received or deleted objects since they could be pruned, and
-        // smear shared object deletions in the marker table. For deleted
-        // entries in the marker table we need to make sure we don't
-        // accidentally overwrite entries.
-        let markers: Vec<_> = {
-            let received = received_objects
-                .clone()
-                .map(|objref| (ObjectKey::from(objref), MarkerValue::Received));
-
-            let deleted = deleted.into_iter().map(|(object_id, version)| {
-                let object_key = ObjectKey(object_id, version);
-                if input_objects
-                    .get(&object_id)
+        let updates = derive_store_updates(
+            &transaction,
+            &effects,
+            &written,
+            mutable_inputs,
+            |id| {
+                input_objects
+                    .get(id)
                     .is_some_and(|object| object.is_shared())
-                {
-                    (object_key, MarkerValue::SharedDeleted(tx_digest))
-                } else {
-                    (object_key, MarkerValue::OwnedDeleted)
-                }
-            });
-
-            // We "smear" shared deleted objects in the marker table to allow for proper
-            // sequencing of transactions that are submitted after the deletion
-            // of the shared object. NB: that we do _not_ smear shared objects
-            // that were taken immutably in the transaction.
-            let smeared_objects = effects.deleted_mutably_accessed_shared_objects();
-            let shared_smears = smeared_objects.into_iter().map(move |object_id| {
-                (
-                    ObjectKey(object_id, lamport_version),
-                    MarkerValue::SharedDeleted(tx_digest),
-                )
-            });
-
-            received.chain(deleted).chain(shared_smears).collect()
-        };
-
-        let live_object_markers_to_delete: Vec<_> = mutable_inputs
-            .into_iter()
-            .filter_map(|(id, ((version, digest), owner))| {
-                owner
-                    .is_address()
-                    .then_some(ObjectReference::new(id, version, digest))
-            })
-            .chain(received_objects)
-            .collect();
-
-        let new_live_object_markers_to_init: Vec<_> = written
-            .values()
-            .filter_map(|new_object| {
-                if new_object.is_address_owned() {
-                    Some(new_object.object_ref())
-                } else {
-                    None
-                }
-            })
-            .collect();
-
-        let deleted = effects
-            .deleted()
-            .into_iter()
-            .chain(effects.unwrapped_then_deleted())
-            .map(ObjectKey::from)
-            .collect();
-
-        let wrapped = effects.wrapped().into_iter().map(ObjectKey::from).collect();
+            },
+            lamport_version,
+        );
 
         let mut capture_misses = 0;
         let superseded = build_superseded_counting(
@@ -169,11 +103,11 @@ impl TransactionOutputs {
             effects,
             events,
             superseded,
-            markers,
-            wrapped,
-            deleted,
-            live_object_markers_to_delete,
-            new_live_object_markers_to_init,
+            markers: updates.markers,
+            wrapped: updates.wrapped,
+            deleted: updates.deleted,
+            live_object_markers_to_delete: updates.live_object_markers_to_delete,
+            new_live_object_markers_to_init: updates.new_live_object_markers_to_init,
             written,
         }
     }
@@ -229,6 +163,122 @@ fn build_superseded(
         read_objects,
         &mut capture_misses,
     )
+}
+
+/// The parts of a [`TransactionOutputs`] that follow from a transaction's
+/// effects rather than from execution's temporary store.
+struct StoreUpdates {
+    markers: Vec<(ObjectKey, MarkerValue)>,
+    wrapped: Vec<ObjectKey>,
+    deleted: Vec<ObjectKey>,
+    live_object_markers_to_delete: Vec<ObjectReference>,
+    new_live_object_markers_to_init: Vec<ObjectReference>,
+}
+
+/// Derives the updates a transaction makes to the store.
+///
+/// `input_was_shared` answers whether an object was shared before the
+/// transaction ran, which decides whether its deletion is marked as owned or
+/// shared. A caller that executed the transaction answers from the input
+/// objects it loaded; a caller applying results from a checkpoint answers from
+/// the input state recorded in the effects.
+fn derive_store_updates<F>(
+    transaction: &VerifiedTransaction,
+    effects: &TransactionEffects,
+    written: &WrittenObjects,
+    mutable_inputs: BTreeMap<ObjectId, (VersionDigest, Owner)>,
+    input_was_shared: F,
+    lamport_version: Version,
+) -> StoreUpdates
+where
+    F: Fn(&ObjectId) -> bool,
+{
+    let tx_digest = *transaction.digest();
+
+    let deleted: HashMap<_, _> = effects.all_tombstones().into_iter().collect();
+
+    // Get the actual set of objects that have been received -- any received
+    // object will show up in the modified-at set.
+    let modified_at: HashSet<_> = effects
+        .modified_at_versions()
+        .into_iter()
+        .map(|modified| (modified.object_id, modified.version))
+        .collect();
+    let possible_to_receive = transaction.transaction().receiving_objects();
+    let received_objects = possible_to_receive
+        .into_iter()
+        .filter(|obj_ref| modified_at.contains(&(obj_ref.object_id, obj_ref.version)));
+
+    // We record any received or deleted objects since they could be pruned, and
+    // smear shared object deletions in the marker table. For deleted
+    // entries in the marker table we need to make sure we don't
+    // accidentally overwrite entries.
+    let markers: Vec<_> = {
+        let received = received_objects
+            .clone()
+            .map(|objref| (ObjectKey::from(objref), MarkerValue::Received));
+
+        let deleted = deleted.into_iter().map(|(object_id, version)| {
+            let object_key = ObjectKey(object_id, version);
+            if input_was_shared(&object_id) {
+                (object_key, MarkerValue::SharedDeleted(tx_digest))
+            } else {
+                (object_key, MarkerValue::OwnedDeleted)
+            }
+        });
+
+        // We "smear" shared deleted objects in the marker table to allow for proper
+        // sequencing of transactions that are submitted after the deletion
+        // of the shared object. NB: that we do _not_ smear shared objects
+        // that were taken immutably in the transaction.
+        let smeared_objects = effects.deleted_mutably_accessed_shared_objects();
+        let shared_smears = smeared_objects.into_iter().map(move |object_id| {
+            (
+                ObjectKey(object_id, lamport_version),
+                MarkerValue::SharedDeleted(tx_digest),
+            )
+        });
+
+        received.chain(deleted).chain(shared_smears).collect()
+    };
+
+    let live_object_markers_to_delete: Vec<_> = mutable_inputs
+        .into_iter()
+        .filter_map(|(id, ((version, digest), owner))| {
+            owner
+                .is_address()
+                .then_some(ObjectReference::new(id, version, digest))
+        })
+        .chain(received_objects)
+        .collect();
+
+    let new_live_object_markers_to_init: Vec<_> = written
+        .values()
+        .filter_map(|new_object| {
+            if new_object.is_address_owned() {
+                Some(new_object.object_ref())
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let deleted = effects
+        .deleted()
+        .into_iter()
+        .chain(effects.unwrapped_then_deleted())
+        .map(ObjectKey::from)
+        .collect();
+
+    let wrapped = effects.wrapped().into_iter().map(ObjectKey::from).collect();
+
+    StoreUpdates {
+        markers,
+        wrapped,
+        deleted,
+        live_object_markers_to_delete,
+        new_live_object_markers_to_init,
+    }
 }
 
 #[cfg(test)]
