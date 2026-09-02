77546196055986484e507893f0797d13f008c409
Yasir <yasir@shariff.dev>
2026-09-02 19:56:27 +0300

    feat(docs): update Proof of Inclusion documentation and examples, add wasm references

--- docs/site/scripts/get-iota-notarization-references.sh
+++ docs/site/scripts/get-iota-notarization-references.sh
@@ -7,10 +7,12 @@ cd tmp
 # Create the target directory structure if it doesn't exist
 mkdir -p ../../content/developer/iota-notarization/single-notarization/references/wasm
 mkdir -p ../../content/developer/iota-notarization/audit-trails/references/wasm
+mkdir -p ../../content/developer/iota-notarization/proof-of-inclusion/references/wasm
 
-# Create additional temporary directories for single-notarization and audit-trails
+# Create a temporary directory for each package.
 mkdir single-notarization
 mkdir audit-trails
+mkdir proof-of-inclusion
 
 # We are going to download the tag.gz files for single-notarization and audit-trails.
 # In each tag.gz file, different folder structures are used:
@@ -26,6 +28,11 @@ cp -Rv ./notarization-docs/docs/wasm/* ../../../content/developer/iota-notarizat
 cd ../audit-trails
 curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota-notarization/0.1/audit-trail-wasm.tar.gz | tar xzv
 cp -Rv ./audit-trail-docs/docs/wasm/* ../../../content/developer/iota-notarization/audit-trails/references/wasm/
+
+# Download and copy Proof of Inclusion docs
+cd ../proof-of-inclusion
+curl -sL https://s3.eu-central-1.amazonaws.com/files.iota.org/iota-wiki/iota-notarization/0.1/proof-of-inclusion-wasm.tar.gz | tar xzv
+cp -Rv ./proof-of-inclusion-docs/docs/wasm/* ../../../content/developer/iota-notarization/proof-of-inclusion/references/wasm/
 
 # Return to root and cleanup
 cd ../..
