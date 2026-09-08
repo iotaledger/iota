69af85c0f491229a88c72d633928e393908ec5c5
Thomas Shufps <shufps80@gmail.com>
2026-09-08 11:43:49 +0200

    Apply suggestion from @Thoralf-M
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

--- crates/iota/tests/cli_tests.rs
+++ crates/iota/tests/cli_tests.rs
@@ -5391,7 +5391,6 @@ async fn test_faucet() -> Result<(), anyhow::Error> {
 async fn test_faucet_batch() -> Result<(), anyhow::Error> {
     let test_cluster = TestClusterBuilder::new()
         .with_fullnode_rpc_port(9000)
-        .with_fullnode_enable_grpc_api(true)
         .build()
         .await;
 
