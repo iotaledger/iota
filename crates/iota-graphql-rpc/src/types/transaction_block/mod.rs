77607377ea8dba8bb308a4da98c410c3d689e3d3
Konstantinos Demartinos <konstantinos.demartinos@iota.org>
2026-09-16 18:07:28 +0300

    chore(graphql-rpc): remove dead cursor type

--- crates/iota-graphql-rpc/src/types/transaction_block/mod.rs
+++ crates/iota-graphql-rpc/src/types/transaction_block/mod.rs
@@ -122,22 +122,6 @@ pub(crate) enum TransactionBlockKindInput {
 
 type Query<ST, GB> = data::Query<ST, transactions::table, GB>;
 
-/// The cursor returned for each `TransactionBlock` in a connection's page of
-/// results. The `checkpoint_viewed_at` will set the consistent upper bound for
-/// subsequent queries made on this cursor.
-#[allow(unused)]
-#[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
-pub(crate) struct TransactionBlockCursor {
-    /// The checkpoint sequence number this was viewed at.
-    #[serde(rename = "c")]
-    pub checkpoint_viewed_at: u64,
-    #[serde(rename = "t")]
-    pub tx_sequence_number: u64,
-    /// The checkpoint sequence number when the transaction was finalized.
-    #[serde(rename = "tc")]
-    pub tx_checkpoint_number: u64,
-}
-
 /// `DataLoader` key for fetching a `TransactionBlock` by its digest, optionally
 /// constrained by a consistency cursor.
 #[derive(Copy, Clone, Hash, Eq, PartialEq, Debug)]
